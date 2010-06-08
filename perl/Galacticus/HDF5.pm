fc772da9952568fdee45e7f6307361275988d567
Andrew Benson <abesnson@its.caltech.edu>
2010-06-07 20:57:50 -0700

    * Migrated perl/Galacticus/HDF5.pm to use PDL::IO:HDF5 rather than h5ls/h5import for accessing Galacticus HDF5 file - results in   much faster data access.
    * Updated documentation to describe installation procedure for PDL::IO:HDF5

--- perl/Galacticus/HDF5.pm
+++ perl/Galacticus/HDF5.pm
@@ -2,6 +2,9 @@
 
 package HDF5;
 use PDL;
+use PDL::IO::HDF5;
+use PDL::IO::HDF5::Dataset;
+use PDL::NiceSlice;
 use Data::Dumper;
 
 %galacticusFunctions = ();
@@ -11,38 +14,19 @@ $status;
 
 sub Get_Times {
     my $dataHash = shift;
-    $hdf5Path = ${$dataHash}{'file'}."/Outputs";
-    open(hPipe,"h5ls -S $hdf5Path |");
-    $outputNumbers = pdl [];
-    $times = pdl [];
+    $outputNumbers   = pdl [];
+    $times           = pdl [];
     $expansionFactor = pdl [];
-    while ( $line = <hPipe> ) {
-	if ( $line =~ m/Output(\d+)/ ) {
-	    $output = $1;
-	    $outputNumbers = $outputNumbers->append($output);
-	    $hdf5Path = ${$dataHash}{'file'}."/Outputs/Output".$output."/outputTime";
-	    open(hdPipe,"h5ls -S -d $hdf5Path |");
-	    $dline = <hdPipe>;
-	    $dline = <hdPipe>;
-	    $dline = <hdPipe>;
-	    close(hdPipe);
-	    $dline =~ s/^\s*//;
-	    $dline =~ s/\s*$//;
-	    $times   = $times->append($dline);
-	    $hdf5Path = ${$dataHash}{'file'}."/Outputs/Output".$output."/outputExpansionFactor";
-	    open(hdPipe,"h5ls -S -d $hdf5Path |");
-	    $dline = <hdPipe>;
-	    $dline = <hdPipe>;
-	    $dline = <hdPipe>;
-	    close(hdPipe);
-	    $dline =~ s/^\s*//;
-	    $dline =~ s/\s*$//;
-	    $expansionFactor = $expansionFactor->append($dline);
-	}
+    $HDFfile = new PDL::IO::HDF5(">".${$dataHash}{'file'});
+    @outputs = sort $HDFfile->group("Outputs")->groups;
+    foreach $output ( @outputs ) {
+	if ( $output =~ m/Output(\d+)/ ) {$outputNumber = $1};
+	$outputNumbers   = $outputNumbers  ->append($outputNumber);
+	$times           = $times          ->append($HDFfile->group("Outputs/".$output)->dataset("outputTime")           ->get);
+	$expansionFactor = $expansionFactor->append($HDFfile->group("Outputs/".$output)->dataset("outputExpansionFactor")->get);
     }
-    close(hPipe);
-    ${${${$dataHash}{'outputs'}}{'outputNumber'}} = $outputNumbers;
-    ${${${$dataHash}{'outputs'}}{'time'}} = $times;
+    ${${${$dataHash}{'outputs'}}{'outputNumber'}}    = $outputNumbers;
+    ${${${$dataHash}{'outputs'}}{'time'}}            = $times;
     ${${${$dataHash}{'outputs'}}{'expansionFactor'}} = $expansionFactor;
 }
 
@@ -66,54 +50,37 @@ sub Select_Output {
 sub Get_History {
     my $dataHash = shift;
     my @dataNames = @{$_[0]};
+    $HDFfile = new PDL::IO::HDF5(">".${$dataHash}{'file'});
     foreach $dataSetName ( @dataNames ) {
-	$hdf5Path = ${$dataHash}{'file'}."/globalHistory/".$dataSetName;
-	open(hPipe,"h5ls -S -d $hdf5Path |");
-	$line = <hPipe>;
-	$line = <hPipe>;
-	$data = pdl [];
-	while ( $line = <hPipe> ) {
-	    $line =~ s/^\s*//;
-	    $line =~ s/\s*$//;
-	    $data = $data->append($line);
-	}
-	close(hPipe);
-	${${${$dataHash}{'history'}}{$dataSetName}} = $data;
-	undef(@data);
+	${${${$dataHash}{'history'}}{$dataSetName}} = $HDFfile->group("globalHistory")->dataset($dataSetName)->get;
     }
 }
 
 sub Count_Trees {
     $dataHash = shift;
     unless ( exists(${$dataHash}{'mergerTreesAvailable'}) ) {
-	$hdf5Path = ${$dataHash}{'file'}."/Outputs/Output1";
-	open(hPipe,"h5ls $hdf5Path |");
-	while ( $line = <hPipe> ) {
-	    if ( $line =~ m/^mergerTree(\d+)/ ) {
-		push(@{${$dataHash}{'mergerTreesAvailable'}},$1);
-	    }
+	$HDFfile = new PDL::IO::HDF5(">".${$dataHash}{'file'});
+	@treesAvailable = $HDFfile->group("Outputs/Output1")->groups;
+	foreach $tree ( @treesAvailable) {
+	    $tree =~ s/mergerTree(\d+)/$1/;
 	}
-	close(hPipe);
+	@{${$dataHash}{'mergerTreesAvailable'}} = sort {$a <=> $b} @treesAvailable;
     }
 }
 
 sub Get_Parameters {
     my $dataHash = shift;
     unless ( exists(${$dataHash}{'parameters'}) ) {
-	$hdf5Path = ${$dataHash}{'file'}."/Parameters";
-	open(hPipe,"h5ls -S -d $hdf5Path |");
-	while ( $line = <hPipe> ) {
-	    if ( $line =~ m/^\s*(\S+)\s*Dataset/ ) {
-		$parameterName = $1;
-		$line = <hPipe>;
-		$line = <hPipe>;
-		$line =~ s/\"//g;
-		$line =~ s/^\s*//;
-		$line =~ s/\s*$//;
-		${${$dataHash}{'parameters'}}{$parameterName} = $line;
+	$HDFfile = new PDL::IO::HDF5(">".${$dataHash}{'file'});
+	@parameterNames = $HDFfile->group("Parameters")->datasets;
+	foreach $parameterName ( @parameterNames ) {
+	    $parameterValue = $HDFfile->group("Parameters")->dataset($parameterName)->get;
+	    if ( ref($parameterValue) eq "PDL::Char" ) {
+		${${$dataHash}{'parameters'}}{$parameterName} = $parameterValue->atstr(0);
+	    } else {
+		${${$dataHash}{'parameters'}}{$parameterName} = $parameterValue->index(0);
 	    }
 	}
-	close(hPipe);
     }
 }
 
@@ -122,33 +89,22 @@ sub Get_Dataset {
     my @dataNames = @{$_[0]};
     undef(@mergerTrees);
     if ( ${$dataHash}{'tree'} eq "all" ) {
-	unless ( exists(${$dataHash}{'mergerTreesAvailable'}) ) {
-	    $hdf5Path = ${$dataHash}{'file'}."/Outputs/Output".${$dataHash}{'output'};
-	    open(hPipe,"h5ls $hdf5Path |");
-	    while ( $line = <hPipe> ) {
-		if ( $line =~ m/^mergerTree(\d+)/ ) {
-		    push(@{${$dataHash}{'mergerTreesAvailable'}},$1);
-		}
-	    }
-	    close(hPipe);
-	}
+	&Count_Trees($dataHash);
 	@mergerTrees = @{${$dataHash}{'mergerTreesAvailable'}};
-	@mergerTrees = sort {$a <=> $b} @mergerTrees;
     } else {
 	$mergerTrees[0] = ${$dataHash}{'tree'};
     }
 
+    # Open the HDF5 file.
+    $HDFfile = new PDL::IO::HDF5(">".${$dataHash}{'file'});
+
+    # Extract a list of available datasets.
     unless ( exists(${$dataHash}{'dataSetsAvailable'}) ) {
-	$hdf5Path = ${$dataHash}{'file'}."/Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTrees[0];
-	open(hPipe,"h5ls $hdf5Path |");
-	while ( $line = <hPipe> ) {
-	    if ( $line =~ m/^(\S+)/ ) {
-		${${$dataHash}{'dataSetsAvailable'}}{$1} = 1;
-	    }
-	}
-	close(hPipe);
+	@dataSets = $HDFfile->group("Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTrees[0])->datasets;
+	foreach $dataSet ( @dataSets ) {${${$dataHash}{'dataSetsAvailable'}}{$dataSet} = 1};
     }
 
+    # Determine the range of data to be extracted.
     if ( exists(${$dataHash}{'dataRange'}) ) {
 	$dataStart = ${${$dataHash}{'dataRange'}}[0];
 	$dataEnd = ${${$dataHash}{'dataRange'}}[1];
@@ -157,6 +113,7 @@ sub Get_Dataset {
 	$dataEnd = -1;
     }
 
+    # Determine if we are to store derived quantities in the HDF5 file.
     if ( exists(${$dataHash}{'store'}) ) {
 	$storeDataSets = ${$dataHash}{'store'};
 	if ( $storeDataSets == 1 ) {
@@ -170,76 +127,40 @@ sub Get_Dataset {
 	unless ( exists(${${$dataHash}{'dataSets'}}{$dataSetName}) ) {
 	    if ( exists(${${$dataHash}{'dataSetsAvailable'}}{$dataSetName}) ) {
 		$data = pdl [];
-		$dataCount = 0;
 		foreach $mergerTree ( @mergerTrees ) {
+		    $thisTreeData = $HDFfile->group("Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTree)->dataset($dataSetName)->get;
 		    if ( $dataSetName eq "volumeWeight" ) {
-			$hdf5Path = ${$dataHash}{'file'}."/Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTree."/nodeIndex";
-			open(hPipe,"h5ls $hdf5Path |");
-			$line = <hPipe>;
-			if ( $line =~ m/\{(\d+)/ ) {
-			    $galaxyCount = $1;
-			} else {
-			    $galaxyCount = 0;
-			}
-			close(hPipe);
+			# Append the volumeWeight property once for each galaxy in this tree.
+			@galaxyCount = $HDFfile->group("Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTree)->dataset("nodeIndex")->dims;
+			$data = $data->append($thisTreeData*ones($galaxyCount[0]));
+		    } else {
+			# Append the dataset.
+			$data = $data->append($thisTreeData);
 		    }
-		    $hdf5Path = ${$dataHash}{'file'}."/Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTree."/".$dataSetName;
-		    open(hPipe,"h5ls -S -d $hdf5Path |");
-		    $line = <hPipe>;
-		    $line = <hPipe>;
-		    while ( $line = <hPipe> ) {
-			++$dataCount;
-			if ( $dataStart <= 0 || ( $dataCount >= $dataStart && $dataCount <= $dataEnd ) ) {
-			    $line =~ s/^\s*//;
-			    $line =~ s/\s*$//;
-			    if ( $dataSetName eq "volumeWeight" && $galaxyCount > 0 ) {
-				$data = $data->append($line*ones($galaxyCount));
-			    } else {
-				$data = $data->append($line);
-			    }
-			}
-		    }
-		    close(hPipe);
 		}
 		${${${$dataHash}{'dataSets'}}{$dataSetName}} = $data;
-		undef(@data);
+		undef($data);
 	    } else {
 		foreach $regEx ( keys(%galacticusFunctions) ) {
 		    if ( $dataSetName =~ m/^$regEx$/ ) {
 			$getFunc = $galacticusFunctions{$regEx};
 			&$getFunc(\%{$dataHash},$dataSetName);
 			if ( $storeDataSets == 1 ) {
-			    $tmpConfigFile = "config.tmp";
-			    $tmpDataFile = "data.tmp";
 			    $dataSets = $dataHash->{'dataSets'};
-			    $dataIndex = -1;
+			    $dataIndexStart = 0;
 			    foreach $mergerTree ( @mergerTrees ) {
 				# Count up the number of entries in this tree. (We do this by counting the nodeIndex property.)
-				open(tmpData,">".$tmpDataFile);
-				$hdf5Path = ${$dataHash}{'file'}."/Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTree."/nodeIndex";
-				open(hPipe,"h5ls -S -d $hdf5Path |");
-				$line = <hPipe>;
-				$line = <hPipe>;
-				$dataCount = 0;
-				while ( $line = <hPipe> ) {
-				    ++$dataCount;
-				    ++$dataIndex;
-				    print tmpData ${$dataSets->{$dataSetName}}->index($dataIndex)."\n";
-				}
-				close(hPipe);
-				close(tmpData);
-				open(configFile,">".$tmpConfigFile);
-				print configFile "PATH /Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTree."/".$dataSetName."\n";
-				print configFile "INPUT-CLASS TEXTFP\n";
-				print configFile "RANK 1\n";
-				print configFile "DIMENSION-SIZES ".$dataCount."\n";
-				print configFile "OUTPUT-CLASS FP\n";
-				print configFile "OUTPUT-SIZE 64\n";
-				print configFile "OUTPUT-ARCHITECTURE NATIVE\n";
-				close(configFile);
-				system("h5import $tmpDataFile -c $tmpConfigFile -o ".${$dataHash}{'file'});
-				unlink($tmpConfigFile);
-				unlink($tmpDataFile);
+				$mergerTreeGroup = $HDFfile->group("Outputs/Output".${$dataHash}{'output'}."/mergerTree".$mergerTree);
+				@galaxyCount = $mergerTreeGroup->dataset("nodeIndex")->dims;
+				$dataCount = $galaxyCount[0];
+				$dataIndexEnd = $dataIndexStart+$dataCount-1;
+				$dataSlice = ${$dataSets->{$dataSetName}}->($dataIndexStart:$dataIndexEnd);
+				$outputDataSet = new PDL::IO::HDF5::Dataset( name    => $dataSetName,
+									     parent  => $mergerTreeGroup,
+									     fileObj => $HDFfile
+									     );
+				$outputDataSet->set($dataSlice);
+				$dataIndexStart += $dataCount;
 			    }
 			}
 		    }
