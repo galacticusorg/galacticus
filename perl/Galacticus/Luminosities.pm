0b36b96e7ba2bb50248868fb7aebdb16fc4dfade
Andrew Benson <abenson@caltech.edu>
2010-06-11 12:45:53 -0700

    * Added "bulgeToTotalLuminosity:..." properties to Galacticus::Luminosities Perl module. Follows same syntax as for   totalStellarLuminosity:... properties but returns the ratio of bulge to total light. Documented (also added missing   documentation for "totalLuminosity:...." property.)

--- perl/Galacticus/Luminosities.pm
+++ perl/Galacticus/Luminosities.pm
@@ -8,7 +8,8 @@ use Data::Dumper;
 use XML::Simple;
 
 %HDF5::galacticusFunctions = ( %HDF5::galacticusFunctions,
-    "^totalStellarLuminosity:([^:]+):([^:]+):z([\\d\\.]+)(:dust[^:]+)?" => "Luminosities::Get_Luminosity"
+    "^totalStellarLuminosity:([^:]+):([^:]+):z([\\d\\.]+)(:dust[^:]+)?" => "Luminosities::Get_Luminosity",
+    "^bulgeToTotalLuminosity:([^:]+):([^:]+):z([\\d\\.]+)(:dust[^:]+)?" => "Luminosities::Get_BulgeToTotal"
     );
 
 my $status = 1;
@@ -31,6 +32,28 @@ sub Get_Luminosity {
 	&HDF5::Get_Dataset($dataSet,\@luminosityDataset);
 	$dataSets = \%{${$dataSet}{'dataSets'}};
 	${$dataSets->{$dataSetName}} = ${$dataSets->{$luminosityDataset[0]}}+${$dataSets->{$luminosityDataset[1]}};
+    } else {
+	die("Get_Luminosity(): unable to parse data set: ".$dataSetName);
+    }
+}
+
+sub Get_BulgeToTotal {
+    $dataSet = shift;
+    $dataSetName = $_[0];
+  
+    # Check that the dataset name matches the expected regular expression.
+    if ( $dataSetName =~ m/^bulgeToTotalLuminosity:([^:]+):([^:]+):z([\d\.]+)(:dust[^:]+)?/ ) {
+	# Extract the dataset descriptor.
+	$filter        = $1;
+	$frame         = $2;
+	$redshift      = $3;
+	$dustExtension = $4;
+	# Construct the name of the corresponding luminosity properties.
+	$luminosityDataset[0] = "diskStellarLuminosity:"    .$filter.":".$frame.":z".$redshift.$dustExtension;
+	$luminosityDataset[1] = "spheroidStellarLuminosity:".$filter.":".$frame.":z".$redshift.$dustExtension;
+	&HDF5::Get_Dataset($dataSet,\@luminosityDataset);
+	$dataSets = \%{${$dataSet}{'dataSets'}};
+	${$dataSets->{$dataSetName}} = ${$dataSets->{$luminosityDataset[1]}}/(${$dataSets->{$luminosityDataset[0]}}+${$dataSets->{$luminosityDataset[1]}});
     } else {
 	die("Get_Luminosity(): unable to parse data set: ".$dataSetName);
     }
