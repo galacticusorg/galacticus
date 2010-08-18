0c695c20587a87d85b6a7c77406704538142b0eb
Andrew Benson <abenson@caltech.edu>
2010-08-18 14:12:57 -0700

    * Fixes problems with the coolingFunctionMethods parameter:
      + Parameter name was incorrect in parameters.xml;
    
      + "CMB Compton" method is now "CMB_Compton";
    
      + Documentation updated on coolingFunctionMethods parameter.
    
    * Fixes bugs in "CIE_from_file" ionization state module:
    
      + Fixes miscalculation of number of temperature points in tables;
    
      + Fixes incorrect tag name when searching for electron density.

--- source/cooling.cooling_function.CMB_Compton.F90
+++ source/cooling.cooling_function.CMB_Compton.F90
@@ -83,7 +83,7 @@ contains
     type(varying_string), intent(in) :: coolingFunctionMethods(:)
  
     ! Check if this cooling function has been selected.
-    if (any(coolingFunctionMethods == 'CMB Compton')) functionSelected=.true.
+    if (any(coolingFunctionMethods == 'CMB_Compton')) functionSelected=.true.
 
     return
   end subroutine Cooling_Function_CMB_Compton_Initialize
