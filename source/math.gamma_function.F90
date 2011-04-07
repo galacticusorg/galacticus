5ff821d065761ffe3e705bf26f601cd47bb796dd
Andrew Benson <abenson@caltech.edu>
2011-04-07 12:52:58 -0700

    * Adds inverse Gamma function calculations.

--- source/math.gamma_function.F90
+++ source/math.gamma_function.F90
@@ -64,10 +64,21 @@
 module Gamma_Functions
   !% Implements calculations of gamma functions.
   private
-  public :: Gamma_Function_Incomplete_Complementary,Gamma_Function_Logarithmic
+  public :: Gamma_Function_Incomplete,Gamma_Function_Incomplete_Complementary,Gamma_Function_Logarithmic,Gamma_Function&
+       &,Inverse_Gamma_Function_Incomplete,Inverse_Gamma_Function_Incomplete_Complementary
 
 contains
 
+  double precision function Gamma_Function_Incomplete(exponent,argument)
+    !% Computes the incomplete gamma function.
+    use FGSL
+    implicit none
+    double precision, intent(in) :: exponent,argument
+
+    Gamma_Function_Incomplete=FGSL_SF_Gamma_Inc_Q(exponent,argument)
+    return
+  end function Gamma_Function_Incomplete
+  
   double precision function Gamma_Function_Incomplete_Complementary(exponent,argument)
     !% Computes the complementary incomplete gamma function.
     use FGSL
@@ -78,6 +89,16 @@ contains
     return
   end function Gamma_Function_Incomplete_Complementary
   
+  double precision function Gamma_Function(argument)
+    !% Computes the gamma function.
+    use FGSL
+    implicit none
+    double precision, intent(in) :: argument
+
+    Gamma_Function=dexp(Gamma_Function_Logarithmic(argument))
+    return
+  end function Gamma_Function
+  
   double precision function Gamma_Function_Logarithmic(argument)
     !% Computes the logarithm of the gamma function.
     use FGSL
@@ -87,5 +108,49 @@ contains
     Gamma_Function_Logarithmic=FGSL_SF_lnGamma(argument)
     return
   end function Gamma_Function_Logarithmic
+
+  double precision function Inverse_Gamma_Function_Incomplete_Complementary(a,P)
+    !% Returns the inverse of the incomplete function. That is, it returns $x$ given $P(a,x)$.
+    use ISO_Varying_String
+    use Incomplete_Gamma
+    use Galacticus_Error
+    implicit none
+    double precision,     intent(in) :: a,P
+    integer                          :: errorState
+    double precision                 :: Q
+    type(varying_string)             :: message
+
+    Q=1.0d0-P
+    call GamInv(a,Inverse_Gamma_Function_Incomplete_Complementary,-1.0d0,P,Q,errorState)
+    if (errorState < 0) then
+       select case (errorState)
+       case (-2)
+          message='input error: a <= 0'
+       case (-3)
+          message='no solution obtained: A/a is too large'
+       case (-4)
+          message='input error: P or Q < 0 or P+Q != 1'
+       case (-6)
+          message='20 iterations were performed - this should not happen'
+       case (-7)
+          message='iteration failed'
+       case (-8)
+          message='accuracy lost'
+       end select
+       call Galacticus_Error_Report('Inverse_Gamma_Function_Incomplete_Complementary',message)
+    end if
+    return
+  end function Inverse_Gamma_Function_Incomplete_Complementary
+  
+  double precision function Inverse_Gamma_Function_Incomplete(a,Q)
+    !% Returns the inverse of the incomplete function. That is, it returns $x$ given $Q(a,x)$.
+    implicit none
+    double precision,     intent(in) :: a,Q
+    double precision                 :: P
+
+    P=1.0d0-Q
+    Inverse_Gamma_Function_Incomplete=Inverse_Gamma_Function_Incomplete_Complementary(a,P)
+    return
+  end function Inverse_Gamma_Function_Incomplete
   
 end module Gamma_Functions
