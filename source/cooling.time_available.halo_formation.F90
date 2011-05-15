3be217bc34b0dc0b4c9257c832b68e8ec45aad4f
Andrew Benson <abenson@caltech.edu>
2011-05-14 20:07:23 -0700

    * Adds a time available for cooling method based on the formation time   of the halo.

---
+++ source/cooling.time_available.halo_formation.F90
@@ -0,0 +1,119 @@
+!! Copyright 2009, 2010, 2011 Andrew Benson <abenson@caltech.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+!!
+!!
+!!    COPYRIGHT 2010. The Jet Propulsion Laboratory/California Institute of Technology
+!!
+!!    The California Institute of Technology shall allow RECIPIENT to use and
+!!    distribute this software subject to the terms of the included license
+!!    agreement with the understanding that:
+!!
+!!    THIS SOFTWARE AND ANY RELATED MATERIALS WERE CREATED BY THE CALIFORNIA
+!!    INSTITUTE OF TECHNOLOGY (CALTECH). THE SOFTWARE IS PROVIDED "AS-IS" TO
+!!    THE RECIPIENT WITHOUT WARRANTY OF ANY KIND, INCLUDING ANY WARRANTIES OF
+!!    PERFORMANCE OR MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE OR
+!!    PURPOSE (AS SET FORTH IN UNITED STATES UCC §2312-§2313) OR FOR ANY
+!!    PURPOSE WHATSOEVER, FOR THE SOFTWARE AND RELATED MATERIALS, HOWEVER
+!!    USED.
+!!
+!!    IN NO EVENT SHALL CALTECH BE LIABLE FOR ANY DAMAGES AND/OR COSTS,
+!!    INCLUDING, BUT NOT LIMITED TO, INCIDENTAL OR CONSEQUENTIAL DAMAGES OF
+!!    ANY KIND, INCLUDING ECONOMIC DAMAGE OR INJURY TO PROPERTY AND LOST
+!!    PROFITS, REGARDLESS OF WHETHER CALTECH BE ADVISED, HAVE REASON TO KNOW,
+!!    OR, IN FACT, SHALL KNOW OF THE POSSIBILITY.
+!!
+!!    RECIPIENT BEARS ALL RISK RELATING TO QUALITY AND PERFORMANCE OF THE
+!!    SOFTWARE AND ANY RELATED MATERIALS, AND AGREES TO INDEMNIFY CALTECH FOR
+!!    ALL THIRD-PARTY CLAIMS RESULTING FROM THE ACTIONS OF RECIPIENT IN THE
+!!    USE OF THE SOFTWARE.
+!!
+!!    In addition, RECIPIENT also agrees that Caltech is under no obligation
+!!    to provide technical support for the Software.
+!!
+!!    Finally, Caltech places no restrictions on RECIPIENT's use, preparation
+!!    of Derivative Works, public display or redistribution of the Software
+!!    other than those specified in the included license and the requirement
+!!    that all copies of the Software released be marked with the language
+!!    provided in this notice.
+!!
+!!    This software is separately available under negotiable license terms
+!!    from:
+!!    California Institute of Technology
+!!    Office of Technology Transfer
+!!    1200 E. California Blvd.
+!!    Pasadena, California 91125
+!!    http://www.ott.caltech.edu
+
+
+!% Contains a module which implements the \cite{cole_hierarchical_2000} method for computing the time available for cooling in hot
+!% halos.
+
+module Cooling_Times_Available_Halo_Formation
+  !% Implements the \cite{cole_hierarchical_2000} method for computing the time available for cooling in hot halos.
+  private
+  public :: Cooling_Time_Available_Halo_Formation_Initialize
+
+contains
+
+  !# <coolingTimeAvailableMethod>
+  !#  <unitName>Cooling_Time_Available_Halo_Formation_Initialize</unitName>
+  !# </coolingTimeAvailableMethod>
+  subroutine Cooling_Time_Available_Halo_Formation_Initialize(coolingTimeAvailableMethod,Cooling_Time_Available_Get&
+       &,Cooling_Time_Available_Increase_Rate_Get)
+    !% Initialize the \cite{cole_hierarchical_2000} cooling time available module.
+    use ISO_Varying_String
+    use Input_Parameters
+    use Galacticus_Error
+    use Tree_Nodes
+    implicit none
+    type(varying_string),          intent(in)    :: coolingTimeAvailableMethod
+    procedure(),          pointer, intent(inout) :: Cooling_Time_Available_Get,Cooling_Time_Available_Increase_Rate_Get
+    
+    if (coolingTimeAvailableMethod == 'halo formation') then
+       ! Set pointers to our implementation.
+       Cooling_Time_Available_Get               => Cooling_Time_Available_Halo_Formation
+       Cooling_Time_Available_Increase_Rate_Get => Cooling_Time_Available_Increase_Rate_Halo_Formation
+       ! Ensure that a halo formation property is supported.
+       if (.not.associated(Tree_Node_Formation_Time)) call Galacticus_Error_Report('Cooling_Time_Available_Halo_Formation_Initialize','method requires that the Tree_Node_Formation_Time property be available')
+    end if
+    return
+  end subroutine Cooling_Time_Available_Halo_Formation_Initialize
+
+  double precision function Cooling_Time_Available_Halo_Formation(thisNode)
+    !% Compute the time available for cooling using the \cite{cole_hierarchical_2000} method. Specifically, the time available is
+    !% assumed to be the time since the halo formation event.
+    use Tree_Nodes
+    implicit none
+    type(treeNode), intent(inout), pointer :: thisNode
+
+    Cooling_Time_Available_Halo_Formation=Tree_Node_Time(thisNode)-Tree_Node_Formation_Time(thisNode)
+    return
+  end function Cooling_Time_Available_Halo_Formation
+  
+  double precision function Cooling_Time_Available_Increase_Rate_Halo_Formation(thisNode)
+    !% Compute the rate of increase of the time available for cooling using the \cite{cole_hierarchical_2000} method. We return a rate
+    !% of 1.
+    use Tree_Nodes
+    implicit none
+    type(treeNode), intent(inout), pointer :: thisNode
+
+    ! Simply return unit rate.
+    Cooling_Time_Available_Increase_Rate_Halo_Formation=1.0d0
+    return
+  end function Cooling_Time_Available_Increase_Rate_Halo_Formation
+  
+end module Cooling_Times_Available_Halo_Formation
