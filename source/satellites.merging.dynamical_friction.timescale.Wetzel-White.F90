3325b3ff31679867d693f8febb14e76978afed17
Andrew Benson <abenson@caltech.edu>
2011-08-29 17:55:40 -0700

    * Adds the Wetzel & White (2010) satellite merger timescale algorithm.

---
+++ source/satellites.merging.dynamical_friction.timescale.Wetzel-White.F90
@@ -0,0 +1,112 @@
+!! Copyright 2009, 2010, 2011 Andrew Benson <abenson@caltech.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+!!
+!!
+!!    COPYRIGHT 2010. The Jet Propulsion Laboratory/California Institute of Technology
+!!
+!!    The California Institute of Technology shall allow RECIPIENT to use and
+!!    distribute this software subject to the terms of the included license
+!!    agreement with the understanding that:
+!!
+!!    THIS SOFTWARE AND ANY RELATED MATERIALS WERE CREATED BY THE CALIFORNIA
+!!    INSTITUTE OF TECHNOLOGY (CALTECH). THE SOFTWARE IS PROVIDED "AS-IS" TO
+!!    THE RECIPIENT WITHOUT WARRANTY OF ANY KIND, INCLUDING ANY WARRANTIES OF
+!!    PERFORMANCE OR MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE OR
+!!    PURPOSE (AS SET FORTH IN UNITED STATES UCC §2312-§2313) OR FOR ANY
+!!    PURPOSE WHATSOEVER, FOR THE SOFTWARE AND RELATED MATERIALS, HOWEVER
+!!    USED.
+!!
+!!    IN NO EVENT SHALL CALTECH BE LIABLE FOR ANY DAMAGES AND/OR COSTS,
+!!    INCLUDING, BUT NOT LIMITED TO, INCIDENTAL OR CONSEQUENTIAL DAMAGES OF
+!!    ANY KIND, INCLUDING ECONOMIC DAMAGE OR INJURY TO PROPERTY AND LOST
+!!    PROFITS, REGARDLESS OF WHETHER CALTECH BE ADVISED, HAVE REASON TO KNOW,
+!!    OR, IN FACT, SHALL KNOW OF THE POSSIBILITY.
+!!
+!!    RECIPIENT BEARS ALL RISK RELATING TO QUALITY AND PERFORMANCE OF THE
+!!    SOFTWARE AND ANY RELATED MATERIALS, AND AGREES TO INDEMNIFY CALTECH FOR
+!!    ALL THIRD-PARTY CLAIMS RESULTING FROM THE ACTIONS OF RECIPIENT IN THE
+!!    USE OF THE SOFTWARE.
+!!
+!!    In addition, RECIPIENT also agrees that Caltech is under no obligation
+!!    to provide technical support for the Software.
+!!
+!!    Finally, Caltech places no restrictions on RECIPIENT's use, preparation
+!!    of Derivative Works, public display or redistribution of the Software
+!!    other than those specified in the included license and the requirement
+!!    that all copies of the Software released be marked with the language
+!!    provided in this notice.
+!!
+!!    This software is separately available under negotiable license terms
+!!    from:
+!!    California Institute of Technology
+!!    Office of Technology Transfer
+!!    1200 E. California Blvd.
+!!    Pasadena, California 91125
+!!    http://www.ott.caltech.edu
+
+
+!+    Contributions to this file made by:  Martin White.
+
+!% Contains a module which implements calculations of satellite merging times using the \cite{wetzel_what_2010} method.
+
+module Dynamical_Friction_Wetzel_White
+  !% Implements calculations of satellite merging times using the \cite{wetzel_what_2010} method.
+  implicit none
+  private
+  public :: Satellite_Time_Until_Merging_Wetzel_White_Initialize
+
+contains
+
+  !# <satelliteMergingMethod>
+  !#  <unitName>Satellite_Time_Until_Merging_Wetzel_White_Initialize</unitName>
+  !# </satelliteMergingMethod>
+  subroutine Satellite_Time_Until_Merging_Wetzel_White_Initialize(satelliteMergingMethod,Satellite_Time_Until_Merging)
+    !% Determine if this method is to be used and set pointer appropriately.
+    use ISO_Varying_String
+    implicit none
+    type(varying_string),                 intent(in)    :: satelliteMergingMethod
+    procedure(double precision), pointer, intent(inout) :: Satellite_Time_Until_Merging
+
+    if (satelliteMergingMethod == 'Wetzel-White') Satellite_Time_Until_Merging => Satellite_Time_Until_Merging_Wetzel_White
+    return
+  end subroutine Satellite_Time_Until_Merging_Wetzel_White_Initialize
+
+  double precision function Satellite_Time_Until_Merging_Wetzel_White(thisNode)
+    !% Return the timescale for merging satellites using the \cite{wetzel_what_2010} method.
+    use Tree_Nodes
+    use Dynamical_Friction_Timescale_Utilities
+    use Cosmology_Functions
+    implicit none
+    type(treeNode),    pointer, intent(inout) :: thisNode
+    type(treeNode),    pointer                :: hostNode
+    double precision,  parameter              :: timeScaleNormalization=0.2d0 ! C_dyn from Wetzel & White (2010).
+    double precision                          :: massRatio
+
+    ! Find the host node.
+    hostNode => thisNode%parentNode
+    ! Compute mass ratio.
+    massRatio=Tree_Node_Mass(hostNode)/Tree_Node_Mass(thisNode)
+    ! Compute dynamical friction timescale using eqn. (2) from Wetzel & White (2010).
+    Satellite_Time_Until_Merging_Wetzel_White= Dynamical_Friction_Timescale_Multiplier()                  &
+         &                                    *timeScaleNormalization                                     &
+         &                                    /Expansion_Rate(Expansion_Factor(Tree_Node_Time(thisNode))) &
+         &                                    *           massRatio                                       &
+         &                                    /dlog(1.0d0+massRatio)
+    return
+  end function Satellite_Time_Until_Merging_Wetzel_White
+
+end module Dynamical_Friction_Wetzel_White
