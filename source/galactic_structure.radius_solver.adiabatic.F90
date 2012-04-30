abddeecde219873eb073bcbaf53ec67d88a6d9b9
Andrew Benson <abenson@its.caltech.edu>
2012-04-30 05:19:13 -0700

    * Make radius/velocity set/get pointers threadprivate in galactic   structure solvers to avoid segfaults when running under OpenMP. This   is necessary due to the recent removal of OpenMP critical section   around the entire galactic structure solver section.

--- source/galactic_structure.radius_solver.adiabatic.F90
+++ source/galactic_structure.radius_solver.adiabatic.F90
@@ -175,8 +175,9 @@ contains
     implicit none
     type(treeNode),                    intent(inout), pointer :: thisNode
     integer,                           parameter              :: iterationMaximum=100
-    procedure(Structure_Get_Template),                pointer :: Radius_Get => null(), Velocity_Get => null()
-    procedure(Structure_Set_Template),                pointer :: Radius_Set => null(), Velocity_Set => null()
+    procedure(Structure_Get_Template), save,          pointer :: Radius_Get => null(), Velocity_Get => null()
+    procedure(Structure_Set_Template), save,          pointer :: Radius_Set => null(), Velocity_Set => null()
+    !$omp threadprivate(Radius_Get,Radius_Set,Velocity_Get,Velocity_Set)
     logical                                                   :: componentActive,galaxyIsPhysicallyPlausible
     double precision                                          :: specificAngularMomentum
 
