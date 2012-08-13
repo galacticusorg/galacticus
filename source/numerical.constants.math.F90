467c4d65f74995630a1d1571fd115c7297487c14
Andrew Benson <abenson@obs.carnegiescience.edu>
2012-08-12 17:07:49 -0700

    * Add a quad-precision Pi constant.

--- source/numerical.constants.math.F90
+++ source/numerical.constants.math.F90
@@ -64,19 +64,21 @@
 module Numerical_Constants_Math
   !% Contains various useful mathematical constants.
   use FGSL
+  use Kind_Numbers
   implicit none
   public
   
   ! Pi.
-  double precision, parameter :: Pi=m_pi
+  double precision    , parameter :: Pi=m_pi
+  real(kind=kind_quad), parameter :: PiQuadPrecision=3.141592653589793238462643383279502884197_kind_quad
 
   ! Natural logarithm of 10.
-  double precision, parameter :: ln10=m_ln10
+  double precision    , parameter :: ln10=m_ln10
 
   ! Natural logarithm of 2.
-  double precision, parameter :: ln2 =m_ln2
+  double precision    , parameter :: ln2 =m_ln2
 
   ! Euler's constant.
-  double precision, parameter :: eulersConstant=m_euler
+  double precision    , parameter :: eulersConstant=m_euler
 
 end module Numerical_Constants_Math
