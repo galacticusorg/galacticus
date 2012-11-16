c32bdbd0f8a2df4114462c7bdd92ff81a1b658ac
Andrew Benson <abenson@obs.carnegiescience.edu>
2012-11-15 19:18:52 -0800

    * Update default option for starFormationTimescaleDisksMethod to be   "integratedSurfaceDensity".

--- source/star_formation.timescales.disks.F90
+++ source/star_formation.timescales.disks.F90
@@ -74,7 +74,7 @@ contains
           ! Get the disk star formation timescale method parameter.
           !@ <inputParameter>
           !@   <name>starFormationTimescaleDisksMethod</name>
-          !@   <defaultValue>KMT09</defaultValue>
+          !@   <defaultValue>integratedSurfaceDensity</defaultValue>
           !@   <attachedTo>module</attachedTo>
           !@   <description>
           !@     The name of the method to be used for computing star formation timescales in disks.
@@ -83,7 +83,7 @@ contains
           !@   <cardinality>1</cardinality>
           !@   <group>starFormation</group>
           !@ </inputParameter>
-          call Get_Input_Parameter('starFormationTimescaleDisksMethod',starFormationTimescaleDisksMethod,defaultValue='KMT09')
+          call Get_Input_Parameter('starFormationTimescaleDisksMethod',starFormationTimescaleDisksMethod,defaultValue='integratedSurfaceDensity')
           ! Include file that makes calls to all available method initialization routines.
           !# <include directive="starFormationTimescaleDisksMethod" type="functionCall" functionType="void">
           !#  <functionArgs>
