aef1898cc86ffb3db772931fa4ee6f39e3dcc755
Andrew Benson <abenson@obs.carnegiescience.edu>
2013-01-04 15:50:26 -0800

    * Fix bug in scaling of ADAF radiative effiency in "switched" accretion   disk implementation (previously compared dimensionless accretion rate   to logarithm of the minimum accretion rate for a thin disk, instead of   to the minimum accretion rate itself).

--- source/accretion_disks.switched.F90
+++ source/accretion_disks.switched.F90
@@ -232,8 +232,8 @@ contains
 
        ! If below the critical accretion rate for transition to a thin disk, reduce the radiative efficiency by a factor
        ! proportional to the accretion rate.
-       if (accretionRateThinDiskMinimumExists.and.massAccretionRateDimensionless < accretionRateThinDiskMinimumLogarithmic) then
-          Accretion_Disk_Switched_ADAF_Radiative_Efficiency_Scaling=massAccretionRateDimensionless/accretionRateThinDiskMinimumLogarithmic
+       if (accretionRateThinDiskMinimumExists.and.massAccretionRateDimensionless < accretionRateThinDiskMinimum) then
+          Accretion_Disk_Switched_ADAF_Radiative_Efficiency_Scaling=massAccretionRateDimensionless/accretionRateThinDiskMinimum
        else
           Accretion_Disk_Switched_ADAF_Radiative_Efficiency_Scaling=1.0d0
        end if
