0eec7b0c5553afa5949b5ae306be1048643ca829
Andrew Benson <abenson@obs.carnegiescience.edu>
2013-06-06 15:39:31 -0700

    * Remove debugging lines left in by mistake.

--- source/objects.nodes.components.hot_halo.standard.F90
+++ source/objects.nodes.components.hot_halo.standard.F90
@@ -838,10 +838,6 @@ contains
     use Numerical_Constants_Astronomical
     use Hot_Halo_Ram_Pressure_Stripping
     use Hot_Halo_Density_Profile
-
-!! AJB HACK
-use kind_numbers
-
     implicit none
     type (treeNode            ), pointer, intent(inout) :: thisNode
     logical                    ,          intent(inout) :: interrupt
@@ -884,10 +880,6 @@ use kind_numbers
                & call thisHotHaloComponent%          massRate(      massAccretionRate)
           if (failedMassAccretionRate > 0.0d0 .or. thisHotHaloComponent%mass() > 0.0d0) &
                & call thisHotHaloComponent%unaccretedMassRate(failedMassAccretionRate)
-
-!! AJB HACK
-if ( thisnode%isprogenitorof(5540507_kind_int8)) write (0,*) thisHotHaloComponent%mass() ,thisHotHaloComponent%mass(),thisbasiccomponent%time()
-
           ! Next compute the cooling rate in this halo.
           call Node_Component_Hot_Halo_Standard_Cooling_Rate(thisNode)
           ! Pipe the cooling rate to which ever component claimed it.
