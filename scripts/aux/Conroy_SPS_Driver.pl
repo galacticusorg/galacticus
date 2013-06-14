8992c5ceb138a156ee415eb4f1a6a9056706fdcd
Andrew Benson <abenson@obs.carnegiescience.edu>
2013-06-14 08:32:22 -0700

    * Update to interface with FSPS v2.4.

--- scripts/aux/Conroy_SPS_Driver.pl
+++ scripts/aux/Conroy_SPS_Driver.pl
@@ -17,7 +17,7 @@ if ( exists($ENV{'GALACTICUS_ROOT_V092'}) ) {
 }
 unshift(@INC,$galacticusPath."perl");
 
-# Driver script for FSPS_v2.3 (Conroy, White & Gunn stellar population code).
+# Driver script for FSPS_v2.4 (Conroy, White & Gunn stellar population code).
 # Andrew Benson (15-Apr-2011)
 
 # Get arguments.
@@ -50,53 +50,53 @@ if ( -e $stellarPopulationFile ) {
 if ( $makeFile == 1 ) {
     
     # Check out the code.
-    unless ( -e $galacticusPath."aux/FSPS_v2.3" ) {
+    unless ( -e $galacticusPath."aux/FSPS_v2.4" ) {
  	print "Conroy_SPS_Driver.pl: downloading source code.\n";
- 	system("svn checkout http://fsps.googlecode.com/svn/trunk/ ".$galacticusPath."aux/FSPS_v2.3");
- 	die("Conroy_SPS_Driver.pl: FATAL - failed to check out svn repository.") unless ( -e $galacticusPath."aux/FSPS_v2.3" );
+ 	system("svn checkout http://fsps.googlecode.com/svn/trunk/ ".$galacticusPath."aux/FSPS_v2.4");
+ 	die("Conroy_SPS_Driver.pl: FATAL - failed to check out svn repository.") unless ( -e $galacticusPath."aux/FSPS_v2.4" );
     }
     
     # Check for updates to the code.
     my $availableRevision;
     my $currentRevision;
-    open(pHndl,"svn info -r HEAD ".$galacticusPath."aux/FSPS_v2.3 |");
+    open(pHndl,"svn info -r HEAD ".$galacticusPath."aux/FSPS_v2.4 |");
     while ( my $line = <pHndl> ) {
  	if ( $line =~ m/Last Changed Rev:\s*(\d+)/ ) {$availableRevision = $1};
     }
     close(pHndl);
-    open(pHndl,"svn info ".$galacticusPath."aux/FSPS_v2.3 |");
+    open(pHndl,"svn info ".$galacticusPath."aux/FSPS_v2.4 |");
     while ( my $line = <pHndl> ) {
  	if ( $line =~ m/Revision:\s*(\d+)/ ) {$currentRevision = $1};
     }
     close(pHndl);
     if ( $currentRevision < $availableRevision ) {
  	print "Conroy_SPS_Driver.pl: updating source code.\n";
- 	system("svn revert -R ".$galacticusPath."aux/FSPS_v2.3"); # Revert the code.
- 	system("svn update ".$galacticusPath."aux/FSPS_v2.3"); # Grab updates
- 	unlink($galacticusPath."aux/FSPS_v2.3/src/galacticus_IMF.f90") # Remove this file to trigger re-patching of the code.
+ 	system("svn revert -R ".$galacticusPath."aux/FSPS_v2.4"); # Revert the code.
+ 	system("svn update ".$galacticusPath."aux/FSPS_v2.4"); # Grab updates
+ 	unlink($galacticusPath."aux/FSPS_v2.4/src/galacticus_IMF.f90") # Remove this file to trigger re-patching of the code.
     }
     
     # Patch the code.
-    unless ( -e $galacticusPath."aux/FSPS_v2.3/src/galacticus_IMF.f90" ) {
+    unless ( -e $galacticusPath."aux/FSPS_v2.4/src/galacticus_IMF.f90" ) {
  	foreach my $file ( "galacticus_IMF.f90", "imf.f90.patch", "Makefile.patch", "ssp_gen.f90.patch", "autosps.f90.patch", "qromb.f90.patch" ) {
-	    my $todir = $galacticusPath."aux/FSPS_v2.3/src/";
+	    my $todir = $galacticusPath."aux/FSPS_v2.4/src/";
 	    $todir .= "nr/"
 		if ( $file eq "qromb.f90.patch" );
- 	    copy($galacticusPath."aux/FSPS_v2.3_Galacticus_Modifications/".$file,$todir.$file);
+ 	    copy($galacticusPath."aux/FSPS_v2.4_Galacticus_Modifications/".$file,$todir.$file);
  	    if ( $file =~ m/\.patch$/ ) {
 		system("cd ".$todir."; patch < $file");
 		die("Conroy_SPS_Driver.pl: unable to patch file: ".$file) unless ( $? == 0 );
 	    }
  	    print "$file\n";
  	}
- 	unlink($galacticusPath."aux/FSPS_v2.3/src/autosps.exe");
+ 	unlink($galacticusPath."aux/FSPS_v2.4/src/autosps.exe");
     }
     
     # Build the code.
-    unless ( -e $galacticusPath."aux/FSPS_v2.3/src/autosps.exe" ) {
+    unless ( -e $galacticusPath."aux/FSPS_v2.4/src/autosps.exe" ) {
  	print "Conroy_SPS_Driver.pl: compiling autosps.exe code.\n";
- 	system("cd ".$galacticusPath."aux/FSPS_v2.3/src; export SPS_HOME=`pwd`; make clean; make -j 1");
- 	die("Conroy_SPS_Driver.pl: FATAL - failed to build autosps.exe code.") unless ( -e $galacticusPath."aux/FSPS_v2.3/src/autosps.exe" );
+ 	system("cd ".$galacticusPath."aux/FSPS_v2.4/src; export SPS_HOME=`pwd`; make clean; make -j 1");
+ 	die("Conroy_SPS_Driver.pl: FATAL - failed to build autosps.exe code.") unless ( -e $galacticusPath."aux/FSPS_v2.4/src/autosps.exe" );
     }
     
     # Initialize the data structure.
@@ -111,7 +111,7 @@ if ( $makeFile == 1 ) {
     # Run the code.
     my $pwd = `pwd`;
     chomp($pwd);
-    $ENV{'SPS_HOME'} = $galacticusPath."aux/FSPS_v2.3";
+    $ENV{'SPS_HOME'} = $galacticusPath."aux/FSPS_v2.4";
     my $iMetallicity = -1;
 
     # Add a description and other metadata to the file.
@@ -121,7 +121,7 @@ if ( $makeFile == 1 ) {
     $hdfFile->attrSet(
 	description   => "Simple stellar population spectra from Conroy, White & Gunn for a ".$imfName." initial mass function",
 	timestamp     => $now,
-	fspsVersion   => "2.3_r".$availableRevision,
+	fspsVersion   => "2.4_r".$availableRevision,
 	createdBy     => "Galacticus",
 	fileFormat    => $fileFormatCurrent
 	);
@@ -171,8 +171,8 @@ if ( $makeFile == 1 ) {
     # Loop over metallicities.
     for(my $iZ=1;$iZ<=22;++$iZ) {
 	my $outFile = "imf".$imfName.".iZ".$iZ;
-	unless ( -e $galacticusPath."aux/FSPS_v2.3/OUTPUTS/".$outFile.".spec" ) {
-	    open(spsPipe,"|aux/FSPS_v2.3/src/autosps.exe");
+	unless ( -e $galacticusPath."aux/FSPS_v2.4/OUTPUTS/".$outFile.".spec" ) {
+	    open(spsPipe,"|aux/FSPS_v2.4/src/autosps.exe");
 	    print spsPipe "6\n";         # IMF.
 	    print spsPipe "0\n";         # Generate SSP.
 	    print spsPipe $iZ."\n";      # Specify metallicity.
@@ -185,7 +185,7 @@ if ( $makeFile == 1 ) {
 	my $ageCount =  0;
 	my $iAge     = -1;
 	my $gotAge   =  0;
-	open(specFile,$galacticusPath."aux/FSPS_v2.3/OUTPUTS/".$outFile.".spec");
+	open(specFile,$galacticusPath."aux/FSPS_v2.4/OUTPUTS/".$outFile.".spec");
 	while ( my $line = <specFile> ) {
 	    chomp($line);
 	    $line =~ s/^\s*//;
