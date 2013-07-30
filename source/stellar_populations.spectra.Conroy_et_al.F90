bc4d0c53d4612f4b370b6fbc5c4153ab2c9b3cdc
Andrew Benson <abenson@obs.carnegiescience.edu>
2013-07-30 09:02:18 -0700

    * Prevent the FSPS stellar population spectra module from writing out a   'galacticus.imf' file if the spectra file does not need to be   (re)made.

--- source/stellar_populations.spectra.Conroy_et_al.F90
+++ source/stellar_populations.spectra.Conroy_et_al.F90
@@ -52,8 +52,10 @@ contains
     use Abundances_Structure
     use ISO_Varying_String
     use Input_Parameters
+    use IO_HDF5
     use Star_Formation_IMF
     use Memory_Management
+    use File_Utilities
     use System_Command
     use Stellar_Population_Spectra_File
     use Galacticus_Input_Paths
@@ -63,9 +65,12 @@ contains
     integer                         , intent(in   )               :: imfIndex
     logical                         , allocatable  , dimension(:) :: imfReadTemporary
     class           (table1D       ), allocatable                 :: imf
-    integer                                                       :: iIMF                        , imfUnit
-    type            (varying_string)                              :: command                     , imfName, &
-         &                                                           stellarPopulationSpectraFile
+    integer                                                       :: iIMF                            , imfUnit, &
+         &                                                           fileFormatVersion
+    logical                                                       :: remakeFile
+    type            (varying_string)                              :: command                         , imfName, &
+         &                                                           stellarPopulationSpectraFileName
+    type            (hdf5Object    )                              :: stellarPopulationSpectraFile
 
     ! Ensure that array for IMF index mappings is sufficiently large.
     if (allocated(imfRead)) then
@@ -88,30 +93,39 @@ contains
        imfName=IMF_Descriptor(imfIndex)
 
        ! Name of the parameter to be used for this IMF.
-       stellarPopulationSpectraFile=char(Galacticus_Input_Path())//'data/stellarPopulations/SSP_Spectra_Conroy-et-al_v2.4_imf'//imfName//'.hdf5'
+       stellarPopulationSpectraFileName=char(Galacticus_Input_Path())//'data/stellarPopulations/SSP_Spectra_Conroy-et-al_v2.4_imf'//imfName//'.hdf5'
 
-       ! Generate the IMF tabulation.
-       call IMF_Tabulate(imfIndex,imf)
-       open(newunit=imfUnit,file="galacticus.imf",status="unknown",form="formatted")
-       do iIMF=1,imf%size()
-          write (imfUnit,'(2(1x,e12.6))') imf%x(iIMF),imf%y(iIMF)
-       end do
-       close(imfUnit)
-       call imf%destroy()
-
-       ! Call the driver script to generate this file.
-       command=char(Galacticus_Input_Path())//'scripts/aux/Conroy_SPS_Driver.pl '//imfName//' '//stellarPopulationSpectraFile
-       command=command//' '//Stellar_Population_Spectra_File_Format_Current()
-       call System_Command_Do(command)
-
+       ! Check if the file exists and has the correct version.
+       remakeFile=.false.
+       if (File_Exists(char(stellarPopulationSpectraFileName))) then
+          !$omp critical(HDF5_Access)
+          call stellarPopulationSpectraFile%openFile     (char(stellarPopulationSpectraFileName),readOnly=.true.)
+          call stellarPopulationSpectraFile%readAttribute('fileFormat',fileFormatVersion                        )
+          if (fileFormatVersion /= Stellar_Population_Spectra_File_Format_Current()) remakeFile=.true.
+          !$omp end critical(HDF5_Access)
+       else
+          remakeFile=.true.
+       end if
+       ! Generate the file if necessary.
+       if (remakeFile) then
+          ! Generate the IMF tabulation.
+          call IMF_Tabulate(imfIndex,imf)
+          open(newunit=imfUnit,file="galacticus.imf",status="unknown",form="formatted")
+          do iIMF=1,imf%size()
+             write (imfUnit,'(2(1x,e12.6))') imf%x(iIMF),imf%y(iIMF)
+          end do
+          close(imfUnit)
+          call imf%destroy()
+          ! Call the driver script to generate this file.
+          command=char(Galacticus_Input_Path())//'scripts/aux/Conroy_SPS_Driver.pl '//imfName//' '//stellarPopulationSpectraFileName
+          command=command//' '//Stellar_Population_Spectra_File_Format_Current()
+          call System_Command_Do(command)
+       end if
        ! Call routine to read in the tabulated data.
-       call Stellar_Population_Spectra_File_Read(imfIndex,stellarPopulationSpectraFile)
-
+       call Stellar_Population_Spectra_File_Read(imfIndex,stellarPopulationSpectraFileName)
        ! Flag that this IMF has been read.
        imfRead(imfIndex)=.true.
-
     end if
-
     return
   end subroutine Stellar_Population_Spectra_Conroy_Initialize_IMF
 
