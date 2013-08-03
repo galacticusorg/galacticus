2b212bf08e826c59cf65925a6cc5bc24a54bba9a
Andrew Benson <abenson@obs.carnegiescience.edu>
2013-08-03 14:32:23 -0700

    * Update license and e-mail address.

--- source/Galacticus.F90
+++ source/Galacticus.F90
@@ -15,8 +15,8 @@
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
 
-!% {\sc Galacticus} is a semi-analytic model of galaxy formation written by Andrew Benson \href{mailto:abenson@caltech.edu}{{\tt
-!% <abenson@caltech.edu>}}.
+!% {\sc Galacticus} is a semi-analytic model of galaxy formation written by Andrew Benson \href{mailto:abenson@obs.carnegiescience.edu}{{\tt
+!% <abenson@obs.carnegiescience.edu>}}.
 
 program Galacticus
   !% The main {\sc Galacticus} program.
