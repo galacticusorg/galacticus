a2c14e0924bdc0f87c24b0848dc8a95e6b973ab2
Andrew Benson <abenson@obs.carnegiescience.edu>
2013-10-18 11:23:52 -0700

    * Add comments to semaphore C-interface file.

--- source/semaphores.c
+++ source/semaphores.c
@@ -1,3 +1,22 @@
+// Copyright 2009, 2010, 2011 Andrew Benson <abenson@obs.carnegiescience.edu>
+//
+// This file is part of Galacticus.
+//
+//    Galacticus is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Galacticus is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+//% Implements Fortran-callable wrappers around the Linux semaphore functions.
+
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <semaphore.h>
@@ -5,6 +24,7 @@
 #include <stdlib.h>
 
 sem_t * Semaphore_Open_C(const char *name, int initialValue) {
+  //% Fortran-callable wrapper around the Linux semaphore open function.
   sem_t *s;
   s = sem_open(name,O_CREAT,S_IRUSR | S_IWUSR,initialValue);
   if ( s == SEM_FAILED ) {
@@ -20,6 +40,7 @@ sem_t * Semaphore_Open_C(const char *name, int initialValue) {
 }
 
 void Semaphore_Close_C(sem_t *s) {
+  //% Fortran-callable wrapper around the Linux semaphore close function.
   int e;
   e = sem_close(s);
   if ( e == -1 ) {
@@ -29,6 +50,7 @@ void Semaphore_Close_C(sem_t *s) {
 }
 
 void Semaphore_Wait_C(sem_t *s) {
+  //% Fortran-callable wrapper around the Linux semaphore wait function.
   int e;
   e = -1;
   while ( e == -1 ) {
@@ -44,6 +66,7 @@ void Semaphore_Wait_C(sem_t *s) {
 }
 
 void Semaphore_Post_C(sem_t *s) {
+  //% Fortran-callable wrapper around the Linux semaphore post function.
   int e;
   e = sem_post(s);
   if ( e == -1 ) {
@@ -53,6 +76,7 @@ void Semaphore_Post_C(sem_t *s) {
 }
 
 void Semaphore_Unlink_C(const char *name) {
+  //% Fortran-callable wrapper around the Linux semaphore unlink function.
   int e;
   e = sem_unlink(name);
   if ( e == -1 ) {
