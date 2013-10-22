5d6d52aee0d6d1d6507aed06f0fc2b27dd3e9136
Andrew Benson <abenson@obs.carnegiescience.edu>
2013-10-22 15:00:46 -0700

    * Expands the functionality of the unique label function generator script to more intelligently handle new-style object-oriented   functions in Galacticus. Specifically, it can now identify instantiations of the default class, or of specific classes of these   functions and add parameter values appropriately to the the unique label.
    * Includes a major clean up of the code in this script.
    
    * Adds new functionality to the Fortran::Utils Perl module to find all lines in a file matching a regex, and also to have that
      module supply regex's for common patterns.
    
    * Adds three new Perl modules, one with additional useful list utilities, one which helps parse directives from Galacticus source
      files, and a third which helps parse interdepencies from Galacticus' build system.

---
+++ perl/Galacticus/Build/Make.pm
@@ -0,0 +1,39 @@
+# Contains a Perl module which provides utilities for Galacticus' Make system.
+
+package Make;
+my $galacticusPath;
+if ( exists($ENV{"GALACTICUS_ROOT_V093"}) ) {
+ $galacticusPath = $ENV{"GALACTICUS_ROOT_V093"};
+ $galacticusPath .= "/" unless ( $galacticusPath =~ m/\/$/ );
+} else {
+ $galacticusPath = "./";
+}
+unshift(@INC, $galacticusPath."perl"); 
+use strict;
+use warnings;
+use Switch;
+
+sub Module_Name {
+    # Return the module name associated with a file.
+    my $fileName = shift;
+    (my %options) = @_
+	if ( scalar(@_) > 1 );
+    my $moduleName;
+    if ( exists($options{'default'}) ) {
+	switch ( $options{'default'} ) {
+	    case ( 'self' ) {
+		$moduleName = $fileName;
+	    }
+	}
+    }
+    my $moduleFileName = "./work/build/".$fileName.".m";
+    if ( -e $moduleFileName ) {
+	open(my $moduleHandle,$moduleFileName);
+	($moduleName =<$moduleHandle>) =~ s/\.\/work\/build\/(.*)\.mod\n$/$1/
+	    unless ( eof($moduleHandle) );
+	close(jHndl);
+    }
+    return $moduleName;
+}
+
+1;
