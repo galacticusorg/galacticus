bc2e900cf1e4c652de819d333382aa87a3f70411
Andrew Benson <abenson@obs.carnegiescience.edu>
2013-11-25 14:27:30 -0800

    * Provide a set of fuuzy Boolean constants, that provide "true",   "false", and "unknown" values.

---
+++ source/numerical.constants.boolean.F90
@@ -0,0 +1,28 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013 Andrew Benson <abenson@obs.carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module of useful Boolean constants.
+
+module Numerical_Constants_Boolean
+  !% Contains various useful Boolean constants.
+  implicit none
+  public
+
+  integer, parameter :: booleanTrue   =+1
+  integer, parameter :: booleanFalse  = 0
+  integer, parameter :: booleanUnknown=-1
+end module Numerical_Constants_Boolean
