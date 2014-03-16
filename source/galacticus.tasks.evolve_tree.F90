05895fdfb9e1265e56c7e2cbf88340cbe1be92d7
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-03-16 14:25:22 -0700

    * Fix merge errors.

--- source/galacticus.tasks.evolve_tree.F90
+++ source/galacticus.tasks.evolve_tree.F90
@@ -81,13 +81,10 @@ contains
     double precision                , dimension(3), save :: loadAverage
     logical                                       , save :: overloaded                      , treeCanEvolve          , &
          &                                                  treeIsFinished                  , evolutionIsEventLimited, &
-    type            (semaphore     ), pointer            :: galacticusMutex
-    character       (len=32        )                     :: treeEvolveLoadAverageMaximumText,treeEvolveThreadsMaximumText
          &                                                  success
-    !$omp threadprivate(activeTasks,totalTasks,loadAverage,overloaded,treeCanEvolve,treeIsFinished,evolutionIsEventLimited,success)
     type            (semaphore     ), pointer            :: galacticusMutex
     character       (len=32        )                     :: treeEvolveLoadAverageMaximumText,treeEvolveThreadsMaximumText
-
+    !$omp threadprivate(activeTasks,totalTasks,loadAverage,overloaded,treeCanEvolve,treeIsFinished,evolutionIsEventLimited,success)
     type            (universe      )                     :: universeWaiting                 , universeProcessed
     type            (universeEvent ), pointer     , save :: thisEvent
     !$omp threadprivate(thisEvent)
