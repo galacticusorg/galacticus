dfca5fd8d733a0d034955f8b4020ef1449e4a65f
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-06-29 15:50:42 -0700

    * Changed parameter priors and halo mass lower limits for PRIMUS survey   stellar mass function HOD analysis.

--- constraints/dataAnalysis/stellarMassFunctions_PRIMUS_z0_1/covarianceMatrix.pl
+++ constraints/dataAnalysis/stellarMassFunctions_PRIMUS_z0_1/covarianceMatrix.pl
@@ -17,7 +17,7 @@ unshift(@INC,$galacticusPath."perl");
 die("Usage: covarianceMatrix.pl <redshiftBin>")
     unless ( scalar(@ARGV) == 1 );
 my $redshiftBin = $ARGV[0];
-die("covarianceMatrix.pl: redshiftBin must be 0, 1, 3, 4, 5 or 6")
+die("covarianceMatrix.pl: redshiftBin must be 0, 1, 2, 3, 4, 5 or 6")
     if ( $redshiftBin < 0 || $redshiftBin > 6 );
 
 # Simply run the generic script with our config file as argument.
