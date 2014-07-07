eb2defa49ba5787938b2b82546e489381ddefd8a
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-07-07 03:57:14 -0700

    * Improvements to lightcone output filter:
      + Square geometry now uses correct equations for large angle surveys.
    
      + Adds a tree postprocessing step which allows trees which lie
        entirely outside the lightcone geometry to be pruned so they do not
        need to be run.

---
+++ source/math.trigonometric_functions.F90
@@ -0,0 +1,75 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014 Andrew Benson <abenson@obs.carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which implements trigonometric functions.
+
+module Trigonometric_Functions
+  !% Implements trigonometric functions.
+  use FGSL
+  implicit none
+  private
+  public :: cot, cosec
+
+  interface cot
+     module procedure cotDouble
+     module procedure cotDoubleComplex
+  end interface cot
+  
+  interface cosec
+     module procedure cosecDouble
+     module procedure cosecDoubleComplex
+  end interface cosec
+  
+contains
+  
+  double precision function cotDouble(x)
+    !% Implements cotangent for double precision {\tt x}.
+    implicit none
+    double precision, intent(in   ) :: x
+    
+    cotDouble=1.0d0/tan(x)
+    return
+  end function cotDouble
+  
+  double complex function cotDoubleComplex(x)
+    !% Implements cotangent for double precision complex {\tt x}.
+    implicit none
+    double complex, intent(in   ) :: x
+    
+    cotDoubleComplex=1.0d0/tan(x)
+    return
+  end function cotDoubleComplex
+  
+  double precision function cosecDouble(x)
+    !% Implements cosecant for double precision {\tt x}.
+    implicit none
+    double precision, intent(in   ) :: x
+    
+    cosecDouble=1.0d0/sin(x)
+    return
+  end function cosecDouble
+  
+  double complex function cosecDoubleComplex(x)
+    !% Implements cosecant for double precision complex {\tt x}.
+    implicit none
+    double complex, intent(in   ) :: x
+    
+    cosecDoubleComplex=1.0d0/sin(x)
+    return
+  end function cosecDoubleComplex
+  
+end module Trigonometric_Functions
