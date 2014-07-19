5eb106d26d0acb52a1d82ee3e50ed4620a074b76
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-07-19 09:11:39 -0700

    * Add standard hot halo data module.

---
+++ source/objects.nodes.components.hot_halo.standard.data.F90
@@ -0,0 +1,40 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013 Andrew Benson <abenson@obs.carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which provides data for the standard hot halo node component.
+
+module Node_Component_Hot_Halo_Standard_Data
+  !% Provides data for the standard hot halo node component.
+  public
+
+  ! Return rate for outflows.
+  double precision            :: hotHaloOutflowReturnRate
+  logical                     :: hotHaloOutflowReturnOnFormation
+  ! Controls on cooling.
+  double precision            :: hotHaloAngularMomentumLossFraction
+  ! Controls on accretion.
+  logical                     :: hotHaloNodeMergerLimitBaryonFraction  , hotHaloOutflowAngularMomentumAlwaysGrows
+  ! Control for starvation of satellites.
+  logical                     :: starveSatellites
+  ! Controls from which halo cooling is computed.
+  integer                     :: hotHaloCoolingFromNode
+  integer         , parameter :: currentNode                         =0, &
+       &                         formationNode                       =1
+  ! Tolerances.
+  double precision, parameter :: outerRadiusOverVirialRadiusMinimum  =1.0d-3
+
+end module Node_Component_Hot_Halo_Standard_Data
