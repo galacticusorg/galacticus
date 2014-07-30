603aa1f87970b693b80a9be843293ad54700615b
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-07-29 22:07:57 -0700

    * Add GAMA and VIPERS stellar mass functions to the constraints   infrastructure.

--- constraints/scripts/stellarMassFunction_VIPERS_z0.55.pl
+++ constraints/scripts/stellarMassFunction_VIPERS_z0.55.pl
@@ -3,25 +3,25 @@ use strict;
 use warnings;
 my $galacticusPath;
 if ( exists($ENV{"GALACTICUS_ROOT_V093"}) ) {
-    $galacticusPath = $ENV{"GALACTICUS_ROOT_V093"};
-    $galacticusPath .= "/" unless ( $galacticusPath =~ m/\/$/ );
+ $galacticusPath  = $ENV{"GALACTICUS_ROOT_V093"};
+ $galacticusPath .= "/" unless ( $galacticusPath =~ m/\/$/ );
 } else {
-    $galacticusPath = "./";
+ $galacticusPath  = "./";
 }
 unshift(@INC,$galacticusPath."perl"); 
 use PDL;
-use PDL::NiceSlice;
+use PDL::IO::HDF5;
 require Galacticus::Options;
 require Galacticus::Constraints::MassFunctions;
 
-# Compute likelihood (and make plots) for a Galacticus model given the stellar mass function data from Caputi et al. (2011;
-# http://adsabs.harvard.edu/abs/2011MNRAS.413..162C) for z=3.50 to 4.25.
+# Compute likelihood (and make a plot) for a Galacticus model given the stellar mass function data from Davidzon (2013;
+# http://adsabs.harvard.edu/abs/2013A%26A...558A..23D).
 
 # Data structure to hold the specification for our mass function.
 my $massFunctionConfig;
 
 # Get name of input and output files.
-die("stellarMassFunction_UKIDSS_UDS_z3.875.pl <galacticusFile> [options]") unless ( scalar(@ARGV) >= 1 );
+die("stellarMassFunction_VIPERS_z0.55.pl <galacticusFile> [options]") unless ( scalar(@ARGV) >= 1 );
 $massFunctionConfig->{'self'          } = $0;
 $massFunctionConfig->{'galacticusFile'} = $ARGV[0];
 # Create a hash of named arguments.
@@ -34,22 +34,22 @@ my %arguments =
 
 # Specify the properties of this mass function.
 my $entry                                    = 0;
-$massFunctionConfig->{'redshift'           } = pdl 3.875;
-$massFunctionConfig->{'analysisLabel'      } = "ukidssUdsStellarMassFunctionZ3.875";
+$massFunctionConfig->{'redshift'           } = pdl 0.55;
+$massFunctionConfig->{'analysisLabel'      } = "vipersStellarMassFunctionZ0.55";
 $massFunctionConfig->{'discrepancyFileName'} = "discrepancy".ucfirst($massFunctionConfig->{'analysisLabel'}).".hdf5";
 $massFunctionConfig->{'massType'           } = "stellarMass";
-$massFunctionConfig->{'massErrorRandomDex' } = 0.173;
-$massFunctionConfig->{'xRange'             } = "1.0e10:1.0e12";
-$massFunctionConfig->{'yRange'             } = "2.0e-7:1.0e-2";
+$massFunctionConfig->{'massErrorRandomDex' } = 0.07;
+$massFunctionConfig->{'xRange'             } = "1.0e9:1.0e13";
+$massFunctionConfig->{'yRange'             } = "1.0e-9:1.0e-1";
 $massFunctionConfig->{'xLabel'             } = "\$M_\\star\$ [\$M_\\odot\$]";
 $massFunctionConfig->{'yLabel'             } = "\${\\rm d}n/{\\rm d}\\log M_\\star\$ [Mpc\$^{-3}\$]";
-$massFunctionConfig->{'title'              } = "Stellar mass function at \$z\\approx 3.875\$";
-$massFunctionConfig->{'systematicParameter'} = "ukidssUdsStellarMassFunctionZ3.875MassSystematic";
+$massFunctionConfig->{'title'              } = "Stellar mass function at \$z\\approx 0.55\$";
+$massFunctionConfig->{'systematicParameter'} = "vipersStellarMassFunctionZ0.55MassSystematic";
 $massFunctionConfig->{'systematicOrder'    } = 1;
 $massFunctionConfig->{'systematicZeroPoint'} = 11.3;
 
 # Read the observed data.
-my $observations                                      = new PDL::IO::HDF5($galacticusPath."data/observations/massFunctionsStellar/Stellar_Mass_Function_UKIDSS_UDS_2011_z3.5_4.25.hdf5");
+my $observations                                      = new PDL::IO::HDF5($galacticusPath."data/observations/massFunctionsStellar/Stellar_Mass_Function_VIPERS_2013_z0.55.hdf5");
 $massFunctionConfig ->{'x'                           }  = $observations->dataset('mass'                )->get    (                  );
 $massFunctionConfig ->{'y'                           }  = $observations->dataset('massFunctionObserved')->get    (                  );
 $massFunctionConfig ->{'yIsPer'                      }  = "ln";
