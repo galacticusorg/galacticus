59347403e855e315cb54f725269f8f8b95d2e0db
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-09-25 14:25:42 -0700

    * Add constraint definitions and scripts for the ZFOURGE survey.

---
+++ constraints/scripts/stellarMassFunction_ZFOURGE_z0.350.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+my $galacticusPath;
+if ( exists($ENV{"GALACTICUS_ROOT_V093"}) ) {
+ $galacticusPath  = $ENV{"GALACTICUS_ROOT_V093"};
+ $galacticusPath .= "/" unless ( $galacticusPath =~ m/\/$/ );
+} else {
+ $galacticusPath  = "./";
+}
+unshift(@INC,$galacticusPath."perl"); 
+use PDL;
+use PDL::IO::HDF5;
+require Galacticus::Options;
+require Galacticus::Constraints::MassFunctions;
+
+# Compute likelihood (and make a plot) for a Galacticus model given the stellar mass function data from Tmoczak (2014;
+# http://adsabs.harvard.edu/abs/2014ApJ...783...85T).
+
+# Data structure to hold the specification for our mass function.
+my $massFunctionConfig;
+
+# Get name of input and output files.
+die("stellarMassFunction_ZFOURGE_z0.350.pl <galacticusFile> [options]") unless ( scalar(@ARGV) >= 1 );
+$massFunctionConfig->{'self'          } = $0;
+$massFunctionConfig->{'galacticusFile'} = $ARGV[0];
+# Create a hash of named arguments.
+my $iArg = -1;
+my %arguments =
+    (
+     quiet => 0
+    );
+&Options::Parse_Options(\@ARGV,\%arguments);
+
+# Specify the properties of this mass function.
+my $entry                                    = 0;
+$massFunctionConfig->{'redshift'           } = pdl 0.350;
+$massFunctionConfig->{'analysisLabel'      } = "zfourgeStellarMassFunctionZ0.350";
+$massFunctionConfig->{'discrepancyFileName'} = "discrepancy".ucfirst($massFunctionConfig->{'analysisLabel'}).".hdf5";
+$massFunctionConfig->{'massType'           } = "stellarMass";
+$massFunctionConfig->{'massErrorRandomDex' } = 0.2;
+$massFunctionConfig->{'xRange'             } = "1.0e9:1.0e13";
+$massFunctionConfig->{'yRange'             } = "1.0e-9:1.0e-1";
+$massFunctionConfig->{'xLabel'             } = "\$M_\\star\$ [\$M_\\odot\$]";
+$massFunctionConfig->{'yLabel'             } = "\${\\rm d}n/{\\rm d}\\log M_\\star\$ [Mpc\$^{-3}\$]";
+$massFunctionConfig->{'title'              } = "Stellar mass function at \$z\\approx 0.350\$";
+$massFunctionConfig->{'systematicParameter'} = "zfourgeStellarMassFunctionZ0.350MassSystematic";
+$massFunctionConfig->{'systematicOrder'    } = 1;
+$massFunctionConfig->{'systematicZeroPoint'} = 11.3;
+
+# Read the observed data.
+my $observations                                      = new PDL::IO::HDF5($galacticusPath."data/observations/massFunctionsStellar/Stellar_Mass_Function_ZFOURGE_2014_z0.20_0.50.hdf5");
+$massFunctionConfig ->{'x'                           }  = $observations->dataset('mass'                )->get    (                  );
+$massFunctionConfig ->{'y'                           }  = $observations->dataset('massFunctionObserved')->get    (                  );
+$massFunctionConfig ->{'yIsPer'                      }  = "ln";
+$massFunctionConfig ->{'xScaling'                    }  = "linear";
+$massFunctionConfig ->{'yScaling'                    }  = "linear";
+$massFunctionConfig ->{'covariance'                  }  = $observations->dataset('covariance'          )->get    (                  );
+($massFunctionConfig->{'observationLabel'            }) = $observations                                 ->attrGet('label'           );
+($massFunctionConfig->{'hubbleConstantObserved'      }) = $observations->group  ('Parameters'          )->attrGet('H_0'             );
+($massFunctionConfig->{'omegaMatterObserved'         }) = $observations->group  ('Parameters'          )->attrGet('Omega_Matter'    );
+($massFunctionConfig->{'omegaDarkEnergyObserved'     }) = $observations->group  ('Parameters'          )->attrGet('Omega_DE'        );
+($massFunctionConfig->{'cosmologyScalingMass'        }) = $observations->dataset('mass'                )->attrGet('cosmologyScaling');
+($massFunctionConfig->{'cosmologyScalingMassFunction'}) = $observations->dataset('massFunction'        )->attrGet('cosmologyScaling');
+
+# Construct the mass function.
+&MassFunctions::Construct(\%arguments,$massFunctionConfig);
+
+exit;
