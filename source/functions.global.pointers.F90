3e2c6d17aabf80602c26ac0588c0a88e9f4a5319
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-09-26 14:32:32 -0700

    * Add functionality to allow global function pointers in cases where it   is necessary to subvert the module hierarchy.

---
+++ source/functions.global.pointers.F90
@@ -0,0 +1,26 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014 Andrew Benson <abenson@obs.carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module providing pointers to global functions.
+
+module Functions_Global
+  !% Provides pointers to global functions.
+  public
+  !# <include directive="functionGlobal" type="functionGlobalPointers" >
+  include 'functionGlobal.pointers.inc'
+  !# </include>
+end module Functions_Global
