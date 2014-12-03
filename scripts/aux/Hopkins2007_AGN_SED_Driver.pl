629735e1412910d16375a730542b21b693ba7849
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-12-03 11:55:55 -0800

    * Add module which provides accretion disk spectra.

---
+++ scripts/aux/Hopkins2007_AGN_SED_Driver.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+my $galacticusPath;
+if ( exists($ENV{'GALACTICUS_ROOT_V093'}) ) {
+    $galacticusPath = $ENV{'GALACTICUS_ROOT_V093'};
+    $galacticusPath .= "/" unless ( $galacticusPath =~ m/\/$/ );
+} else {
+    $galacticusPath = "./";
+}
+unshift(@INC,$galacticusPath."perl");
+use DateTime;
+use File::Copy;
+use PDL;
+use PDL::NiceSlice;
+use PDL::IO::HDF5;
+require Galacticus::AGNLuminosities;
+
+# Driver script for Hopkins (2007) AGN SED model.
+# Andrew Benson (04-August-2014)
+
+# Get arguments.
+die "Usage: Hopkins2007_AGN_SED_Driver.pl <fileFormatVersion>"
+    unless ( scalar(@ARGV) == 1 );
+my $fileFormat = $ARGV[0];
+
+# Ensure the requested file format version is compatible.
+my $fileFormatCurrent = pdl long(1);
+die('Hopkins2007_AGN_SED_Driver.pl: this script supports file format version '.$fileFormatCurrent.' but version '.$fileFormat.' was requested')
+    unless ( $fileFormat == $fileFormatCurrent );
+
+# Determine if we need to make the file.
+my $fileName = $galacticusPath."data/blackHoles/AGN_SEDs_Hopkins2007.hdf5";
+my $makeFile = 0;
+if ( -e $fileName ) {
+    my $hdfFile = new PDL::IO::HDF5($fileName);
+    my @attributes = $hdfFile->attrs();
+    if ( grep {$_ eq "fileFormat"} @attributes  ) {
+	my @fileFormatCurrentFile = $hdfFile->attrGet('fileFormat');
+	$makeFile = 1
+	    unless ( $fileFormatCurrentFile[0] == $fileFormatCurrent );
+    } else {
+	$makeFile = 1;
+    }
+} else {
+    $makeFile = 1;
+}
+
+# Check if we need to make the file.
+&AGNLuminosities::Build_AGN_Spectra($fileFormat)
+    if ( $makeFile == 1 );
+
+exit;
+
---
+++ source/accretion_disks.spectra.F90
@@ -0,0 +1,44 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014 Andrew Benson <abenson@obs.carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module that implements calculations of accretion disk spectra.
+
+module Accretion_Disk_Spectra
+  !% Implements calculations of accretion disk spectra.
+  use ISO_Varying_String
+  use Galacticus_Nodes
+  !# <include directive="accretionDiskSpectra" type="functionModules" >
+  include 'accretionDiskSpectra.functionModules.inc'
+  !# </include>
+  private
+
+  !# <include directive="accretionDiskSpectra" type="function" >
+  !#  <descriptiveName>Accretion Disk Spectra</descriptiveName>
+  !#  <description>Class providing spectra of accretion disks.</description>
+  !#  <default>hopkins2007</default>
+  !#  <defaultThreadPrivate>yes</defaultThreadPrivate>
+  !#  <method name="spectrum" >
+  !#   <description>Returns the spectrum (in units of $L_\odot$~Hz$^{-1}$) of the accretion disk at the given wavelength (in units of \AA) for {\tt node} .</description>
+  !#   <type>double precision</type>
+  !#   <pass>yes</pass>
+  !#   <argument>type            (treeNode), intent(inout) :: node</argument>
+  !#   <argument>double precision          , intent(in   ) :: wavelength</argument>
+  !#  </method>
+  include 'accretionDiskSpectra.type.inc'
+  !# </include>
+
+end module Accretion_Disk_Spectra
