b7ec454e16cd592d022aeb4558eef7a00322a214
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-12-09 12:15:58 -0800

    * Add missing contributor meta-data.

--- source/accretion.halo.Naoz_Barkana_2007.F90
+++ source/accretion.halo.Naoz_Barkana_2007.F90
@@ -15,6 +15,8 @@
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
 
+!+ Contributions to this file made by: Daniel McAndrew.
+
   !% An implementation of accretion from the \gls{igm} onto halos using filtering mass of the \gls{igm}
   !% calculated from an equation from \cite{naoz_formation_2007}.
 
