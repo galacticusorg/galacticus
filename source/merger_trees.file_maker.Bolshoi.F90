f0481c26e1a6fb0574cc6c1deb26c5e79bd6d7c4
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-12-17 11:04:01 -0800

    * Update GPL headers.

--- source/merger_trees.file_maker.Bolshoi.F90
+++ source/merger_trees.file_maker.Bolshoi.F90
@@ -1,6 +1,7 @@
 !! Copyright 2009, 2010, 2011, 2012, 2013, 2014 Andrew Benson <abenson@obs.carnegiescience.edu>
 !!
 !! This file is part of Galacticus.
+!!
 !!    Galacticus is free software: you can redistribute it and/or modify
 !!    it under the terms of the GNU General Public License as published by
 !!    the Free Software Foundation, either version 3 of the License, or
