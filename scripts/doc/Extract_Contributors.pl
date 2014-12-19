8d2ccca35025ab336afa4300070ac2c52726e49f
Andrew Benson <abenson@obs.carnegiescience.edu>
2014-12-19 11:09:42 -0800

    * Avoid hyperlinking to include files when creating list of contributors   since include files do not have a hyperreference.

--- scripts/doc/Extract_Contributors.pl
+++ scripts/doc/Extract_Contributors.pl
@@ -129,7 +129,7 @@ foreach my $person ( @sortedNames ) {
     print oHndl "\\item[".$name."] \\hfill\n";
     print oHndl "\\begin{itemize}\n";
     foreach my $fileName ( sort(keys(%{$contributions->{$person}})) ) {
-	unless ( $fileName =~ m/\.pm$/ ) {
+	unless ( $fileName =~ m/\.pm$/ || $fileName =~ m/\.Inc$/ ) {
 	    print oHndl "\\item \\hyperlink{".$fileName."}{\\normalfont \\ttfamily ".latex_encode($fileName)."}\n";
 	} else {
 	    print oHndl "\\item {\\normalfont \\ttfamily ".latex_encode($fileName)."}\n";
