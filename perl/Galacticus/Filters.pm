41bfb5a9ca4b37596285e4a16b41fda1b27fe06a
Andrew Benson <abenson@obs.carnegiescience.edu>
2015-01-21 09:31:03 -0800

    * Add GALACTICUS_ROOT_V094 pathy to path searched for filter files by   Galacticus::Filters Perl module, so that it works correctly when run   outside of the Galacticus install directory. (grafted from 15d7fb7379b2e32a5fcc936e281c475982ecfb74)

--- perl/Galacticus/Filters.pm
+++ perl/Galacticus/Filters.pm
@@ -1,8 +1,17 @@
 # Contains a Perl module which implements loading of filters for Galacticus analysis packages.
 
+# Contributions to this file from: Andrew Benson; Christoph Behrens.
+
 package Filters;
 use strict;
 use warnings;
+my $galacticusPath;
+if ( exists($ENV{"GALACTICUS_ROOT_V094"}) ) {
+    $galacticusPath = $ENV{"GALACTICUS_ROOT_V094"};
+    $galacticusPath .= "/" unless ( $galacticusPath =~ m/\/$/ );
+} else {
+    $galacticusPath = "./";
+}
 use utf8;
 use PDL;
 use XML::Simple;
@@ -11,7 +20,7 @@ sub Load {
     # Get the name of the requested filter.
     my $filterName = shift;
     # Load a filter response from file.
-    my $filterFile        = "./data/filters/".$filterName.".xml";
+    my $filterFile        = $galacticusPath."data/filters/".$filterName.".xml";
     my $xml               = new XML::Simple;
     my $filter            = $xml->XMLin($filterFile);
     my $filterWavelengths = pdl [];
