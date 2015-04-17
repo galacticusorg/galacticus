c036c4adcc691d15e2eb59e33ecbef8274e02301
Andrew Benson <abenson@obs.carnegiescience.edu>
2015-04-17 09:25:04 -0700

    * Update function call keywords to match new syntax in two test suite   codes.

--- source/tests.linear_growth.cosmological_constant.F90
+++ source/tests.linear_growth.cosmological_constant.F90
@@ -46,7 +46,7 @@ program Tests_Linear_Growth_Cosmological_Constant
   cosmologyFunctionsDefault => cosmologyFunctions()
   do iExpansion=1,size(redshift)
      expansionFactor=cosmologyFunctionsDefault%expansionFactorFromRedshift(redshift(iExpansion))
-     linearGrowth=Linear_Growth_Factor(aExpansion=expansionFactor,component=linearGrowthComponentDarkMatter,normalize=normalizeMatterDominated)/expansionFactor
+     linearGrowth=Linear_Growth_Factor(expansionFactor=expansionFactor,component=linearGrowthComponentDarkMatter,normalize=normalizeMatterDominated)/expansionFactor
      write (message,'(a,f6.1,a)') "dark matter linear growth factor [z=",redshift(iExpansion),"]"
      call Assert(trim(message),linearGrowth,growthFactorDarkEnergy(iExpansion),relTol=1.0d-3)
   end do
