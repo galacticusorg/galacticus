b1d77f7ac21804d15ec7103c753077200c666af7
Andrew Benson <abenson@obs.carnegiescience.edu>
2015-05-13 10:31:11 -0700

    * Add an auto-generated enumeration for galactic structure options.

--- source/galactic_structure.options.F90
+++ source/galactic_structure.options.F90
@@ -15,143 +15,59 @@
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
 
-!% Contains a module which provides various internal option codes for the galactic structure functions.
+!% Contains a module which provides various enumerations for the galactic structure functions.
 
 module Galactic_Structure_Options
   !% Provides various internal option codes for the galactic structure functions.
   implicit none
   public
 
-  ! Values used to represent different mass types.
-  !@ <enumeration>
-  !@  <name>massType</name>
-  !@  <description>Used to specify the mass type(s) to be queried in galactic structure functions.</description>
-  !@  <functions>yes</functions>
-  !@  <entry label="massTypeUnknown"   />
-  !@  <entry label="massTypeAll"       />
-  !@  <entry label="massTypeDark"      />
-  !@  <entry label="massTypeBaryonic"  />
-  !@  <entry label="massTypeGalactic"  />
-  !@  <entry label="massTypeGaseous"   />
-  !@  <entry label="massTypeStellar"   />
-  !@  <entry label="massTypeBlackHole" />
-  !@ </enumeration>
-  integer, parameter :: massTypeCount    =7
-  integer, parameter :: massTypeUnknown  =-1
-  integer, parameter :: massTypeAll      =0
-  integer, parameter :: massTypeDark     =1
-  integer, parameter :: massTypeBaryonic =2
-  integer, parameter :: massTypeGalactic =3
-  integer, parameter :: massTypeGaseous  =4
-  integer, parameter :: massTypeStellar  =5
-  integer, parameter :: massTypeBlackHole=6
-  character(len=9), parameter, dimension(0:massTypeCount-1) :: massTypesName    = &
-       &                                                        [                 &
-       &                                                         'all      ',     &
-       &                                                         'dark     ',     &
-       &                                                         'baryonic ',     &
-       &                                                         'galactic ',     &
-       &                                                         'gaseous  ',     &
-       &                                                         'stellar  ',     &
-       &                                                         'blackHole'      &
-       &                                                        ]
+  !# <enumeration>
+  !#  <name>massType</name>
+  !#  <description>Used to specify the mass type(s) to be queried in galactic structure functions.</description>
+  !#  <encodeFunction>yes</encodeFunction>
+  !#  <entry label="all"       />
+  !#  <entry label="dark"      />
+  !#  <entry label="baryonic"  />
+  !#  <entry label="galactic"  />
+  !#  <entry label="gaseous"   />
+  !#  <entry label="stellar"   />
+  !#  <entry label="blackHole" />
+  !# </enumeration>
 
-  ! Values used to represent different component types.
-  !@ <enumeration>
-  !@  <name>componentType</name>
-  !@  <description>Used to specify the component(s) to be queried in galactic structure functions.</description>
-  !@  <entry label="componentTypeUnknown"   />
-  !@  <entry label="componentTypeAll"       />
-  !@  <entry label="componentTypeDisk"      />
-  !@  <entry label="componentTypeSpheroid"  />
-  !@  <entry label="componentTypeHotHalo"   />
-  !@  <entry label="componentTypeDarkHalo"  />
-  !@  <entry label="componentTypeBlackHole" />
-  !@ </enumeration>
-  integer, parameter :: componentTypeCount    =7
-  integer, parameter :: componentTypeUnknown  =-1
-  integer, parameter :: componentTypeAll      =0
-  integer, parameter :: componentTypeDisk     =1
-  integer, parameter :: componentTypeSpheroid =2
-  integer, parameter :: componentTypeHotHalo  =3
-  integer, parameter :: componentTypeColdHalo =4
-  integer, parameter :: componentTypeDarkHalo =5
-  integer, parameter :: componentTypeBlackHole=6
-  character(len=9), parameter, dimension(0:componentTypeCount-1) :: componentTypesName    = &
-       &                                                        [                           &
-       &                                                         'all      ',               &
-       &                                                         'disk     ',               &
-       &                                                         'spheroid ',               &
-       &                                                         'hotHalo  ',               &
-       &                                                         'coldHalo ',               &
-       &                                                         'darkHalo ',               &
-       &                                                         'blackHole'                &
-       &                                                         ]
 
-  ! Coordinate system options.
-  !@ <enumeration>
-  !@  <name>coordinateSystem</name>
-  !@  <description>Used to specify the coordinate system of the input coordinates in galactic structure functions.</description>
-  !@  <entry label="coordinateSystemSpherical"   />
-  !@  <entry label="coordinateSystemCylindrical" />
-  !@  <entry label="coordinateSystemCartesian"   />
-  !@ </enumeration>
-  integer         , parameter :: coordinateSystemSpherical  =1
-  integer         , parameter :: coordinateSystemCylindrical=2
-  integer         , parameter :: coordinateSystemCartesian  =3
+  !# <enumeration>
+  !#  <name>componentType</name>
+  !#  <description>Used to specify the component(s) to be queried in galactic structure functions.</description>
+  !#  <encodeFunction>yes</encodeFunction>
+  !#  <entry label="all"       />
+  !#  <entry label="disk"      />
+  !#  <entry label="spheroid"  />
+  !#  <entry label="hotHalo"   />
+  !#  <entry label="coldHalo"  />
+  !#  <entry label="darkHalo"  />
+  !#  <entry label="blackHole" />
+  !# </enumeration>
+ 
+  !# <enumeration>
+  !#  <name>coordinateSystem</name>
+  !#  <description>Used to specify the coordinate system of the input coordinates in galactic structure functions.</description>
+  !#  <entry label="spherical"   />
+  !#  <entry label="cylindrical" />
+  !#  <entry label="cartesian"   />
+  !# </enumeration>
 
-  ! Weighting options.
-  !@ <enumeration>
-  !@  <name>weightBy</name>
-  !@  <description>Used to specify by which quantity to weight the results in galactic structure functions.</description>
-  !@  <entry label="weightByMass"       />
-  !@  <entry label="weightByLuminosity" />
-  !@ </enumeration>
-  integer         , parameter :: weightByMass               =0
-  integer         , parameter :: weightByLuminosity         =1
-  integer         , parameter :: weightIndexNull            =0
+  !# <enumeration>
+  !#  <name>weightBy</name>
+  !#  <description>Used to specify by which quantity to weight the results in galactic structure functions.</description>
+  !#  <entry label="mass"       />
+  !#  <entry label="luminosity" />
+  !# </enumeration>
+
+  ! Null value to use when no weighting is to be applied.
+  integer         , parameter, public :: weightIndexNull=0
 
   ! Suitably large value to represent infinite radius.
-  double precision, parameter :: radiusLarge                =1.0d10
-
-contains
-
-  integer function Galactic_Structure_Mass_Type_Decode(massTypeName)
-    !% Decode a mass type from a string, returning the appropriate identifier.
-    use Galacticus_Error
-    implicit none
-    character(len=*), intent(in   ) :: massTypeName
-    integer                         :: i
-
-    Galactic_Structure_Mass_Type_Decode=massTypeUnknown
-    do i=0,massTypeCount-1
-       if (trim(massTypeName) == trim(massTypesName(i))) then
-          Galactic_Structure_Mass_Type_Decode=i
-          exit
-       end if
-    end do
-    if (Galactic_Structure_Mass_Type_Decode == massTypeUnknown) &
-         & call Galacticus_Error_Report('Galactic_Structure_Mass_Type_Decode','unrecognized mass specifier ['//trim(massTypeName)//']')
-    return
-  end function Galactic_Structure_Mass_Type_Decode
-
-  integer function Galactic_Structure_Component_Type_Decode(componentTypeName)
-    !% Decode a component type from a string, returning the appropriate identifier.
-    use Galacticus_Error
-    implicit none
-    character(len=*), intent(in   ) :: componentTypeName
-    integer                         :: i
-
-    Galactic_Structure_Component_Type_Decode=componentTypeUnknown
-    do i=0,componentTypeCount-1
-       if (trim(componentTypeName) == trim(componentTypesName(i))) then
-          Galactic_Structure_Component_Type_Decode=i
-          exit
-       end if
-    end do
-    if (Galactic_Structure_Component_Type_Decode == componentTypeUnknown) &
-         & call Galacticus_Error_Report('Galactic_Structure_Component_Type_Decode','unrecognized component specifier ['//trim(componentTypeName)//']')
-    return
-  end function Galactic_Structure_Component_Type_Decode
+  double precision, parameter, public :: radiusLarge    =1.0d10
 
 end module Galactic_Structure_Options
