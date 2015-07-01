4934e3abb5b7039d5f0440d7395a3fa56551bf5d
Andrew Benson <abenson@obs.carnegiescience.edu>
2015-07-01 16:24:40 -0700

    * Fix formatting.

--- source/cooling.cooling_rate.White-Frenk.F90
+++ source/cooling.cooling_rate.White-Frenk.F90
@@ -90,14 +90,13 @@ contains
     type            (treeNode                    ), intent(inout), pointer :: thisNode
     class           (nodeComponentHotHalo        )               , pointer :: thisHotHaloComponent
     class           (hotHaloMassDistributionClass)               , pointer :: defaultHotHaloMassDistribution
-    class           (darkMatterHaloScaleClass)               , pointer :: darkMatterHaloScale_
+    class           (darkMatterHaloScaleClass    )               , pointer :: darkMatterHaloScale_
     double precision                                                       :: coolingDensity      , infallRadius  , infallRadiusGrowthRate, &
          &                                                                    outerRadius         , virialVelocity
 
     ! Get the virial velocity.
     darkMatterHaloScale_ => darkMatterHaloScale()
     virialVelocity=darkMatterHaloScale_%virialVelocity(thisNode)
-
     ! Return zero cooling rate if virial velocity exceeds critical value.
     if (virialVelocity > zeroCoolingRateAboveVelocity) then
        Cooling_Rate_White_Frenk=0.0d0
