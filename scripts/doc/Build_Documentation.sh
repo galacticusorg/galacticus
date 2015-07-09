04b11a40e4f6d2d2e34c148db53d47fc8ef0ba78
Andrew Benson <abenson@obs.carnegiescience.edu>
2015-07-09 09:37:46 -0700

    * Move documentation build script from csh to sh shell.

--- scripts/doc/Build_Documentation.sh
+++ scripts/doc/Build_Documentation.sh
@@ -1,45 +1,45 @@
-#!/usr/bin/env tcsh
+#!/bin/sh
 
 # Build the Galacticus documentation.
 # Andrew Benson (20-February-2011)
 
 # Ensure that nodeComponent and treeNode objects are built, along with any functions.
 make all
-if ( $? != 0 ) then
+if [ $? -ne 0 ]; then
  echo Failed to build all executables
  exit 1
-endif
+fi
 
 # Extract source code data.
 scripts/doc/Extract_Data.pl source doc/data
-if ( $? != 0 ) then
+if [ $? -ne 0 ]; then
  echo Failed to extract source code data
  exit 1
-endif
+fi
 
 # Extract contributor data.
 scripts/doc/Extract_Contributors.pl . doc/contributions.tex
-if ( $? != 0 ) then
+if [ $? -ne 0 ]; then
  echo Failed to extract contributor data
  exit 1
-endif
+fi
 
 # Analyze source code.
 scripts/doc/Code_Analyzer.pl source doc/source_documentation.tex
-if ( $? != 0 ) then
+if [ $? -ne 0 ]; then
  echo Failed to analyze source code
  exit 1
-endif
+fi
 
 # Move to the documentation folder.
 cd doc
 
 # Demangle the bibliography.
 Bibliography_Demangle.pl
-if ( $? != 0 ) then
+if [ $? -ne 0 ]; then
  echo Failed to demangle bibliography
  exit 1
-endif
+fi
 
 # Order method descriptions.
 ls methods/*.tex | sort | awk '{print "\\input{"substr($1,1,length($1)-4)"}"}' > autoMethods.tex
@@ -54,44 +54,44 @@ ls enumerations/definitions/*.tex | sort | awk '{print "\\input{"substr($1,1,len
 ls enumerations/specifiers/*.tex | sort | awk '{print "\\input{"substr($1,1,length($1)-4)"}"}' > autoEnumerationSpecifiers.tex
 
 # Compile the manual.
-@ iPass = 1
-while( $iPass <= 6 )
+iPass=1
+while [ $iPass -le 6 ]; do
  # Run pdflatex.
  pdflatex Galacticus | grep -v -i -e overfull -e underfull | sed -r /'^$'/d | sed -r /'\[[0-9]*\]'/d
- if ( $? != 0 ) then
+ if [ $? -ne 0 ]; then
   echo pdflatex failed
   exit 1
- endif
+ fi
 
  # Run bibtex.
  bibtex Galacticus
- if ( $? != 0 ) then
+ if [ $? -ne 0 ]; then
   echo bibtex failed
   exit 1
- endif
+ fi
 
  # Run makeindex.
  makeindex Galacticus
- if ( $? != 0 ) then
+ if [ $? -ne 0 ]; then
   echo makeindex failed for main index
   exit 1
- endif
+ fi
 
  # Run makeindex for code index.
  makeindex -s Galacticus.isty Galacticus.cdx -o Galacticus.cnd
- if ( $? != 0 ) then
+ if [ $? -ne 0 ]; then
   echo makeindex failed for code index
   exit 1
- endif
+ fi
 
  # Run makeglossaries.
  makeglossaries Galacticus
- if ( $? != 0 ) then
+ if [ $? -ne 0 ]; then
   echo make glossaries failed
   exit 1
- endif
+ fi
 
- @ iPass++
-end
+ iPass=$((iPass+1))
+done
 
 exit 0
