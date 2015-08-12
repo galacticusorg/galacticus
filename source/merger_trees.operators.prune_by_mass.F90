b87ed78fbf49562d9e48f503844e332e05aa91f3
Andrew Benson <abenson@obs.carnegiescience.edu>
2015-08-12 13:55:56 -0700

    * Add validation of input parameters to merger tree operator classes.

--- source/merger_trees.operators.prune_by_mass.F90
+++ source/merger_trees.operators.prune_by_mass.F90
@@ -46,6 +46,7 @@ contains
     type(inputParameters              ), intent(in   ) :: parameters
     !# <inputParameterList label="allowedParameterNames" />
     
+    call parameters%checkParameters(allowedParameterNames)
     !# <inputParameter>
     !#   <name>massThreshold</name>
     !#   <source>parameters</source>
