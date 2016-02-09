d16b5a5dccfa41600bc83a3a57c2d71baa6fbd79
Andrew Benson <abenson@obs.carnegiescience.edu>
2016-02-09 14:35:20 -0800

    * Update copyright years.

--- source/dark_matter_profiles.structure.mass_definitions.F90
+++ source/dark_matter_profiles.structure.mass_definitions.F90
@@ -1,11 +1,14 @@
-!! Copyright 2009, 2010, 2011, 2012, 2013, 2014 Andrew Benson <abenson@obs.carnegiescience.edu>
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016
+!!    Andrew Benson <abenson@obs.carnegiescience.edu>
 !!
 !! This file is part of Galacticus.
 !!
 !!    Galacticus is free software: you can redistribute it and/or modify
 !!    it under the terms of the GNU General Public License as published by
 !!    the Free Software Foundation, either version 3 of the License, or
-!!    (at your option) any later version. is distributed in the hope that it will be useful,
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
 !!    but WITHOUT ANY WARRANTY; without even the implied warranty of
 !!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 !!    GNU General Public License for more details.
