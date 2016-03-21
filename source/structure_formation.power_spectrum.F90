99dfbfe5a7bb9a89e543ee62a5d17693c225dfc4
Andrew Benson <abenson@obs.carnegiescience.edu>
2016-03-21 09:15:20 -0700

    * Refactor the power spectrum module to function as a new-style function   class.

--- source/structure_formation.power_spectrum.F90
+++ source/structure_formation.power_spectrum.F90
@@ -16,63 +16,36 @@
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
 
-!% Contains a module which implements the cosmological power spectrum.
+!% Contains a module which implements a cosmological power spectra class.
 
 module Power_Spectra
-  !% Implements the cosmological power spectrum.
-  use Tables
-  use ISO_Varying_String
-  implicit none
-  private
-  public :: Power_Spectrum, Power_Spectrum_Logarithmic_Derivative, Power_Spectrum_Dimensionless
-  
-contains
-  
-  double precision function Power_Spectrum_Dimensionless(wavenumber)
-    !% Return the dimensionless power spectrum, $\Delta^2(k)$, for $k=${\normalfont \ttfamily wavenumber} [Mpc$^{-1}$].
-    use Numerical_Constants_Math
-    implicit none
-    double precision, intent(in   ) :: wavenumber
-
-    Power_Spectrum_Dimensionless=+4.0d0                         &
-         &                       *Pi                            &
-         &                       *               wavenumber **3 &
-         &                       *Power_Spectrum(wavenumber)    &
-         &                       /(                             &
-         &                         +2.0d0                       &
-         &                         *Pi                          &
-         &                        )**3
-    return
-  end function Power_Spectrum_Dimensionless
-
-  double precision function Power_Spectrum_Logarithmic_Derivative(wavenumber)
-    !% Return the logarithmic derivative of the power spectrum, ${\mathrm d}\ln P(k)/{\mathrm d}\ln k$, for $k=${\normalfont
-    !% \ttfamily wavenumber} [Mpc$^{-1}$].
-    use Power_Spectra_Primordial_Transferred
-    implicit none
-    double precision                                         , intent(in   ) :: wavenumber
-    class           (powerSpectrumPrimordialTransferredClass), pointer       :: powerSpectrumPrimordialTransferred_
-
-    powerSpectrumPrimordialTransferred_ => powerSpectrumPrimordialTransferred()
-    Power_Spectrum_Logarithmic_Derivative=powerSpectrumPrimordialTransferred_%logarithmicDerivative(wavenumber)
-    return
-  end function Power_Spectrum_Logarithmic_Derivative
-
-  double precision function Power_Spectrum(wavenumber)
-    !% Return the cosmological power spectrum for $k=${\normalfont \ttfamily wavenumber} [Mpc$^{-1}$].
-    use Cosmological_Mass_Variance
-    use Power_Spectra_Primordial_Transferred
-    implicit none
-    double precision                                         , intent(in   ) :: wavenumber
-    class           (cosmologicalMassVarianceClass          ), pointer       :: cosmologicalMassVariance_
-    class           (powerSpectrumPrimordialTransferredClass), pointer       :: powerSpectrumPrimordialTransferred_
-
-    ! Get required objects.
-    powerSpectrumPrimordialTransferred_ => powerSpectrumPrimordialTransferred()
-    cosmologicalMassVariance_           => cosmologicalMassVariance          ()
-    ! Compute the power spectrum.    
-    Power_Spectrum=powerSpectrumPrimordialTransferred_%power(wavenumber)*cosmologicalMassVariance_%powerNormalization()
-    return
-  end function Power_Spectrum
+  !% Implements linear-theory power spectra.
+ 
+  !# <functionClass>
+  !#  <name>powerSpectrum</name>
+  !#  <descriptiveName>Linear Theory Power Spectrum</descriptiveName>
+  !#  <description>Class providing linear theory power spectra.</description>
+  !#  <default>standard</default>
+  !#  <defaultThreadPrivate>yes</defaultThreadPrivate>
+  !#  <stateful>no</stateful>
+  !#  <method name="power" >
+  !#   <description>Return the linear power spectrum for $k=${\normalfont \ttfamily wavenumber} [Mpc$^{-1}$].</description>
+  !#   <type>double precision</type>
+  !#   <pass>yes</pass>
+  !#   <argument>double precision, intent(in   ) :: wavenumber</argument>
+  !#  </method>
+  !#  <method name="powerDimensionless" >
+  !#   <description>Return the dimensionless linear power spectrum for $k=${\normalfont \ttfamily wavenumber} [Mpc$^{-1}$].</description>
+  !#   <type>double precision</type>
+  !#   <pass>yes</pass>
+  !#   <argument>double precision, intent(in   ) :: wavenumber</argument>
+  !#  </method>
+  !#  <method name="powerLogarithmicDerivative" >
+  !#   <description>Return the logarithmic derivative of the power spectrum, ${\mathrm d}\ln P(k)/{\mathrm d}\ln k$, for $k=${\normalfont \ttfamily wavenumber} [Mpc$^{-1}$].</description>
+  !#   <type>double precision</type>
+  !#   <pass>yes</pass>
+  !#   <argument>double precision, intent(in   ) :: wavenumber</argument>
+  !#  </method>
+  !# </functionClass>
 
 end module Power_Spectra
