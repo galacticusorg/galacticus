b45ab0b94904fcbc4b0a054088d935e21efdec7f
Andrew Benson <abenson@obs.carnegiescience.edu>
2016-07-05 11:53:31 -0700

    * Formatting only.

--- source/galacticus.output.times.F90
+++ source/galacticus.output.times.F90
@@ -183,8 +183,8 @@ contains
 
     if (present(findClosest).and.findClosest) then
        Galacticus_Output_Time_Index=Search_Array_For_Closest(outputTimes,time)
-   else
-       Galacticus_Output_Time_Index=Search_Array(outputTimes,time)
+    else
+       Galacticus_Output_Time_Index=Search_Array            (outputTimes,time)
        if (Values_Differ(time,outputTimes(Galacticus_Output_Time_Index),relTol=1.0d-6)) &
             & call Galacticus_Error_Report('Galacticus_Output_Time_Index','time does not correspond to an output')
     end if
