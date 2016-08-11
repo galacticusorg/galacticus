8e3addc07ce514c99af029a7a5db03f1f3db1e56
Andrew Benson <abenson@obs.carnegiescience.edu>
2016-08-11 10:41:52 -0700

    * Begin refactoring of build system scripts, and documentation of the   build system.

---
+++ scripts/build/includeDependencies.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+my $galacticusPath;
+if ( exists($ENV{"GALACTICUS_ROOT_V094"}) ) {
+    $galacticusPath = $ENV{"GALACTICUS_ROOT_V094"};
+    $galacticusPath .= "/" unless ( $galacticusPath =~ m/\/$/ );
+} else {
+    $galacticusPath = "./";
+}
+unshift(@INC, $galacticusPath."perl"); 
+use List::Uniq ':all';
+use File::Slurp;
+use Data::Dumper;
+
+# Construct Makefile rules for source files which have dependencies on include files.
+# Andrew Benson (08-August-2016)
+
+# Get arguments.
+die "Usage: includeDependencies.pl <galacticusDirectory>"
+    unless ( scalar(@ARGV) == 1 );
+my $galacticusDirectoryName = $ARGV[0];
+
+# Specify the source directory.
+my $sourceDirectoryName = $galacticusDirectoryName."/source";
+
+# Build a list of pre-existing C include file paths. This includes all paths searched by GCC, any files specified in environ, and
+# the source directory.
+my @cIncludeDirectoryNames =
+    (
+     $sourceDirectoryName, 
+     map {$_ =~ m/^\s*(\/\S+)/ ? $1 : ()} split(/\n/,`echo | cpp -Wp,-v 2>&1`),     
+    );
+foreach my $environmentVariable ( "GALACTICUS_CFLAGS", "GALACTICUS_CPPFLAGS" ) {
+    push(@cIncludeDirectoryNames,map {$_ =~ m/^\-I(.*)/ ? $1 : ()} split(" ",$ENV{$environmentVariable}))
+	if ( exists($ENV{$environmentVariable}) );
+}
+
+# Open an output file
+open(my $makeFile,">".$ENV{'BUILDPATH'}."/Makefile_Include_Deps");
+
+# Initialize a list of files on which Makefile_Use_Deps will depend.
+my @dependencyFileNames;
+
+# Scan the source directory.
+opendir(my $sourceDirectory,$sourceDirectoryName)
+    or die "includeDependencies.pl: can not open the source directory: #!";
+while ( my $fileName = readdir($sourceDirectory) ) {
+    # Skip temporary files.
+    next
+	if ( $fileName =~ m/^\.\#/ );
+    # Skip non-source files.
+    next
+	unless
+	(
+	 $fileName =~ m/\.f(90)??$/i # Fortran and Fortran-90
+	 ||
+	 $fileName =~ m/\.inc$/i     # Fortran include files
+	 ||
+	 $fileName =~ m/\.c(pp)??$/  # C and C++ source files
+	 ||
+	 $fileName =~ m/\.h$/        # C/C++ include files
+	);
+    # Parse the file. We match include lines, extracting the name of the included file into a list of hashes. Each hash also holds
+    # an "automatic" element which is true unless the "NO_USES" qualifier appears in the include statement line (which instructs
+    # us to ignore that include file for the purposes of finding module "use" dependencies).
+    my $fileFullName  = $sourceDirectoryName."/".$fileName;
+    my @includedFiles = 
+	map
+         {
+	     (
+	      $_ =~ /^\s*#??include\s*[<'"]((.+)\.(inc|h)\d*)['">](\s*\!\s*NO_USES)?/
+	      &&
+	      (
+	       $3 eq "inc"
+	       ||
+	       ($3 eq "h" && ! -e $sourceDirectoryName."/".$1 && ! grep {-e $_."/".$1} @cIncludeDirectoryNames) # C-include files must not be pre-existing in source or system directories,
+	       )
+	     )
+          ?
+             {fileName => $1, automatic => !defined($4)} 
+	  :
+             ()
+         }
+        read_file($fileFullName, chomp => 1);
+    # Output the dependencies.
+    (my $objectFileName = $fileName)  =~ s/\.[^\.]+$/.o/;
+    print $makeFile $ENV{'BUILDPATH'}."/".$objectFileName.": ".join(" ",sort(map {$ENV{'BUILDPATH'}."/".$_->{'fileName'}} @includedFiles))."\n\n"
+	if ( scalar(@includedFiles) > 0 );
+    # Generate a list of include files on which Makefile_Use_Deps will depend. This is the unpreprocessed include file in the
+    # source directory if such exists, or the raw include file in the build directory otherwise.
+    foreach my $includedFile ( @includedFiles ) {
+	(my $fileNameUnpreprocessed = $includedFile->{'fileName'}) =~ s/\.inc$/.Inc/;
+	push(@dependencyFileNames,-e $sourceDirectoryName."/".$fileNameUnpreprocessed ? $fileNameUnpreprocessed : $includedFile->{'fileName'})
+	   if ( $includedFile->{'fileName'} =~ m/\.inc$/ && $includedFile->{'automatic'} );
+    }
+}
+closedir($sourceDirectory);
+# Add dependencies for Makefile_Use_Deps, such that it will be regenerated after any automatically-built include files are
+# created. This will ensure that any module "uses" in the automatically-generated include files will be discovered.
+print $makeFile "\n".$ENV{'BUILDPATH'}."/Makefile_Use_Deps: ".join(" ",map {$ENV{'BUILDPATH'}."/".$_} sort(@dependencyFileNames))."\n";
+close($makeFile);
+
+exit;
---
+++ scripts/build/parameterDependencies.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+my $galacticusPath;
+if ( exists($ENV{"GALACTICUS_ROOT_V094"}) ) {
+    $galacticusPath = $ENV{"GALACTICUS_ROOT_V094"};
+    $galacticusPath .= "/" unless ( $galacticusPath =~ m/\/$/ );
+} else {
+    $galacticusPath = "./";
+}
+unshift(@INC, $galacticusPath."perl"); 
+use XML::Simple;
+use Data::Dumper;
+use File::Slurp;
+use List::Uniq ':all';
+use List::MoreUtils qw{ any };
+require Galacticus::Doc::Parameters;
+require Galacticus::Build::Directives;
+
+# Scan source files for input parameter definitions for a given executable.
+# Andrew Benson (18-October-2011)
+
+# Get the name of the executable to find parameters for.
+die("Usage: parameterDependencies.pl <sourceDirectory> <executable>")
+    unless ( scalar(@ARGV) == 2 );
+my $sourceDirectoryName = $ARGV[0];
+my $executableName      = $ARGV[1];
+
+# Include files to exclude from parameter search.
+my @includeFilesExcluded = ( "fftw3.f03" );
+
+# Build a list of object file dependencies.
+(my $dependencyFileName = $ENV{'BUILDPATH'}."/".$executableName) =~ s/\.exe/\.d/;
+my @objectFiles = map { $_ =~ /^$ENV{'BUILDPATH'}\/(.+\.o)$/ ? $1 : () } read_file($dependencyFileName, chomp => 1);
+
+# Open the source diretory, finding F90 and cpp files.
+my $output;
+opendir(my $sourceDirectory,$sourceDirectoryName);
+while ( my $fileName = readdir($sourceDirectory) ) {
+    # Skip junk files.
+    next
+	if ( $fileName =~ m/^\.\#/ );
+    # Skip non-F90, non-cpp files
+    next
+	unless ( $fileName =~ m/\.(F90|cpp)$/ );
+    # Find corresponding object file name.    
+    (my $objectFileName = $fileName) =~ s/\.(F90|cpp)$/\.o/;
+    # Skip non-dependency files.
+    next
+	unless ( grep {$_ eq $objectFileName} @objectFiles );
+    # For Fortran files, check for a ".p" parameter file in the build directory. These files are created by
+    # Galacticus::Build::SourceTree::Process::FunctionClass.
+    if ( $fileName =~ m/\.F90$/) {
+	(my $rootFileName = $ENV{'BUILDPATH'}."/".$fileName) =~ s/\.F90$/./;
+	push(@{$output->{'parameters'}},read_file($rootFileName."p", chomp => 1, err_mode => 'quiet'))
+	    if ( grep {$_ eq $rootFileName."o"} @objectFiles );
+    }
+    # Process Fortran and C++ files.
+    # Create a stack of files to process.
+    my @fileStack = ( $sourceDirectoryName."/".$fileName );
+    while ( scalar(@fileStack) > 0 ) {
+	my $fileToProcess = shift(@fileStack);
+	# Find "include" lines in the file, extract the name of the included file, filter out any include files which are to be
+	# excluded from parameter search, and push those remaining onto the file stack.
+	push
+	    (
+	     @fileStack,
+	     map 
+	      {($_ =~ m/^\s*include\s*[\'\"](.*)[\'\"]/ && ! grep {$1 eq $_} @includeFilesExcluded) ? $ENV{'BUILDPATH'}."/".$1 : ()} 
+	      read_file($fileToProcess, chomp => 1)
+	    );
+	# Find all "inputParameter" directives, extract names from them, and push to the list of parameters.
+	push
+	    (
+	     @{$output->{'parameters'}},
+	     map 
+	      {exists($_->{'regEx'}) ? "regEx:".&Parameters::ExpandRegEx($_->{'regEx'},$sourceDirectoryName) : $_->{'name'}}
+	      &Directives::Extract_Directives($fileToProcess,"inputParameter",comment => qr/^\s*(!|\/\/)\@/) 
+	    );
+    }
+}
+close($sourceDirectory);
+
+# Remove duplicated parameters.
+@{$output->{'parameters'}} = uniq({sort => 1}, @{$output->{'parameters'}});
+
+# Serialize to XML.
+my $xmlOutput        = new XML::Simple (NoAttr=>1, RootName=>"parameters");
+my $outputSerialized = $xmlOutput->XMLout($output);
+
+# Escape square brackets in the output to that they get correctly parsed by FoX.
+$outputSerialized =~ s/\[/&#x005B;/g;
+$outputSerialized =~ s/\]/&#x005D;/g;
+
+# Output the results.
+(my $outputFileName = $executableName) =~ s/\.exe/.parameters.xml/;
+open(oHndl,">".$ENV{'BUILDPATH'}."/".$outputFileName);
+print oHndl $outputSerialized;
+close(oHndl);
+
+exit;
