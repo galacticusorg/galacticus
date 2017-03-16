97f44a1ae2399c626524adedc7ba0780638576d6
Andrew Benson <abenson@obs.carnegiescience.edu>
2017-03-16 11:49:15 -0700

    * Allow the "very simple" merger mass movements code to define a major   merger.

--- source/satellites.merging.mass_movements.very_simple.F90
+++ source/satellites.merging.mass_movements.very_simple.F90
@@ -25,6 +25,9 @@ module Satellite_Merging_Mass_Movements_Very_Simple
   private
   public :: Satellite_Merging_Mass_Movements_Very_Simple_Initialize
 
+  ! Mass ratio above which a merger is considered to be "major".
+  double precision :: majorMergerMassRatio
+
 contains
 
   !# <satelliteMergingMassMovementsMethod>
@@ -33,24 +36,53 @@ contains
   subroutine Satellite_Merging_Mass_Movements_Very_Simple_Initialize(satelliteMergingMassMovementsMethod,Satellite_Merging_Mass_Movement_Get)
     !% Test if this method is to be used and set procedure pointer appropriately.
     use ISO_Varying_String
+    use Input_Parameters
     implicit none
     type     (varying_string                             ), intent(in   )          :: satelliteMergingMassMovementsMethod
     procedure(Satellite_Merging_Mass_Movement_Very_Simple), intent(inout), pointer :: Satellite_Merging_Mass_Movement_Get
 
-    if (satelliteMergingMassMovementsMethod == 'verySimple') Satellite_Merging_Mass_Movement_Get => Satellite_Merging_Mass_Movement_Very_Simple
+    if (satelliteMergingMassMovementsMethod == 'verySimple') then
+       Satellite_Merging_Mass_Movement_Get => Satellite_Merging_Mass_Movement_Very_Simple
+       !@ <inputParameter>
+       !@   <name>majorMergerMassRatio</name>
+       !@   <defaultValue>0.0</defaultValue>
+       !@   <attachedTo>module</attachedTo>
+       !@   <description>
+       !@     The mass ratio above which mergers are considered to be ``major'' in the very simple merger mass movements method.
+       !@   </description>
+       !@   <type>real</type>
+       !@   <cardinality>1</cardinality>
+       !@ </inputParameter>
+       call Get_Input_Parameter("majorMergerMassRatio",majorMergerMassRatio,defaultValue=0.0d0)
+    end if
     return
   end subroutine Satellite_Merging_Mass_Movements_Very_Simple_Initialize
 
   subroutine Satellite_Merging_Mass_Movement_Very_Simple(thisNode,gasMovesTo,starsMoveTo,hostGasMovesTo,hostStarsMoveTo,mergerIsMajor)
     !% Determine where stars and gas move as the result of a merger event using a simple algorithm.
     use Galacticus_Nodes
+    use Galactic_Structure_Enclosed_Masses
+    use Galactic_Structure_Options
     implicit none
-    type   (treeNode), intent(inout) :: thisNode
-    integer          , intent(  out) :: gasMovesTo   , hostGasMovesTo, hostStarsMoveTo, starsMoveTo
-    logical          , intent(  out) :: mergerIsMajor
-    !GCC$ attributes unused :: thisNode
+    type            (treeNode), intent(inout) :: thisNode
+    integer                   , intent(  out) :: gasMovesTo   , hostGasMovesTo, hostStarsMoveTo, starsMoveTo
+    logical                   , intent(  out) :: mergerIsMajor
+    type            (treeNode), pointer       :: hostNode
+    double precision                          :: hostMass     , satelliteMass
     
-    mergerIsMajor  =.false.
+    ! Determine if this merger is considered major.
+    if      (majorMergerMassRatio <= 0.0d0) then
+       mergerIsMajor=.true.
+    else if (majorMergerMassRatio >  1.0d0) then
+       mergerIsMajor=.false.
+    else
+       ! Find the node to merge with.
+       hostNode => thisNode%mergesWith()
+       ! Find the baryonic masses of the two galaxies.
+       satelliteMass=Galactic_Structure_Enclosed_Mass(thisNode,massType=massTypeGalactic)
+       hostMass     =Galactic_Structure_Enclosed_Mass(hostNode,massType=massTypeGalactic)    
+       mergerIsMajor=satelliteMass >= majorMergerMassRatio*hostMass
+    end if
     gasMovesTo     =movesToDisk
     starsMoveTo    =movesToDisk
     hostGasMovesTo =doesNotMove
