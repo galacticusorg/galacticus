336adee586f144850f4e6db4ae0a0dbc5717bb75
Andrew Benson <abenson@obs.carnegiescience.edu>
2017-04-07 13:53:36 -0700

    * Add an ALFALFA HI mass function output analysis class.

---
+++ source/galacticus.output.analyses.distribution_operator.random_error.HI_mass_ALFALFA.F90
@@ -0,0 +1,141 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+  !% Contains a module which implements a random error output analysis distribution operator class providing errors in HI mass for
+  !% the ALFALFA survey.
+
+  use Output_Analysis_Molecular_Ratios
+
+  !# <outputAnalysisDistributionOperator name="outputAnalysisDistributionOperatorRandomErrorALFLF">
+  !#  <description>A random error output analysis distribution operator class providing errors in HI mass for the ALFALFA survey. Specifically, $\sigma_{\mathrm obs} = a + \exp\left(-{\log_{10}(M_{\mathrm HI}/M_\odot)-b\over c}\right)$.</description>
+  !# </outputAnalysisDistributionOperator>
+  type, extends(outputAnalysisDistributionOperatorRandomError) :: outputAnalysisDistributionOperatorRandomErrorALFLF
+     !% A random error output distribution operator class providing errors in HI mass for the ALFALFA survey.
+     private
+     double precision                                             :: a                            , b, &
+          &                                                          c
+     class           (outputAnalysisMolecularRatioClass), pointer :: outputAnalysisMolecularRatio_
+   contains
+     procedure :: rootVariance => randomErrorHIALFALFARootVariance
+  end type outputAnalysisDistributionOperatorRandomErrorALFLF
+
+  interface outputAnalysisDistributionOperatorRandomErrorALFLF
+     !% Constructors for the ``randomErrorHIALFALFA'' output analysis distribution operator class.
+     module procedure randomErrorHIALFALFAConstructorParameters
+     module procedure randomErrorHIALFALFAConstructorInternal
+  end interface outputAnalysisDistributionOperatorRandomErrorALFLF
+
+contains
+
+  function randomErrorHIALFALFAConstructorParameters(parameters) result(self)
+    !% Constructor for the ``randomErrorHIALFALFA'' output analysis distribution operator class which takes a parameter set as input.
+    use Input_Parameters2
+    implicit none
+    type            (outputAnalysisDistributionOperatorRandomErrorALFLF)                :: self
+    type            (inputParameters                                   ), intent(inout) :: parameters
+    class           (outputAnalysisMolecularRatioClass                 ), pointer       :: outputAnalysisMolecularRatio_
+    double precision                                                                    :: a                            , b, &
+         &                                                                                 c
+    !# <inputParameterList label="allowedParameterNames" />
+
+    ! Check and read parameters.
+    call parameters%checkParameters(allowedParameterNames)
+    !# <inputParameter>
+    !#   <name>a</name>
+    !#   <source>parameters</source>
+    !#   <defaultValue>0.1d0</defaultValue>
+    !#   <variable>a</variable>
+    !#   <description>Parameter $a$ in the ALFALFA HI mass error model.</description>
+    !#   <type>float</type>
+    !#   <cardinality>0..1</cardinality>
+    !# </inputParameter>
+    !# <inputParameter>
+    !#   <name>b</name>
+    !#   <source>parameters</source>
+    !#   <defaultValue>5.885d0</defaultValue>
+    !#   <variable>b</variable>
+    !#   <description>Parameter $b$ in the ALFALFA HI mass error model.</description>
+    !#   <type>float</type>
+    !#   <cardinality>0..1</cardinality>
+    !# </inputParameter>
+    !# <inputParameter>
+    !#   <name>c</name>
+    !#   <source>parameters</source>
+    !#   <defaultValue>0.505d0</defaultValue>
+    !#   <variable>c</variable>
+    !#   <description>Parameter $c$ in the ALFALFA HI mass error model.</description>
+    !#   <type>float</type>
+    !#   <cardinality>0..1</cardinality>
+    !# </inputParameter>
+    !# <objectBuilder class="outputAnalysisMolecularRatio" name="outputAnalysisMolecularRatio_" source="parameters"/>
+    ! Construct the object.
+    self=outputAnalysisDistributionOperatorRandomErrorALFLF(a,b,c,outputAnalysisMolecularRatio_)
+    return
+  end function randomErrorHIALFALFAConstructorParameters
+
+  function randomErrorHIALFALFAConstructorInternal(a,b,c,outputAnalysisMolecularRatio_) result(self)
+    !% Internal constructor for the ``randomErrorHIALFALFA'' output analysis distribution operator class.
+    implicit none
+    type            (outputAnalysisDistributionOperatorRandomErrorALFLF)                        :: self
+    double precision                                                    , intent(in   )         :: a                            , b, &
+         &                                                                                         c
+    class           (outputAnalysisMolecularRatioClass                 ), intent(in   ), target :: outputAnalysisMolecularRatio_
+   !# <constructorAssign variables="a, b, c, *outputAnalysisMolecularRatio_"/>
+
+    return
+  end function randomErrorHIALFALFAConstructorInternal
+
+  double precision function randomErrorHIALFALFARootVariance(self,propertyValue,node)
+    !% Computes errors on $\log_{10}($HI masses$)$ for the ALFALFA survey analysis. Uses a simple fitting function. See {\tt
+    !% constraints/dataAnalysis/hiMassFunction\_ALFALFA\_z0.00/alfalfaHIMassErrorModel.pl} for details.
+    implicit none
+    class           (outputAnalysisDistributionOperatorRandomErrorALFLF), intent(inout) :: self
+    double precision                                                    , intent(in   ) :: propertyValue
+    type            (treeNode                                          ), intent(inout) :: node
+    double precision                                                                    :: exponentialArgument, exponentialTerm, &
+         &                                                                                 molecularFraction  , mass
+    
+    ! Compute the random error on the mass.
+    exponentialArgument=+(                          &
+         &                +max(propertyValue,6.0d0) &
+         &                -self%b                   &
+         &               )                          &
+         &              /  self%c
+    if (exponentialArgument > 0.0d0) then
+       exponentialTerm=exp(-exponentialArgument)
+    else
+       exponentialTerm=0.0d0
+    end if
+    randomErrorHIALFALFARootVariance=+self%a          &
+         &                           +exponentialTerm 
+    ! Add in quadrature a term accounting for the scatter in the molecular ratio model.
+    mass                            =10.0d0**propertyValue
+    molecularFraction               =        self%outputAnalysisMolecularRatio_%ratio       (mass,node)
+    randomErrorHIALFALFARootVariance=sqrt(                                                                 &
+         &                                +randomErrorHIALFALFARootVariance                            **2 &
+         &                                +(                                                               &
+         &                                  +self%outputAnalysisMolecularRatio_%ratioScatter(mass,node)    &
+         &                                  *molecularFraction                                             &
+         &                                  /(                                                             &
+         &                                    +1.0                                                         &
+         &                                    +molecularFraction                                           &
+         &                                   )                                                             &
+         &                                 )                                                           **2 &
+         &                               )
+    return
+  end function randomErrorHIALFALFARootVariance
---
+++ source/galacticus.output.analyses.property_operator.HI_mass.F90
@@ -0,0 +1,86 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which implements a conversion of ISM mass to HI mass analysis property operator class.
+
+  use Output_Analysis_Molecular_Ratios
+  
+  !# <outputAnalysisPropertyOperator name="outputAnalysisPropertyOperatorHIMass">
+  !#  <description>A conversion of ISM mass to HI mass analysis property operator class.</description>
+  !# </outputAnalysisPropertyOperator>
+  type, extends(outputAnalysisPropertyOperatorClass) :: outputAnalysisPropertyOperatorHIMass
+     !% A conversion of ISM mass to HI mass property operator class.
+     private
+     class(outputAnalysisMolecularRatioClass), pointer :: outputAnalysisMolecularRatio_
+   contains
+     procedure :: operate => hiMassOperate
+  end type outputAnalysisPropertyOperatorHIMass
+
+  interface outputAnalysisPropertyOperatorHIMass
+     !% Constructors for the ``hiMass'' output analysis class.
+     module procedure hiMassConstructorParameters
+     module procedure hiMassConstructorInternal
+  end interface outputAnalysisPropertyOperatorHIMass
+
+contains
+
+  function hiMassConstructorParameters(parameters) result(self)
+    !% Constructor for the ``hiMass'' output analysis property operator class which takes a parameter set as input.
+    use Input_Parameters2
+    implicit none
+    type (outputAnalysisPropertyOperatorHIMass)                :: self
+    type (inputParameters                     ), intent(inout) :: parameters
+    class(outputAnalysisMolecularRatioClass   ), pointer       :: outputAnalysisMolecularRatio_
+    !# <inputParameterList label="allowedParameterNames" />
+
+    ! Check and read parameters.
+    call parameters%checkParameters(allowedParameterNames)
+    !# <objectBuilder class="outputAnalysisMolecularRatio" name="outputAnalysisMolecularRatio_" source="parameters" />
+    self=outputAnalysisPropertyOperatorHIMass(outputAnalysisMolecularRatio_)
+    return
+  end function hiMassConstructorParameters
+
+  function hiMassConstructorInternal(outputAnalysisMolecularRatio_) result (self)
+    !% Internal constructor for the ``hiMass'' output analysis distribution operator class.
+    use Input_Parameters2
+    implicit none
+    type (outputAnalysisPropertyOperatorHIMass)                        :: self
+    class(outputAnalysisMolecularRatioClass   ), intent(in   ), target :: outputAnalysisMolecularRatio_
+    !# <constructorAssign variables="*outputAnalysisMolecularRatio_"/>
+
+    return
+  end function hiMassConstructorInternal
+
+  double precision function hiMassOperate(self,propertyValue,node,propertyType,outputIndex)
+    !% Implement an hiMass output analysis property operator.
+    use, intrinsic :: ISO_C_Binding
+    use            :: Galacticus_Error
+    use            :: Numerical_Constants_Astronomical
+    implicit none
+    class           (outputAnalysisPropertyOperatorHIMass), intent(inout)           :: self
+    double precision                                      , intent(in   )           :: propertyValue
+    type            (treeNode                            ), intent(inout), optional :: node
+    integer                                               , intent(inout), optional :: propertyType
+    integer         (c_size_t                            ), intent(in   ), optional :: outputIndex
+    !GCC$ attributes unused :: propertyType, outputIndex
+
+    if (.not.present(node)) call Galacticus_Error_Report('hiMassOperate','node must be provided')
+    hiMassOperate=+propertyValue                                                &
+         &        *self%outputAnalysisMolecularRatio_%ratio(propertyValue,node)
+    return
+  end function hiMassOperate
