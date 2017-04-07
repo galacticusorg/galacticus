b10a5bb214f27a57738ab8b8d207257f1cb243d8
Andrew Benson <abenson@obs.carnegiescience.edu>
2017-04-07 13:46:52 -0700

    * Add constraint definitions and scripts for the remaining redshift bins   of the Leauthaud et al. (2012) SHMR constraint.

---
+++ constraints/scripts/stellarHaloMassRelation_COSMOS_Leauthaud2012_Z3.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Cwd;
+use lib exists($ENV{'GALACTICUS_ROOT_V094'}) ? $ENV{'GALACTICUS_ROOT_V094'}.'/perl' : cwd().'/perl';
+use Galacticus::Constraints::StellarHaloMassRelation;
+
+# Compute likelihood (and make a plot) for a Galacticus model given the stellar mass-halo mass relation of Leauthaud et
+# al. (2012).
+
+# Get name of input and output files.
+die("stellarHaloMassRelation_Leauthaud2012_Z3.pl <galacticusFile> [options]")
+    unless ( scalar(@ARGV) >= 1 );
+my $galacticusFileName = $ARGV[0];
+# Create a hash of named options.
+my $iArg = -1;
+my %options =
+    (
+     quiet => 0
+    );
+&Galacticus::Options::Parse_Options(\@ARGV,\%options);
+
+# Evaluate the model.
+&Galacticus::Constraints::StellarHaloMassRelation::COSMOS2012($galacticusFileName,3,%options);
+
+exit;
