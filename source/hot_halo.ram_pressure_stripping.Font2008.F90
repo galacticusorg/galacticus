2979e7db3ddcc8474c4e7377ee4b183dec601464
Andrew Benson <abenson@obs.carnegiescience.edu>
2017-06-02 14:39:52 -0700

    * Optimize Font2008 ram pressure stripping radius calculation by using   the previous radius as a guess for the new radius.

--- source/hot_halo.ram_pressure_stripping.Font2008.F90
+++ source/hot_halo.ram_pressure_stripping.Font2008.F90
@@ -22,22 +22,28 @@
 module Hot_Halo_Ram_Pressure_Stripping_Font2008
   !% Implements a module which implements a model of ram pressure stripping of hot halos based on the methods of
   !% \cite{font_colours_2008}.
+  use Kind_Numbers
   use Galacticus_Nodes
   implicit none
   private
   public :: Hot_Halo_Ram_Pressure_Stripping_Font2008_Initialize
 
   ! Pointers to the satellite node.
-  type            (treeNode), pointer :: satelliteNode
+  type            (treeNode      ), pointer :: satelliteNode
   !$omp threadprivate(satelliteNode)
   ! The ram pressure force (per unit area) used in root finding.
-  double precision                    :: ramPressureForce
+  double precision                          :: ramPressureForce
   !$omp threadprivate(ramPressureForce)
   ! Parameters of the ram pressure stripping model.
-  double precision                    :: ramPressureStrippingFormFactor
+  double precision                          :: ramPressureStrippingFormFactor
+  ! Optimization.
+  integer         (kind=kind_int8)          :: lastUniqueID                  =-1
+  double precision                          :: radiusLast                    =-1.0d0
+  !$omp threadprivate(lastUniqueID,radiusLast)
 
+  
 contains
-
+ 
   !# <hotHaloRamPressureStrippingMethod>
   !#  <unitName>Hot_Halo_Ram_Pressure_Stripping_Font2008_Initialize</unitName>
   !# </hotHaloRamPressureStrippingMethod>
@@ -67,63 +73,75 @@ contains
     return
   end subroutine Hot_Halo_Ram_Pressure_Stripping_Font2008_Initialize
 
-  double precision function Hot_Halo_Ram_Pressure_Stripping_Font2008_Get(thisNode)
+  double precision function Hot_Halo_Ram_Pressure_Stripping_Font2008_Get(node)
     !% Computes the hot halo ram pressure stripping radius, assuming a null calculation in which that radius always equals the
     !% virial radius.
     use Dark_Matter_Halo_Scales
     use Hot_Halo_Ram_Pressure_Forces
     use Root_Finder
     implicit none
-    type            (treeNode                ), intent(inout), target :: thisNode
+    type            (treeNode                ), intent(inout), target :: node
     double precision                          , parameter             :: toleranceAbsolute             =0.0d0 , toleranceRelative=1.0d-3
     double precision                          , parameter             :: radiusSmallestOverRadiusVirial=1.0d-6
     class           (darkMatterHaloScaleClass), pointer               :: darkMatterHaloScale_
     type            (rootFinder              ), save                  :: finder
     !$omp threadprivate(finder)
     double precision                                                  :: virialRadius
-
+ 
     ! Get the virial radius of the satellite.
     darkMatterHaloScale_ => darkMatterHaloScale              (        )
-    virialRadius         =  darkMatterHaloScale_%virialRadius(thisNode)
-    ! Test whether thisNode is a satellite.
-    if (thisNode%isSatellite()) then
+    virialRadius         =  darkMatterHaloScale_%virialRadius(node)
+    ! Test whether node is a satellite.
+    if (node%isSatellite()) then
        ! Set a pointer to the satellite node.
-       satelliteNode => thisNode
+       satelliteNode => node
        ! Get the ram pressure force due to the hot halo.
-       ramPressureForce=Hot_Halo_Ram_Pressure_Force(thisNode)
-       ! Find the radial range within which the pericenter must lie. The pericenter must be smaller than (or equal to) the
-       ! current radius of the orbit.
+       ramPressureForce=Hot_Halo_Ram_Pressure_Force(node)
+       ! Find the radial range within which the ram pressure radius must lie.
        if      (Hot_Halo_Ram_Pressure_Stripping_Radius_Solver(                               virialRadius) >= 0.0d0) then
           ! The ram pressure force is not sufficiently strong to strip even at the satellite virial radius - simply return the
           ! virial radius as the stripping radius in this case.
           Hot_Halo_Ram_Pressure_Stripping_Font2008_Get=virialRadius
-       else if (Hot_Halo_Ram_Pressure_Stripping_Radius_Solver(radiusSmallestOverRadiusVirial*virialRadius) <= 0.0d0) then
-          ! The ram pressure force can strip to (essentially) arbitrarily small radii.
-          Hot_Halo_Ram_Pressure_Stripping_Font2008_Get=0.0d0
        else
-          ! Solver for the ram pressure stripping radius.
-          if (.not.finder%isInitialized()) then
-             call finder%rootFunction(Hot_Halo_Ram_Pressure_Stripping_Radius_Solver)
-             call finder%tolerance   (toleranceAbsolute,toleranceRelative          )
-          end if
-          if (                                                                                                &
-               &   Hot_Halo_Ram_Pressure_Stripping_Radius_Solver(radiusSmallestOverRadiusVirial*virialRadius) &
-               &  <=                                                                                          &
-               &   0.0d0                                                                                      &
-               & ) then
+          if (Hot_Halo_Ram_Pressure_Stripping_Radius_Solver(radiusSmallestOverRadiusVirial*virialRadius) <= 0.0d0) then
+             ! The ram pressure force can strip to (essentially) arbitrarily small radii.
              Hot_Halo_Ram_Pressure_Stripping_Font2008_Get=0.0d0
           else
-             call finder%rangeExpand(                                                                           &
-                  &                  rangeExpandDownward          =0.5d0                                      , &
-                  &                  rangeExpandType              =rangeExpandMultiplicative                  , &
-                  &                  rangeDownwardLimit           =radiusSmallestOverRadiusVirial*virialRadius, &
-                  &                  rangeExpandDownwardSignExpect=rangeExpandSignExpectPositive                &
-                  &                 )
-             Hot_Halo_Ram_Pressure_Stripping_Font2008_Get=finder%find(rootGuess=virialRadius)
+             ! Solver for the ram pressure stripping radius.
+             if (.not.finder%isInitialized()) then
+                call finder%rootFunction(Hot_Halo_Ram_Pressure_Stripping_Radius_Solver)
+                call finder%tolerance   (toleranceAbsolute,toleranceRelative          )
+             end if
+             ! If we have a previously found radius, and if the node is the same as the previous node for which this function was
+             ! called, then use that previous radius as a guess for the new solution. Note that we do not reset the previous
+             ! radius between ODE steps (i.e. we do not make use of "calculationReset" events to reset the radius) as we
+             ! specifically want to retain knowledge from the previous step.
+             if (radiusLast > 0.0d0 .and. node%uniqueID() == lastUniqueID) then
+                call finder%rangeExpand(                                                                           &
+                     &                  rangeExpandDownward          =0.9d0                                      , &
+                     &                  rangeExpandUpward            =1.1d0                                      , &
+                     &                  rangeExpandType              =rangeExpandMultiplicative                  , &
+                     &                  rangeDownwardLimit           =radiusSmallestOverRadiusVirial*virialRadius, &
+                     &                  rangeUpwardLimit             =                               virialRadius, &
+                     &                  rangeExpandDownwardSignExpect=rangeExpandSignExpectPositive              , &
+                     &                  rangeExpandUpwardSignExpect  =rangeExpandSignExpectNegative                &
+                     &                 )
+             else
+                call finder%rangeExpand(                                                                           &
+                     &                  rangeExpandDownward          =0.5d0                                      , &
+                     &                  rangeExpandType              =rangeExpandMultiplicative                  , &
+                     &                  rangeDownwardLimit           =radiusSmallestOverRadiusVirial*virialRadius, &
+                     &                  rangeExpandDownwardSignExpect=rangeExpandSignExpectPositive                &
+                     &                 )
+                radiusLast  =virialRadius
+                lastUniqueID=node%uniqueID()
+             end if
+             Hot_Halo_Ram_Pressure_Stripping_Font2008_Get=finder%find(rootGuess=radiusLast)
+             radiusLast=Hot_Halo_Ram_Pressure_Stripping_Font2008_Get
           end if
        end if
     else
-       ! If thisNode is not a satellite, return a ram pressure stripping radius equal to the virial radius.
+       ! If node is not a satellite, return a ram pressure stripping radius equal to the virial radius.
        Hot_Halo_Ram_Pressure_Stripping_Font2008_Get=virialRadius
     end if
     return
