e2cfbd0d99cec9424014fbb95be272d761eb8e49
Andrew Benson <abenson@obs.carnegiescience.edu>
2017-07-20 14:54:44 -0700

    * Add a galactic filter which passes only main branch nodes.

---
+++ source/galactic.filters.main_branch.F90
@@ -0,0 +1,59 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which implements a filter which passes only main branch halos.
+
+  !# <galacticFilter name="galacticFilterMainBranch">
+  !#  <description>A filter which passes only main branch halos.</description>
+  !# </galacticFilter>
+  type, extends(galacticFilterClass) :: galacticFilterMainBranch
+     !% A galactic filter class which passes only main branch halos.
+     private
+   contains
+     procedure :: passes => mainBranchPasses
+  end type galacticFilterMainBranch
+
+  interface galacticFilterMainBranch
+     !% Constructors for the ``mainBranch'' galactic filter class.
+     module procedure mainBranchConstructorParameters
+  end interface galacticFilterMainBranch
+
+contains
+
+  function mainBranchConstructorParameters(parameters) result(self)
+    !% Constructor for the ``mainBranch'' galactic filter class which takes a parameter set as input.
+    use Input_Parameters2
+    implicit none
+    type(galacticFilterMainBranch)                :: self
+    type(inputParameters         ), intent(inout) :: parameters
+    !GCC$ attributes unused :: parameters
+
+    self=galacticFilterMainBranch()
+    return
+  end function mainBranchConstructorParameters
+
+  logical function mainBranchPasses(self,node)
+    !% Implement a galactic filter which passes only main branch halos.
+    implicit none
+    class(galacticFilterMainBranch), intent(inout) :: self
+    type (treeNode                ), intent(inout) :: node
+    !GCC$ attributes unused :: self
+
+    mainBranchPasses=node%isOnMainBranch()
+    return
+  end function mainBranchPasses
