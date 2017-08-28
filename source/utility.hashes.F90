ccea86abd9e9d1641875a63f3df038b55067414a
Andrew Benson <abenson@obs.carnegiescience.edu>
2017-08-28 11:37:30 -0700

    * Add support for double precision value hashes (using generics).

--- source/utility.hashes.F90
+++ source/utility.hashes.F90
@@ -22,18 +22,24 @@ module Hashes
   !% Implements ``hashes'' (i.e. associative arrays).
   use ISO_Varying_String
   implicit none
-  private
-  public :: integerScalarHash
 
-  type integerScalarHash
-     !% Derived type for integer hashes.
+  !# <generic identifier="Type">
+  !#  <instance label="integer" intrinsic="integer"          />
+  !#  <instance label="double"  intrinsic="double precision" />
+  !# </generic>
+
+  private
+  public :: {Type¦label}ScalarHash
+
+  type :: {Type¦label}ScalarHash
+     !% Derived type for {Type¦label} hashes.
      private
-     integer                              :: allocatedSize   , elementCount
-     integer                , allocatable :: hashValues   (:)
-     type   (varying_string), allocatable :: hashKeys     (:)
+     integer                                       :: allocatedSize   , elementCount
+     {Type¦intrinsic}                , allocatable :: hashValues   (:)
+     type            (varying_string), allocatable :: hashKeys     (:)
    contains
      !@ <objectMethods>
-     !@   <object>integerScalarHash</object>
+     !@   <object>{Type¦label}ScalarHash</object>
      !@   <objectMethod>
      !@     <method>initialize</method>
      !@     <description>Initialize the hash.</description>
@@ -74,7 +80,7 @@ module Hashes
      !@     <method>values</method>
      !@     <description>Return an array of all values in the hash.</description>
      !@     <type>\void</type>
-     !@     <arguments>\textcolor{red}{\textless integer[:]\textgreater} values\arginout</arguments>
+     !@     <arguments>\textcolor{red}{\textless {Type¦intrinsic}[:]\textgreater} values\arginout</arguments>
      !@   </objectMethod>
      !@   <objectMethod>
      !@     <method>exists</method>
@@ -95,214 +101,231 @@ module Hashes
      !@     <arguments></arguments>
      !@   </objectMethod>
      !@ </objectMethods>
-     procedure :: initialize           =>Initialize_Integer_Scalar
-     procedure :: Set_Integer_Scalar_VS
-     procedure :: Set_Integer_Scalar_CH
-     generic   :: set        => Set_Integer_Scalar_VS,Set_Integer_Scalar_CH
-     procedure :: Delete_Integer_Scalar_VS
-     procedure :: Delete_Integer_Scalar_CH
-     generic   :: delete     => Delete_Integer_Scalar_VS,Delete_Integer_Scalar_CH
-     procedure :: Value_Integer_Scalar_VS
-     procedure :: Value_Integer_Scalar_CH
-     procedure :: Value_Integer_Scalar_I
-     generic   :: value      => Value_Integer_Scalar_VS,Value_Integer_Scalar_CH,Value_Integer_Scalar_I
-     procedure :: key                     =>Key_Integer_Scalar_I
-     procedure :: Exists_Integer_Scalar_VS
-     procedure :: Exists_Integer_Scalar_CH
-     procedure :: keys                    =>Keys_Integer_Scalar
-     procedure :: values                  =>Values_Integer_Scalar
-     generic   :: exists     => Exists_Integer_Scalar_VS,Exists_Integer_Scalar_CH
-     procedure :: size   =>Size_Integer_Scalar
-     procedure :: destroy=>Destroy_Integer_Scalar
-  end type integerScalarHash
+     final     ::                        {Type¦label}ScalarDestructor
+     procedure :: initialize           =>Initialize_{Type¦label}_Scalar
+     procedure :: Set_{Type¦label}_Scalar_VS
+     procedure :: Set_{Type¦label}_Scalar_CH
+     generic   :: set        => Set_{Type¦label}_Scalar_VS,Set_{Type¦label}_Scalar_CH
+     procedure :: Delete_{Type¦label}_Scalar_VS
+     procedure :: Delete_{Type¦label}_Scalar_CH
+     generic   :: delete     => Delete_{Type¦label}_Scalar_VS,Delete_{Type¦label}_Scalar_CH
+     procedure :: Value_{Type¦label}_Scalar_VS
+     procedure :: Value_{Type¦label}_Scalar_CH
+     procedure :: Value_{Type¦label}_Scalar_I
+     generic   :: value      => Value_{Type¦label}_Scalar_VS,Value_{Type¦label}_Scalar_CH,Value_{Type¦label}_Scalar_I
+     procedure :: key                     =>Key_{Type¦label}_Scalar_I
+     procedure :: Exists_{Type¦label}_Scalar_VS
+     procedure :: Exists_{Type¦label}_Scalar_CH
+     procedure :: keys                    =>Keys_{Type¦label}_Scalar
+     procedure :: values                  =>Values_{Type¦label}_Scalar
+     generic   :: exists     => Exists_{Type¦label}_Scalar_VS,Exists_{Type¦label}_Scalar_CH
+     procedure :: size   =>Size_{Type¦label}_Scalar
+     procedure :: destroy=>Destroy_{Type¦label}_Scalar
+  end type {Type¦label}ScalarHash
 
+  interface {Type¦label}ScalarHash
+     module procedure {Type¦label}ScalarHashConstructor
+  end interface {Type¦label}ScalarHash
+  
   ! The number of new elements by which to extend hashes that need to grow.
   integer, parameter :: hashSizeIncrement=128
 
 contains
 
-  subroutine Initialize_Integer_Scalar(thisHash)
-    !% Routine to initialize (or re-initialize) an integer hash.
+  function {Type¦label}ScalarHashConstructor() result(self)
+     !% Constructor for scalar hashes.
+     implicit none
+     type({Type¦label}ScalarHash) :: self
+
+     call Initialize_{Type¦label}_Scalar(self)
+     return
+   end function {Type¦label}ScalarHashConstructor
+  
+  subroutine Initialize_{Type¦label}_Scalar(thisHash)
+    !% Routine to initialize (or re-initialize) a hash.
     implicit none
-    class(integerScalarHash), intent(  out) :: thisHash
+    class({Type¦label}ScalarHash), intent(  out) :: thisHash
 
     thisHash%elementCount =0
     thisHash%allocatedSize=0
     if (allocated(thisHash%hashValues)) deallocate(thisHash%hashValues)
     if (allocated(thisHash%hashKeys  )) deallocate(thisHash%hashKeys  )
     return
-  end subroutine Initialize_Integer_Scalar
+  end subroutine Initialize_{Type¦label}_Scalar
 
-  integer function Size_Integer_Scalar(thisHash)
+  integer function Size_{Type¦label}_Scalar(thisHash)
     !% Returns the number of elements in the specified {\normalfont \ttfamily Hash}.
     implicit none
-    class(integerScalarHash), intent(in   ) :: thisHash
+    class({Type¦label}ScalarHash), intent(in   ) :: thisHash
 
-    Size_Integer_Scalar=thisHash%elementCount
+    Size_{Type¦label}_Scalar=thisHash%elementCount
     return
-  end function Size_Integer_Scalar
+  end function Size_{Type¦label}_Scalar
 
-  logical function Exists_Integer_Scalar_CH(thisHash,keyCH)
+  logical function Exists_{Type¦label}_Scalar_CH(thisHash,keyCH)
     !% Returns true if the specified {\normalfont \ttfamily key} exists in the specified {\normalfont \ttfamily thisHash}, false otherwise.
     implicit none
-    class    (integerScalarHash), intent(in   ) :: thisHash
+    class    ({Type¦label}ScalarHash), intent(in   ) :: thisHash
     character(len=*            ), intent(in   ) :: keyCH
     type     (varying_string   ), save          :: key
     !$omp threadprivate(key)
     key=trim(keyCH)
-    Exists_Integer_Scalar_CH=Exists_Integer_Scalar_VS(thisHash,key)
+    Exists_{Type¦label}_Scalar_CH=Exists_{Type¦label}_Scalar_VS(thisHash,key)
     return
-  end function Exists_Integer_Scalar_CH
+  end function Exists_{Type¦label}_Scalar_CH
 
-  logical function Exists_Integer_Scalar_VS(thisHash,key)
+  logical function Exists_{Type¦label}_Scalar_VS(thisHash,key)
     !% Returns true if the specified {\normalfont \ttfamily key} exists in the specified {\normalfont \ttfamily thisHash}, false otherwise.
     implicit none
-    class(integerScalarHash), intent(in   ) :: thisHash
+    class({Type¦label}ScalarHash), intent(in   ) :: thisHash
     type (varying_string   ), intent(in   ) :: key
 
     if (thisHash%elementCount > 0) then
-       Exists_Integer_Scalar_VS=any(thisHash%hashKeys(1:thisHash%elementCount) == key)
+       Exists_{Type¦label}_Scalar_VS=any(thisHash%hashKeys(1:thisHash%elementCount) == key)
     else
-       Exists_Integer_Scalar_VS=.false.
+       Exists_{Type¦label}_Scalar_VS=.false.
     end if
     return
-  end function Exists_Integer_Scalar_VS
+  end function Exists_{Type¦label}_Scalar_VS
 
-  subroutine Delete_Integer_Scalar_CH(thisHash,keyCH)
+  subroutine Delete_{Type¦label}_Scalar_CH(thisHash,keyCH)
     !% Deletes entry {\normalfont \ttfamily key} from {\normalfont \ttfamily thisHash}.
     implicit none
     character(len=*            ), intent(in   ) :: keyCH
-    class    (integerScalarHash), intent(inout) :: thisHash
+    class    ({Type¦label}ScalarHash), intent(inout) :: thisHash
     type     (varying_string   ), save          :: key
     !$omp threadprivate(key)
     key=trim(keyCH)
-    call Delete_Integer_Scalar_VS(thisHash,key)
+    call Delete_{Type¦label}_Scalar_VS(thisHash,key)
     return
-  end subroutine Delete_Integer_Scalar_CH
+  end subroutine Delete_{Type¦label}_Scalar_CH
 
-  subroutine Delete_Integer_Scalar_VS(thisHash,key)
+  subroutine Delete_{Type¦label}_Scalar_VS(thisHash,key)
     !% Deletes entry {\normalfont \ttfamily key} from {\normalfont \ttfamily Hash}.
     use Arrays_Search
     use Galacticus_Error
     use, intrinsic :: ISO_C_Binding
     implicit none
     type   (varying_string   ), intent(in   ) :: key
-    class  (integerScalarHash), intent(inout) :: thisHash
+    class  ({Type¦label}ScalarHash), intent(inout) :: thisHash
     integer(c_size_t         )   , save          :: iKey
 
-    if (Exists_Integer_Scalar_VS(thisHash,key)) then
+    if (Exists_{Type¦label}_Scalar_VS(thisHash,key)) then
        iKey=Search_Array(thisHash%hashKeys(1:thisHash%elementCount),key)
        thisHash%hashKeys  (ikey:thisHash%elementCount-1)=thisHash%hashKeys  (ikey+1:thisHash%elementCount)
        thisHash%hashValues(ikey:thisHash%elementCount-1)=thisHash%hashValues(ikey+1:thisHash%elementCount)
        thisHash%elementCount                        =thisHash%elementCount-1
     else
-       call Galacticus_Error_Report('Delete_Integer_Scalar_VS','key '''//char(key)//''' does not exist in hash')
+       call Galacticus_Error_Report('Delete_{Type¦label}_Scalar_VS','key '''//char(key)//''' does not exist in hash')
     end if
     return
-  end subroutine Delete_Integer_Scalar_VS
+  end subroutine Delete_{Type¦label}_Scalar_VS
 
-  function Key_Integer_Scalar_I(thisHash,indexValue) result (key)
+  function Key_{Type¦label}_Scalar_I(thisHash,indexValue) result (key)
     !% Returns the key of entry number {\normalfont \ttfamily index} in {\normalfont \ttfamily thisHash}.
     implicit none
     type   (varying_string   )                :: key
     integer                   , intent(in   ) :: indexValue
-    class  (integerScalarHash), intent(in   ) :: thisHash
+    class  ({Type¦label}ScalarHash), intent(in   ) :: thisHash
 
     key=thisHash%hashKeys(indexValue)
     return
-  end function Key_Integer_Scalar_I
+  end function Key_{Type¦label}_Scalar_I
 
-  subroutine Keys_Integer_Scalar(thisHash,keys)
+  subroutine Keys_{Type¦label}_Scalar(thisHash,keys)
     !% Returns an array of all keys in {\normalfont \ttfamily thisHash}.
     implicit none
     type (varying_string   ), allocatable, dimension(:), intent(inout) :: keys
-    class(integerScalarHash)                           , intent(in   ) :: thisHash
+    class({Type¦label}ScalarHash)                           , intent(in   ) :: thisHash
 
     if (allocated(keys)) deallocate(keys)
     allocate(keys(thisHash%elementCount))
     keys=thisHash%hashKeys(1:thisHash%elementCount)
     return
-  end subroutine Keys_Integer_Scalar
+  end subroutine Keys_{Type¦label}_Scalar
 
-  subroutine Values_Integer_Scalar(thisHash,values)
+  subroutine Values_{Type¦label}_Scalar(thisHash,values)
     !% Returns an array of all values in {\normalfont \ttfamily thisHash}.
     implicit none
-    integer                   , allocatable, dimension(:), intent(inout) :: values
-    class  (integerScalarHash)                           , intent(in   ) :: thisHash
+    {Type¦intrinsic}                   , allocatable, dimension(:), intent(inout) :: values
+    class  ({Type¦label}ScalarHash)                           , intent(in   ) :: thisHash
 
     if (allocated(values)) deallocate(values)
     allocate(values(thisHash%elementCount))
     values=thisHash%hashValues(1:thisHash%elementCount)
     return
-  end subroutine Values_Integer_Scalar
+  end subroutine Values_{Type¦label}_Scalar
 
-  integer function Value_Integer_Scalar_I(thisHash,indexValue)
+   function Value_{Type¦label}_Scalar_I(thisHash,indexValue)
     !% Returns the value of entry number {\normalfont \ttfamily index} in {\normalfont \ttfamily Hash}.
     implicit none
-    class  (integerScalarHash), intent(in   ) :: thisHash
-    integer                   , intent(in   ) :: indexValue
+    {Type¦intrinsic}                                        :: Value_{Type¦label}_Scalar_I
+    class           ({Type¦label}ScalarHash), intent(in   ) :: thisHash
+    integer                                 , intent(in   ) :: indexValue
 
-    Value_Integer_Scalar_I=thisHash%hashValues(indexValue)
+    Value_{Type¦label}_Scalar_I=thisHash%hashValues(indexValue)
     return
-  end function Value_Integer_Scalar_I
+  end function Value_{Type¦label}_Scalar_I
 
-  integer function Value_Integer_Scalar_CH(thisHash,keyCH)
+  function Value_{Type¦label}_Scalar_CH(thisHash,keyCH)
     !% Returns the value of {\normalfont \ttfamily Key} in {\normalfont \ttfamily Hash}.
     implicit none
-    character(len=*            ), intent(in   ) :: keyCH
-    class    (integerScalarHash), intent(in   ) :: thisHash
-    type     (varying_string   ), save          :: key
+    {Type¦intrinsic}                                        :: Value_{Type¦label}_Scalar_CH
+    character       (len=*                 ), intent(in   ) :: keyCH
+    class           ({Type¦label}ScalarHash), intent(in   ) :: thisHash
+    type            (varying_string        ), save          :: key
     !$omp threadprivate(key)
     key=trim(keyCH)
-    Value_Integer_Scalar_CH=Value_Integer_Scalar_VS(thisHash,key)
+    Value_{Type¦label}_Scalar_CH=Value_{Type¦label}_Scalar_VS(thisHash,key)
     return
-  end function Value_Integer_Scalar_CH
+  end function Value_{Type¦label}_Scalar_CH
 
-  integer function Value_Integer_Scalar_VS(thisHash,key)
+  function Value_{Type¦label}_Scalar_VS(thisHash,key)
     !% Returns the value of {\normalfont \ttfamily key} in {\normalfont \ttfamily thisHash}.
     use Arrays_Search
     use Galacticus_Error
     use, intrinsic :: ISO_C_Binding
     implicit none
-    class  (integerScalarHash), intent(in   ) :: thisHash
-    type   (varying_string   ), intent(in   ) :: key
-    integer(c_size_t         )                :: iKey
+    {Type¦intrinsic}                                        :: Value_{Type¦label}_Scalar_VS
+    class           ({Type¦label}ScalarHash), intent(in   ) :: thisHash
+    type            (varying_string        ), intent(in   ) :: key
+    integer         (c_size_t              )                :: iKey
 
-    if (Exists_Integer_Scalar_VS(thisHash,key)) then
+    if (Exists_{Type¦label}_Scalar_VS(thisHash,key)) then
        iKey=Search_Array(thisHash%hashKeys(1:thisHash%elementCount),key)
-       Value_Integer_Scalar_VS=thisHash%hashValues(iKey)
+       Value_{Type¦label}_Scalar_VS=thisHash%hashValues(iKey)
     else
-       Value_Integer_Scalar_VS=0
-       call Galacticus_Error_Report('Value_Integer_Scalar','key '''//char(key)//''' does not exist in hash')
+       Value_{Type¦label}_Scalar_VS=0
+       call Galacticus_Error_Report('Value_{Type¦label}_Scalar','key '''//char(key)//''' does not exist in hash')
     end if
     return
-  end function Value_Integer_Scalar_VS
+  end function Value_{Type¦label}_Scalar_VS
 
-  subroutine Set_Integer_Scalar_CH(thisHash,keyCH,value)
+  subroutine Set_{Type¦label}_Scalar_CH(thisHash,keyCH,value)
     !% Sets the value of {\normalfont \ttfamily key} in {\normalfont \ttfamily thisHash} to {\normalfont \ttfamily value}.
     implicit none
-    integer                     , intent(in   ) :: value
-    character(len=*            ), intent(in   ) :: keyCH
-    class    (integerScalarHash), intent(inout) :: thisHash
-    type     (varying_string   ), save          :: key
+    {Type¦intrinsic}                        , intent(in   ) :: value
+    character       (len=*                 ), intent(in   ) :: keyCH
+    class           ({Type¦label}ScalarHash), intent(inout) :: thisHash
+    type            (varying_string        ), save          :: key
 
     key=trim(keyCH)
-    call Set_Integer_Scalar_VS(thisHash,key,value)
+    call Set_{Type¦label}_Scalar_VS(thisHash,key,value)
     return
-  end subroutine Set_Integer_Scalar_CH
+  end subroutine Set_{Type¦label}_Scalar_CH
 
-  subroutine Set_Integer_Scalar_VS(thisHash,key,value)
+  subroutine Set_{Type¦label}_Scalar_VS(thisHash,key,value)
     !% Sets the value of {\normalfont \ttfamily key} in {\normalfont \ttfamily thisHash} to {\normalfont \ttfamily value}.
     use Arrays_Search
     use, intrinsic :: ISO_C_Binding
     implicit none
-    integer                                              , intent(in   ) :: Value
-    type   (varying_string   )                           , intent(in   ) :: Key
-    class  (integerScalarHash)                           , intent(inout) :: thisHash
-    integer(c_size_t         )                                           :: iKey
-    logical                                                              :: keyExists
-    integer                   , allocatable, dimension(:)                :: valuesTemporary
-    type   (varying_string   ), allocatable, dimension(:)                :: keysTemporary
+    {Type¦intrinsic}                        , intent(in   )               :: Value
+    type            (varying_string        ), intent(in   )               :: Key
+    class           ({Type¦label}ScalarHash), intent(inout)               :: thisHash
+    integer         (c_size_t              )                              :: iKey
+    logical                                                               :: keyExists
+    {Type¦intrinsic}                        , allocatable  , dimension(:) :: valuesTemporary
+    type            (varying_string        ), allocatable  , dimension(:) :: keysTemporary
 
     ! Check if key already exists.
     if (thisHash%elementCount > 0) then
@@ -356,13 +379,13 @@ contains
        end if
     end if
     return
-  end subroutine Set_Integer_Scalar_VS
+  end subroutine Set_{Type¦label}_Scalar_VS
 
-  subroutine Destroy_Integer_Scalar(thisHash)
+  subroutine Destroy_{Type¦label}_Scalar(thisHash)
     !% Destroys {\normalfont \ttfamily thisHash}.
     implicit none
-    class  (integerScalarHash), intent(inout) :: thisHash
-    integer                                   :: i
+    class  ({Type¦label}ScalarHash), intent(inout) :: thisHash
+    integer                                        :: i
 
     if (allocated(thisHash%hashValues)) deallocate(thisHash%hashValues)
     if (allocated(thisHash%hashKeys  )) then
@@ -372,6 +395,15 @@ contains
        deallocate(thisHash%hashKeys)
     end if
     return
-  end subroutine Destroy_Integer_Scalar
+  end subroutine Destroy_{Type¦label}_Scalar
+
+  subroutine {Type¦label}ScalarDestructor(self)
+    !% Destroys {\normalfont \ttfamily thisHash}.
+    implicit none
+    type({Type¦label}ScalarHash), intent(inout) :: self
+
+    call Destroy_{Type¦label}_Scalar(self)
+    return
+  end subroutine {Type¦label}ScalarDestructor
 
 end module Hashes
