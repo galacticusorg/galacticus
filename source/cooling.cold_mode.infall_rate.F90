12939f42b7dbd4becc2df7cdda70df97fc837251
Andrew Benson <abenson@obs.carnegiescience.edu>
2017-09-08 16:07:52 -0700

    * Refactor cold mode infall rate code into a functionClass.

--- source/cooling.cold_mode.infall_rate.F90
+++ source/cooling.cold_mode.infall_rate.F90
@@ -20,189 +20,21 @@
 
 module Cooling_Cold_Mode_Infall_Rates
   !% Implements calculations of the infall rate from the cold mode.
-  use ISO_Varying_String
   use Galacticus_Nodes
   implicit none
   private
-  public :: Cooling_Cold_Mode_Infall_Rate, Cooling_Cold_Mode_Infall_Output,&
-       & Cooling_Cold_Mode_Infall_Output_Count, Cooling_Cold_Mode_Infall_Output_Names
 
-  ! Flag to indicate if this module has been initialized.
-  logical                                           :: coldModeInfallRateInitialized      =.false.
-  ! Name of cooling rate available method used.
-  type     (varying_string               )          :: coldModeInfallRateMethod
-  ! Pointer to the function that actually does the calculation.
-  procedure(Cooling_Cold_Mode_Infall_Rate), pointer :: Cooling_Cold_Mode_Infall_Rate_Get  => null()
-
-  ! Option controlling whether infall rates are output.
-  logical :: outputColdModeInfallRate, coldModeInfallRateOutputInitialized=.false.
-
-contains
-
-  subroutine Cooling_Cold_Mode_Infall_Rate_Initialize()
-    !% Initialize the cooling rate module.
-    use Galacticus_Error
-    use Input_Parameters
-    !# <include directive="coldModeInfallRateMethod" type="moduleUse">
-    include 'cooling.cold_mode.infall_rate.modules.inc'
-    !# </include>
-    implicit none
-
-    ! Initialize if necessary.
-    if (.not.coldModeInfallRateInitialized) then
-       !$omp critical(Cooling_Cold_Mode_Infall_Rate_Initialization)
-       if (.not.coldModeInfallRateInitialized) then
-          ! Get the cooling rate method parameter.
-          !@ <inputParameter>
-          !@   <name>coldModeInfallRateMethod</name>
-          !@   <defaultValue>dynamicalTime</defaultValue>
-          !@   <attachedTo>module</attachedTo>
-          !@   <description>
-          !@     The name of the method to be used when computing the infall rate from the cold mode.
-          !@   </description>
-          !@   <type>string</type>
-          !@   <cardinality>1</cardinality>
-          !@ </inputParameter>
-          call Get_Input_Parameter('coldModeInfallRateMethod',coldModeInfallRateMethod,defaultValue='dynamicalTime')
-          ! Include file that makes calls to all available method initialization routines.
-          !# <include directive="coldModeInfallRateMethod" type="functionCall" functionType="void">
-          !#  <functionArgs>coldModeInfallRateMethod,Cooling_Cold_Mode_Infall_Rate_Get</functionArgs>
-          include 'cooling.cold_mode.infall_rate.inc'
-          !# </include>
-          if (.not.associated(Cooling_Cold_Mode_Infall_Rate_Get)) call Galacticus_Error_Report('Cooling_Cold_Mode_Infall_Rate','method ' &
-               &//char(coldModeInfallRateMethod)//' is unrecognized')
-          coldModeInfallRateInitialized=.true.
-       end if
-       !$omp end critical(Cooling_Cold_Mode_Infall_Rate_Initialization)
-    end if
-    return
-  end subroutine Cooling_Cold_Mode_Infall_Rate_Initialize
-
-  double precision function Cooling_Cold_Mode_Infall_Rate(thisNode)
-    !% Return the cold mode infall rate for {\normalfont \ttfamily thisNode} (in units of $M_\odot$ Gyr$^{-1}$).
-    implicit none
-    type(treeNode), intent(inout) :: thisNode
-
-    ! Initialize the module.
-    call Cooling_Cold_Mode_Infall_Rate_Initialize()
-    ! Get the cooling rate using the selected method.
-    Cooling_Cold_Mode_Infall_Rate=Cooling_Cold_Mode_Infall_Rate_Get(thisNode)
-    return
-  end function Cooling_Cold_Mode_Infall_Rate
-
-  subroutine Cooling_Cold_Mode_Infall_Output_Initialize()
-    !% Initialize output in the cold mode infall rate module.
-    use Input_Parameters
-    implicit none
-
-    ! Initialize if necessary.
-    if (.not.coldModeInfallRateOutputInitialized) then
-       !$omp critical(Cooling_Cold_Mode_Infall_Output_Initialization)
-       if (.not.coldModeInfallRateOutputInitialized) then
-          ! Get options controlling output.
-          !@ <inputParameter>
-          !@   <name>outputColdModeInfallRate</name>
-          !@   <defaultValue>false</defaultValue>
-          !@   <attachedTo>module</attachedTo>
-          !@   <description>
-          !@    Determines whether or not cold mode infall rates are output.
-          !@   </description>
-          !@   <type>boolean</type>
-          !@   <cardinality>1</cardinality>
-          !@ </inputParameter>
-          call Get_Input_Parameter('outputColdModeInfallRate',outputColdModeInfallRate,defaultValue=.false.)
-          coldModeInfallRateOutputInitialized=.true.
-       end if
-       !$omp end critical(Cooling_Cold_Mode_Infall_Output_Initialization)
-    end if
-    return
-  end subroutine Cooling_Cold_Mode_Infall_Output_Initialize
-
-  !# <mergerTreeOutputNames>
-  !#  <unitName>Cooling_Cold_Mode_Infall_Output_Names</unitName>
-  !#  <sortName>Cooling_Cold_Mode_Infall_Output</sortName>
-  !# </mergerTreeOutputNames>
-  subroutine Cooling_Cold_Mode_Infall_Output_Names(thisNode,integerProperty,integerPropertyNames,integerPropertyComments&
-       &,integerPropertyUnitsSI,doubleProperty,doublePropertyNames,doublePropertyComments,doublePropertyUnitsSI,time)
-    !% Set names of cold mode infall rate properties to be written to the \glc\ output file.
-    use Numerical_Constants_Astronomical
-    implicit none
-    type            (treeNode            )              , intent(inout) :: thisNode
-    double precision                                    , intent(in   ) :: time
-    integer                                             , intent(inout) :: doubleProperty         , integerProperty
-    character       (len=*               ), dimension(:), intent(inout) :: doublePropertyComments , doublePropertyNames   , &
-         &                                                                 integerPropertyComments, integerPropertyNames
-    double precision                      , dimension(:), intent(inout) :: doublePropertyUnitsSI  , integerPropertyUnitsSI
-    !GCC$ attributes unused :: thisNode, integerProperty, integerPropertyNames, integerPropertyComments, integerPropertyUnitsSI, time
-    
-    ! Initialize the module.
-    call Cooling_Cold_Mode_Infall_Output_Initialize()
-
-    if (outputColdModeInfallRate) then
-       doubleProperty=doubleProperty+1
-       !@ <outputProperty>
-       !@   <name>coldModeInfallRate</name>
-       !@   <datatype>real</datatype>
-       !@   <cardinality>0..1</cardinality>
-       !@   <description>Rate of infall of cold mode material onto the galaxy.</description>
-       !@   <label>???</label>
-       !@   <outputType>nodeData</outputType>
-       !@   <group>hotHalo</group>
-       !@ </outputProperty>
-       doublePropertyNames   (doubleProperty)='coldModeInfallRate'
-       doublePropertyComments(doubleProperty)='Rate of infall of cold mode material onto the galaxy.'
-       doublePropertyUnitsSI (doubleProperty)=massSolar/gigaYear
-    end if
-    return
-  end subroutine Cooling_Cold_Mode_Infall_Output_Names
-
-  !# <mergerTreeOutputPropertyCount>
-  !#  <unitName>Cooling_Cold_Mode_Infall_Output_Count</unitName>
-  !#  <sortName>Cooling_Cold_Mode_Infall_Output</sortName>
-  !# </mergerTreeOutputPropertyCount>
-  subroutine Cooling_Cold_Mode_Infall_Output_Count(thisNode,integerPropertyCount,doublePropertyCount,time)
-    !% Account for the number of cold mode infall rate properties to be written to the the \glc\ output file.
-    implicit none
-    type            (treeNode            ), intent(inout) :: thisNode
-    double precision                      , intent(in   ) :: time
-    integer                               , intent(inout) :: doublePropertyCount  , integerPropertyCount
-    integer                               , parameter     :: propertyCount      =1
-    !GCC$ attributes unused :: thisNode, integerPropertyCount, time
-    
-    ! Initialize the module.
-    call Cooling_Cold_Mode_Infall_Output_Initialize()
-
-    if (outputColdModeInfallRate) doublePropertyCount=doublePropertyCount+propertyCount
-    return
-  end subroutine Cooling_Cold_Mode_Infall_Output_Count
-
-  !# <mergerTreeOutputTask>
-  !#  <unitName>Cooling_Cold_Mode_Infall_Output</unitName>
-  !#  <sortName>Cooling_Cold_Mode_Infall_Output</sortName>
-  !# </mergerTreeOutputTask>
-  subroutine Cooling_Cold_Mode_Infall_Output(thisNode,integerProperty,integerBufferCount,integerBuffer,doubleProperty&
-       &,doubleBufferCount,doubleBuffer,time,instance)
-    !% Store cold mode infall rate properties in the \glc\ output file buffers.
-    use Kind_Numbers
-    use Multi_Counters
-    implicit none
-    double precision                , intent(in   ) :: time
-    type            (treeNode      ), intent(inout) :: thisNode
-    integer                         , intent(inout) :: doubleBufferCount     , doubleProperty, integerBufferCount, &
-         &                                             integerProperty
-    integer         (kind=kind_int8), intent(inout) :: integerBuffer    (:,:)
-    double precision                , intent(inout) :: doubleBuffer     (:,:)
-    type            (multiCounter  ), intent(inout) :: instance
-    !GCC$ attributes unused :: time, integerProperty, integerBufferCount, integerBuffer, instance
-    
-    ! Initialize the module.
-    call Cooling_Cold_Mode_Infall_Output_Initialize()
-
-    if (outputColdModeInfallRate) then
-       doubleProperty=doubleProperty+1
-       doubleBuffer(doubleBufferCount,doubleProperty)=Cooling_Cold_Mode_Infall_Rate(thisNode)
-    end if
-    return
-  end subroutine Cooling_Cold_Mode_Infall_Output
+  !# <functionClass>
+  !#  <name>coldModeInfallRate</name>
+  !#  <descriptiveName>Infall rates in cold mode accretion.</descriptiveName>
+  !#  <description>Class providing models of the infall rate of gas in the cold mode.</description>
+  !#  <default>dynamicalTime</default>
+  !#  <method name="infallRate" >
+  !#   <description>Returns the cold mode infall rate for {\normalfont \ttfamily thisNode} (in units of $M_\odot$ Gyr$^{-1}$).</description>
+  !#   <type>double precision</type>
+  !#   <pass>yes</pass>
+  !#   <argument>type(treeNode), intent(inout) :: node</argument>
+  !#  </method>
+  !# </functionClass>
 
 end module Cooling_Cold_Mode_Infall_Rates
