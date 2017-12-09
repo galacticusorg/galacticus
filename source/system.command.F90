8f79889091e70ffe246fa9969367cb2fbbbef21b
Andrew Benson <abenson@obs.carnegiescience.edu>
2017-12-08 17:03:20 -0800

    * Overload the system command function so it can accept arguments as   either characters or varying_strings.

--- source/system.command.F90
+++ source/system.command.F90
@@ -24,24 +24,39 @@ module System_Command
   private
   public :: System_Command_Do
 
+  interface System_Command_Do
+     module procedure System_Command_Char
+     module procedure System_Command_VarStr
+  end interface System_Command_Do
+  
 contains
 
-  subroutine System_Command_Do(command,iStatus)
+  subroutine System_Command_VarStr(command,iStatus)
     !% Executes the system command {\normalfont \ttfamily command}, optionally returning the resulting status in {\normalfont \ttfamily iStatus}.
     use ISO_Varying_String
-    use Galacticus_Error
     implicit none
     type   (varying_string), intent(in   )           :: command
     integer                , intent(  out), optional :: iStatus
-    integer                                          :: iStatusActual
 
-    call System(char(command),iStatusActual)
+    call System_Command_Char(char(command),iStatus)
+    return
+  end subroutine System_Command_VarStr
+
+  subroutine System_Command_Char(command,iStatus)
+    !% Executes the system command {\normalfont \ttfamily command}, optionally returning the resulting status in {\normalfont \ttfamily iStatus}.
+    use Galacticus_Error
+    implicit none
+    character(len=*), intent(in   )           :: command
+    integer         , intent(  out), optional :: iStatus
+    integer                                   :: iStatusActual
+
+    call System(command,iStatusActual)
     if (present(iStatus)) then
        iStatus=iStatusActual
     else
-       if (iStatusActual /= 0) call Galacticus_Error_Report('failed to execute system command:'//char(10)//' --> '//char(command)//{introspection:location})
+       if (iStatusActual /= 0) call Galacticus_Error_Report('failed to execute system command:'//char(10)//' --> '//command//{introspection:location})
     end if
     return
-  end subroutine System_Command_Do
+  end subroutine System_Command_Char
 
 end module System_Command
