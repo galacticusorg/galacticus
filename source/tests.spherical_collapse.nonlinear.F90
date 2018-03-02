29a433035931f37aac046204b8f64176d9fc205f
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-03-02 14:05:14 -0800

    * Add a test of nonlinear spherical collapse model.

---
+++ source/tests.spherical_collapse.nonlinear.F90
@@ -0,0 +1,71 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a program which tests nonlinear collapse solution in an Einstein-de Sitter cosmology.
+
+program Tests_Spherical_Collapse_NonLinear
+  !% Tests nonlinear collapse solution in an Einstein-de Sitter cosmology.
+  use Memory_Management
+  use Tables
+  use Unit_Tests
+  use Input_Parameters
+  use Cosmology_Functions
+  use Cosmology_Parameters
+  use Linear_Growth
+  use Spherical_Collapse_Matter_Lambda
+  implicit none
+  ! Target nonlinear overdensities are computed using the analytic solutions for expanding/collapsing perturbations in an
+  ! Einstein-de Sitter universe.
+  double precision                                , dimension(5), parameter :: overdensityLinear         =[-1.500000d0,-1.000000d0,+0.000000d0,+1.00000d0,+1.50000d0], &
+       &                                                                       overdensityNonLinearTarget=[-0.653016d0,-0.540387d0,+0.000000d0,+3.69477d0,+4.82042d1]
+  double precision                                , dimension(5)            :: overdensityNonLinear
+  type            (table2DLinLinLin              )                          :: linearToNonLinear
+  type            (cosmologyFunctionsMatterLambda)                          :: cosmologyFunctions_
+  type            (cosmologyParametersSimple     )                          :: cosmologyParameters_
+  type            (linearGrowthSimple            )                          :: linearGrowth_
+  integer                                                                   :: i
+
+  ! Read in basic code memory usage.
+  call Code_Memory_Usage('tests.spherical_collapse.nonlinear.size')
+  ! Initialize cosmology.
+  cosmologyParameters_=cosmologyParametersSimple     (                                      &
+       &                                              OmegaMatter    = 1.00d0             , &
+       &                                              OmegaBaryon    = 0.00d0             , &
+       &                                              OmegaDarkEnergy= 0.00d0             , &
+       &                                              temperatureCMB = 2.73d0             , &
+       &                                              HubbleConstant =70.00d0               &
+       &                                             )
+  cosmologyFunctions_ =cosmologyFunctionsMatterLambda(                                      &
+       &                                                              cosmologyParameters_  &
+       &                                             )
+  linearGrowth_       =linearGrowthSimple            (                                      &
+       &                                                              cosmologyParameters_, &
+       &                                                              cosmologyFunctions_   &
+       &                                             )
+  ! Get a table of linear to nonlinear overdensity mapping.
+  call Spherical_Collapse_Matter_Lambda_Nonlinear_Mapping(cosmologyFunctions_%cosmicTime(1.0d0),linearToNonLinear,linearGrowth_,cosmologyFunctions_)
+  ! Begin unit tests.
+  call Unit_Tests_Begin_Group("Spherical collapse: nonlinear solutions")
+  do i=1,size(overdensityLinear)
+     overdensityNonLinear(i)=linearToNonLinear%interpolate(overdensityLinear(i),cosmologyFunctions_%cosmicTime(1.0d0))
+  end do
+  call Assert('Linear → nonlinear spherical collapse mapping',overdensityNonLinear,overdensityNonLinearTarget,relTol=1.0d-2,absTol=1.0d-3)
+  ! End unit tests.
+  call Unit_Tests_End_Group()
+  call Unit_Tests_Finish   ()
+end program Tests_Spherical_Collapse_NonLinear
