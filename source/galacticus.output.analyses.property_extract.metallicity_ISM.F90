b55e18ef8d7d9ca8d423bdd9c88c84724ef026c1
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-03-15 14:52:09 -0700

    * Change the Blanc2017 mass-metallicity relation analyis class to   compute 12+log(O/H) directly from the oxygen abundance rather than   scaling from Solar abundance using the total metallicity.

--- source/galacticus.output.analyses.property_extract.metallicity_ISM.F90
+++ source/galacticus.output.analyses.property_extract.metallicity_ISM.F90
@@ -24,6 +24,7 @@
   type, extends(outputAnalysisPropertyExtractorClass) :: outputAnalysisPropertyExtractorMetallicityISM
      !% An ISM metallicity output analysis property extractor class.
      private
+     integer :: indexElement
    contains
      procedure :: extract => metallicityISMExtract
      procedure :: type    => metallicityISMType
@@ -32,6 +33,7 @@
   interface outputAnalysisPropertyExtractorMetallicityISM
      !% Constructors for the ``metallicityISM'' output analysis class.
      module procedure metallicityISMConstructorParameters
+     module procedure metallicityISMConstructorInternal
   end interface outputAnalysisPropertyExtractorMetallicityISM
 
 contains
@@ -39,36 +41,68 @@ contains
   function metallicityISMConstructorParameters(parameters) result(self)
     !% Constructor for the ``metallicityISM'' output analysis property extractor class which takes a parameter set as input.
     use Input_Parameters
+    use Abundances_Structure
     implicit none
-    type(outputAnalysisPropertyExtractorMetallicityISM)                :: self
-    type(inputParameters                              ), intent(inout) :: parameters
-    !GCC$ attributes unused :: parameters
-
-    self=outputAnalysisPropertyExtractorMetallicityISM()
+    type     (outputAnalysisPropertyExtractorMetallicityISM)                :: self
+    type     (inputParameters                              ), intent(inout) :: parameters
+    integer                                                                 :: indexElement
+    character(len=3                                        )                :: element
+    
+    !# <inputParameter>
+    !#   <name>element</name>
+    !#   <source>parameters</source>
+    !#   <description>The atomic symbol for the element to use to define metallicity.</description>
+    !#   <type>string</type>
+    !#   <cardinality>0..1</cardinality>
+    !# </inputParameter>
+    indexElement=Abundances_Index_From_Name(element)
+    self=outputAnalysisPropertyExtractorMetallicityISM(indexElement)
+    !# <inputParametersValidate source="parameters"/>
     return
   end function metallicityISMConstructorParameters
 
+  function metallicityISMConstructorInternal(indexElement) result(self)
+    !% Internal constructor for the ``metallicityISM'' output analysis property extractor class.
+    implicit none
+    type(outputAnalysisPropertyExtractorMetallicityISM)                :: self
+    integer                                            , intent(in   ) :: indexElement
+    !# <constructorAssign variables="indexElement"/>
+
+    return
+  end function metallicityISMConstructorInternal
+
   double precision function metallicityISMExtract(self,node)
-    !% Implement a metallicityISM output analysis.
+    !% Extracts the metallicity (defined as the mass ratio of a specified element to hydrogen) in the ISM.
     use Abundances_Structure
     implicit none
-    class           (outputAnalysisPropertyExtractorMetallicityISM), intent(inout) :: self
-    type            (treeNode                                     ), intent(inout) :: node
-    class           (nodeComponentDisk                            ), pointer       :: disk
-    class           (nodeComponentSpheroid                        ), pointer       :: spheroid
-    type            (abundances                                   )                :: abundancesDisk, abundancesSpheroid
-    double precision                                                               :: massGas       , massMetals
+    class           (outputAnalysisPropertyExtractorMetallicityISM), intent(inout)               :: self
+    type            (treeNode                                     ), intent(inout)               :: node
+    class           (nodeComponentDisk                            ), pointer                     :: disk
+    class           (nodeComponentSpheroid                        ), pointer                     :: spheroid
+    double precision                                               , allocatable  , dimension(:) :: massElementsDisk, massElementsSpheroid
+    type            (abundances                                   )                              :: abundancesDisk  , abundancesSpheroid
+    double precision                                                                             :: massGas         , massMetals
+    integer                                                                                      :: countElements
     !GCC$ attributes unused :: self
 
-    disk               =>  node          %disk         ()
-    spheroid           =>  node          %spheroid     ()
-    abundancesDisk     =   disk          %abundancesGas()
-    abundancesSpheroid =                                  spheroid          %abundancesGas()
-    massGas            =  +disk          %massGas      ()+spheroid          %massGas      ()
-    massMetals         =  +abundancesDisk%metallicity  ()+abundancesSpheroid%metallicity  ()
-    if (massGas > 0.0d0) then
-       metallicityISMExtract=+massMetals &
-            &                /massGas
+    disk               => node    %disk         ()
+    spheroid           => node    %spheroid     ()
+    abundancesDisk     =  disk    %abundancesGas()
+    abundancesSpheroid =  spheroid%abundancesGas()
+    if (disk%massGas() > 0.0d0 .or. spheroid%massGas() > 0.0d0) then
+       countElements=abundancesDisk%serializeCount()
+       allocate(massElementsDisk    (countElements))
+       allocate(massElementsSpheroid(countElements))
+       call abundancesDisk    %serialize(massElementsDisk    )
+       call abundancesSpheroid%serialize(massElementsSpheroid)
+       metallicityISMExtract=+(                                                              &
+            &                  +massElementsDisk    (self%indexElement)                      &
+            &                  +massElementsSpheroid(self%indexElement)                      &
+            &                 )                                                              &
+            &                /(                                                              &
+            &                  +disk    %massGas()*abundancesDisk    %hydrogenMassFraction() &
+            &                  +spheroid%massGas()*abundancesSpheroid%hydrogenMassFraction() &
+            &                 )
     else
        metallicityISMExtract=0.0d0
     end if
