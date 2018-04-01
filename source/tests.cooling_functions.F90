3972b124030b9d3ba70acad336a857318aa1fc61
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-04-01 22:48:15 +0000

    * Update results in cooling functions test suite entry to match latest   Cloudy version.

--- source/tests.cooling_functions.F90
+++ source/tests.cooling_functions.F90
@@ -88,10 +88,10 @@ program Test_Cooling_Functions
        &            /ergs                  &
        &            /coolantCMBCompton     &
        &            /gigaYear
-  call Assert('CMB Compton cooling timescale at z=0',timescaleCooling,976.74743888107764d0,relTol=1.0d-6)
+  call Assert('CMB Compton cooling timescale at z=0',timescaleCooling,976.65342064763729d0,relTol=1.0d-6)
   ! Compute Cloudy cooling time.
   coolantAtomicCIECloudy=+coolingFunctionAtomicCIECloudy_%coolingFunction(numberDensityHydrogen,temperature,gasAbundances,chemicalDensities,radiation)
-  call Assert('Cloudy CIE cooling function',coolantAtomicCIECloudy,1.4972d-30,relTol=1.0d-6)
+  call Assert('Cloudy CIE cooling function',coolantAtomicCIECloudy,1.4177100259738058d-30,relTol=1.0d-6)
   call Unit_Tests_End_Group       ()
   ! End unit tests.
   call Unit_Tests_End_Group       ()
