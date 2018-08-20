9d82537ddfca2a948e03747e56696ef470564a68
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-08-20 12:33:37 -0700

    * Refactor expulsive feedback modules into functionClasses.

--- source/star_formation.feedback_expulsion.spheroids.superwind.F90
+++ source/star_formation.feedback_expulsion.spheroids.superwind.F90
@@ -16,92 +16,102 @@
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
 
-!% Contains a module which implements a ``superwind'' expulsive outflow rate (as in \citep{baugh_can_2005}) due to star
-!% formation feedback in galactic spheroids.
+  !% Implementation of a ``superwind'' expulsive outflow rate due to star formation feedback in galactic spheroids.
+  
+  !# <starFormationExpulsiveFeedbackSpheroids name="starFormationExpulsiveFeedbackSpheroidsSuperWind">
+  !#  <description>A superwind expulsive outflow rate due to star formation feedback in galactic spheroids.</description>
+  !# </starFormationExpulsiveFeedbackSpheroids>
+  type, extends(starFormationExpulsiveFeedbackSpheroidsClass) :: starFormationExpulsiveFeedbackSpheroidsSuperWind
+     !% Implementation of a superwind expulsive outflow rate due to star formation feedback in galactic spheroids.
+     private
+     double precision :: velocityCharacteristic, massLoading
+   contains
+     procedure :: outflowRate => superWindOutflowRate
+  end type starFormationExpulsiveFeedbackSpheroidsSuperWind
 
-module Star_Formation_Expulsive_Feedback_Spheroids_Superwind
-  !% Implements a``superwind'' outflow rate (as in \citep{baugh_can_2005}) due to star formation feedback in
-  !% galactic spheroids.
-  implicit none
-  private
-  public :: Star_Formation_Expulsive_Feedback_Spheroids_SW_Initialize
-
-  ! Parameters of the feedback model.
-  double precision :: spheroidSuperwindMassLoading, spheroidSuperwindVelocity
+  interface starFormationExpulsiveFeedbackSpheroidsSuperWind
+     !% Constructors for the superwind expulsive star formation feedback in spheroids class.
+     module procedure superWindConstructorParameters
+     module procedure superWindConstructorInternal
+  end interface starFormationExpulsiveFeedbackSpheroidsSuperWind
 
 contains
 
-  !# <starFormationExpulsiveFeedbackSpheroidsMethod>
-  !#  <unitName>Star_Formation_Expulsive_Feedback_Spheroids_SW_Initialize</unitName>
-  !# </starFormationExpulsiveFeedbackSpheroidsMethod>
-  subroutine Star_Formation_Expulsive_Feedback_Spheroids_SW_Initialize(starFormationExpulsiveFeedbackSpheroidsMethod&
-       &,Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_Get)
-    !% Initializes the ``superwind'' spheroid star formation expulsive feedback module.
-    use ISO_Varying_String
+  function superWindConstructorParameters(parameters) result(self)
+    !% Constructor for the superwind expulsive star formation feedback in spheroids class which takes a parameter set as input.
     use Input_Parameters
     implicit none
-    type     (varying_string                                            ), intent(in   )          :: starFormationExpulsiveFeedbackSpheroidsMethod
-    procedure(Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_SW), intent(inout), pointer :: Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_Get
+    type            (starFormationExpulsiveFeedbackSpheroidsSuperWind)                :: self
+    type            (inputParameters                                 ), intent(inout) :: parameters
+    double precision                                                                  :: velocityCharacteristic, massLoading
 
-    if (starFormationExpulsiveFeedbackSpheroidsMethod == 'superwind') then
-       Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_Get => Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_SW
-       ! Get parameters of for the feedback calculation.
-       !# <inputParameter>
-       !#   <name>spheroidSuperwindMassLoading</name>
-       !#   <cardinality>1</cardinality>
-       !#   <defaultValue>2.0d0</defaultValue>
-       !#   <description>The mass loading of the spheroid superwind.</description>
-       !#   <group>starFormation</group>
-       !#   <source>globalParameters</source>
-       !#   <type>real</type>
-       !# </inputParameter>
-       !# <inputParameter>
-       !#   <name>spheroidSuperwindVelocity</name>
-       !#   <cardinality>1</cardinality>
-       !#   <defaultSource>km/s</defaultSource>
-       !#   <defaultValue>200.0d0</defaultValue>
-       !#   <description>The velocity scale of the spheroid superwind.</description>
-       !#   <group>starFormation</group>
-       !#   <source>globalParameters</source>
-       !#   <type>real</type>
-       !# </inputParameter>
-    end if
+    !# <inputParameter>
+    !#   <name>velocityCharacteristic</name>
+    !#   <source>parameters</source>
+    !#   <defaultValue>200.0d0</defaultValue>
+    !#   <description>The velocity scale at which the \gls{sne}-driven superwind outflow rate transitions to a constant in spheroids.</description>
+    !#   <type>real</type>
+    !#   <cardinality>0..1</cardinality>
+    !# </inputParameter>
+    !# <inputParameter>
+    !#   <name>massLoading</name>
+    !#   <source>parameters</source>
+    !#   <defaultValue>2.0d0</defaultValue>
+    !#   <description>The mass-loading of ``superwind'' expulsive outflows in spheroids..</description>
+    !#   <type>real</type>
+    !#   <cardinality>0..1</cardinality>
+    !# </inputParameter>
+    self=starFormationExpulsiveFeedbackSpheroidsSuperWind(velocityCharacteristic,massLoading)
+    !# <inputParametersValidate source="parameters"/>
     return
-  end subroutine Star_Formation_Expulsive_Feedback_Spheroids_SW_Initialize
+  end function superWindConstructorParameters
 
-  double precision function Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_SW(thisNode,starFormationRate,energyInputRate)
-    !% Returns the expulsive outflow rate (in $M_\odot$ Gyr$^{-1}$) for star formation in the galactic spheroid of {\normalfont \ttfamily thisNode}. The outflow
+  function superWindConstructorInternal(velocityCharacteristic,massLoading) result(self)
+    !% Internal constructor for the superwind expulsive star formation feedback from spheroids class.
+    implicit none
+    type            (starFormationExpulsiveFeedbackSpheroidsSuperWind)                :: self
+    double precision                                                  , intent(in   ) :: velocityCharacteristic, massLoading
+    !# <constructorAssign variables="velocityCharacteristic, massLoading"/>
+    
+    return
+  end function superWindConstructorInternal
+
+  double precision function superWindOutflowRate(self,node,rateEnergyInput,rateStarFormation)
+    !% Returns the expulsive outflow rate (in $M_\odot$ Gyr$^{-1}$) for star formation in the galactic spheroid of {\normalfont \ttfamily node}. The outflow
     !% rate is given by
     !% \begin{equation}
     !% \dot{M}_\mathrm{outflow} = f_\mathrm{SW,0} \left\{ \begin{array}{ll} 1 & \hbox{ if } V_\mathrm{spheroid} < V_\mathrm{spheroid,SW} \\ (V_\mathrm{spheroid,SW}/V_\mathrm{spheroid})^2 &  \hbox{ if } V_\mathrm{spheroid} \ge V_\mathrm{spheroid,SW} \end{array} \right. ,
     !% \end{equation}
-    !%  where $V_\mathrm{spheroid,SW}=${\normalfont \ttfamily
-    !% [spheroidSuperwindVelocity]} and $f_\mathrm{SW,0}=${\normalfont \ttfamily [spheroidSuperwindMassLoading]}. Note that the velocity $V_\mathrm{
-    !% spheroid}$ is whatever characteristic value returned by the spheroid method. This scaling is functionally similar to
-    !% that adopted by \cite{cole_hierarchical_2000} and \cite{baugh_can_2005}, except that they specifically used the
-    !% circular velocity at half-mass radius.
-    use Galacticus_Nodes
+    !% where $V_\mathrm{spheroid,SW}=${\normalfont \ttfamily [velocityCharacteristic]} and $f_\mathrm{SW,0}=${\normalfont \ttfamily
+    !% [massLoadnig]}. Note that the velocity $V_\mathrm{ spheroid}$ is whatever characteristic value returned by the spheroid
+    !% method. This scaling is functionally similar to that adopted by \cite{cole_hierarchical_2000} and \cite{baugh_can_2005},
+    !% except that they specifically used the circular velocity at half-mass radius.
     use Stellar_Feedback
     implicit none
-    type            (treeNode             ), intent(inout) :: thisNode
-    double precision                       , intent(in   ) :: energyInputRate               , starFormationRate
-    class           (nodeComponentSpheroid), pointer       :: thisSpheroidComponent
-    double precision                                       :: outflowRateToStarFormationRate, spheroidVelocity
-    !GCC$ attributes unused :: starFormationRate
-    
-    ! Get spheroid circular velocity.
-    thisSpheroidComponent => thisNode%spheroid()
-    spheroidVelocity=thisSpheroidComponent%velocity()
+    class           (starFormationExpulsiveFeedbackSpheroidsSuperWind), intent(inout) :: self
+    type            (treeNode                                        ), intent(inout) :: node
+    class           (nodeComponentSpheroid                           ), pointer       :: spheroid
+    double precision                                                  , intent(in   ) :: rateEnergyInput , rateStarFormation
+    double precision                                                                  :: velocitySpheroid, outflowRateToStarFormationRate
+    !GCC$ attributes unused :: rateStarFormation
 
+    spheroid         => node%spheroid    ()
+    velocitySpheroid =  spheroid%velocity()
     ! Check for zero velocity spheroid.
-    if (spheroidVelocity <= 0.0d0) then
-       Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_SW=0.0d0 ! No well defined answer in this case.
+    if (velocitySpheroid <= 0.0d0) then
+       superWindOutflowRate=0.0d0 ! No well defined answer in this case.
     else
-       outflowRateToStarFormationRate=spheroidSuperwindMassLoading*(spheroidSuperwindVelocity/max(spheroidVelocity,spheroidSuperwindVelocity))**2
-       Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_SW=outflowRateToStarFormationRate*energyInputRate &
-            &/feedbackEnergyInputAtInfinityCanonical
+       outflowRateToStarFormationRate=+      self%massLoading                 &
+            &                         *(                                      &
+            &                           +    self%velocityCharacteristic      &
+            &                           /max(                                 &
+            &                                     velocitySpheroid      ,     & 
+            &                                self%velocityCharacteristic      &
+            &                               )                                 &
+            &                         )**2
+       superWindOutflowRate          =+outflowRateToStarFormationRate         &
+            &                         *rateEnergyInput                        &
+            &                         /feedbackEnergyInputAtInfinityCanonical
     end if
     return
-  end function Star_Formation_Expulsive_Feedback_Spheroid_Outflow_Rate_SW
-
-end module Star_Formation_Expulsive_Feedback_Spheroids_Superwind
+  end function superWindOutflowRate
