c18c3565eceaaf3a7c2c9fdf7d9152d1c2ff0f8b
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-09-07 15:03:59 -0700

    * Add a module for setting system resource limits.

---
+++ source/rlimit.c
@@ -0,0 +1,32 @@
+// Copyright 2009, 2010, 2011 Andrew Benson <abenson@obs.carnegiescience.edu>
+//
+// This file is part of Galacticus.
+//
+//    Galacticus is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Galacticus is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+//% Implements Fortran-callable wrappers around the Linux resource limit functions.
+
+#include <sys/time.h>
+#include <sys/resource.h>
+
+int rLimitCPU = RLIMIT_CPU;
+
+int setResourceLimit_C(int resource, long limit) {
+  //% Fortran-callable wrapper around the setrlimit() function to set a resource limit.
+  struct rlimit rlim;
+  rlim.rlim_cur = limit;
+  rlim.rlim_max = RLIM_INFINITY;
+  int status = setrlimit(resource,&rlim);
+  return status;
+}
