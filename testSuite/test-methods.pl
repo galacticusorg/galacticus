cfcca453146e5cf231f21b9bda82edbe899fb866
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-09-15 12:57:41 -0700

    * Allow a separate environment variable to specify the location of   dynamic datasets.

--- testSuite/test-methods.pl
+++ testSuite/test-methods.pl
@@ -6,20 +6,22 @@ use warnings;
 # that they at least run to completion.
 # Andrew Benson (04-Sep-2010)
 
+# Determine dynamic data path.
+my $dataDynamicPath = exists($ENV{'GALACTICUS_DYNAMIC_DATA_PATH'}) ? $ENV{'GALACTICUS_DYNAMIC_DATA_PATH'} : $ENV{'GALACTICUS_DATA_PATH'};
 # Remove automatically generated files to force them to be regenerated (and, therefore, to test that the generating code works
 # correctly).
 #
 # FSPS stellar population synthesis code and associated file.
-system("rm -f ".$ENV{'GALACTICUS_DATA_PATH'}."/dynamic/stellarPopulations/SSP_Spectra_Conroy-et-al_v2.5_imfSalpeter.hdf5");
+system("rm -f ".$dataDynamicPath."/dynamic/stellarPopulations/SSP_Spectra_Conroy-et-al_v2.5_imfSalpeter.hdf5");
 system("rm -rf ../aux/FSPS_v2.4");
 # Core files (older than 7 days).
 system("find ../ -name '".$_."' -ctime +7 -exec rm {} \\;")
     foreach ( "core.*", "vgcore.*" );
 # Noninstantaneous recycling files (older than 14 days).
-system("find ".$ENV{'GALACTICUS_DATA_PATH'}."/dynamic/stellarPopulations -name '".$_."' -ctime +14 -exec rm {} \\;")
+system("find ".$dataDynamicPath."/dynamic/stellarPopulations -name '".$_."' -ctime +14 -exec rm {} \\;")
     foreach ( "Stellar_*_Yield_*_*.xml", "Stellar_Recycled_Fraction_*_*.xml", "Stellar_Energy_Input_*_*.xml" );
 # CAMB transfer function files (older than 14 days).
-system("find ".$ENV{'GALACTICUS_DATA_PATH'}."/dynamic/largeScaleStructure -name 'transfer_function_CAMB_*.xml' -ctime +14 -exec rm {} \\;");
+system("find ".$dataDynamicPath."/dynamic/largeScaleStructure -name 'transfer_function_CAMB_*.xml' -ctime +14 -exec rm {} \\;");
 
 # Simply run the models.
 system("cd ..; scripts/aux/launch.pl testSuite/test-methods.xml");
