2578d494bcc9e4f609ca994d2a7f988e8de06172
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-09-20 07:37:14 -0700

    * Ensure variable is initialized to avoid compiler warning.

--- source/models.likelihoods.SED_fit.F90
+++ source/models.likelihoods.SED_fit.F90
@@ -481,21 +481,24 @@ contains
        allocate(weights(0))
     end if
     ! Iterate over bands.
-    sedFitEvaluate      =0.0d0
+    sedFitEvaluate                =0.0d0
     starFormationRateNormalization=1.0d0
     do iMagnitude=0,size(self%magnitude)
        ! Compute luminosity.
        if (useRapidEvaluation) then
-          if (iMagnitude > 0.0d0)                                                             &
-               & luminosity=+sum(weights*massToLightRatios(:,iMagnitude))                     &
-               &            *10.0d0**(                                                        &
-               &                      -0.4d0                                                  &
-               &                      *dust%attenuation(                                      &
-               &                                        self%wavelengthEffective(iMagnitude), &
-               &                                        stellarAgeArbitrary                 , &
-               &                                        vBandAttenuation                      &
-               &                                       )                                      &
-               &                     )
+          if (iMagnitude > 0) then 
+             luminosity=+sum(weights*massToLightRatios(:,iMagnitude))                     &
+                  &     *10.0d0**(                                                        &
+                  &               -0.4d0                                                  &
+                  &               *dust%attenuation(                                      &
+                  &                                 self%wavelengthEffective(iMagnitude), &
+                  &                                 stellarAgeArbitrary                 , &
+                  &                                 vBandAttenuation                      &
+                  &                                )                                      &
+                  &              )
+          else
+             luminosity=0.0d0
+          end if
        else
           ! Integrate star formation history to get luminosity.
           integrationReset=.true.
