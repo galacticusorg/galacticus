4a487f6fda69a38a1200f69d8b79ccd6b91453c2
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-09-28 13:50:06 -0700

    * Fix name of default atomic collisional excitation class.

--- source/atomic.rates.excitation.collisional.F90
+++ source/atomic.rates.excitation.collisional.F90
@@ -27,7 +27,7 @@ module Atomic_Rates_Excitation_Collisional
   !#  <name>atomicExcitationRateCollisional</name>
   !#  <descriptiveName>Atomic Collisional Excitation</descriptiveName>
   !#  <description>Class providing atomic collisional excitation rates.</description>
-  !#  <default>scholzWalters91</default>
+  !#  <default>scholzWalters1991</default>
   !#  <defaultThreadPrivate>yes</defaultThreadPrivate>
   !#  <method name="coolingRate" >
   !#   <description>Return the collisional excitation cooling rate , in units of J/m$^3$/s, for ion of given {\normalfont \ttfamily atomicNumber} and {\normalfont \ttfamily electronNumber} at temperature {\normalfont \ttfamily T} (in Kelvin).</description>
