3510538d36e2419a4c5494077018c471f4187a1a
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-10-03 17:15:23 -0700

    * Refactor stellar feedback modules into a functionClass.

--- source/stellar_astrophysics.feedback.standard.F90
+++ source/stellar_astrophysics.feedback.standard.F90
@@ -16,119 +16,129 @@
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
 
-!% Contains a module which implements a simple calculation of energy feedback from stellar populations.
+  !% Implements a stellar feedback class which performs a simple calculation of energy feedback from stellar populations.
 
-module Stellar_Feedback_Standard
-  !% Implements a simple calculation of energy feedback from stellar populations.
-  use Numerical_Constants_Astronomical
-  implicit none
-  private
-  public :: Stellar_Feedback_Standard_Initialize
+  !# <stellarFeedback name="stellarFeedbackStandard">
+  !#  <description>A stellar feedback class which performs a simple calculation of energy feedback from stellar populations.</description>
+  !# </stellarFeedback>
+  type, extends(stellarFeedbackClass) :: stellarFeedbackStandard
+     !% A stellar feedback class which performs a simple calculation of energy feedback from stellar populations.
+     private
+     double precision :: initialMassForSupernovaeTypeII, supernovaEnergy
+   contains
+     procedure :: energyInputCumulative => standardEnergyInputCumulative
+  end type stellarFeedbackStandard
+  
+  interface stellarFeedbackStandard
+     !% Constructors for the {\normalfont \ttfamily standard} stellar feedback class.
+     module procedure standardConstructorParameters
+     module procedure standardConstructorInternal
+  end interface stellarFeedbackStandard
 
-  ! Parameters controlling the module.
-  double precision            :: initialMassForSupernovaeTypeII
-  double precision            :: supernovaEnergy
-  double precision, parameter :: populationIIIMaximumMetallicity=1.0d-4*metallicitySolar
+  ! Module-scope variables used in integrands.
+  double precision :: standardMassInitial, standardMetallicity
+  !$omp threadprivate(standardMassInitial,standardMetallicity)
 
-  ! Global variables used in integrands.
-  double precision            :: initialMassGlobal                                      , metallicityGlobal
-  !$omp threadprivate(initialMassGlobal,metallicityGlobal)
 contains
 
-  !# <stellarFeedbackMethod>
-  !#  <unitName>Stellar_Feedback_Standard_Initialize</unitName>
-  !# </stellarFeedbackMethod>
-  subroutine Stellar_Feedback_Standard_Initialize(stellarFeedbackMethod,Stellar_Feedback_Cumulative_Energy_Input_Get)
-    !% Initialize the ``standard'' stellar feedback module.
-    use ISO_Varying_String
+  function standardConstructorParameters(parameters) result(self)
+    !% Constructor for the {\normalfont \ttfamily standard} stellar feedback class which takes a parameter list as input.
     use Input_Parameters
+    use Numerical_Constants_Units
+    use Numerical_Constants_Astronomical
+    use Numerical_Constants_Prefixes
     implicit none
-    type     (varying_string  ), intent(in   )          :: stellarFeedbackMethod
-    procedure(Stellar_Feedback_Cumulative_Energy_Input_Standard), intent(inout), pointer :: Stellar_Feedback_Cumulative_Energy_Input_Get
+    type            (stellarFeedbackStandard)                :: self
+    type            (inputParameters        ), intent(inout) :: parameters
+    double precision                                         :: initialMassForSupernovaeTypeII, supernovaEnergy
 
-    if (stellarFeedbackMethod == 'standard') then
-       ! Set procedure pointers.
-       Stellar_Feedback_Cumulative_Energy_Input_Get => Stellar_Feedback_Cumulative_Energy_Input_Standard
-
-       ! Read in parameters required by this module.
-       !# <inputParameter>
-       !#   <name>initialMassForSupernovaeTypeII</name>
-       !#   <cardinality>1</cardinality>
-       !#   <defaultValue>8.0d0</defaultValue>
-       !#   <description>The minimum mass that a star must have in order that is result in a Type II supernova.</description>
-       !#   <source>globalParameters</source>
-       !#   <type>real</type>
-       !# </inputParameter>
-       !# <inputParameter>
-       !#   <name>supernovaEnergy</name>
-       !#   <cardinality>1</cardinality>
-       !#   <defaultValue>1.0d51</defaultValue>
-       !#   <description>The energy produced by a supernova (in ergs).</description>
-       !#   <source>globalParameters</source>
-       !#   <type>real</type>
-       !# </inputParameter>
-       ! Convert energy to MSolar (km/s)^2.
-       supernovaEnergy=supernovaEnergy*ergs/massSolar/kilo**2
-    end if
+    !# <inputParameter>
+    !#   <name>initialMassForSupernovaeTypeII</name>
+    !#   <cardinality>1</cardinality>
+    !#   <defaultValue>8.0d0</defaultValue>
+    !#   <description>The minimum mass that a star must have in order that is result in a Type II supernova.</description>
+    !#   <source>parameters</source>
+    !#   <type>real</type>
+    !# </inputParameter>
+    !# <inputParameter>
+    !#   <name>supernovaEnergy</name>
+    !#   <cardinality>1</cardinality>
+    !#   <defaultValue>1.0d51</defaultValue>
+    !#   <description>The energy produced by a supernova (in ergs).</description>
+    !#   <source>parameters</source>
+    !#   <type>real</type>
+    !# </inputParameter>
+    ! Convert energy to M☉ (km/s)².
+    supernovaEnergy=supernovaEnergy*ergs/massSolar/kilo**2
+    self           =stellarFeedbackStandard(initialMassForSupernovaeTypeII,supernovaEnergy)
+    !# <inputParametersValidate source="parameters"/>
     return
-  end subroutine Stellar_Feedback_Standard_Initialize
-
-  double precision function Stellar_Feedback_Cumulative_Energy_Input_Standard(initialMass,age,metallicity)
+  end function standardConstructorParameters
+  
+  function standardConstructorInternal(initialMassForSupernovaeTypeII,supernovaEnergy) result(self)
+    !% Constructor for the {\normalfont \ttfamily standard} stellar feedback class which takes a parameter list as input.
+    implicit none
+    type            (stellarFeedbackStandard)                :: self
+    double precision                         , intent(in   ) :: initialMassForSupernovaeTypeII, supernovaEnergy
+    !# <constructorAssign variables="initialMassForSupernovaeTypeII, supernovaEnergy"/>
+    
+    return
+  end function standardConstructorInternal
+  
+  double precision function standardEnergyInputCumulative(self,initialMass,age,metallicity)
     !% Compute the cumulative energy input from a star of given {\normalfont \ttfamily initialMass}, {\normalfont \ttfamily age} and {\normalfont \ttfamily metallicity}.
     use Stellar_Astrophysics
     use Supernovae_Type_Ia
     use Supernovae_Population_III
     use Numerical_Integration
+    use Numerical_Constants_Astronomical
+    use FGSL
     implicit none
-    double precision                            , intent(in   ) :: age                 , initialMass, metallicity
-    double precision                                            :: energySNe           , energyWinds, lifetime
+    class           (stellarFeedbackStandard   ), intent(inout) :: self
+    double precision                            , intent(in   ) :: age                                                    , initialMass, metallicity
+    double precision                            , parameter     :: populationIIIMaximumMetallicity=1.0d-4*metallicitySolar
+    double precision                                            :: energySNe                                              , energyWinds, lifetime
     type            (fgsl_function             )                :: integrandFunction
     type            (fgsl_integration_workspace)                :: integrationWorkspace
 
     ! Begin with zero energy input.
-    Stellar_Feedback_Cumulative_Energy_Input_Standard=0.0d0
-
+    standardEnergyInputCumulative=0.0d0
     ! Check if the star is sufficiently massive to result in a Type II supernova.
-    if (initialMass >= initialMassForSupernovaeTypeII ) then
+    if (initialMass >= self%initialMassForSupernovaeTypeII ) then
        ! Get the lifetime of the star.
        lifetime=Star_Lifetime(initialMass,metallicity)
        ! If lifetime is exceeded, assume a SNe has occurred.
        if (age >= lifetime) then
           energySNe=0.0d0
           ! Check for pair instability supernovae.
-          if (metallicity <= populationIIIMaximumMetallicity) energySNe=energySNe+SNePopIII_Cumulative_Energy(initialMass,age&
-               &,metallicity)
+          if (metallicity <= populationIIIMaximumMetallicity) energySNe=energySNe+SNePopIII_Cumulative_Energy(initialMass,age,metallicity)
           ! Population II star - normal supernova.
-          energySNe=energySNe+supernovaEnergy
+          energySNe=energySNe+self%supernovaEnergy
           ! Add the supernova energy.
-          Stellar_Feedback_Cumulative_Energy_Input_Standard=Stellar_Feedback_Cumulative_Energy_Input_Standard+energySNe
+          standardEnergyInputCumulative=standardEnergyInputCumulative+energySNe
        end if
     end if
-
     ! Add in contribution from Type Ia supernovae.
-    Stellar_Feedback_Cumulative_Energy_Input_Standard=Stellar_Feedback_Cumulative_Energy_Input_Standard&
-         &+SNeIa_Cumulative_Number(initialMass,age,metallicity)*supernovaEnergy
-
+    standardEnergyInputCumulative=+standardEnergyInputCumulative                        &
+         &                        +SNeIa_Cumulative_Number(initialMass,age,metallicity) &
+         &                        *self%supernovaEnergy
     ! Add in the contribution from stellar winds.
-    initialMassGlobal=initialMass
-    metallicityGlobal=metallicity
-    energyWinds=Integrate(0.0d0,age,Wind_Energy_Integrand,integrandFunction,integrationWorkspace&
-         &,toleranceAbsolute=1.0d-3*Stellar_Feedback_Cumulative_Energy_Input_Standard,toleranceRelative=1.0d-3)
+    standardMassInitial=initialMass
+    standardMetallicity=metallicity
+    energyWinds=Integrate(0.0d0,age,standardWindEnergyIntegrand,integrandFunction,integrationWorkspace,toleranceAbsolute=1.0d-3*standardEnergyInputCumulative,toleranceRelative=1.0d-3)
     call Integrate_Done(integrandFunction,integrationWorkspace)
-    Stellar_Feedback_Cumulative_Energy_Input_Standard=Stellar_Feedback_Cumulative_Energy_Input_Standard+energyWinds
-
+    standardEnergyInputCumulative=standardEnergyInputCumulative+energyWinds
     return
-  end function Stellar_Feedback_Cumulative_Energy_Input_Standard
+  end function standardEnergyInputCumulative
 
-  double precision function Wind_Energy_Integrand(age)
+  double precision function standardWindEnergyIntegrand(age)
     !% Integrand used in evaluating cumulative energy input from winds.
     use Stellar_Astrophysics_Winds
     implicit none
     double precision, intent(in   ) :: age
 
-    Wind_Energy_Integrand=0.5d0*Stellar_Winds_Mass_Loss_Rate(initialMassGlobal,age,metallicityGlobal)&
-         &*Stellar_Winds_Terminal_Velocity(initialMassGlobal,age,metallicityGlobal)**2
+    standardWindEnergyIntegrand=+0.5d0                                                                           &
+         &                      *Stellar_Winds_Mass_Loss_Rate   (standardMassInitial,age,standardMetallicity)    &
+         &                      *Stellar_Winds_Terminal_Velocity(standardMassInitial,age,standardMetallicity)**2
     return
-  end function Wind_Energy_Integrand
-
-end module Stellar_Feedback_Standard
+  end function standardWindEnergyIntegrand
