aaab5c0d70217712af1d16341a0af7e429011faf
Andrew Benson <abenson@obs.carnegiescience.edu>
2018-10-09 10:55:06 -0700

    * Formatting only.

--- source/galacticus.display.F90
+++ source/galacticus.display.F90
@@ -291,18 +291,18 @@ contains
     !$ if (omp_in_parallel()) threadNumber=omp_get_thread_num()+1
     select case (indentationLevel(threadNumber)*indentSpaces)
     case (0)
-       write (indentationFormat(threadNumber),'(a)') '(a)'
-       write (indentationFormatNoNewLine(threadNumber),'(a)') '(a,$)'
+       write (indentationFormat         (threadNumber),'(a)'     ) '(a)'
+       write (indentationFormatNoNewLine(threadNumber),'(a)'     ) '(a,$)'
     case (1:9)
-       write (indentationFormat(threadNumber),'(a,i1,a)') '(',indentationLevel(threadNumber)*indentSpaces,'x,a)'
+       write (indentationFormat         (threadNumber),'(a,i1,a)') '(',indentationLevel(threadNumber)*indentSpaces,'x,a)'
        write (indentationFormatNoNewLine(threadNumber),'(a,i1,a)') '(',indentationLevel(threadNumber)*indentSpaces,'x,a,$)'
     case (10:99)
-       write (indentationFormat(threadNumber),'(a,i2,a)') '(',indentationLevel(threadNumber)*indentSpaces,'x,a)'
+       write (indentationFormat         (threadNumber),'(a,i2,a)') '(',indentationLevel(threadNumber)*indentSpaces,'x,a)'
        write (indentationFormatNoNewLine(threadNumber),'(a,i2,a)') '(',indentationLevel(threadNumber)*indentSpaces,'x,a,$)'
     end select
     !$ if (.not.omp_in_parallel()) then
-    !$    indentationLevel          =indentationLevel(1)
-    !$    indentationFormat         =indentationFormat(1)
+    !$    indentationLevel          =indentationLevel          (1)
+    !$    indentationFormat         =indentationFormat         (1)
     !$    indentationFormatNoNewLine=indentationFormatNoNewLine(1)
     !$ end if
     return
