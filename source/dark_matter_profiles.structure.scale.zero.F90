48cf3b18f86ed2b2ace9f13b49f7359f222b9b9c
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-01-17 09:58:00 -0800

    * Add a "zero" dark matter halo structure scale radius class whch is   useful for when we don't want to track dark matter halo scale radii.

---
+++ source/dark_matter_profiles.structure.scale.zero.F90
@@ -0,0 +1,61 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+  !% An implementation of dark matter halo profile scale radii which returns zero radii---useful when scale radii are not
+  !% relevant.
+  
+  !# <darkMatterProfileScaleRadius name="darkMatterProfileScaleRadiusZero">
+  !#  <description>Dark matter halo scale radii class in which are assumed to be zero.</description>
+  !# </darkMatterProfileScaleRadius>
+  type, extends(darkMatterProfileScaleRadiusClass) :: darkMatterProfileScaleRadiusZero
+     !% A dark matter halo profile scale radius class in which are assumed to be zero.
+     private
+   contains
+     procedure :: radius => zeroRadius
+  end type darkMatterProfileScaleRadiusZero
+  
+  interface darkMatterProfileScaleRadiusZero
+     !% Constructors for the {\normalfont \ttfamily zero} dark matter halo profile scale radius class.
+     module procedure zeroConstructorParameters
+  end interface darkMatterProfileScaleRadiusZero
+
+contains
+
+  function zeroConstructorParameters(parameters) result(self)
+    !% Constructor for the {\normalfont \ttfamily zero} dark matter halo profile scale radius class which takes a
+    !% parameter list as input.
+    use Input_Parameters
+    implicit none
+    type(darkMatterProfileScaleRadiusZero)                :: self
+    type(inputParameters                 ), intent(inout) :: parameters
+    !GCC$ attributes unused :: parameters
+
+    self=darkMatterProfileScaleRadiusZero()
+    return
+  end function zeroConstructorParameters
+  
+  double precision function zeroRadius(self,node)
+    !% Compute the scale radius of the dark matter profile of {\normalfont \ttfamily node}.
+    implicit none
+    class(darkMatterProfileScaleRadiusZero), intent(inout), target :: self
+    type (treeNode                        ), intent(inout), target :: node
+    !GCC$ attributes unused :: self, node
+    
+    zeroRadius=0.0d0
+    return
+  end function zeroRadius
