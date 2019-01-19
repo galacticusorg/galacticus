46805bc66f799ab463323c9dd49312dd22f2ac87
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-01-18 16:28:07 -0800

    * Update copyright year.

--- source/models.likelihoods.independent_likelihoods.F90
+++ source/models.likelihoods.independent_likelihoods.F90
@@ -1,4 +1,5 @@
-!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019
 !!    Andrew Benson <abenson@carnegiescience.edu>
 !!
 !! This file is part of Galacticus.
@@ -15,7 +16,7 @@
 !!
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
-  
+
   !% Implementation of a model likelihood class which combines other likelihoods assumed to be independent.
 
   type, public :: posteriorSampleLikelihoodList
