ba1519718213d1edcf9ce3a8c80c1ce133ebece5
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-02-25 11:04:47 -0800

    * Formatting only.

--- source/merger_trees.construct.build.masses.distribution.halo_mass_function.F90
+++ source/merger_trees.construct.build.masses.distribution.halo_mass_function.F90
@@ -29,9 +29,9 @@
      !% Implementation of merger tree halo mass function sampling class in which the sampling rate is proportional to the halo mass function.
      private
      class           (haloMassFunctionClass), pointer :: haloMassFunction_ => null()
-     class           (haloEnvironmentClass ), pointer :: haloEnvironment_ => null()
-     double precision                                 :: abundanceMinimum, abundanceMaximum, &
-          &                                              modifier1       , modifier2
+     class           (haloEnvironmentClass ), pointer :: haloEnvironment_  => null()
+     double precision                                 :: abundanceMinimum           , abundanceMaximum, &
+          &                                              modifier1                  , modifier2
    contains
      final     ::           haloMassFunctionDestructor
      procedure :: sample => haloMassFunctionSample
