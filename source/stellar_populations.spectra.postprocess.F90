bf18c768942664a2d9182baefe56a3c5f82ee798
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-03-09 00:46:04 +0000

    * Ensure stellarPopulationSpectraPostprocessorList class is defined   before it is used.

--- source/stellar_populations.spectra.postprocess.F90
+++ source/stellar_populations.spectra.postprocess.F90
@@ -37,6 +37,13 @@ module Stellar_Population_Spectra_Postprocess
   !#  </method>
   !# </functionClass>
   
+  type :: stellarPopulationSpectraPostprocessorList
+     !% Ttype used to build linked list of stellar population spectra postprocessors.
+     class(stellarPopulationSpectraPostprocessorClass), pointer :: stellarPopulationSpectraPostprocessor_
+   contains
+     final :: stellarPopulationSpectraPostprocessorListDestructor
+  end type stellarPopulationSpectraPostprocessorList
+
   !# <functionClass>
   !#  <name>stellarPopulationSpectraPostprocessorBuilder</name>
   !#  <descriptiveName>Builder for postprocessors for stellar population spectra</descriptiveName>
@@ -50,13 +57,6 @@ module Stellar_Population_Spectra_Postprocess
   !#  </method>
   !# </functionClass>
   
-  type :: stellarPopulationSpectraPostprocessorList
-     !% Ttype used to build linked list of stellar population spectra postprocessors.
-     class(stellarPopulationSpectraPostprocessorClass), pointer :: stellarPopulationSpectraPostprocessor_
-   contains
-     final :: stellarPopulationSpectraPostprocessorListDestructor
-  end type stellarPopulationSpectraPostprocessorList
-
 contains
 
   subroutine stellarPopulationSpectraPostprocessorListDestructor(self)
