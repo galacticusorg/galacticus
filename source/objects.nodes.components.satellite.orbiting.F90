b4f2b2b8fedddb948a6f26c736399d1e8e386080
Xiaolong Du <xdu@carnegiescience.edu>
2019-03-25 21:42:00 -0700

    * Ensure that parameter names in the orbiting satellite model are detected and   included into the list of allowed parameter names.

--- source/objects.nodes.components.satellite.orbiting.F90
+++ source/objects.nodes.components.satellite.orbiting.F90
@@ -121,6 +121,7 @@ module Node_Component_Satellite_Orbiting
   ! Option controlling how to initialize the bound mass of satellite halos.
   type(varying_string)        :: satelliteBoundMassInitializeType
   double precision            :: satelliteMaximumRadiusOverVirialRadius      , satelliteDensityContrast
+
 contains
 
   !# <mergerTreePreTreeConstructionTask>
@@ -184,19 +185,19 @@ contains
   !# <nodeComponentThreadInitializationTask>
   !#  <unitName>Node_Component_Satellite_Orbiting_Thread_Initialize</unitName>
   !# </nodeComponentThreadInitializationTask>
-  subroutine Node_Component_Satellite_Orbiting_Thread_Initialize(parameters)
+  subroutine Node_Component_Satellite_Orbiting_Thread_Initialize(globalParameters_)
     !% Initializes the tree node orbiting satellite module.
     use Galacticus_Nodes, only : defaultSatelliteComponent
     use Input_Parameters
     implicit none
-    type(inputParameters), intent(inout) :: parameters
+    type(inputParameters), intent(inout) :: globalParameters_
 
     if (defaultSatelliteComponent%orbitingIsActive()) then
-       !# <objectBuilder class="darkMatterHaloScale"        name="darkMatterHaloScale_"        source="parameters"/>
-       !# <objectBuilder class="satelliteDynamicalFriction" name="satelliteDynamicalFriction_" source="parameters"/>
-       !# <objectBuilder class="satelliteTidalHeatingRate"  name="satelliteTidalHeatingRate_"  source="parameters"/>
-       !# <objectBuilder class="satelliteTidalStripping"    name="satelliteTidalStripping_"    source="parameters"/>
-       !# <objectBuilder class="virialOrbit"                name="virialOrbit_"                source="parameters"/>
+       !# <objectBuilder class="darkMatterHaloScale"        name="darkMatterHaloScale_"        source="globalParameters_"/>
+       !# <objectBuilder class="satelliteDynamicalFriction" name="satelliteDynamicalFriction_" source="globalParameters_"/>
+       !# <objectBuilder class="satelliteTidalHeatingRate"  name="satelliteTidalHeatingRate_"  source="globalParameters_"/>
+       !# <objectBuilder class="satelliteTidalStripping"    name="satelliteTidalStripping_"    source="globalParameters_"/>
+       !# <objectBuilder class="virialOrbit"                name="virialOrbit_"                source="globalParameters_"/>
     end if
     return
   end subroutine Node_Component_Satellite_Orbiting_Thread_Initialize
