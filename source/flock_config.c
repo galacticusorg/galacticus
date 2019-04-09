88f2d8b61fff0bca2af15ac8fe321b646f67a67d
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-04-09 15:19:30 -0700

    * Update copyright information in C and C++ source files.

--- source/flock_config.c
+++ source/flock_config.c
@@ -1,4 +1,6 @@
-// Copyright 2009, 2010, 2011 Andrew Benson <abenson@obs.carnegiescience.edu>
+// Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+//           2019
+//    Andrew Benson <abenson@carnegiescience.edu>
 //
 // This file is part of Galacticus.
 //
