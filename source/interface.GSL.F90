49cb24c52fb54e6cf4b159e0fd6322de1b256666
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-04-09 15:21:44 -0700

    * Add an interface module to low-level aspects of the GSL library   (currently gsl_function objects).
    * Add a numerical differentiation class which interfaces with the GSL
      implementations of this. Includes a test case.

---
+++ source/interface.GSL.F90
@@ -0,0 +1,77 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which interfaces with low-level aspects of the GSL library.
+
+! Specify an explicit dependence on the interface.GSL.C.o object file.
+!: $(BUILDPATH)/interface.GSL.C.o
+
+
+module Interface_GSL
+  !% Interfaces with low-level aspects of the GSL library.
+  use, intrinsic :: ISO_C_Binding, only : c_ptr, c_funptr
+  private
+  public :: gslFunction, gslFunctionDestroy, gslFunctionTemplate
+
+  abstract interface
+     !% Interface for {\normalfont \ttfamily gsl\_function} type. We ignore the {\normalfont \ttfamily params} argument as this is not used by \glc.
+     double precision function gslFunctionTemplate(x)
+       double precision, intent(in   ), value :: x
+     end function gslFunctionTemplate
+  end interface
+
+   interface
+      !% Interfaces to C functions.
+      function gslFunctionConstructor(f) bind(c,name="gslFunctionConstructor")
+        !% Interface to a C function which establishes a {\normalfont \ttfamily gsl\_function} type.
+        import c_ptr, c_funptr
+        type(c_ptr   )        :: gslFunctionConstructor
+        type(c_funptr), value :: f
+      end function gslFunctionConstructor
+
+      subroutine gslFunctionDestructor(f) bind(c,name="gslFunctionDestructor")
+        !% Interface to a C function which destroys a {\normalfont \ttfamily gsl\_function} type.
+        import c_funptr
+        type(c_funptr), value :: f
+      end subroutine gslFunctionDestructor
+   end interface
+  
+contains
+
+  function gslFunction(f)
+    !% Return a {\normalfont \ttfamily c\_ptr} object for the given function {\normalfont \ttfamily f}.
+    use, intrinsic :: ISO_C_Binding, only : c_funloc
+    implicit none
+    type     (c_ptr              ) :: gslFunction
+    procedure(gslFunctionTemplate) :: f
+
+    gslFunction=gslFunctionConstructor(c_funloc(f))
+    return
+  end function gslFunction
+  
+  subroutine gslFunctionDestroy(f)
+    !% Destroy a {\normalfont \ttfamily c\_ptr} to a {\normalfont \ttfamily gsl\_function} object.
+    implicit none
+    type(c_ptr), intent(in   ) :: f
+
+    call gslFunctionDestructor(f)
+    return
+  end subroutine gslFunctionDestroy
+  
+end module Interface_GSL
---
+++ source/tests.differentiation.F90
@@ -0,0 +1,48 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a program to test differentiation functions.
+
+program Test_Differentiation
+  !% Tests that numerical differentiation functions work.
+  use Galacticus_Display            , only : Galacticus_Verbosity_Level_Set, verbosityStandard
+  use Unit_Tests                    , only : Unit_Tests_Begin_Group        , Unit_Tests_End_Group, Unit_Tests_Finish, Assert
+  use Numerical_Differentiation     , only : differentiator
+  use Test_Differentiation_Functions, only : function1
+  implicit none
+  integer                         , parameter            :: countStep      =10
+  double precision                , dimension(countStep) :: x              =[0.0d0,0.1d0,0.3d0,0.5d0,0.7d0,0.9d0,1.1d0,1.3d0,1.5d0,1.7d0], y
+  double precision                , parameter            :: stepSize       =0.1d0
+  type            (differentiator)                       :: differentiator_
+  integer                                                :: i
+
+  ! Set verbosity level.
+  call Galacticus_Verbosity_Level_Set(verbosityStandard)
+  ! Begin unit tests.
+  call Unit_Tests_Begin_Group("Numerical differentiation")
+  differentiator_=differentiator(function1)
+  do i=1,countStep
+     y(i)=differentiator_%derivative(x(i),stepSize)
+  end do
+  call Assert("d/dx(sin(x))=cos(x)",y,cos(x),relTol=1.0d-6)
+  ! End unit tests.
+  call Unit_Tests_End_Group()
+  call Unit_Tests_Finish   ()
+
+end program Test_Differentiation
