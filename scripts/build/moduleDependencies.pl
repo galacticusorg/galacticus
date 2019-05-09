2ff263fc20816ca8051650d1815fa97dc1b984ff
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-05-09 15:53:26 +0000

    * Modify most of the scripts used in building Galacticus so that they   scan only those source files which have changed since the script was   last run. This speeds up these scripts dueing recompilation by large   factors.

--- scripts/build/moduleDependencies.pl
+++ scripts/build/moduleDependencies.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use lib $ENV{'GALACTICUS_EXEC_PATH'}."/perl";
+use Storable;
+use List::ExtraUtils;
 
 # Build Makefile rules which describe how to construct module files from source files.
 # Andrew Benson (12-September-2016)
@@ -19,8 +22,14 @@ if ( -e $sourceDirectoryDescriptors[0]->{'path'} ) {
     system("mkdir -p ".$workDirectoryName."/".$_->{'leaf'})
 	foreach ( @sourceDirectoryDescriptors );    
 }
-# Open the output Makefile.
-open(my $makefile,">".$sourceDirectoryName."/".$workDirectoryName."Makefile_Module_Dependencies");
+# Initialize data structure to hold per-file information.
+my $modulesPerFile;
+my $havePerFile = -e $workDirectoryName."Makefile_Module_Dependencies.blob";
+my $updateTime;
+if ( $havePerFile ) {
+    $modulesPerFile = retrieve($workDirectoryName."Makefile_Module_Dependencies.blob");
+    $updateTime     = -M       $workDirectoryName."Makefile_Module_Dependencies.blob" ;
+}
 # Iterate over source directories.
 foreach my $sourceDirectoryDescriptor ( @sourceDirectoryDescriptors ) {
     # Find all source files to process in this directory.
@@ -31,8 +40,23 @@ foreach my $sourceDirectoryDescriptor ( @sourceDirectoryDescriptors ) {
     # Iterate over source files.
     foreach my $sourceFileName ( @sourceFileNames ) {
 	# Initialize list of modules provided, and a stack of file names to process.
-	my @modulesProvided;
 	my @sourceFileNameStack = ( $sourceDirectoryDescriptor->{'path'}."/".$sourceFileName );
+	(my $fileIdentifier = $sourceDirectoryDescriptor->{'path'}."/".$sourceFileName) =~ s/\//_/g;
+	$fileIdentifier =~ s/^\._??//;
+	# Check if file is updated. If it is not, skip processing it. If it is, remove previous record of uses and rescan.
+	my $rescan = 1;
+	if ( $havePerFile && exists($modulesPerFile->{$fileIdentifier}) ) {
+	    $rescan = 0
+		unless ( grep {-M $_ < $updateTime} &List::ExtraUtils::as_array($modulesPerFile->{$fileIdentifier}->{'files'}) );
+	}
+	next
+	    unless ( $rescan );
+	delete($modulesPerFile->{$fileIdentifier})
+    	    if ( $havePerFile && exists($modulesPerFile->{$fileIdentifier}) );
+	push(@{$modulesPerFile->{$fileIdentifier}->{'files'}},$sourceDirectoryDescriptor->{'path'}."/".$sourceFileName);
+        $modulesPerFile->{$fileIdentifier}->{'sourceFileName'           } = $sourceFileName;
+        $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'} = $sourceDirectoryDescriptor;
+        @{$modulesPerFile->{$fileIdentifier}->{'modulesProvided'}} = ();
 	while ( scalar(@sourceFileNameStack) > 0 ) {
 	    # Open and read the source file.
 	    my $sourceFilePathName = pop(@sourceFileNameStack);
@@ -45,47 +69,58 @@ foreach my $sourceDirectoryDescriptor ( @sourceDirectoryDescriptors ) {
 		if ( $line !~ m/^\s*module\s+procedure/i && $line =~ m/^\s*module\s+([a-zA-Z0-9_]+)/i ) {
 		    # Construct the names of the module file and object file generated from this source file.
 		    my $moduleFileName  = lc($1).".mod";
-		    (my $objectFileName = $sourceFileName) =~ s/\.(f|f90)$/.o/i;
-		    push(@modulesProvided,$moduleFileName);
-		    # Construct a Makefile rule for this module file.
-		    print $makefile $workDirectoryName.$moduleFileName.": ".$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$objectFileName."\n";
-		    print $makefile "\t\@if [ ! -f ".$workDirectoryName.$moduleFileName." ]; then \\\n";
-		    print $makefile "\t  rm "      .$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$objectFileName." ; \\\n";
-		    print $makefile "\t  \$(MAKE) ".$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$objectFileName." ; \\\n";
-		    print $makefile "\tfi\n\n";
-		    (my $objectDependencyFileName = $objectFileName) =~ s/.o$/.d/;
-		    print $makefile $workDirectoryName.$moduleFileName.".d: ".$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$objectDependencyFileName."\n";
-		    print $makefile "\t\@echo ".$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$objectFileName          ." > " .$workDirectoryName.$moduleFileName.".d~\n";
-		    print $makefile "\t\@cat " .$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$objectDependencyFileName." >> ".$workDirectoryName.$moduleFileName.".d~\n";
-		    print $makefile "\t\@if cmp -s ".$workDirectoryName.$moduleFileName.".d ".$workDirectoryName.$moduleFileName.".d~ ; then \\\n";
-		    print $makefile "\t rm ".$workDirectoryName.$moduleFileName.".d~ ; \\\n";
-		    print $makefile "\telse \\\n";
-		    print $makefile "\t mv ".$workDirectoryName.$moduleFileName.".d~ ".$workDirectoryName.$moduleFileName.".d ; \\\n";
-		    print $makefile "\tfi\n\n";
-		    # Create rule for making a *.mod.gv file which is used in building GraphViz descriptions of source
-		    # file dependencies.
-		    print $makefile $workDirectoryName.$moduleFileName.".gv: ".$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$objectDependencyFileName." ".$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$sourceFileName.".gv\n";
-		    print $makefile "\t\@echo ".$sourceDirectoryDescriptor->{'leaf'}.$sourceFileName." > ".$workDirectoryName.lc($moduleFileName).".gv\n";
-
+		    push(@{$modulesPerFile->{$fileIdentifier}->{'modulesProvided'}},$moduleFileName);
 		}
 		# Detect include files and add to the stack of files to process.
-		push(@sourceFileNameStack,$sourceDirectoryName."/".$1)
-		    if ( $line =~ m/include\s+\'(\w+)\'/i );
+		if ( $line =~ m/include\s+\'(\w+)\'/i ) {
+		    my $includeFileName = $sourceDirectoryName."/".$1;
+		    push(@sourceFileNameStack,$includeFileName);
+		    push(@{$modulesPerFile->{$fileIdentifier}->{'files'}},$includeFileName);
+		}
 	    }
 	    close($sourceFile);
-	    # Create a rule for the module list file if needed.
-	    if ( scalar(@modulesProvided) > 0 ) {
-		(my $modulesProvidedFileName = $sourceFileName)  =~ s/\.(f|f90)$/\.m/i;
-		print $makefile $workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$modulesProvidedFileName.":\n";
-		my $director = "> ";
-		foreach ( @modulesProvided ) {
-		    print $makefile "\t\@echo ".$workDirectoryName.$_." ".$director." ".$workDirectoryName.$sourceDirectoryDescriptor->{'leaf'}.$modulesProvidedFileName."\n";
-		    $director = ">>";
-		}
-		print $makefile "\n";
-	    }
 	}
     }
 }
+# Create the output Makefile.
+open(my $makefile,">".$sourceDirectoryName."/".$workDirectoryName."Makefile_Module_Dependencies");
+foreach my $fileIdentifier ( sort(keys(%{$modulesPerFile})) ) {
+    if ( scalar(@{$modulesPerFile->{$fileIdentifier}->{'modulesProvided'}}) > 0 ) {
+	foreach my $moduleFileName ( @{$modulesPerFile->{$fileIdentifier}->{'modulesProvided'}} ) {
+	    # Construct a Makefile rule for this module file.
+	    (my $objectFileName = $modulesPerFile->{$fileIdentifier}->{'sourceFileName'}) =~ s/\.(f|f90)$/.o/i;
+	    print $makefile $workDirectoryName.$moduleFileName.": ".$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$objectFileName."\n";
+	    print $makefile "\t\@if [ ! -f ".$workDirectoryName.$moduleFileName." ]; then \\\n";
+	    print $makefile "\t  rm "      .$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$objectFileName." ; \\\n";
+	    print $makefile "\t  \$(MAKE) ".$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$objectFileName." ; \\\n";
+	    print $makefile "\tfi\n\n";
+	    (my $objectDependencyFileName = $objectFileName) =~ s/.o$/.d/;
+	    print $makefile $workDirectoryName.$moduleFileName.".d: ".$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$objectDependencyFileName."\n";
+	    print $makefile "\t\@echo ".$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$objectFileName          ." > " .$workDirectoryName.$moduleFileName.".d~\n";
+	    print $makefile "\t\@cat " .$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$objectDependencyFileName." >> ".$workDirectoryName.$moduleFileName.".d~\n";
+	    print $makefile "\t\@if cmp -s ".$workDirectoryName.$moduleFileName.".d ".$workDirectoryName.$moduleFileName.".d~ ; then \\\n";
+	    print $makefile "\t rm ".$workDirectoryName.$moduleFileName.".d~ ; \\\n";
+	    print $makefile "\telse \\\n";
+	    print $makefile "\t mv ".$workDirectoryName.$moduleFileName.".d~ ".$workDirectoryName.$moduleFileName.".d ; \\\n";
+	    print $makefile "\tfi\n\n";
+	    # Create rule for making a *.mod.gv file which is used in building GraphViz descriptions of source
+	    # file dependencies.
+	    print $makefile $workDirectoryName.$moduleFileName.".gv: ".$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$objectDependencyFileName." ".$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$modulesPerFile->{$fileIdentifier}->{'sourceFileName'}.".gv\n";
+	    print $makefile "\t\@echo ". $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$modulesPerFile->{$fileIdentifier}->{'sourceFileName'}." > ".$workDirectoryName.lc($moduleFileName).".gv\n";
+	}
+	# Create a rule for the module list file if needed.
+	(my $modulesProvidedFileName = $modulesPerFile->{$fileIdentifier}->{'sourceFileName'})  =~ s/\.(f|f90)$/\.m/i;
+	print $makefile $workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$modulesProvidedFileName.":\n";
+	my $director = "> ";
+	foreach ( @{$modulesPerFile->{$fileIdentifier}->{'modulesProvided'}} ) {
+	    print $makefile "\t\@echo ".$workDirectoryName.$_." ".$director." ".$workDirectoryName. $modulesPerFile->{$fileIdentifier}->{'sourceDirectoryDescriptor'}->{'leaf'}.$modulesProvidedFileName."\n";
+	    $director = ">>";
+	}
+	print $makefile "\n";
+    }
+}
+close($makefile);
+# Output the per file module use data.
+store($modulesPerFile,$workDirectoryName."Makefile_Module_Dependencies.blob");
 
 exit 0;
