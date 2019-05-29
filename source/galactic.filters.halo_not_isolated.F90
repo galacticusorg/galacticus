c16e7d142d57f3629a410b74c62e214ced498fbe
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-05-29 10:37:48 -0700

    * Add a galactic filter which select non-isolated (i.e. satellite) halos.

---
+++ source/galactic.filters.halo_not_isolated.F90
@@ -0,0 +1,60 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which implements a filter which passes only non-isolated halos.
+
+  !# <galacticFilter name="galacticFilterHaloNotIsolated">
+  !#  <description>A filter which passes only non-isolated halos.</description>
+  !# </galacticFilter>
+  type, extends(galacticFilterClass) :: galacticFilterHaloNotIsolated
+     !% A galactic filter class which passes only non-isolated halos.
+     private
+   contains
+     procedure :: passes => haloNotIsolatedPasses
+  end type galacticFilterHaloNotIsolated
+
+  interface galacticFilterHaloNotIsolated
+     !% Constructors for the ``haloNotIsolated'' galactic filter class.
+     module procedure haloNotIsolatedConstructorParameters
+  end interface galacticFilterHaloNotIsolated
+
+contains
+
+  function haloNotIsolatedConstructorParameters(parameters) result(self)
+    !% Constructor for the ``haloNotIsolated'' galactic filter class which takes a parameter set as input.
+    use Input_Parameters
+    implicit none
+    type(galacticFilterHaloNotIsolated)                :: self
+    type(inputParameters              ), intent(inout) :: parameters
+    !GCC$ attributes unused :: parameters
+
+    self=galacticFilterHaloNotIsolated()
+    return
+  end function haloNotIsolatedConstructorParameters
+
+  logical function haloNotIsolatedPasses(self,node)
+    !% Implement a galactic filter which passes only isolated halos.
+    implicit none
+    class(galacticFilterHaloNotIsolated), intent(inout) :: self
+    type (treeNode                     ), intent(inout) :: node
+    !GCC$ attributes unused :: self
+
+    haloNotIsolatedPasses=node%isSatellite()
+    return
+  end function haloNotIsolatedPasses
