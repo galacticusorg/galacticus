d731554a8e95608216f4f31e5d1151733adc1a09
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-06-20 08:03:29 -0700

    * Refactor various property output modules into node property extractor   classes.

---
+++ source/nodes.property_extractor.cooling_rate.F90
@@ -0,0 +1,138 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which implements a cooling rate property extractor class.
+
+  use Cooling_Rates, only : coolingRate, coolingRateClass
+
+  !# <nodePropertyExtractor name="nodePropertyExtractorRateCooling">
+  !#  <description>A cooling rate property extractor class.</description>
+  !# </nodePropertyExtractor>
+  type, extends(nodePropertyExtractorScalar) :: nodePropertyExtractorRateCooling
+     !% A rateCooling property extractor class.
+     private
+     class(coolingRateClass), pointer :: coolingRate_ => null()
+   contains
+     final     ::                rateCoolingDestructor
+     procedure :: extract     => rateCoolingExtract
+     procedure :: name        => rateCoolingName
+     procedure :: description => rateCoolingDescription
+     procedure :: unitsInSI   => rateCoolingUnitsInSI
+     procedure :: type        => rateCoolingType
+  end type nodePropertyExtractorRateCooling
+
+  interface nodePropertyExtractorRateCooling
+     !% Constructors for the ``rateCooling'' output analysis class.
+     module procedure rateCoolingConstructorParameters
+     module procedure rateCoolingConstructorInternal
+  end interface nodePropertyExtractorRateCooling
+
+contains
+  
+  function rateCoolingConstructorParameters(parameters) result(self)
+    !% Constructor for the {\normalfont \ttfamily rateCooling} property extractor class which takes a parameter set as input.
+    use Input_Parameters, only : inputParameter, inputParameters
+    implicit none
+    type (nodePropertyExtractorRateCooling)                :: self
+    type (inputParameters                 ), intent(inout) :: parameters
+    class(coolingRateClass                ), pointer       :: coolingRate_
+
+    !# <objectBuilder class="coolingRate" name="coolingRate_" source="parameters"/>
+    self=nodePropertyExtractorRateCooling(coolingRate_)
+    !# <inputParametersValidate source="parameters"/>
+    !# <objectDestructor name="coolingRate_"/>
+    return
+  end function rateCoolingConstructorParameters
+
+  function rateCoolingConstructorInternal(coolingRate_) result(self)
+    !% Internal constructor for the {\normalfont \ttfamily rateCooling} property extractor class.
+    implicit none
+    type (nodePropertyExtractorRateCooling)                        :: self
+    class(coolingRateClass                ), intent(in   ), target :: coolingRate_
+    !# <constructorAssign variables="*coolingRate_"/>
+
+    return
+  end function rateCoolingConstructorInternal
+
+  subroutine rateCoolingDestructor(self)
+    !% Destructor for the {\normalfont \ttfamily rateCooling} property extractor class.
+    implicit none
+    type(nodePropertyExtractorRateCooling), intent(inout) :: self
+
+    !# <objectDestructor name="self%coolingRate_"/>
+    return
+  end subroutine rateCoolingDestructor
+  
+  double precision function rateCoolingExtract(self,node,instance)
+    !% Implement a last isolated redshift output analysis.
+    use Galacticus_Nodes, only : nodeComponentBasic
+    implicit none
+    class(nodePropertyExtractorRateCooling), intent(inout)           :: self
+    type (treeNode                        ), intent(inout)           :: node
+    type (multiCounter                    ), intent(inout), optional :: instance
+    !GCC$ attributes unused :: self, instance
+    
+    rateCoolingExtract=self%coolingRate_%rate(node)
+    return
+  end function rateCoolingExtract
+
+  function rateCoolingName(self)
+    !% Return the name of the last isolated redshift property.
+    implicit none
+    type (varying_string                  )                :: rateCoolingName
+    class(nodePropertyExtractorRateCooling), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    rateCoolingName=var_str('hotHaloRateCooling')
+    return
+  end function rateCoolingName
+
+  function rateCoolingDescription(self)
+    !% Return a description of the rateCooling property.
+    implicit none
+    type (varying_string                  )                :: rateCoolingDescription
+    class(nodePropertyExtractorRateCooling), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    rateCoolingDescription=var_str('The rate of mass cooling in the hot halo [M☉/Gyr].')
+    return
+  end function rateCoolingDescription
+
+  double precision function rateCoolingUnitsInSI(self)
+    !% Return the units of the last isolated redshift property in the SI system.
+    use Numerical_Constants_Astronomical, only : massSolar, gigaYear
+    implicit none
+    class(nodePropertyExtractorRateCooling), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    rateCoolingUnitsInSI=massSolar/gigaYear
+    return
+  end function rateCoolingUnitsInSI
+
+  integer function rateCoolingType(self)
+    !% Return the type of the last isolated redshift property.
+    use Output_Analyses_Options
+    implicit none
+    class(nodePropertyExtractorRateCooling), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    rateCoolingType=outputAnalysisPropertyTypeLinear
+    return
+  end function rateCoolingType
+  
---
+++ source/nodes.property_extractor.half_mass_radius.F90
@@ -0,0 +1,111 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which implements a radiusHalfMass property extractor class.
+
+  !# <nodePropertyExtractor name="nodePropertyExtractorRadiusHalfMass">
+  !#  <description>A radiusHalfMass property extractor class.</description>
+  !# </nodePropertyExtractor>
+  type, extends(nodePropertyExtractorScalar) :: nodePropertyExtractorRadiusHalfMass
+     !% A half-mass radius property extractor class.
+     private
+   contains
+     procedure :: extract     => radiusHalfMassExtract
+     procedure :: name        => radiusHalfMassName
+     procedure :: description => radiusHalfMassDescription
+     procedure :: unitsInSI   => radiusHalfMassUnitsInSI
+     procedure :: type        => radiusHalfMassType
+  end type nodePropertyExtractorRadiusHalfMass
+
+  interface nodePropertyExtractorRadiusHalfMass
+     !% Constructors for the ``radiusHalfMass'' output analysis class.
+     module procedure radiusHalfMassConstructorParameters
+  end interface nodePropertyExtractorRadiusHalfMass
+
+contains
+  
+  function radiusHalfMassConstructorParameters(parameters) result(self)
+    !% Constructor for the {\normalfont \ttfamily radiusHalfMass} property extractor class which takes a parameter set as input.
+    use Input_Parameters, only : inputParameters
+    implicit none
+    type (nodePropertyExtractorRadiusHalfMass)                :: self
+    type (inputParameters                    ), intent(inout) :: parameters
+    !GCC$ attributes unused :: parameters
+    
+    self=nodePropertyExtractorRadiusHalfMass()
+    return
+  end function radiusHalfMassConstructorParameters
+  
+  double precision function radiusHalfMassExtract(self,node,instance)
+    !% Implement a last isolated redshift output analysis.
+    use Galactic_Structure_Enclosed_Masses, only : Galactic_Structure_Radius_Enclosing_Mass
+    use Galactic_Structure_Options        , only : massTypeStellar
+    implicit none
+    class(nodePropertyExtractorRadiusHalfMass), intent(inout)           :: self
+    type (treeNode                           ), intent(inout)           :: node
+    type (multiCounter                       ), intent(inout), optional :: instance
+    !GCC$ attributes unused :: self, instance
+    
+    radiusHalfMassExtract=Galactic_Structure_Radius_Enclosing_Mass(node,fractionalMass=0.5d0,massType=massTypeStellar)
+    return
+  end function radiusHalfMassExtract
+
+  function radiusHalfMassName(self)
+    !% Return the name of the last isolated redshift property.
+    implicit none
+    type (varying_string                     )                :: radiusHalfMassName
+    class(nodePropertyExtractorRadiusHalfMass), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    radiusHalfMassName=var_str('radiusHalfMassStellar')
+    return
+  end function radiusHalfMassName
+
+  function radiusHalfMassDescription(self)
+    !% Return a description of the radiusHalfMass property.
+    implicit none
+    type (varying_string                     )                :: radiusHalfMassDescription
+    class(nodePropertyExtractorRadiusHalfMass), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    radiusHalfMassDescription=var_str('Radius enclosing half the galaxy stellar mass [Mpc].')
+    return
+  end function radiusHalfMassDescription
+
+  double precision function radiusHalfMassUnitsInSI(self)
+    !% Return the units of the last isolated redshift property in the SI system.
+    use Numerical_Constants_Astronomical, only : massSolar
+    implicit none
+    class(nodePropertyExtractorRadiusHalfMass), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    radiusHalfMassUnitsInSI=massSolar
+    return
+  end function radiusHalfMassUnitsInSI
+
+  integer function radiusHalfMassType(self)
+    !% Return the type of the last isolated redshift property.
+    use Output_Analyses_Options
+    implicit none
+    class(nodePropertyExtractorRadiusHalfMass), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    radiusHalfMassType=outputAnalysisPropertyTypeLinear
+    return
+  end function radiusHalfMassType
