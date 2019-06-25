0478819c39bae2b4b25e3414a4c8535a7f9f3d12
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-06-24 17:39:57 -0700

    * Add a halo bias property extractor, and remove the corresponding   functionality from the halo model output module.

---
+++ source/nodes.property_extractor.halo_bias.F90
@@ -0,0 +1,140 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which implements an ISM mass output analysis property extractor class.
+
+  use Dark_Matter_Halo_Biases, only : darkMatterHaloBiasClass, darkMatterHaloBias
+
+  !# <nodePropertyExtractor name="nodePropertyExtractorHaloBias">
+  !#  <description>A node property extractor class for halo bias.</description>
+  !# </nodePropertyExtractor>
+  type, extends(nodePropertyExtractorScalar) :: nodePropertyExtractorHaloBias
+     !% A node property extractor for halo bias.
+     private
+     class(darkMatterHaloBiasClass), pointer :: darkMatterHaloBias_ => null()
+   contains
+     final     ::                haloBiasDestructor
+     procedure :: extract     => haloBiasExtract
+     procedure :: type        => haloBiasType
+     procedure :: name        => haloBiasName
+     procedure :: description => haloBiasDescription
+     procedure :: unitsInSI   => haloBiasUnitsInSI
+  end type nodePropertyExtractorHaloBias
+
+  interface nodePropertyExtractorHaloBias
+     !% Constructors for the ``haloBias'' output analysis class.
+     module procedure haloBiasConstructorParameters
+     module procedure haloBiasConstructorInternal
+  end interface nodePropertyExtractorHaloBias
+
+contains
+
+  function haloBiasConstructorParameters(parameters) result(self)
+    !% Constructor for the {\normalfont \ttfamily haloBias} node property extractor class which takes a parameter set as input.
+    use Input_Parameters
+    implicit none
+    type (nodePropertyExtractorHaloBias)                :: self
+    type (inputParameters              ), intent(inout) :: parameters
+    class(darkMatterHaloBiasClass      ), pointer       :: darkMatterHaloBias_
+
+    !# <objectBuilder class="darkMatterHaloBias" name="darkMatterHaloBias_" source="parameters"/>
+    self=nodePropertyExtractorHaloBias(darkMatterHaloBias_)
+    !# <inputParametersValidate source="parameters"/>
+    !# <objectDestructor name="darkMatterHaloBias_"/>
+    return
+  end function haloBiasConstructorParameters
+
+  function haloBiasConstructorInternal(darkMatterHaloBias_) result(self)
+    !% Internal constructor for the {\normalfont \ttfamily haloBias} node property extractor class.
+    implicit none
+    type (nodePropertyExtractorHaloBias)                        :: self
+    class(darkMatterHaloBiasClass      ), intent(in   ), target :: darkMatterHaloBias_
+    !# <constructorAssign variables="*darkMatterHaloBias_"/>
+
+    return
+  end function haloBiasConstructorInternal
+  
+  subroutine haloBiasDestructor(self)
+    !% Destructor for the {\normalfont \ttfamily haloBias} property extractor class.
+    implicit none
+    type(nodePropertyExtractorHaloBias), intent(inout) :: self
+
+    !# <objectDestructor name="self%darkMatterHaloBias_"/>
+    return
+  end subroutine haloBiasDestructor
+  
+  double precision function haloBiasExtract(self,node,instance)
+    !% Implement a {\normalfont \ttfamily haloBias} node property extractor.
+    implicit none
+    class           (nodePropertyExtractorHaloBias), intent(inout)           :: self
+    type            (treeNode                     ), intent(inout), target   :: node
+    type            (multiCounter                 ), intent(inout), optional :: instance
+    type            (treeNode                     )               , pointer  :: nodeIsolated
+    !GCC$ attributes unused :: instance
+
+    nodeIsolated => node
+    do while (nodeIsolated%isSatellite())
+       nodeIsolated => nodeIsolated%parent
+    end do
+    haloBiasExtract=self%darkMatterHaloBias_%bias(nodeIsolated)
+    return
+  end function haloBiasExtract
+
+  integer function haloBiasType(self)
+    !% Return the type of the stellar mass property.
+    use Output_Analyses_Options, only : outputAnalysisPropertyTypeLinear
+    implicit none
+    class(nodePropertyExtractorHaloBias), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    haloBiasType=outputAnalysisPropertyTypeLinear
+    return
+  end function haloBiasType
+
+  function haloBiasName(self)
+    !% Return the name of the haloBias property.
+    implicit none
+    type (varying_string               )                :: haloBiasName
+    class(nodePropertyExtractorHaloBias), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    haloBiasName=var_str('nodeBias')
+    return
+  end function haloBiasName
+
+  function haloBiasDescription(self)
+    !% Return a description of the haloBias property.
+    implicit none
+    type (varying_string               )                :: haloBiasDescription
+    class(nodePropertyExtractorHaloBias), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    haloBiasDescription=var_str('The linear bias for this node.')
+    return
+  end function haloBiasDescription
+
+  double precision function haloBiasUnitsInSI(self)
+    !% Return the units of the {\normalfont \ttfamily haloBias} property in the SI system.
+    implicit none
+    class(nodePropertyExtractorHaloBias), intent(inout) :: self
+    !GCC$ attributes unused :: self
+
+    haloBiasUnitsInSI=0.0d0
+    return
+  end function haloBiasUnitsInSI
