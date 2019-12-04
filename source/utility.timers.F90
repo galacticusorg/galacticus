dfcb005779840c3626202e25a79994e89e52e5ee
Andrew Benson <abenson@obs.carnegiescience.edu>
2019-12-04 11:12:02 -0800

    * Add a class that provides simple CPU timers.

---
+++ source/utility.timers.F90
@@ -0,0 +1,125 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!% Contains a module which implements timers.
+
+module Timers
+  !% Implements timers.
+  implicit none
+  private
+  public :: timer
+
+  type :: timer
+     !% Type used to perform timing.
+     real :: timeStart, timeStop
+   contains
+     !@ <objectMethods>
+     !@   <object>timer</object>
+     !@   <objectMethod>
+     !@     <method>start</method>
+     !@     <arguments></arguments>
+     !@     <type>\void</type>
+     !@     <description>Start the timer.</description>
+     !@   </objectMethod>
+     !@   <objectMethod>
+     !@     <method>stop</method>
+     !@     <arguments></arguments>
+     !@     <type>\void</type>
+     !@     <description>Stop the timer.</description>
+     !@   </objectMethod>
+     !@   <objectMethod>
+     !@     <method>report</method>
+     !@     <arguments></arguments>
+     !@     <type>\doublezero</type>
+     !@     <description>Report the time recorded as a double precision value.</description>
+     !@   </objectMethod>
+     !@   <objectMethod>
+     !@     <method>reportText</method>
+     !@     <arguments></arguments>
+     !@     <type>\textcolor{red}{\textless character(len=12)\textgreater}</type>
+     !@     <description>Report the time recorded as a character value.</description>
+     !@   </objectMethod>
+     !@ </objectMethods>
+     procedure :: start      => timerStart
+     procedure :: stop       => timerStop
+     procedure :: report     => timerReport
+     procedure :: reportText => timerReportText
+  end type timer
+
+  interface timer
+     !% Constructors for the {\normalfont \ttfamily timer} class.
+     module procedure timerConstructorInternal
+  end interface timer
+  
+contains
+
+  function timerConstructorInternal() result(self)
+    !% Constructor for the {\normalfont \ttfamily timer} class.
+    type(timer) :: self
+
+    self%timeStart=-huge(0.0)
+    self%timeStop =-huge(0.0)
+    return
+  end function timerConstructorInternal
+
+  subroutine timerStart(self)
+    !% Start the timer.
+    implicit none
+    class(timer), intent(inout) :: self
+    
+    call CPU_Time(self%timeStart)
+    return
+  end subroutine timerStart
+  
+  subroutine timerStop(self)
+    !% Stop the timer.
+    implicit none
+    class(timer), intent(inout) :: self
+    
+    call CPU_Time(self%timeStop)
+    return
+  end subroutine timerStop
+
+  double precision function timerReport(self)
+    !% Report the time recorded by the time as a double precision value.
+    implicit none
+    class(timer), intent(inout) :: self
+
+    timerReport=+self%timeStop  &
+         &      -self%timeStart
+    return
+  end function timerReport
+  
+  function timerReportText(self)
+    !% Report the time recorded by the time as a text value.
+    implicit none
+    character       (len=14)                :: timerReportText
+    class           (timer ), intent(inout) :: self
+    double precision                        :: time
+
+    time=self%report()
+    if (time > 1000.0d0 .or. time < 0.1d0) then
+       write (timerReportText,'(e12.6,1x,a1)') time,'s'
+    else
+       write (timerReportText,'(f12.6,1x,a1)') time,'s'
+    end if
+    return
+  end function timerReportText
+
+end module Timers
