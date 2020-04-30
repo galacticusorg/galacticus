0133f95786cbb3008f4b6532243e3344bf214251
Andrew Benson <abenson@obs.carnegiescience.edu>
2020-04-30 16:38:32 -0700

    * Add functionality to allow extraction of GSLK integer constant at   compile time.

--- perl/Galacticus/Build/SourceTree/Process/GSLConstants.pm
+++ perl/Galacticus/Build/SourceTree/Process/GSLConstants.pm
@@ -19,27 +19,34 @@ sub Process_GSLConstant {
     my $depth = 0;
     while ( $node ) {
 	if ( $node->{'type'} eq "gslConstant" && ! $node->{'directive'}->{'processed'} ) {
-	    # Generate, compile, and execute a minimal C code which simply outputs the relevant GSL constant. We use a macro to
-	    # define the precision with which the constant should be output to preserve full precision (see
-	    # https://stackoverflow.com/a/19897395).
+	    # Generate, compile, and execute a minimal C code which simply outputs the relevant GSL constant.
+	    my $type = exists($node->{'directive'}->{'type'}) ? $node->{'directive'}->{'type'} : "double precision";
+	    # For double precision constants we use a macro to define the precision with which the constant should be output to
+	    # preserve full precision (see https://stackoverflow.com/a/19897395).
 	    (undef, my $tmpSourceFileName) = tempfile('tempXXXXX', SUFFIX => '.c', OPEN => 0, DIR => $ENV{'BUILDPATH'});
 	    (undef, my $tmpExecFileName  ) = tempfile('tempXXXXX', SUFFIX => '.x', OPEN => 0, DIR => $ENV{'BUILDPATH'});
 	    open(my $tmpSource,">".$tmpSourceFileName);
 	    print $tmpSource "\#include <stdio.h>\n";
 	    print $tmpSource "\#include <float.h>\n";
 	    print $tmpSource "\#include <gsl/".$node->{'directive'}->{'gslHeader'}.".h>\n";
-	    print $tmpSource "\#ifdef LDBL_DECIMAL_DIG\n";
-	    print $tmpSource "  \#define OP_LDBL_Digs (LDBL_DECIMAL_DIG)\n";
-	    print $tmpSource "\#else  \n";
-	    print $tmpSource "  \#ifdef DECIMAL_DIG\n";
-	    print $tmpSource "    \#define OP_LDBL_Digs (DECIMAL_DIG)\n";
-	    print $tmpSource "  \#else  \n";
-	    print $tmpSource "    \#define OP_LDBL_Digs (LDBL_DIG + 3)\n";
-	    print $tmpSource "  \#endif\n";
-	    print $tmpSource "\#endif\n";
-	    print $tmpSource "int main () {\n";
-	    print $tmpSource " printf(\"%.*e\", OP_LDBL_Digs - 1, ".$node->{'directive'}->{'gslSymbol'}.");\n";
-	    print $tmpSource "}\n";
+	    if ( $type eq "double precision" ) {
+		print $tmpSource "\#ifdef LDBL_DECIMAL_DIG\n";
+		print $tmpSource "  \#define OP_LDBL_Digs (LDBL_DECIMAL_DIG)\n";
+		print $tmpSource "\#else  \n";
+		print $tmpSource "  \#ifdef DECIMAL_DIG\n";
+		print $tmpSource "    \#define OP_LDBL_Digs (DECIMAL_DIG)\n";
+		print $tmpSource "  \#else  \n";
+		print $tmpSource "    \#define OP_LDBL_Digs (LDBL_DIG + 3)\n";
+		print $tmpSource "  \#endif\n";
+		print $tmpSource "\#endif\n";
+		print $tmpSource "int main () {\n";
+		print $tmpSource " printf(\"%.*e\", OP_LDBL_Digs - 1, ".$node->{'directive'}->{'gslSymbol'}.");\n";
+		print $tmpSource "}\n";
+	    } elsif ( $type eq "integer" ) {
+		print $tmpSource "int main () {\n";
+		print $tmpSource " printf(\"%i\", ".$node->{'directive'}->{'gslSymbol'}.");\n";
+		print $tmpSource "}\n";	
+	    }
 	    close($tmpSource);
 	    system($ENV{'CCOMPILER'}." -o ".$tmpExecFileName." ".$tmpSourceFileName." ".$ENV{'CFLAGS'});
 	    die("Galacticus::Build::SourceTree::Process::GSLConstants: failed to compile")
@@ -49,7 +56,7 @@ sub Process_GSLConstant {
 	    close($gslPipe);
 	    unlink($tmpSourceFileName,$tmpExecFileName);
 	    $gslConstant =~ s/e/d/;
-	    my $code = "double precision, parameter, public :: ".$node->{'directive'}->{'variable'}."=".$gslConstant."\n";
+	    my $code = $type.", parameter, public :: ".$node->{'directive'}->{'variable'}."=".$gslConstant."\n";
 	    my $newNode =
 	    {
 		type       => "code",
