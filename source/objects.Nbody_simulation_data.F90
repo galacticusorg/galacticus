ad005b82c0a789afa4689219d083e14b3cfe1c8d
Andrew Benson <abenson@obs.carnegiescience.edu>
2020-05-01 14:44:18 -0700

    * Add integer and real property hashes to the N-body data object.
    * Provide a function returning types (integer/real) of named properties.

--- source/objects.Nbody_simulation_data.F90
+++ source/objects.Nbody_simulation_data.F90
@@ -23,24 +23,36 @@ module NBody_Simulation_Data
   !% Provides a class to store N-body simulation data.
   use :: IO_HDF5     , only : hdf5Object
   use :: Kind_Numbers, only : kind_int8
+  use :: Hashes      , only : rank1IntegerSizeTHash, rank1DoubleHash
   implicit none
   private
-  public :: nBodyData
+  public :: nBodyData, nBodyDataPropertyType
 
   type :: nBodyData
      !% A class to store N-body simulation data.
-     type            (hdf5Object)                              :: analysis
-     double precision            , allocatable, dimension(:,:) :: position           , velocity
-     integer                     , allocatable, dimension(  :) :: identifier
-     double precision                                          :: lengthSoftening    , massParticle
-     integer(kind=kind_int8)     , allocatable, dimension(  :) :: particleIDs        , particleIDsPrevious
-     integer                     , allocatable, dimension(:,:) :: boundStatusPrevious, sampleWeightPrevious
+     type            (hdf5Object           )                              :: analysis
+     double precision                       , allocatable, dimension(:,:) :: position           , velocity
+     integer                                , allocatable, dimension(  :) :: identifier
+     double precision                                                     :: lengthSoftening    , massParticle
+     integer         (kind_int8            ), allocatable, dimension(  :) :: particleIDs        , particleIDsPrevious
+     integer                                , allocatable, dimension(:,:) :: boundStatusPrevious, sampleWeightPrevious
+     type            (rank1IntegerSizeTHash)                              :: propertiesInteger
+     type            (rank1DoubleHash      )                              :: propertiesReal
   end type nBodyData
 
   interface nBodyData
      module procedure nBodyDataConstructor
   end interface nBodyData
 
+  !# <enumeration>
+  !#  <name>propertyType</name>
+  !#  <description>Enumeration of property types for N-body data properties.</description>
+  !#  <visibility>public</visibility>
+  !#  <entry label="unknown"/>
+  !#  <entry label="integer"/>
+  !#  <entry label="real"   />
+  !# </enumeration>
+  
 contains
 
   function nBodyDataConstructor() result (self)
@@ -50,5 +62,39 @@ contains
 
     return
   end function nBodyDataConstructor
+
+  integer function nBodyDataPropertyType(propertyName)
+    !% Returns the type of the named property.
+    implicit none
+    character(len=*), intent(in   ) :: propertyName
+
+    select case (propertyName)
+    case('particleID'               )
+       nBodyDataPropertyType=propertyTypeInteger
+    case('descendentID'             )
+       nBodyDataPropertyType=propertyTypeInteger
+    case('progenitorCount'          )
+       nBodyDataPropertyType=propertyTypeInteger
+    case('hostID'                   )
+       nBodyDataPropertyType=propertyTypeInteger
+    case('hostRootID'               )
+       nBodyDataPropertyType=propertyTypeInteger
+    case('descendentHostID'         )
+       nBodyDataPropertyType=propertyTypeInteger
+    case('isPhantom'                )
+       nBodyDataPropertyType=propertyTypeInteger
+    case('alwaysIsolated'           )
+       nBodyDataPropertyType=propertyTypeInteger
+    case('expansionFactor'          )
+       nBodyDataPropertyType=propertyTypeReal
+    case('descendentExpansionFactor')
+       nBodyDataPropertyType=propertyTypeReal
+    case('massVirial'               )
+       nBodyDataPropertyType=propertyTypeReal
+    case default
+       nBodyDataPropertyType=propertyTypeUnknown
+    end select
+    return
+  end function nBodyDataPropertyType
 
 end module NBody_Simulation_Data
