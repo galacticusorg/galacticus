cdcf08f1bfc0d8d80221a9df29086408827ee369
Andrew Benson <abenson@obs.carnegiescience.edu>
2020-05-01 14:45:41 -0700

    * Add support for reaeding/writing halo IDs to the IRATE IO class.

--- source/utility.IO.IRATE.F90
+++ source/utility.IO.IRATE.F90
@@ -94,19 +94,21 @@ contains
     return
   end function irateConstructor
 
-  subroutine irateReadHalos(self,snapshot,redshift,center,velocity,mass)
+  subroutine irateReadHalos(self,snapshot,redshift,center,velocity,mass,IDs)
     !% Read requested properties of halos from an \gls{irate} file.
-    use :: Cosmology_Parameters            , only : hubbleUnitsLittleH
-    use :: IO_HDF5                         , only : hdf5Object
-    use :: ISO_Varying_String              , only : char              , trim
-    use :: Numerical_Constants_Astronomical, only : massSolar         , megaParsec
-    use :: Numerical_Constants_Prefixes    , only : hecto             , kilo
+    use, intrinsic :: ISO_C_Binding                   , only : c_size_t
+    use            :: Cosmology_Parameters            , only : hubbleUnitsLittleH
+    use            :: IO_HDF5                         , only : hdf5Object
+    use            :: ISO_Varying_String              , only : char              , trim
+    use            :: Numerical_Constants_Astronomical, only : massSolar         , megaParsec
+    use            :: Numerical_Constants_Prefixes    , only : hecto             , kilo
     implicit none
     class           (irate     ), intent(inout)                                        :: self
     integer                     , intent(in   )                                        :: snapshot
     double precision            , intent(  out)                             , optional :: redshift
     double precision            , intent(  out), allocatable, dimension(:,:), optional :: center          , velocity
     double precision            , intent(  out), allocatable, dimension(  :), optional :: mass
+    integer         (c_size_t  ), intent(  out), allocatable, dimension(  :), optional :: IDs
     double precision                           , allocatable, dimension(  :)           :: unitsInCGS
     type            (hdf5Object)                                                       :: irateFile       , snapshotGroup, &
          &                                                                                halosGroup      , thisDataset
@@ -121,6 +123,11 @@ contains
     call snapshotGroup%readAttribute("Redshift",redshiftInternal,allowPseudoScalar=.true.)
     expansionFactor=self%cosmologyFunctions_%expansionFactorFromRedshift(redshiftInternal)
     if (present(redshift)) redshift=redshiftInternal
+    if (present(IDs     )) then
+       thisDataset=halosGroup%openDataset("HaloID"  )
+       call thisDataset%readDataset(datasetValue=IDs    )
+       call thisDataset%close()
+    end if
     if (present(mass    )) then
        thisDataset=halosGroup%openDataset("Mass"    )
        call thisDataset%readDataset(datasetValue=mass   )
@@ -199,18 +206,20 @@ contains
     return
   end subroutine irateCopyCosmology
 
-  subroutine irateWriteHalos(self,snapshot,redshift,center,velocity,mass,overwrite,objectsOverwritable)
+  subroutine irateWriteHalos(self,snapshot,redshift,center,velocity,mass,IDs,overwrite,objectsOverwritable)
     !% Read requested properties of halos to an \gls{irate} file.
-    use :: IO_HDF5                         , only : hdf5Object
-    use :: ISO_Varying_String              , only : char
-    use :: Numerical_Constants_Astronomical, only : massSolar , megaParsec
-    use :: Numerical_Constants_Prefixes    , only : hecto     , kilo
+    use, intrinsic :: ISO_C_Binding                   , only : c_size_t
+    use            :: IO_HDF5                         , only : hdf5Object
+    use            :: ISO_Varying_String              , only : char
+    use            :: Numerical_Constants_Astronomical, only : massSolar , megaParsec
+    use            :: Numerical_Constants_Prefixes    , only : hecto     , kilo
     implicit none
     class           (irate     ), intent(inout)                           :: self
     integer                     , intent(in   )                           :: snapshot
     double precision            , intent(in   )                           :: redshift
     double precision            , intent(in   ), dimension(:,:), optional :: center       , velocity
     double precision            , intent(in   ), dimension(  :), optional :: mass
+    integer         (c_size_t  ), intent(in   ), dimension(  :), optional :: IDs
     logical                     , intent(in   )                , optional :: overwrite    , objectsOverwritable
     type            (hdf5Object)                                          :: irateFile    , snapshotGroup      , &
          &                                                                   halosGroup   , thisDataset
@@ -224,6 +233,9 @@ contains
     snapshotGroup=irateFile    %openGroup(snapshotLabel)
     halosGroup   =snapshotGroup%openGroup('HaloCatalog')
     call snapshotGroup%writeAttribute(redshift,"Redshift")
+    if (present(IDs     )) then
+       call halosGroup%writeDataset   (IDs                                   ,'HaloID'  ,'Halo IDs'                                          )
+    end if
     if (present(mass    )) then
        call halosGroup%writeDataset   (mass                                  ,'Mass'    ,'Halo masses'           ,datasetReturned=thisDataset)
        call thisDataset%writeAttribute('Msolar'                              ,'unitname'                                                     )
