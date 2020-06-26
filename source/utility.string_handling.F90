91a351e8e3aed1fb3016a28634dda4e56a9991bd
Andrew Benson <abenson@obs.carnegiescience.edu>
2020-06-26 13:39:01 -0700

    * Change C-to-Fortran string conversion function to properly handle   null-terminated strings.

--- source/utility.string_handling.F90
+++ source/utility.string_handling.F90
@@ -327,16 +327,18 @@ contains
 
   function String_C_to_Fortran(charArray)
     !% Convert a C-style character array into a Fortran varying string variable.
-    use, intrinsic :: ISO_C_Binding     , only : c_char
+    use, intrinsic :: ISO_C_Binding     , only : c_char, c_null_char
     use            :: ISO_Varying_String, only : varying_string, assignment(=), operator(//)
     implicit none
     type     (varying_string)                :: String_C_to_Fortran
-    character(kind=c_char   ), intent(in   ) :: charArray          (:)
+    character(kind=c_char   ), intent(in   ), dimension(*) :: charArray
     integer                                  :: i
 
     String_C_to_Fortran=""
-    do i=1,size(charArray)
+    i=1
+    do while (charArray(i) /= c_null_char)
        String_C_to_Fortran=String_C_to_Fortran//charArray(i)
+       i=i+1
     end do
     return
   end function String_C_to_Fortran
