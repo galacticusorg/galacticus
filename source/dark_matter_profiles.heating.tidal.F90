94229bcc4a90aeaf2ccefdb55071da64812a33d2
Andrew Benson <abenson@obs.carnegiescience.edu>
2020-09-17 11:05:42 -0700

    * Add memoization to the "tidal" dark matter halo heating class - use   this to memoize the normalized heating rate.

--- source/dark_matter_profiles.heating.tidal.F90
+++ source/dark_matter_profiles.heating.tidal.F90
@@ -19,22 +19,45 @@
 
   !% A dark matter halo profile heating class which accounts for heating from tidal shocking.
 
+  use :: Kind_Numbers, only : kind_int8
+
   !# <darkMatterProfileHeating name="darkMatterProfileHeatingTidal">
   !#  <description>A dark matter profile heating model which accounts for heating due to tidal shocking.</description>
   !# </darkMatterProfileHeating>
-
   type, extends(darkMatterProfileHeatingClass) :: darkMatterProfileHeatingTidal
      !% A dark matter profile heating class which accounts for heating due to tidal shocking.
      private
-   contains
-     procedure :: specificEnergy                 => tidalSpecificEnergy
-     procedure :: specificEnergyGradient         => tidalSpecificEnergyGradient
-     procedure :: specificEnergyIsEverywhereZero => tidalSpecificEnergyIsEverywhereZero
+    double precision            :: specificEnergyOverRadiusSquared_, specificEnergyOverRadiusSquaredParent_
+    integer         (kind_int8) :: lastUniqueID                    , parentUniqueID
+  contains
+     !@ <objectMethods>
+     !@   <object>darkMatterProfileHeatingTidal</object>
+     !@   <objectMethod>
+     !@     <method>calculationReset</method>
+     !@     <type>\void</type>
+     !@     <arguments>\textcolor{red}{\textless type(treeNode)\textgreater} node\arginout</arguments>
+     !@     <description>Reset memoized calculations.</description>
+     !@   </objectMethod>
+     !@   <objectMethod>
+     !@     <method>specificEnergyOverRadiusSquared</method>
+     !@     <type>\void</type>
+     !@     <arguments>\textcolor{red}{\textless type(treeNode)\textgreater} node\arginout</arguments>
+     !@     <description>Compute $Q = E / r^2$.</description>
+     !@   </objectMethod>
+     !@ </objectMethods>
+     final     ::                                    tidalDestructor
+     procedure :: autoHook                        => tidalAutoHook
+     procedure :: calculationReset                => tidalCalculationReset
+     procedure :: specificEnergy                  => tidalSpecificEnergy
+     procedure :: specificEnergyGradient          => tidalSpecificEnergyGradient
+     procedure :: specificEnergyIsEverywhereZero  => tidalSpecificEnergyIsEverywhereZero
+     procedure :: specificEnergyOverRadiusSquared => tidalSpecificEnergyOverRadiusSquared
   end type darkMatterProfileHeatingTidal
 
   interface darkMatterProfileHeatingTidal
      !% Constructors for the {\normalfont \ttfamily tidal} dark matter profile heating class.
      module procedure tidalConstructorParameters
+     module procedure tidalConstructorInternal
   end interface darkMatterProfileHeatingTidal
 
 contains
@@ -51,62 +74,127 @@ contains
     return
   end function tidalConstructorParameters
 
+  function tidalConstructorInternal() result(self)
+    !% Internal constructor for the {\normalfont \ttfamily tidal} dark matter profile heating scales class.
+    implicit none
+    type(darkMatterProfileHeatingTidal) :: self
+
+    self%specificEnergyOverRadiusSquared_      =-1.0d0
+    self%specificEnergyOverRadiusSquaredParent_=-1.0d0
+    self%lastUniqueID                          =-1_kind_int8
+    self%parentUniqueID                        =-1_kind_int8
+    return
+  end function tidalConstructorInternal
+
+  subroutine tidalAutoHook(self)
+    !% Attach to the calculation reset event.
+    use :: Events_Hooks, only : calculationResetEvent, openMPThreadBindingAllLevels
+    implicit none
+    class(darkMatterProfileHeatingTidal), intent(inout) :: self
+
+    call calculationResetEvent%attach(self,tidalCalculationReset,openMPThreadBindingAllLevels)
+    return
+  end subroutine tidalAutoHook
+
+  subroutine tidalCalculationReset(self,node)
+    !% Reset the stored tidal radii.
+    implicit none
+    class(darkMatterProfileHeatingTidal), intent(inout) :: self
+    type (treeNode                     ), intent(inout) :: node
+
+    self   %specificEnergyOverRadiusSquared_      =-1.0d0
+    self   %specificEnergyOverRadiusSquaredParent_=-1.0d0
+    self   %lastUniqueID                          =node       %uniqueID()
+    if (associated(node%parent)) then
+       self%parentUniqueID                        =node%parent%uniqueID()
+    else
+       self%parentUniqueID                        =-1_kind_int8
+    end if
+    return
+  end subroutine tidalCalculationReset
+
+  subroutine tidalDestructor(self)
+    !% Destructor for the {\normalfont \ttfamily tidal} dark matter profile heating class.
+    use :: Events_Hooks, only : calculationResetEvent
+    implicit none
+    type(darkMatterProfileHeatingTidal), intent(inout) :: self
+
+    call calculationResetEvent%detach(self,tidalCalculationReset)
+    return
+  end subroutine tidalDestructor
+
   double precision function tidalSpecificEnergy(self,node,darkMatterProfileDMO_,radius)
     !% Returns the specific energy of heating in the given {\normalfont \ttfamily node}.
-    use :: Galacticus_Nodes, only : nodeComponentSatellite, treeNode
     implicit none
     class           (darkMatterProfileHeatingTidal), intent(inout) :: self
     type            (treeNode                     ), intent(inout) :: node
     class           (darkMatterProfileDMOClass    ), intent(inout) :: darkMatterProfileDMO_
     double precision                               , intent(in   ) :: radius
-    class           (nodeComponentSatellite       ), pointer       :: satellite
-    double precision                                               :: specificEnergyOverRadiusSquared
-    !$GLC attributes unused :: self, darkMatterProfileDMO_
+    !$GLC attributes unused :: darkMatterProfileDMO_
 
-    satellite                       =>      node     %satellite             ()
-    specificEnergyOverRadiusSquared =  max(                                     &
-         &                                 +0.0d0                             , &
-         &                                 +satellite%tidalHeatingNormalized()  &
-         &                                )
-    tidalSpecificEnergy             =      +specificEnergyOverRadiusSquared     &
-         &                                 *radius                      **2
+    tidalSpecificEnergy=+self%specificEnergyOverRadiusSquared(node)    &
+         &              *radius                                    **2
     return
   end function tidalSpecificEnergy
 
   double precision function tidalSpecificEnergyGradient(self,node,darkMatterProfileDMO_,radius)
     !% Returns the gradient of the specific energy of heating in the given {\normalfont \ttfamily node}.
-    use :: Galacticus_Nodes, only : nodeComponentSatellite, treeNode
     implicit none
     class           (darkMatterProfileHeatingTidal), intent(inout) :: self
     type            (treeNode                     ), intent(inout) :: node
     class           (darkMatterProfileDMOClass    ), intent(inout) :: darkMatterProfileDMO_
     double precision                               , intent(in   ) :: radius
-    class           (nodeComponentSatellite       ), pointer       :: satellite
-    double precision                                               :: specificEnergyOverRadiusSquared
-    !$GLC attributes unused :: self, darkMatterProfileDMO_
+    !$GLC attributes unused :: darkMatterProfileDMO_
 
-    satellite                       =>      node     %satellite             ()
-    specificEnergyOverRadiusSquared =  max(                                     &
-         &                                 +0.0d0                             , &
-         &                                 +satellite%tidalHeatingNormalized()  &
-         &                                )
-    tidalSpecificEnergyGradient     =      +2.0d0                               &
-         &                                 *specificEnergyOverRadiusSquared     &
-         &                                 *radius
+    tidalSpecificEnergyGradient=+2.0d0                                      &
+         &                      *self%specificEnergyOverRadiusSquared(node) &
+         &                      *radius
     return
   end function tidalSpecificEnergyGradient
 
   logical function tidalSpecificEnergyIsEverywhereZero(self,node,darkMatterProfileDMO_)
     !% Returns true if the specific energy is everywhere zero in the given {\normalfont \ttfamily node}.
-    use :: Galacticus_Nodes, only : nodeComponentSatellite, treeNode
     implicit none
     class(darkMatterProfileHeatingTidal), intent(inout) :: self
     type (treeNode                     ), intent(inout) :: node
     class(darkMatterProfileDMOClass    ), intent(inout) :: darkMatterProfileDMO_
-    class(nodeComponentSatellite       ), pointer       :: satellite
-    !$GLC attributes unused :: self, darkMatterProfileDMO_
+    !$GLC attributes unused :: darkMatterProfileDMO_
 
-    satellite                           => node     %satellite             ()
-    tidalSpecificEnergyIsEverywhereZero =  satellite%tidalHeatingNormalized() <= 0.0d0
+    tidalSpecificEnergyIsEverywhereZero=self%specificEnergyOverRadiusSquared(node) <= 0.0d0
     return
   end function tidalSpecificEnergyIsEverywhereZero
+
+  double precision function tidalSpecificEnergyOverRadiusSquared(self,node)
+    !% Compute $Q = E / r^2$.
+    use :: Galacticus_Nodes, only : nodeComponentSatellite, treeNode
+    implicit none
+    class(darkMatterProfileHeatingTidal), intent(inout) :: self
+    type (treeNode                     ), intent(inout) :: node
+    class(nodeComponentSatellite       ), pointer       :: satellite
+
+    if     (                                        &
+         &   node%uniqueID() /= self%parentUniqueID &
+         &  .and.                                   &
+         &   node%uniqueID() /= self%lastUniqueID   &
+         & ) call self%calculationReset(node)
+    if (node%uniqueID() == self%parentUniqueID) then
+       if (self%specificEnergyOverRadiusSquaredParent_ < 0.0d0) then
+          satellite                                   =>      node     %satellite             ()
+          self%specificEnergyOverRadiusSquaredParent_ =  max(                                     &
+               &                                             +0.0d0                             , &
+               &                                             +satellite%tidalHeatingNormalized()  &
+               &                                            )
+       end if
+       tidalSpecificEnergyOverRadiusSquared=self%specificEnergyOverRadiusSquaredParent_
+    else
+       if (self%specificEnergyOverRadiusSquared_       < 0.0d0) then
+          satellite                                   =>      node     %satellite             ()
+          self%specificEnergyOverRadiusSquared_       =  max(                                     &
+               &                                             +0.0d0                             , &
+               &                                             +satellite%tidalHeatingNormalized()  &
+               &                                            )
+       end if
+       tidalSpecificEnergyOverRadiusSquared=self%specificEnergyOverRadiusSquared_
+    end if
+    return
+  end function tidalSpecificEnergyOverRadiusSquared
