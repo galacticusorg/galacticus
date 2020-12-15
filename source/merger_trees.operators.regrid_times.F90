028ca57d289cbc0063f75bff6280e29c28180bc0
Andrew Benson <abenson@obs.carnegiescience.edu>
2020-12-15 11:37:43 -0800

    * Modify the "regridTimes" merger tree operator to draw the list of   times at which to regrid from an outputTimes object.

--- source/merger_trees.operators.regrid_times.F90
+++ source/merger_trees.operators.regrid_times.F90
@@ -19,45 +19,27 @@
 
   !% Contains a module which implements a merger tree operator which restructures the tree onto a fixed grid of timesteps.
 
-  use :: Cosmological_Density_Field, only : criticalOverdensityClass
-  use :: Cosmology_Functions       , only : cosmologyFunctionsClass
-  use :: Linear_Growth             , only : linearGrowthClass
+  use :: Output_Times, only : outputTimesClass
 
   !# <mergerTreeOperator name="mergerTreeOperatorRegridTimes">
   !#  <description>
   !#   A merger tree operator class which will interpolate the merger tree structure onto a new array of timesteps. The timestep
-  !#   array is specified via the parameters:
-  !#   \begin{description}
-  !#   \item[{\normalfont \ttfamily [snapshotSpacing]}] The spacing of the timesteps. Five options are available: {\normalfont
-  !#   \ttfamily linear} will space timesteps uniformly in expansion factor, {\normalfont \ttfamily logarithmic} will space
-  !#   timesteps uniformly in the logarithm of expansion factor, {\normalfont \ttfamily logCriticalDensity} will space timesteps
-  !#   uniformly in the logarithm of critical density, $\delta_\mathrm{c}$, {\normalfont \ttfamily millennium} will use times
-  !#   corresponding to the redshifts of snapshots in the Millennium Simulation database, while {\normalfont \ttfamily read} will
-  !#   use times corresponding to the redshifts specified in the {\normalfont \ttfamily mergerTreeRegridRedshifts} parameter;
-  !#   \item[{\normalfont \ttfamily [expansionFactorStart]}] The smallest expansion factor in the array (ignored for {\normalfont
-  !#   \ttfamily millennium} and {\normalfont \ttfamily read} spacings);
-  !#   \item[{\normalfont \ttfamily [expansionFactorEnd]}] The largest expansion factor in the array (ignored for {\normalfont
-  !#   \ttfamily millennium} and {\normalfont \ttfamily read} spacings);
-  !#   \item[{\normalfont \ttfamily [regridCount]}] The number of timesteps in the array;
-  !#   \end{description}
-  !#   Along each branch of the tree, new halos are inserted at times corresponding to the times in the resulting array. The
-  !#   masses of these nodes are linearly interpolated between the existing nodes on the branch. Once these new nodes have been
-  !#   added, all other nodes are removed from the tree\footnote{The base node of the tree is never removed, even if it does not
-  !#   lie on one of the times in the constructed array.} The processing is useful to construct representations of trees as they
-  !#   would be if only sparse time sampling were available. As such, it is useful for exploring how the number of snapshots in
-  !#   merger trees extracted from N-body simulations\index{merger tree!N-body} affects the properties of galaxies that form in
-  !#   them.
+  !#   array is specified via an \refClass{outputTimesClass} object. Along each branch of the tree, new halos are inserted at
+  !#   times corresponding to the times in the resulting array. The masses of these nodes are linearly interpolated between the
+  !#   existing nodes on the branch. Once these new nodes have been added, all other nodes are removed from the tree\footnote{The
+  !#   base node of the tree is never removed, even if it does not lie on one of the times in the constructed array.} The
+  !#   processing is useful to construct representations of trees as they would be if only sparse time sampling were available. As
+  !#   such, it is useful for exploring how the number of snapshots in merger trees extracted from N-body simulations\index{merger
+  !#   tree!N-body} affects the properties of galaxies that form in them.
   !#  </description>
   !# </mergerTreeOperator>
   type, extends(mergerTreeOperatorClass) :: mergerTreeOperatorRegridTimes
      !% A merger tree operator class which restructures the tree onto a fixed grid of timesteps.
      private
-     class           (cosmologyFunctionsClass ), pointer                   :: cosmologyFunctions_  => null()
-     class           (criticalOverdensityClass), pointer                   :: criticalOverdensity_ => null()
-     class           (linearGrowthClass       ), pointer                   :: linearGrowth_        => null()
-     logical                                                               :: dumpTrees
-     double precision                                                      :: snapTolerance
-     double precision                          , allocatable, dimension(:) :: timeGrid
+     class           (outputTimesClass), pointer                   :: outputTimes_  => null()
+     logical                                                       :: dumpTrees
+     double precision                                              :: snapTolerance
+     double precision                  , allocatable, dimension(:) :: timeGrid
    contains
      final     ::                        regridTimesDestructor
      procedure :: operatePreEvolution => regridTimesOperatePreEvolution
@@ -69,180 +51,42 @@
      module procedure regridTimesConstructorInternal
   end interface mergerTreeOperatorRegridTimes
 
-  ! Enumeration for snapshot spacing.
-  !# <enumeration>
-  !#  <name>snapshotSpacing</name>
-  !#  <description>Specifies the spacing of snapshots when regridding merger trees.</description>
-  !#  <encodeFunction>yes</encodeFunction>
-  !#  <entry label="linear"                 />
-  !#  <entry label="logarithmic"            />
-  !#  <entry label="logCriticalOverdensity" />
-  !#  <entry label="millennium"             />
-  !#  <entry label="list"                   />
-  !# </enumeration>
-
 contains
 
   function regridTimesConstructorParameters(parameters) result(self)
     !% Constructor for the regrid times merger tree operator class which takes a parameter set as input.
     use :: Galacticus_Error, only : Galacticus_Error_Report
     implicit none
-    type            (mergerTreeOperatorRegridTimes)                              :: self
-    type            (inputParameters              ), intent(inout)               :: parameters
-    class           (cosmologyFunctionsClass      ), pointer                     :: cosmologyFunctions_
-    class           (criticalOverdensityClass     ), pointer                     :: criticalOverdensity_
-    class           (linearGrowthClass            ), pointer                     :: linearGrowth_
-    double precision                               , allocatable  , dimension(:) :: snapshotTimes
-    logical                                                                      :: dumpTrees
-    integer                                                                      :: regridCount          , snapshotSpacing   , &
-         &                                                                          iTime
-    double precision                                                             :: expansionFactorStart , expansionFactorEnd, &
-         &                                                                          snapTolerance
-    type            (varying_string               )                              :: snapshotSpacingText
+    type            (mergerTreeOperatorRegridTimes)                :: self
+    type            (inputParameters              ), intent(inout) :: parameters
+    class           (outputTimesClass             ), pointer       :: outputTimes_
+    logical                                                        :: dumpTrees
+    double precision                                               :: snapTolerance
 
-    !# <objectBuilder class="cosmologyFunctions"  name="cosmologyFunctions_"  source="parameters"/>
-    !# <objectBuilder class="criticalOverdensity" name="criticalOverdensity_" source="parameters"/>
-    !# <objectBuilder class="linearGrowth"        name="linearGrowth_"        source="parameters"/>
-    !# <inputParameter>
-    !#   <name>dumpTrees</name>
-    !#   <source>parameters</source>
-    !#   <defaultValue>.false.</defaultValue>
-    !#   <description>Specifies whether or not to dump merger trees as they are regridded.</description>
-    !# </inputParameter>
-    !# <inputParameter>
-    !#   <name>snapTolerance</name>
-    !#   <source>parameters</source>
-    !#   <defaultValue>0.0d0</defaultValue>
-    !#   <description>The fractional tolerance used in deciding if a node should be snapped to a time on the grid.</description>
-    !# </inputParameter>
-    !# <inputParameter>
-    !#   <name>regridCount</name>
-    !#   <source>parameters</source>
-    !#   <defaultValue>100</defaultValue>
-    !#   <description>Number of points in time to use when regridding merger trees.</description>
-    !# </inputParameter>
-    !# <inputParameter>
-    !#   <name>expansionFactorStart</name>
-    !#   <source>parameters</source>
-    !#   <defaultValue>0.1d0</defaultValue>
-    !#   <description>Starting expansion factor to use when regridding merger trees.</description>
-    !# </inputParameter>
-    !# <inputParameter>
-    !#   <name>expansionFactorEnd</name>
-    !#   <source>parameters</source>
-    !#   <defaultValue>1.0d0</defaultValue>
-    !#   <description>Ending expansion factor to use when regridding merger trees.</description>
-    !# </inputParameter>
-    !# <inputParameter>
-    !#   <name>snapshotSpacing</name>
-    !#   <variable>snapshotSpacingText</variable>
-    !#   <source>parameters</source>
-    !#   <defaultValue>var_str('logarithmic')</defaultValue>
-    !#   <description>Type of spacing to use in merger tree regridding (linear or logarithmic).</description>
-    !# </inputParameter>
-    ! Find the spacing type to be used.
-    snapshotSpacing=enumerationSnapshotSpacingEncode(char(snapshotSpacingText),includesPrefix=.false.)
-    ! Read redshifts if necessary.
-    if (snapshotSpacing == snapshotSpacingList) then
-       allocate(snapshotTimes(parameters%count('snapshotRedshifts')))
-       if (size(snapshotTimes) /= regridCount) call Galacticus_Error_Report('mismatch between [regridCount] and size of [snapshotRedshifts]'//{introspection:location})
-       !# <inputParameter>
-       !#   <name>snapshotRedshifts</name>
-       !#   <variable>snapshotTimes</variable>
-       !#   <source>parameters</source>
-       !#   <description>The redshifts at which merger trees are to regridded when the {\normalfont \ttfamily [snapshotSpacing]}$=${\normalfont \ttfamily list} option is selected.</description>
-       !# </inputParameter>
-       do iTime=1,size(snapshotTimes)
-          snapshotTimes(iTime)=cosmologyFunctions_%cosmicTime(cosmologyFunctions_%expansionFactorFromRedshift(snapshotTimes(iTime)))
-       end do
-    end if
-    ! Build the instance.
-    self=mergerTreeOperatorRegridTimes(snapTolerance,regridCount,expansionFactorStart,expansionFactorEnd,snapshotSpacing,dumpTrees,snapshotTimes,cosmologyFunctions_,criticalOverdensity_,linearGrowth_)
+    !# <objectBuilder class="outputTimes" name="outputTimes_" source="parameters"/>
+    self=mergerTreeOperatorRegridTimes(snapTolerance,dumpTrees,outputTimes_)
     !# <inputParametersValidate source="parameters"/>
-    !# <objectDestructor name="cosmologyFunctions_" />
-    !# <objectDestructor name="criticalOverdensity_"/>
-    !# <objectDestructor name="linearGrowth_"       />
+    !# <objectDestructor name="outputTimes_"/>
     return
   end function regridTimesConstructorParameters
 
-  function regridTimesConstructorInternal(snapTolerance,regridCount,expansionFactorStart,expansionFactorEnd,snapshotSpacing,dumpTrees,snapshotTimes,cosmologyFunctions_,criticalOverdensity_,linearGrowth_) result(self)
+  function regridTimesConstructorInternal(snapTolerance,dumpTrees,outputTimes_) result(self)
     !% Internal constructor for the regrid times merger tree operator class.
     use :: Galacticus_Error , only : Galacticus_Error_Report
-    use :: Memory_Management, only : allocateArray
-    use :: Numerical_Ranges , only : Make_Range             , rangeTypeLinear, rangeTypeLogarithmic
-    use :: Sorting          , only : sort
     implicit none
-    type            (mergerTreeOperatorRegridTimes)                                        :: self
-    integer                                        , intent(in   )                         :: regridCount
-    double precision                               , intent(in   )                         :: expansionFactorStart, expansionFactorEnd, &
-         &                                                                                    snapTolerance
-    integer                                        , intent(in   )                         :: snapshotSpacing
-    logical                                        , intent(in   )                         :: dumpTrees
-    double precision                               , intent(in   ), optional, dimension(:) :: snapshotTimes
-    class           (cosmologyFunctionsClass      ), intent(in   ), target                 :: cosmologyFunctions_
-    class           (criticalOverdensityClass     ), intent(in   ), target                 :: criticalOverdensity_
-    class           (linearGrowthClass            ), intent(in   ), target                 :: linearGrowth_
-    integer                                                                                :: iTime
-    !# <constructorAssign variables="dumpTrees, snapTolerance, *cosmologyFunctions_, *criticalOverdensity_, *linearGrowth_"/>
+    type            (mergerTreeOperatorRegridTimes)                        :: self
+    double precision                               , intent(in   )         :: snapTolerance
+    logical                                        , intent(in   )         :: dumpTrees
+    class           (outputTimesClass             ), intent(in   ), target :: outputTimes_
+    integer         (c_size_t                     )                        :: i
+    !# <constructorAssign variables="dumpTrees, snapTolerance, *outputTimes_"/>
 
     ! Validate arguments.
-    if (regridCount < 2) call Galacticus_Error_Report('regridCount > 2 is required'//{introspection:location})
-    ! Construct array of grid expansion factors.
-    call allocateArray(self%timeGrid,[regridCount])
-    select case (snapshotSpacing)
-    case (snapshotSpacingLinear                )
-       self%timeGrid=Make_Range(expansionFactorStart,expansionFactorEnd,regridCount,rangeTypeLinear     )
-       ! Convert expansion factors to time.
-       do iTime=1,regridCount
-          self%timeGrid(iTime)=self%cosmologyFunctions_%cosmicTime(self%timeGrid(iTime))
-       end do
-    case (snapshotSpacingLogarithmic           )
-       self%timeGrid=Make_Range(expansionFactorStart,expansionFactorEnd,regridCount,rangeTypeLogarithmic)
-       ! Convert expansion factors to time.
-       do iTime=1,regridCount
-          self%timeGrid(iTime)=self%cosmologyFunctions_%cosmicTime(self%timeGrid(iTime))
-       end do
-    case (snapshotSpacingLogCriticalOverdensity)
-       ! Build a logarithmic grid in critical overdensity.
-       self%timeGrid=Make_Range(                                                                                             &
-            &                   +self%criticalOverdensity_%value(self%cosmologyFunctions_%cosmicTime(expansionFactorStart))  &
-            &                   /self%linearGrowth_       %value(self%cosmologyFunctions_%cosmicTime(expansionFactorStart)), &
-            &                   +self%criticalOverdensity_%value(self%cosmologyFunctions_%cosmicTime(expansionFactorEnd  ))  &
-            &                   /self%linearGrowth_       %value(self%cosmologyFunctions_%cosmicTime(expansionFactorEnd  )), &
-            &                    regridCount                                                                               , &
-            &                    rangeTypeLogarithmic                                                                        &
-            &                  )
-       ! Convert critical overdensity to time.
-       do iTime=1,regridCount
-          self%timeGrid(iTime)=self%criticalOverdensity_%timeOfCollapse(self%timeGrid(iTime))
-       end do
-    case (snapshotSpacingMillennium            )
-       ! Use the timesteps used in the original Millennium Simulation as reported by Croton et al.  (2006; MNRAS; 365;
-       ! 11; http://adsabs.harvard.edu/abs/2006MNRAS.365...11C). Note that we specifically use the redshifts for these
-       ! snapshots as reported by the Millennium Database using query: "select z from Snapshots..MR".
-       ! Check for consistent number of timesteps.
-       if (regridCount /= 60) call Galacticus_Error_Report('"millennium" grid spacing requires exactly 60 timesteps'//{introspection:location})
-       ! Convert redshifts to time.
-       self%timeGrid=[                                                                         &
-            &         19.915688d0,18.243723d0,16.724525d0,15.343073d0,14.085914d0,12.940780d0, &
-            &         11.896569d0,10.943864d0,10.073462d0, 9.277915d0, 8.549912d0, 7.883204d0, &
-            &          7.272188d0, 6.711586d0, 6.196833d0, 5.723864d0, 5.288833d0, 4.888449d0, &
-            &          4.519556d0, 4.179469d0, 3.865683d0, 3.575905d0, 3.308098d0, 3.060419d0, &
-            &          2.831182d0, 2.618862d0, 2.422044d0, 2.239486d0, 2.070027d0, 1.912633d0, &
-            &          1.766336d0, 1.630271d0, 1.503636d0, 1.385718d0, 1.275846d0, 1.173417d0, &
-            &          1.077875d0, 0.988708d0, 0.905463d0, 0.827699d0, 0.755036d0, 0.687109d0, &
-            &          0.623590d0, 0.564177d0, 0.508591d0, 0.456577d0, 0.407899d0, 0.362340d0, &
-            &          0.319703d0, 0.279802d0, 0.242469d0, 0.207549d0, 0.174898d0, 0.144383d0, &
-            &          0.115883d0, 0.089288d0, 0.064493d0, 0.041403d0, 0.019933d0, 0.000000d0  &
-            &        ]
-       do iTime=1,regridCount
-          self%timeGrid(iTime)=self%cosmologyFunctions_%cosmicTime(self%cosmologyFunctions_%expansionFactorFromRedshift(self%timeGrid(iTime)))
-       end do
-    case (snapshotSpacingList                  )
-       if (.not.present(snapshotTimes)) call Galacticus_Error_Report('"list" grid spacing requires a list of snapshot times be supplied'//{introspection:location})
-       self%timeGrid=snapshotTimes
-       call sort(self%timeGrid)
-    end select
+    if (self%outputTimes_%count() < 2_c_size_t) call Galacticus_Error_Report('2 or more output times are required'//{introspection:location})
+    allocate(self%timeGrid(self%outputTimes_%count()))
+    do i=1,self%outputTimes_%count()
+       self%timeGrid(i)=self%outputTimes_%time(i)
+    end do
     return
   end function regridTimesConstructorInternal
 
@@ -251,9 +95,7 @@ contains
     implicit none
     type(mergerTreeOperatorRegridTimes), intent(inout) :: self
 
-    !# <objectDestructor name="self%linearGrowth_"       />
-    !# <objectDestructor name="self%criticalOverdensity_"/>
-    !# <objectDestructor name="self%cosmologyFunctions_" />
+    !# <objectDestructor name="self%outputTimes_"/>
     return
   end subroutine regridTimesDestructor
 
