004dcec628ad3e99309044056ea6a4d79d150d72
Andrew Benson <abenson@obs.carnegiescience.edu>
2021-01-04 10:26:00 -0800

    * Update copyright years.

--- source/dark_matter_halos.mass_accretion_history.Diemer2020.F90
+++ source/dark_matter_halos.mass_accretion_history.Diemer2020.F90
@@ -1,5 +1,5 @@
 !! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
-!!           2019, 2020
+!!           2019, 2020, 2021
 !!    Andrew Benson <abenson@carnegiescience.edu>
 !!
 !! This file is part of Galacticus.
@@ -16,7 +16,7 @@
 !!
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
-  
+
   !% An implementation of dark matter halo mass accretion histories computed using the fitting function of \cite{diemer_splashback_2020}.
 
   use :: Cosmology_Functions       , only : cosmologyFunctionsClass
--- source/nBody.operator.select_properties.F90
+++ source/nBody.operator.select_properties.F90
@@ -1,5 +1,5 @@
 !! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
-!!           2019, 2020
+!!           2019, 2020, 2021
 !!    Andrew Benson <abenson@carnegiescience.edu>
 !!
 !! This file is part of Galacticus.
@@ -16,7 +16,7 @@
 !!
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
-  
+
   !% Contains a module which implements an N-body data operator which selects particles matching a list of integer properties.
   
   !# <nbodyOperator name="nbodyOperatorSelectProperties">
