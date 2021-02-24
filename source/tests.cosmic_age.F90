8faca930e8b479bc6bc577885b74603de1eccb78
Andrew Benson <abenson@obs.carnegiescience.edu>
2021-02-24 11:53:17 -0800

    * Remove use of default objects in a test suite code.

--- source/tests.cosmic_age.F90
+++ source/tests.cosmic_age.F90
@@ -23,25 +23,21 @@ program Tests_Cosmic_Age
   use :: Cosmology_Functions     , only : cosmologyFunctions       , cosmologyFunctionsClass, cosmologyFunctionsMatterDarkEnergy, cosmologyFunctionsMatterLambda
   use :: Cosmology_Parameters    , only : cosmologyParametersSimple, hubbleUnitsTime
   use :: Display                 , only : displayVerbositySet      , verbosityLevelStandard
-  use :: ISO_Varying_String      , only : assignment(=)            , varying_string
-  use :: Input_Parameters        , only : inputParameters
   use :: Numerical_Constants_Math, only : Pi
   use :: Unit_Tests              , only : Assert                   , Unit_Tests_Begin_Group , Unit_Tests_End_Group              , Unit_Tests_Finish
   implicit none
   double precision                                    , dimension(8), parameter :: redshift                                      =[0.0000000000d+0,1.0000000000d+0,3.0000000000d+0,9.0000000000d+0,3.0000000000d+1,1.0000000000d+2,3.0000000000d+2,1.0000000000d+3]
-  double precision                                    , dimension(8), parameter :: ageEdS                                        =[0.6518682071d+0,0.2304700578d+0,0.0814833670d+0,0.0206137656d+0,0.0037766710d+0,0.0006421713d+0,0.0001248044d+0,0.0000205705d+0]
+  double precision                                    , dimension(8), parameter :: ageEdS                                        =[0.0651868207d+0,0.0230470058d+0,0.0081483367d+0,0.0020613766d+0,0.0003776671d+0,0.0000642171d+0,0.0000124804d+0,0.0000020571d+0]
   double precision                                    , dimension(8), parameter :: ageOpen                                       =[0.0790841462d+0,0.0327062977d+0,0.0128686687d+0,0.0035287732d+0,0.0006745641d+0,0.0001164482d+0,0.0000227374d+0,0.0000037552d+0]
   double precision                                    , dimension(8), parameter :: ageCosmologicalConstant                       =[0.0942699818d+0,0.0402619685d+0,0.0147878493d+0,0.0037621019d+0,0.0006895264d+0,0.0001172509d+0,0.0000227901d+0,0.0000037578d+0]
   double precision                                    , dimension(8), parameter :: ageClosed                                     =[3.4369560000d-2,8.6126540000d-3,2.7752830000d-3,6.7037040000d-4,1.2048780000d-4,2.0363050000d-5,3.9509380000d-6,6.5106720000d-7]
-  class           (cosmologyFunctionsClass           ), pointer                 :: cosmologyFunctions_
   type            (cosmologyParametersSimple         )                          :: cosmologyParametersClosed                                                                                                                                                       , cosmologyParametersCosmologicalConstant         , &
-       &                                                                           cosmologyParametersOpen
-  type            (cosmologyFunctionsMatterLambda    )                          :: cosmologyFunctionsCosmologicalConstant                                                                                                                                          , cosmologyFunctionsOpen
+       &                                                                           cosmologyParametersOpen                                                                                                                                                         , cosmologyParametersEdS
+  type            (cosmologyFunctionsMatterLambda    )                          :: cosmologyFunctionsCosmologicalConstant                                                                                                                                          , cosmologyFunctionsOpen                          , &
+       &                                                                           cosmologyFunctionsEdS
   type            (cosmologyFunctionsMatterDarkEnergy)                          :: cosmologyFunctionsDarkEnergyClosed                                                                                                                                              , cosmologyFunctionsDarkEnergyCosmologicalConstant, &
        &                                                                           cosmologyFunctionsDarkEnergyOmegaMinusOneThird
-  type            (varying_string                    )                          :: parameterFile
   character       (len=1024                          )                          :: message
-  type            (inputParameters                   )                          :: parameters
   integer                                                                       :: iExpansion
   double precision                                                              :: age                                                                                                                                                                             , expansionFactor                                 , &
        &                                                                           expansionFactorSymmetric                                                                                                                                                        , timeTurnaround
@@ -50,33 +46,108 @@ program Tests_Cosmic_Age
   call displayVerbositySet(verbosityLevelStandard)
   ! Begin unit tests.
   call Unit_Tests_Begin_Group("Cosmic age")
-  ! Cosmology functions for in an Einstein-de Sitter universe. For this case, we use the default settings.
-  parameterFile='testSuite/parameters/cosmicAge/EdS.xml'
-  parameters=inputParameters(parameterFile)
-  call parameters%markGlobal()
-  cosmologyFunctions_ => cosmologyFunctions()
-  ! Define other cosmological parameters.
-  cosmologyParametersOpen                =cosmologyParametersSimple( 0.3d0,0.0d0,0.0d0,2.78d0,10000.0d0)
-  cosmologyParametersCosmologicalConstant=cosmologyParametersSimple( 0.3d0,0.0d0,0.7d0,2.78d0,10000.0d0)
-  cosmologyParametersClosed              =cosmologyParametersSimple(10.0d0,0.0d0,0.0d0,2.78d0,10000.0d0)
-  ! Cosmology functions for an open Universe.
-  cosmologyFunctionsOpen                          =cosmologyFunctionsMatterLambda    (cosmologyParametersOpen                                   )
-  ! Cosmology functions for a cosmological constant Universe.
-  cosmologyFunctionsCosmologicalConstant          =cosmologyFunctionsMatterLambda    (cosmologyParametersCosmologicalConstant                   )
-  ! Cosmology functions for a cosmological constant Universe using dark energy method.
-  cosmologyFunctionsDarkEnergyCosmologicalConstant=cosmologyFunctionsMatterDarkEnergy(cosmologyParametersCosmologicalConstant,-1.0d0      ,0.0d0)
-  ! Cosmology functions for a closed Universe using dark energy method.
-  cosmologyFunctionsDarkEnergyClosed              =cosmologyFunctionsMatterDarkEnergy(cosmologyParametersClosed              , 0.0d0      ,0.0d0)
-  ! Cosmology functions for a closed Universe using dark energy method.
-  cosmologyFunctionsDarkEnergyOmegaMinusOneThird  =cosmologyFunctionsMatterDarkEnergy(cosmologyParametersCosmologicalConstant,-1.0d0/3.0d0,0.0d0)
+  ! Define cosmological parameters.
+  !# <referenceConstruct object="cosmologyParametersEdS"                 >
+  !#  <constructor>
+  !#   cosmologyParametersSimple(                             &amp;
+  !#    &amp;                    OmegaMatter         =1.00d0, &amp;
+  !#    &amp;                    OmegaBaryon         =0.00d0, &amp;
+  !#    &amp;                    OmegaDarkEnergy     =0.00d0, &amp;
+  !#    &amp;                    temperatureCMB      =2.78d0, &amp;
+  !#    &amp;                    HubbleConstant      =1.00d4  &amp;
+  !#    &amp;                   )
+  !#  </constructor>
+  !# </referenceConstruct>
+  !# <referenceConstruct object="cosmologyParametersOpen"                >
+  !#  <constructor>
+  !#   cosmologyParametersSimple(                             &amp;
+  !#    &amp;                    OmegaMatter         =0.30d0, &amp;
+  !#    &amp;                    OmegaBaryon         =0.00d0, &amp;
+  !#    &amp;                    OmegaDarkEnergy     =0.00d0, &amp;
+  !#    &amp;                    temperatureCMB      =2.78d0, &amp;
+  !#    &amp;                    HubbleConstant      =1.00d4  &amp;
+  !#    &amp;                   )
+  !#  </constructor>
+  !# </referenceConstruct>
+  !# <referenceConstruct object="cosmologyParametersClosed">
+  !#  <constructor>
+  !#   cosmologyParametersSimple(                             &amp;
+  !#    &amp;                    OmegaMatter         =1.00d1, &amp;
+  !#    &amp;                    OmegaBaryon         =0.00d0, &amp;
+  !#    &amp;                    OmegaDarkEnergy     =0.00d0, &amp;
+  !#    &amp;                    temperatureCMB      =2.78d0, &amp;
+  !#    &amp;                    HubbleConstant      =1.00d4  &amp;
+  !#    &amp;                   )
+  !#  </constructor>
+  !# </referenceConstruct>
+  !# <referenceConstruct object="cosmologyParametersCosmologicalConstant">
+  !#  <constructor>
+  !#   cosmologyParametersSimple(                             &amp;
+  !#    &amp;                    OmegaMatter         =0.30d0, &amp;
+  !#    &amp;                    OmegaBaryon         =0.00d0, &amp;
+  !#    &amp;                    OmegaDarkEnergy     =0.70d0, &amp;
+  !#    &amp;                    temperatureCMB      =2.78d0, &amp;
+  !#    &amp;                    HubbleConstant      =1.00d4  &amp;
+  !#    &amp;                   )
+  !#  </constructor>
+  !# </referenceConstruct>
+  ! Build cosmological function objects.
+  !# <referenceConstruct object="cosmologyFunctionsEdS"                           >
+  !#  <constructor>
+  !#   cosmologyFunctionsMatterLambda    (                                                                     &amp;
+  !#    &amp;                             cosmologyParameters_       =cosmologyParametersEdS                   &amp;
+  !#    &amp;                            )
+  !#  </constructor>
+  !# </referenceConstruct>
+  !# <referenceConstruct object="cosmologyFunctionsOpen"                          >
+  !#  <constructor>
+  !#   cosmologyFunctionsMatterLambda    (                                                                     &amp;
+  !#    &amp;                             cosmologyParameters_       =cosmologyParametersOpen                  &amp;
+  !#    &amp;                            )
+  !#  </constructor>
+  !# </referenceConstruct>
+  !# <referenceConstruct object="cosmologyFunctionsCosmologicalConstant"          >
+  !#  <constructor>
+  !#   cosmologyFunctionsMatterLambda    (                                                                     &amp;
+  !#    &amp;                             cosmologyParameters_       =cosmologyParametersCosmologicalConstant  &amp;
+  !#    &amp;                            )
+  !#  </constructor>
+  !# </referenceConstruct>
+  !# <referenceConstruct object="cosmologyFunctionsDarkEnergyClosed"              >
+  !#  <constructor>
+  !#   cosmologyFunctionsMatterDarkEnergy(                                                                     &amp;
+  !#    &amp;                             cosmologyParameters_       =cosmologyParametersClosed              , &amp;
+  !#    &amp;                             darkEnergyEquationOfStateW0=+0.0d0                                 , &amp;
+  !#    &amp;                             darkEnergyEquationOfStateW1=+0.0d0                                   &amp;
+  !#    &amp;                            )
+  !#  </constructor>
+  !# </referenceConstruct>
+  !# <referenceConstruct object="cosmologyFunctionsDarkEnergyCosmologicalConstant">
+  !#  <constructor>
+  !#   cosmologyFunctionsMatterDarkEnergy(                                                                     &amp;
+  !#    &amp;                             cosmologyParameters_       =cosmologyParametersCosmologicalConstant, &amp;
+  !#    &amp;                             darkEnergyEquationOfStateW0=-1.0d0                                 , &amp;
+  !#    &amp;                             darkEnergyEquationOfStateW1=+0.0d0                                   &amp;
+  !#    &amp;                            )
+  !#  </constructor>
+  !# </referenceConstruct>
+  !# <referenceConstruct object="cosmologyFunctionsDarkEnergyOmegaMinusOneThird"  >
+  !#  <constructor>
+  !#   cosmologyFunctionsMatterDarkEnergy(                                                                     &amp;
+  !#    &amp;                             cosmologyParameters_       =cosmologyParametersCosmologicalConstant, &amp;
+  !#    &amp;                             darkEnergyEquationOfStateW0=-1.0d0/3.0d0                           , &amp;
+  !#    &amp;                             darkEnergyEquationOfStateW1=+0.0d0                                   &amp;
+  !#    &amp;                            )
+  !#  </constructor>
+  !# </referenceConstruct>
   ! Compute the time of maximum expansion for the Universe. In this simple, OmegaM=10, OmegaDE=0 Universe this is analytically
   ! calculable and equals:
   timeTurnaround=(5.0d0/27.0d0)*Pi/cosmologyParametersClosed%HubbleConstant(hubbleUnitsTime)
   ! Evaluate ages for matter + cosmological constant universes.
   call Unit_Tests_Begin_Group("Matter + Cosmological Constant")
   do iExpansion=1,size(redshift)
-     expansionFactor=cosmologyFunctions_%expansionFactorFromRedshift(redshift       (iExpansion))
-     age            =cosmologyFunctions_%cosmicTime                 (expansionFactor            )
+     expansionFactor=cosmologyFunctionsEdS%expansionFactorFromRedshift(redshift       (iExpansion))
+     age            =cosmologyFunctionsEdS%cosmicTime                 (expansionFactor            )
      write (message,'(a,f6.1,a)') "cosmic age: EdS                       [z=",redshift(iExpansion),"]"
      call Assert(trim(message),age,ageEdS                 (iExpansion),relTol=1.0d-3)
      age=cosmologyFunctionsOpen                %cosmicTime(expansionFactor)
