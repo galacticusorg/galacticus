1e06231447bd3857931e8781cd26d187b92f9c5c
Andrew Benson <abenson@carnegiescience.edu>
2021-02-26 15:27:59 +0000

    * Fix LaTeX syntax error.

--- source/nodes.property_extractor.position_orbital.F90
+++ source/nodes.property_extractor.position_orbital.F90
@@ -23,7 +23,7 @@
   !#  <description>
   !#   An orbital position output analysis property extractor class. Specifically, the orbital position is defined relative to the
   !#   top-level halo in any sub-halo hierarchy. That is, relative to the host halo which is itself not a sub-halo of any other
-  !#   halo. If the position of a (sub)$^i$-halo with respect to the center of its (sub)^{i-1}-halo host is $\mathbf{x}_i$ then
+  !#   halo. If the position of a (sub)$^i$-halo with respect to the center of its (sub)$^{i-1}$-halo host is $\mathbf{x}_i$ then
   !#   the orbital potision computed by this class is
   !#   \begin{equation}
   !#    \mathbf{x} = sum_{i=1}^N \mathbf{x}_i,
