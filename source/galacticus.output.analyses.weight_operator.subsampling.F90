b0f9dfae63deaa63f42f26b06da0ed2455c15192
Andrew Benson <abenson@carnegiescience.edu>
2021-08-26 17:13:18 +0000

    feat: Add an analysis weight operator which multiplies by the node subsampling weight

---
+++ source/galacticus.output.analyses.weight_operator.subsampling.F90
@@ -0,0 +1,78 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019, 2020, 2021
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!!{
+Contains a module which implements a subsampling weight operator class.
+!!}
+
+  !![
+  <outputAnalysisWeightOperator name="outputAnalysisWeightOperatorSubsampling">
+   <description>A subsampling weight operator class.</description>
+  </outputAnalysisWeightOperator>
+  !!]
+  type, extends(outputAnalysisWeightOperatorClass) :: outputAnalysisWeightOperatorSubsampling
+     !!{
+     A subsampling weight operator class.
+     !!}
+     private
+   contains
+     procedure :: operate => subsamplingOperate
+  end type outputAnalysisWeightOperatorSubsampling
+
+  interface outputAnalysisWeightOperatorSubsampling
+     !!{
+     Constructors for the ``subsampling'' output analysis class.
+     !!}
+     module procedure subsamplingConstructorParameters
+  end interface outputAnalysisWeightOperatorSubsampling
+
+contains
+
+  function subsamplingConstructorParameters(parameters) result(self)
+    !!{
+    Constructor for the ``subsampling'' output analysis weight operator class which takes a parameter set as input.
+    !!}
+    use :: Input_Parameters, only : inputParameters
+    implicit none
+    type(outputAnalysisWeightOperatorSubsampling)                :: self
+    type(inputParameters                        ), intent(inout) :: parameters
+    !$GLC attributes unused :: parameters
+
+    ! Construct the object.
+    self=outputAnalysisWeightOperatorSubsampling()
+    return
+  end function subsamplingConstructorParameters
+
+  double precision function subsamplingOperate(self,weightValue,node,propertyValue,propertyValueIntrinsic,propertyType,propertyQuantity,outputIndex)
+    !!{
+    Implement an subsampling output analysis weight operator.
+    !!}
+    use, intrinsic :: ISO_C_Binding, only : c_size_t
+    implicit none
+    class           (outputAnalysisWeightOperatorSubsampling), intent(inout) :: self
+    type            (treeNode                               ), intent(inout) :: node
+    double precision                                         , intent(in   ) :: propertyValue, propertyValueIntrinsic, &
+         &                                                                      weightValue
+    integer                                                  , intent(in   ) :: propertyType , propertyQuantity
+    integer         (c_size_t                               ), intent(in   ) :: outputIndex
+    !$GLC attributes unused :: self, propertyValue, propertyValueIntrinsic, propertyType, propertyQuantity, outputIndex
+
+    subsamplingOperate=weightValue*node%subsamplingWeight()
+    return
+  end function subsamplingOperate
