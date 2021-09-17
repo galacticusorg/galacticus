7653c433849d8c09120b56431843011d4311ec48
Andrew Benson <abenson@carnegiescience.edu>
2021-09-17 09:14:53 -0700

    Dev (#196)
    * Ondaro-Mallea Halo Mass Function  (#195)
    
    * remove make.log
    
    * remove gal.log
    
    * remove parameters/new_k50_slope_test_ETHOS.xml
    
    Co-authored-by: David Gonzalez-Dysinger <dgonzalez-dysinger@mies.stanford.edu>
    
    * fix(style): Bring Ondaro-Mallea halo mass function class code into conformant style
    
    Co-authored-by: DavidGonzalezDysinger <66810407+DavidGonzalezDysinger@users.noreply.github.com>
    Co-authored-by: David Gonzalez-Dysinger <dgonzalez-dysinger@mies.stanford.edu>

---
+++ source/structure_formation.halo_mass_function.Ondaro-Mallea2021.F90
@@ -0,0 +1,187 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019, 2020
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+!!{
+Contains a module which implements the dark matter halo mass function class of \cite{2021arXiv210208958O} for non-universal
+primordial power spectra and structure growth rates.
+!!}
+  use :: Cosmological_Density_Field, only : cosmologicalMassVarianceClass
+  use :: Linear_Growth             , only : linearGrowthClass
+  
+  !![
+  <haloMassFunction name="haloMassFunctionOndaroMallea2021">
+    <description>
+      The dark matter halo mass function class of \cite{2021arXiv210208958O} for non-universal
+      primordial power spectra and structure growth rates. The mass function is given by
+      \begin{equation}
+      n(M) = n^\prime(M) f_2(n_\mathrm{eff}) f_3(\alpha_\mathrm{eff}),
+      \end{equation}
+      where $n^\prime(M)$ is some other mass function,
+      \begin{equation}
+      f_2(n_\mathrm{eff})=n_0 n_\mathrm{eff}^2 + n_1 n_\mathrm{eff} + n_0,
+      \end{equation}
+      and
+      \begin{equation}
+      f_3(\alpha_\mathrm{eff})=a_0 \alpha_\mathrm{eff}^2 + a_1.
+      \end{equation}
+      Here
+      \begin{equation}
+      n_\mathrm{eff} = -3 -2 \frac{\mathrm{d} \log \sigma(R)}{\mathrm{d} \log R} = -3 -6 \frac{\mathrm{d} \log \sigma(M)}{\mathrm{d} \log M},
+      \end{equation}
+      where $M$ is halo mass, and $\sigma(M)$ is the fractional root-variance in the linear theory cosmological density field on that scale, and
+      \begin{equation}
+      \alpha_\mathrm{eff}(a) = \frac{\mathrm{d} \log D}{\mathrm{d} \log a},
+      \end{equation}
+      where $D(a)$ is the linear growth factor and $a$ is the expansion factor.
+    </description>
+  </haloMassFunction>
+  !!]
+  type, extends(haloMassFunctionClass) :: haloMassFunctionOndaroMallea2021
+     !!{
+     The halo mass function class of \cite{2021arXiv210208958O} for non-universal
+     primordial power spectra and structure growth rates.
+     !!}
+     private
+     double precision                               , dimension(0:2) :: coefficientsN
+     double precision                               , dimension(0:1) :: coefficientsA
+     class           (cosmologicalMassVarianceClass), pointer        :: cosmologicalMassVariance_ => null()
+     class           (linearGrowthClass            ), pointer        :: linearGrowth_             => null()
+     class           (haloMassFunctionClass        ), pointer        :: haloMassFunction_         => null()
+    contains
+     final     ::                 ondaroMallea2021Destructor
+     procedure :: differential => ondaroMallea2021Differential
+  end type haloMassFunctionOndaroMallea2021
+
+  interface haloMassFunctionOndaroMallea2021
+     !!{
+     Constructors for the {\normalfont \ttfamily ondaroMallea2021} halo mass function class.
+     !!}
+     module procedure ondaroMallea2021ConstructorParameters
+     module procedure ondaroMallea2021ConstructorInternal
+  end interface haloMassFunctionOndaroMallea2021
+
+contains
+
+  function ondaroMallea2021ConstructorParameters(parameters) result(self)
+    !!{
+    Constructor for the {\normalfont \ttfamily ondaroMallea2021} halo mass function class which takes a parameter set as input.
+    !!}
+    use :: Input_Parameters, only : inputParameter, inputParameters
+    implicit none
+    type            (haloMassFunctionOndaroMallea2021)                 :: self
+    type            (inputParameters                 ), intent(inout)  :: parameters
+    class           (haloMassFunctionClass           ), pointer        :: haloMassFunction_
+    class           (cosmologicalMassVarianceClass   ), pointer        :: cosmologicalMassVariance_
+    class           (cosmologyParametersClass        ), pointer        :: cosmologyParameters_
+    class           (linearGrowthClass               ), pointer        :: linearGrowth_
+    double precision                                  , dimension(0:2) :: coefficientsN
+    double precision                                  , dimension(0:1) :: coefficientsA
+
+    !![
+    <objectBuilder class="cosmologyParameters"      name="cosmologyParameters_"      source="parameters"/>
+    <objectBuilder class="cosmologicalMassVariance" name="cosmologicalMassVariance_" source="parameters"/>
+    <objectBuilder class="linearGrowth"             name="linearGrowth_"             source="parameters"/>
+    <objectBuilder class="haloMassFunction"         name="haloMassFunction_"         source="parameters"/>
+    <inputParameter>
+      <name>coefficientsN</name>
+      <source>parameters</source>
+      <defaultValue>[-0.1178d0,-0.3389d0,0.3022d0]</defaultValue>
+      <defaultSource>\cite[][Table~3, row 4]{2021arXiv210208958O}</defaultSource>
+      <description>The coefficients, $n_{0\ldots2}$, appearing in equation~(7) of the \cite{2021arXiv210208958O} halo mass function model.</description>
+    </inputParameter>
+    <inputParameter>
+      <name>coefficientsA</name>
+      <source>parameters</source>
+      <defaultValue>[-1.0785d0,2.9700d0]</defaultValue>
+      <defaultSource>\cite[][Table~3, row 4]{2021arXiv210208958O}</defaultSource>
+      <description>The coefficients, $a_{0\ldots1}$, appearing in equation~(8) of the \cite{2021arXiv210208958O} halo mass function model.</description>
+    </inputParameter>
+    !!]
+    self=haloMassFunctionOndaroMallea2021(coefficientsN,coefficientsA,cosmologyParameters_,cosmologicalMassVariance_,linearGrowth_,haloMassFunction_)
+    !![
+    <inputParametersValidate source="parameters"/>
+    <objectDestructor name="cosmologyParameters_"     />
+    <objectDestructor name="cosmologicalMassVariance_"/>
+    <objectDestructor name="linearGrowth_"            />
+    <objectDestructor name="haloMassFunction_"        />
+    !!]
+    return
+  end function ondaroMallea2021ConstructorParameters
+
+  function ondaroMallea2021ConstructorInternal(coefficientsN,coefficientsA,cosmologyParameters_,cosmologicalMassVariance_,linearGrowth_,haloMassFunction_) result(self)
+    !!{
+    Internal constructor for the {\normalfont \ttfamily ondaroMallea2021} halo mass function class.
+    !!}
+    implicit none
+    type            (haloMassFunctionOndaroMallea2021)                                :: self
+    class           (cosmologyParametersClass        ), target        , intent(in   ) :: cosmologyParameters_
+    class           (cosmologicalMassVarianceClass   ), target        , intent(in   ) :: cosmologicalMassVariance_
+    class           (linearGrowthClass               ), target        , intent(in   ) :: linearGrowth_
+    class           (haloMassFunctionClass           ), target        , intent(in   ) :: haloMassFunction_
+    double precision                                  , dimension(0:2), intent(in   ) :: coefficientsN
+    double precision                                  , dimension(0:1), intent(in   ) :: coefficientsA
+    !![
+    <constructorAssign variables="coefficientsN, coefficientsA, *cosmologyParameters_, *cosmologicalMassVariance_, *linearGrowth_, *haloMassFunction_"/>
+    !!]
+
+    return
+  end function ondaroMallea2021ConstructorInternal
+
+  subroutine ondaroMallea2021Destructor(self)
+    !!{
+    Destructor for the {\normalfont \ttfamily ondaroMallea2021} halo mass function class.
+    !!}
+    implicit none
+    type(haloMassFunctionOndaroMallea2021), intent(inout) :: self
+    
+    !![
+    <objectDestructor name="self%cosmologyParameters_"     />
+    <objectDestructor name="self%cosmologicalMassVariance_"/>
+    <objectDestructor name="self%haloMassFunction_"        />
+    <objectDestructor name="self%linearGrowth_"            />
+    !!]
+    return
+  end subroutine ondaroMallea2021Destructor
+
+  double precision function ondaroMallea2021Differential(self,time,mass,node)
+    !!{
+    Return the differential halo mass function at the given time and mass.
+    !!}
+    implicit none
+    class           (haloMassFunctionOndaroMallea2021), intent(inout)           :: self
+    double precision                                  , intent(in   )           :: time                       , mass
+    type            (treeNode                        ), intent(inout), optional :: node
+    double precision                                                            :: slopeEffectivePowerSpectrum, rateGrowthLinear, &
+         &                                                                         factorPowerSpectrum        , factorGrowth
+
+    ondaroMallea2021Differential=+self%haloMassFunction_        %differential                        (time,mass,node)
+    rateGrowthLinear            =+self%linearGrowth_            %logarithmicDerivativeExpansionFactor(time          )
+    slopeEffectivePowerSpectrum =-3.0d0                                                                                                              &
+         &                       -6.0d0                                                                                                              &
+         &                       *self%cosmologicalMassVariance_%rootVarianceLogarithmicGradient     (mass,time     )
+    factorPowerSpectrum         =+self                          %coefficientsN                       (0             )*slopeEffectivePowerSpectrum**2 &
+    &                            +self                          %coefficientsN                       (1             )*slopeEffectivePowerSpectrum    &
+    &                            +self                          %coefficientsN                       (2             )
+    factorGrowth                =+self                          %coefficientsA                       (0             )*rateGrowthLinear               &
+         &                       +self                          %coefficientsA                       (1             )
+    ondaroMallea2021Differential=+ondaroMallea2021Differential                                                                                       &
+            &                    *factorPowerSpectrum                                                                                                &
+            &                    *factorGrowth     
+    return
+  end function ondaroMallea2021Differential
