b2dd0bb6aa3b236b6db8e9a1edaa4a2d9fca6f32
Andrew Benson <abenson@obs.carnegiescience.edu>
2021-10-12 08:47:28 -0700

    fix: Make removal of non-ASCII characters from bibliography more comprehensive

--- doc/Bibliography_Demangle.pl
+++ doc/Bibliography_Demangle.pl
@@ -15,6 +15,11 @@ while ( my $line = <iHndl> ) {
     # zwaan_hipass_2005 title
     $line =~ s/\{\\\$\{\\textbackslash\}Omega\\\$\\_\{\{\\textbackslash\}rm\} \{HI\}\}/\$\\Omega_{\\rm HI}\$/;
 
+    # Unknown symbols.
+    $line =~ s/�/ /g;
+    $line =~ s/�/ /g;
+    $line =~ s/�/ /g;
+
     # Percentages.
     $line =~ s/[^\\]%/\\%/g;
 
@@ -26,21 +31,95 @@ while ( my $line = <iHndl> ) {
     # Tildes.
     $line =~ s/{\\textasciitilde}/\$\\sim\$/g;
 
+    # Greek letters.
+    $line =~ s/α/\$\\alpha\$/g;
+    $line =~ s/β/\$\\beta\$/g;
+    $line =~ s/γ/\$\\gamma\$/g;
+    $line =~ s/δ/\$\\delta\$/g;
+    $line =~ s/ɛ/\$\\epsilon\$/g;
+    $line =~ s/ϵ/\$\\epsilon\$/g;
+    $line =~ s/ε/\$\\epsilon\$/g;
+    $line =~ s/ζ/\$\\zeta\$/g;
+    $line =~ s/η/\$\\eta\$/g;
+    $line =~ s/θ/\$\\theta\$/g;
+    $line =~ s/ϑ/\$\\theta\$/g;
+    $line =~ s/ι/\$\\iota\$/g;
+    $line =~ s/κ/\$\\kappa\$/g;
+    $line =~ s/ϰ/\$\\kappa\$/g;
+    $line =~ s/λ/\$\\lambda\$/g;
+    $line =~ s/μ/\$\\mu\$/g;
+    $line =~ s/ν/\$\\nu\$/g;
+    $line =~ s/ξ/\$\\xi\$/g;
+    $line =~ s/ℴ/\$\\omicron\$/g;
+    $line =~ s/π/\$\\pi\$/g;
+    $line =~ s/ϖ/\$\\pi\$/g;
+    $line =~ s/ρ/\$\\rho\$/g;
+    $line =~ s/ϱ/\$\\rho\$/g;
+    $line =~ s/σ/\$\\sigma\$/g;
+    $line =~ s/ς/\$\\sigma\$/g;
+    $line =~ s/τ/\$\\tau\$/g;
+    $line =~ s/υ/\$\\upsilon\$/g;
+    $line =~ s/ϕ/\$\\phi\$/g;
+    $line =~ s/φ/\$\\phi\$/g;
+    $line =~ s/χ/\$\\chi\$/g;
+    $line =~ s/ψ/\$\\psi\$/g;
+    $line =~ s/ω/\$\\omega\$/g;
+    $line =~ s/Γ/\$\\Gamma\$/g;
+    $line =~ s/Δ/\$\\Delta\$/g;
+    $line =~ s/Θ/\$\\Theta\$/g;
+    $line =~ s/I/\$\\Iota\$/g;
+    $line =~ s/K/\$\\Kappa\$/g;
+    $line =~ s/Λ/\$\\Lambda\$/g;
+    $line =~ s/Ξ/\$\\Xi\$/g;
+    $line =~ s/Π/\$\\Pi\$/g;
+    $line =~ s/Σ/\$\\Sigma\$/g;
+    $line =~ s/Φ/\$\\Phi\$/g;
+    $line =~ s/Ψ/\$\\Psi\$/g;
+    $line =~ s/Ω/\$\\Omega\$/g;
+    
     # Astronomical symbols.
     $line =~ s/⊙/\$\\odot\$/g;
+    $line =~ s/☉/\$\\odot\$/g;
+    $line =~ s/★/\$\\star\$/g;
+    $line =~ s/⋆/\$\\star\$/g;
 
-    # Less than symbol.
+    # Mathematical symbols.
     $line =~ s/{\\textless}/\$<\$/g;
-
-    # Greater than symbol.
     $line =~ s/{\\textgreater}/\$>\$/g;
-
-    # Exponentiation.
     $line =~ s/{\\textasciicircum}([\+\-\d\.]+)/\$^{$1}\$/g;
     $line =~ s/{\\textasciicircum}/^/g;
-
-    # Em dashes.
+    $line =~ s/×/\$\\times\$/g;
+    $line =~ s/±/\$\\pm\$/g;
+    $line =~ s/∓/\$\\mp\$/g;
+    $line =~ s/°/\$^\\circ\$/g;
+    $line =~ s/˜/\$\\sim\$/g;
+    $line =~ s/∼/\$\\sim\$/g;
+    $line =~ s/̃/\$\\sim\$/g;
+    $line =~ s/≈/\$\\aproox\$/g;
+    $line =~ s/≃/\$\\aproox\$/g;
+    $line =~ s/≡/\$\\equiv\$/;
+    $line =~ s/≳/\$\\gtrapprox\$/g;
+    $line =~ s/≲/\$\\lessapprox\$/g;
+    $line =~ s/√/\$\\sqrt{}\$/g;
+    $line =~ s/∗/\$\\star\$/g;
+    $line =~ s/Ṁ/\$\\dot{M}\$/g;
+    $line =~ s/≤/\$\\ge\$/g;
+    $line =~ s/≥/\$\\le\$/g;
+    $line =~ s/≠/\$\\ne\$/g;
+    $line =~ s/∝/\$\\propto\$/g;
+    $line =~ s/ℳ/\$\\mathcal{M}\$/g;
+    $line =~ s/ˆ/ /g;
+     
+    # Quotes.
+    $line =~ s/‘/`/g;
+    $line =~ s/’/'/g;
+    $line =~ s/“/``/g;
+    $line =~ s/”/''/g;
+    
+    # Dashes.
     $line =~ s/–/--/g;
+    $line =~ s/−/-/g;
+    $line =~ s/—/-/g;
 
     # Accents.
     $line =~ s/Ç/\\c{C}/g;
@@ -60,6 +139,7 @@ while ( my $line = <iHndl> ) {
     $line =~ s/Å/\\AA/g;
     $line =~ s/Á/\\'A/g;
     $line =~ s/É/\\'E/g;
+    $line =~ s/ȯ/\\.o/g;
     $line =~ s/ô/\\^o/g;
     $line =~ s/ö/\\"o/g;
     $line =~ s/ò/\\`o/g;
@@ -74,10 +154,20 @@ while ( my $line = <iHndl> ) {
     $line =~ s/ú/\\'u/g;
     $line =~ s/ñ/\\~n/g;
     $line =~ s/Ñ/\\~N/g;
+    $line =~ s/ß/{\\ss}/g;
+    $line =~ s/Ã/\\~A/g;
+    $line =~ s/ø/\\o{}/g;
+    $line =~ s/š/\\v{s}/g;
+    $line =~ s/ć/\\'c/g;
+    $line =~ s/Ž/\\v{Z}/g;
+    $line =~ s/ł/\\L{}/g;
 
     # Ligatures.
     $line =~ s/æ/ae/g;
     $line =~ s/Æ/Ae/g;
+    $line =~ s/Œ/Oe/g;
+    $line =~ s/ﬃ/ffi/g;
+    $line =~ s/ﬀ/ff/g;
 
     # Write the modified line to file.
     print oHndl $line;
