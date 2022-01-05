3f0f30f41c22f5f6bffbad17a11055960eff5634
Andrew Benson <abenson@obs.carnegiescience.edu>
2022-01-05 11:15:13 -0800

    fix: Update copyright year to 2022

--- source/galacticus.banner.F90
+++ source/galacticus.banner.F90
@@ -1,5 +1,5 @@
 !! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
-!!           2019, 2020, 2021
+!!           2019, 2020, 2021, 2022
 !!    Andrew Benson <abenson@carnegiescience.edu>
 !!
 !! This file is part of Galacticus.
@@ -54,7 +54,7 @@ contains
           write (0,*) '  ####  #### ### ####  ###   ## ###  ###   #### ##  '
           write (0,*)
           write (0,*) '© 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,'
-          write (0,*) '  2017, 2018, 2019, 2020, 2021'
+          write (0,*) '  2017, 2018, 2019, 2020, 2021, 2022'
           write (0,*) '  - Andrew Benson'
           write (0,*)
        end if
