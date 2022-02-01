b72b7596c18ae4df3606663b1d763537d24ac26b
Andrew Benson <abenson@obs.carnegiescience.edu>
2022-01-31 16:34:32 -0800

    feat: Add a nodeOperator which implements a simple, empirical model to embed an elliptical galaxy in the main branch of a halo

---
+++ source/nodes.operators.empirical.massive_elliptical.F90
@@ -0,0 +1,192 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019, 2020, 2021, 2022
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+  !!{
+  Implements a node operator class that inserts an empirical model of the formation history of a massive ellptical galaxy.
+  !!}
+
+  !![
+  <nodeOperator name="nodeOperatorEmpiricalMassiveElliptical">
+   <description>A node operator class that inserts an empirical model of the formation history of a massive ellptical galaxy.</description>
+  </nodeOperator>
+  !!]
+  type, extends(nodeOperatorClass) :: nodeOperatorEmpiricalMassiveElliptical
+     !!{     
+     A node operator class that inserts an empirical model of the formation history of a massive ellptical galaxy. The galaxy is
+     assumed to grow in the main branch of the tree with a constant specific star formation rate, such that it mass is given by:
+     \begin{equation}
+       M_\star(t) = M_{\star,0} \exp(-\phi_\star [t-t_0]),
+     \end{equation}
+     where $M_{\star,0}=${\normalfont \ttfamily [massStellarFinal]} is the stellar mass in the root node of the tree,
+     $\phi_\star=${\normalfont \ttfamily [rateStarFormationSpecific]}, and $t_0$ is the cosmic time at the root node of the tree.
+     !!}
+     private
+     double precision :: massStellarFinal                  , rateStarFormationSpecific                , &
+          &              angularMomentumPseudoSpecificFinal, rateAngularMomentumPseudoSpecificSpecific
+   contains
+     procedure :: nodeInitialize        => empiricalMassiveEllipticalNodeInitialize
+     procedure :: differentialEvolution => empiricalMassiveEllipticalDifferentialEvolution
+  end type nodeOperatorEmpiricalMassiveElliptical
+  
+  interface nodeOperatorEmpiricalMassiveElliptical
+     !!{
+     Constructors for the {\normalfont \ttfamily empiricalMassiveElliptical} node operator class.
+     !!}
+     module procedure empiricalMassiveEllipticalConstructorParameters
+     module procedure empiricalMassiveEllipticalConstructorInternal
+  end interface nodeOperatorEmpiricalMassiveElliptical
+  
+contains
+
+  function empiricalMassiveEllipticalConstructorParameters(parameters) result(self)
+    !!{
+    Constructor for the {\normalfont \ttfamily starFormation} node operator class which takes a parameter set as input.
+    !!}
+    use :: Input_Parameters, only : inputParameters
+    implicit none
+    type            (nodeOperatorEmpiricalMassiveElliptical)                :: self
+    type            (inputParameters                       ), intent(inout) :: parameters
+    double precision                                                        :: massStellarFinal                  , rateStarFormationSpecific                , &
+         &                                                                     angularMomentumPseudoSpecificFinal, rateAngularMomentumPseudoSpecificSpecific
+    
+    !![
+    <inputParameter>
+      <name>massStellarFinal</name>
+      <source>parameters</source>
+      <description>The final stellar mass of the elliptical galaxy.</description>
+    </inputParameter>
+    <inputParameter>
+      <name>rateStarFormationSpecific</name>
+      <source>parameters</source>
+      <description>The specific star formation rate of the elliptical galaxy.</description>
+    </inputParameter>
+    <inputParameter>
+      <name>angularMomentumPseudoSpecificFinal</name>
+      <source>parameters</source>
+      <description>The final specific pseudo-angular momentum of the elliptical galaxy.</description>
+    </inputParameter>
+    <inputParameter>
+      <name>rateAngularMomentumPseudoSpecificSpecific</name>
+      <source>parameters</source>
+      <description>The specific growth rate of the specific pseudo-angular momentum of the elliptical galaxy.</description>
+    </inputParameter>
+    !!]
+    self=nodeOperatorEmpiricalMassiveElliptical(massStellarFinal,rateStarFormationSpecific,angularMomentumPseudoSpecificFinal,rateAngularMomentumPseudoSpecificSpecific)
+    !![
+    <inputParametersValidate source="parameters"/>
+    !!]
+    return
+  end function empiricalMassiveEllipticalConstructorParameters
+
+  function empiricalMassiveEllipticalConstructorInternal(massStellarFinal,rateStarFormationSpecific,angularMomentumPseudoSpecificFinal,rateAngularMomentumPseudoSpecificSpecific) result(self)
+    !!{
+    Internal constructor for the {\normalfont \ttfamily empiricalMassiveElliptical} node operator class.
+    !!}
+    implicit none
+    type            (nodeOperatorEmpiricalMassiveElliptical)                :: self
+    double precision                                        , intent(in   ) :: massStellarFinal                  , rateStarFormationSpecific                , &
+         &                                                                     angularMomentumPseudoSpecificFinal, rateAngularMomentumPseudoSpecificSpecific
+    !![
+    <constructorAssign variables="massStellarFinal, rateStarFormationSpecific, angularMomentumPseudoSpecificFinal, rateAngularMomentumPseudoSpecificSpecific"/>
+    !!]
+
+    return
+  end function empiricalMassiveEllipticalConstructorInternal
+
+  subroutine empiricalMassiveEllipticalNodeInitialize(self,node)
+    !!{
+    Initialize nodes for the massive elliptical.
+    !!}
+    use :: Galacticus_Nodes, only : nodeComponentBasic, nodeComponentSpheroid
+    implicit none
+    class           (nodeOperatorEmpiricalMassiveElliptical), intent(inout)          :: self
+    type            (treeNode                              ), intent(inout), target  :: node
+    type            (treeNode                              )               , pointer :: nodeRoot
+    class           (nodeComponentBasic                    )               , pointer :: basicLeaf  , basicRoot
+    class           (nodeComponentSpheroid                 )               , pointer :: spheroid
+    double precision                                                                 :: timeLeaf   , timeRoot       , &
+         &                                                                              massStellar, angularMomentum
+
+    ! Initialize only the leaf node on the main branch.
+    if (associated(node%firstChild).or..not.node%isOnMainBranch()) return
+    ! Get times in the leaf and root nodes.
+    nodeRoot => node
+    do while (associated(nodeRoot%parent))
+       nodeRoot => nodeRoot%parent
+    end do
+    basicLeaf => node     %basic   (                 )
+    basicRoot => nodeRoot %basic   (                 )
+    spheroid  => node     %spheroid(autoCreate=.true.)
+    timeLeaf  =  basicLeaf%time    (                 )
+    timeRoot  =  basicRoot%time    (                 )
+    ! Compute the initial mass of the elliptical galaxy.
+    massStellar    =+self%massStellarFinal                               &
+         &          *exp(                                                &
+         &               +self%rateStarFormationSpecific                 &
+         &               *(                                              &
+         &                 +timeLeaf                                     &
+         &                 -timeRoot                                     &
+         &                )                                              &
+         &              )
+    ! Compute the initial angular momentum the elliptical galaxy.
+    angularMomentum=+self%angularMomentumPseudoSpecificFinal             &
+         &          *exp(                                                &
+         &               +self%rateAngularMomentumPseudoSpecificSpecific &
+         &               *(                                              &
+         &                 +timeLeaf                                     &
+         &                 -timeRoot                                     &
+         &                )                                              &
+         &              )                                                &
+         &          *massStellar
+    ! Set the properties
+    call spheroid%    massStellarSet(massStellar    )
+    call spheroid%angularMomentumSet(angularMomentum)
+    return
+  end subroutine empiricalMassiveEllipticalNodeInitialize
+
+  subroutine empiricalMassiveEllipticalDifferentialEvolution(self,node,interrupt,functionInterrupt,propertyType)
+    !!{
+    Impose the star formation rate for the massive elliptical.
+    !!}
+    use :: Galacticus_Nodes, only : propertyTypeInactive, nodeComponentSpheroid
+    implicit none
+    class           (nodeOperatorEmpiricalMassiveElliptical), intent(inout), target  :: self
+    type            (treeNode                              ), intent(inout)          :: node
+    logical                                                 , intent(inout)          :: interrupt
+    procedure       (interruptTask                         ), intent(inout), pointer :: functionInterrupt
+    integer                                                 , intent(in   )          :: propertyType
+    class           (nodeComponentSpheroid                 )               , pointer :: spheroid
+    double precision                                                                 :: rateStarFormation, rateAngularMomentum
+
+    ! Return immediately for inactive property evolution.
+    if (propertyType == propertyTypeInactive) return
+    ! Compute and set the rates of star formation and angular momentum growth.
+    spheroid            =>    node    %spheroid                                 ()
+    rateStarFormation   =  +  spheroid%massStellar                              () &
+         &                 *  self    %rateStarFormationSpecific
+    rateAngularMomentum =  +  spheroid%angularMomentum                          () &
+         &                 *(                                                      &
+         &                   +self    %rateStarFormationSpecific                   &
+         &                   +self    %rateAngularMomentumPseudoSpecificSpecific   &
+         &                   )
+    call spheroid%    massStellarRate(rateStarFormation  )
+    call spheroid%angularMomentumRate(rateAngularMomentum)
+    return
+  end subroutine empiricalMassiveEllipticalDifferentialEvolution
+
