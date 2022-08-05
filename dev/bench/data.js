2617884ade24738795ca259f5d01fa47a3b455c7
github-action-benchmark <github@users.noreply.github.com>
2022-08-05 23:01:01 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 9cdf8f4b349912c271b50e166dcca29c7f80102f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659728113241,
+  "lastUpdate": 1659740461968,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Benchmark": [
@@ -28,6 +28,43 @@ window.BENCHMARK_DATA = {
             "value": 41.239,
             "unit": "seconds",
             "range": 1.90165953840311
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "committer": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "id": "9cdf8f4b349912c271b50e166dcca29c7f80102f",
+          "message": "feat: Add a validation workflow\n\nIncludes validation of the subhalo orbital evolution model",
+          "timestamp": "2022-08-05T21:43:47Z",
+          "url": "https://github.com/galacticusorg/galacticus/commit/9cdf8f4b349912c271b50e166dcca29c7f80102f"
+        },
+        "date": 1659740460494,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloMassFunction",
+            "value": 59.6374034521766,
+            "unit": "|logℒ|"
+          },
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloRadialDistribution",
+            "value": 25.9438445610862,
+            "unit": "|logℒ|"
+          },
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
+            "value": 1.79769313486232e+292,
+            "unit": "|logℒ|"
           }
         ]
       }
