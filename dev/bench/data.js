fdf56e7cc410cd9b536cb783fbb7db28f1b7257e
github-action-benchmark <github@users.noreply.github.com>
2022-08-05 19:35:13 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3c857a66d3bf1eeccf294e6b0759544d8a044b91

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1659728113241,
+  "repoUrl": "https://github.com/galacticusorg/galacticus",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "committer": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "id": "3c857a66d3bf1eeccf294e6b0759544d8a044b91",
+          "message": "fix: Correct push task in benchmark workflow",
+          "timestamp": "2022-08-05T18:38:22Z",
+          "url": "https://github.com/galacticusorg/galacticus/commit/3c857a66d3bf1eeccf294e6b0759544d8a044b91"
+        },
+        "date": 1659728112089,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 41.239,
+            "unit": "seconds",
+            "range": 1.90165953840311
+          }
+        ]
+      }
+    ]
+  }
+}
