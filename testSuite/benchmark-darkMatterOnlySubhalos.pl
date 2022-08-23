973fcf2af08830765f9db86026a83c68c05d9b29
Andrew Benson <abenson@carnegiescience.edu>
2022-08-23 20:29:41 +0000

    fix: Print results of dark matter-only subhalos benchmark model

--- testSuite/benchmark-darkMatterOnlySubhalos.pl
+++ testSuite/benchmark-darkMatterOnlySubhalos.pl
@@ -33,6 +33,7 @@ for(my $i=0;$i<11;++$i) {
 # Find average and standard deviation of run times.
 my $runTimeAverage           = $runTimes->average();
 my $runTimeStandardDeviation = $runTimes->stdv   ();
+print "Benchmark results: ".$runTimeAverage." ± ".$runTimeStandardDeviation." s";
 
 # Generate JSON report.
 my @output =
