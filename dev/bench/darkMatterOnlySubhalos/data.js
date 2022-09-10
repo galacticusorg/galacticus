0955f1a28fc3bc15e431739b4e08c17bb4d55a7a
github-action-benchmark <github@users.noreply.github.com>
2022-09-10 15:58:19 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 177d7819ed7bbc7abaebbdf14a764bc8a5c2e35b

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662821929515,
+  "lastUpdate": 1662825499399,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
@@ -2029,6 +2029,45 @@ window.BENCHMARK_DATA = {
             "value": 57.488,
             "unit": "seconds",
             "range": 0.0782789882407195
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "177d7819ed7bbc7abaebbdf14a764bc8a5c2e35b",
+          "message": "Merge pull request #294 from galacticusorg/dev\n\nAllow more `functionClass` objects to support auto-descriptors",
+          "timestamp": "2022-09-10T06:26:23-07:00",
+          "tree_id": "bbc9951cd311f3631e464e30181dc2739203f1d2",
+          "url": "https://github.com/galacticusorg/galacticus/commit/177d7819ed7bbc7abaebbdf14a764bc8a5c2e35b"
+        },
+        "date": 1662825497350,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloMassFunction",
+            "value": 56.4862273307091,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloRadialDistribution",
+            "value": 25.5165582430658,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
+            "value": 15423.3632579593,
+            "unit": "-logℒ"
           }
         ]
       }
