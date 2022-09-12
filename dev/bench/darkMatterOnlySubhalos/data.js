3204bd41db464342e61f81cd47e6baeaab7f652a
github-action-benchmark <github@users.noreply.github.com>
2022-09-12 22:57:46 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 9029615d474e7c7bff0b5e1577d801351ac6de40

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663010870904,
+  "lastUpdate": 1663023466644,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
@@ -2236,6 +2236,36 @@ window.BENCHMARK_DATA = {
             "value": 81.899,
             "unit": "seconds",
             "range": 0.263110053020186
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "9029615d474e7c7bff0b5e1577d801351ac6de40",
+          "message": "fix: Ensure object is null initialized",
+          "timestamp": "2022-09-12T14:23:42-07:00",
+          "tree_id": "9c26718d1ad15efd1ce77666434d0fac20eefebf",
+          "url": "https://github.com/galacticusorg/galacticus/commit/9029615d474e7c7bff0b5e1577d801351ac6de40"
+        },
+        "date": 1663023465672,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 57.507,
+            "unit": "seconds",
+            "range": 0.116379121839276
           }
         ]
       }
