6fbe4bc2457e305e8bbe3b359e21e4630628c2bd
github-action-benchmark <github@users.noreply.github.com>
2022-09-25 09:19:17 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for 1aa340a7dba4eed8f69baa0b2054f9e2ed4c81ef

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663748131166,
+  "lastUpdate": 1664097557593,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
@@ -1702,6 +1702,36 @@ window.BENCHMARK_DATA = {
             "value": 352.633,
             "unit": "seconds",
             "range": 0.501009081756177
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1aa340a7dba4eed8f69baa0b2054f9e2ed4c81ef",
+          "message": "Merge pull request #302 from galacticusorg/cambStaticFix\n\nAdd workaround for missing `pthread_mutex_destroy` for static builds of CAMB",
+          "timestamp": "2022-09-24T22:15:49-07:00",
+          "tree_id": "a730a270bf15c6434e6d4c003773c45815d81ac8",
+          "url": "https://github.com/galacticusorg/galacticus/commit/1aa340a7dba4eed8f69baa0b2054f9e2ed4c81ef"
+        },
+        "date": 1664097556578,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 374.63,
+            "unit": "seconds",
+            "range": 0.828948731830183
           }
         ]
       }
