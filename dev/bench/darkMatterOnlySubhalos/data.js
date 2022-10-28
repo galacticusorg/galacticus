c1743547fca5ae4de53f296f46e1f180aab8110c
github-action-benchmark <github@users.noreply.github.com>
2022-10-28 03:39:37 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for a47ff0c17d4558c1ed3ce985513cb63fcd161860

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1666603492091,
+  "lastUpdate": 1666928377065,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
@@ -3946,6 +3946,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 15423.3632579593,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "a47ff0c17d4558c1ed3ce985513cb63fcd161860",
+          "message": "feat: Add support for fractional order regular modified Bessel functions",
+          "timestamp": "2022-10-27T21:53:18Z",
+          "tree_id": "9a9746007df0b1060406674b1ab3e2b8874f2de2",
+          "url": "https://github.com/galacticusorg/galacticus/commit/a47ff0c17d4558c1ed3ce985513cb63fcd161860"
+        },
+        "date": 1666928375883,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 50.088,
+            "unit": "seconds",
+            "range": 0.0215777663339221
           }
         ]
       }
