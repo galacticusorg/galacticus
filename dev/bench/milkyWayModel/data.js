c1eff2638beaf260bef92de79f16e4cf826ec77b
github-action-benchmark <github@users.noreply.github.com>
2022-11-15 19:13:41 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for 6eab8997cd73cb0a474228ade542d133890ad138

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668514027282,
+  "lastUpdate": 1668539621966,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
@@ -3712,6 +3712,36 @@ window.BENCHMARK_DATA = {
             "name": "Milky Way model - Likelihood - localGroupStellarMassHaloMassRelation",
             "value": 10.448716099978,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "6eab8997cd73cb0a474228ade542d133890ad138",
+          "message": "fix: Avoid out-of-range error when integrating Vega spectrum\n\nThe interpolation of the Vega spectrum used for computing AB-Vega offsets previously had no `extrapolationType` defined, causing it to fail if integration extended outside of the tabulated range (e.g. for some ionizing luminosity filters). The `extrapolationType` is now set to `zero` so that zero flux is assumed outside of the tabulated range.",
+          "timestamp": "2022-11-15T08:28:30-08:00",
+          "tree_id": "f719a6b9bcd1a3c271446d6abae1156039d42e13",
+          "url": "https://github.com/galacticusorg/galacticus/commit/6eab8997cd73cb0a474228ade542d133890ad138"
+        },
+        "date": 1668539620630,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 173.808,
+            "unit": "seconds",
+            "range": 0.153966230065361
           }
         ]
       }
