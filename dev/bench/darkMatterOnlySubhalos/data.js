092363adcda40688b2af870947eed4110abaea82
github-action-benchmark <github@users.noreply.github.com>
2022-11-15 19:13:36 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 6eab8997cd73cb0a474228ade542d133890ad138

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668514023041,
+  "lastUpdate": 1668539616825,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
@@ -4658,6 +4658,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 26209.4879648531,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "6eab8997cd73cb0a474228ade542d133890ad138",
+          "message": "fix: Avoid out-of-range error when integrating Vega spectrum\n\nThe interpolation of the Vega spectrum used for computing AB-Vega offsets previously had no `extrapolationType` defined, causing it to fail if integration extended outside of the tabulated range (e.g. for some ionizing luminosity filters). The `extrapolationType` is now set to `zero` so that zero flux is assumed outside of the tabulated range.",
+          "timestamp": "2022-11-15T08:28:30-08:00",
+          "tree_id": "f719a6b9bcd1a3c271446d6abae1156039d42e13",
+          "url": "https://github.com/galacticusorg/galacticus/commit/6eab8997cd73cb0a474228ade542d133890ad138"
+        },
+        "date": 1668539615152,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 47.907,
+            "unit": "seconds",
+            "range": 0.666351333757287
           }
         ]
       }
