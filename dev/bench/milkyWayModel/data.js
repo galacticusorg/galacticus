fb0413d9b67b4d8107934a2aaaa265cce56c52f3
github-action-benchmark <github@users.noreply.github.com>
2022-11-29 11:24:09 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for 380311c81f358602f5bfc449c3a59549db810f16

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669721040177,
+  "lastUpdate": 1669721049105,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
@@ -4162,6 +4162,60 @@ window.BENCHMARK_DATA = {
             "value": 185.571,
             "unit": "seconds",
             "range": 0.30274229965706
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "380311c81f358602f5bfc449c3a59549db810f16",
+          "message": "Merge pull request #335 from galacticusorg/satelliteDistanceMinimum\n\nAdd a `nodeOperator` and `nodePropertyExtractor` to track the minimum distance of approach of a satellite to the center of its host halo",
+          "timestamp": "2022-11-28T21:33:33-08:00",
+          "tree_id": "7cb9bf93ba71ef9efadac4301d6ea1f0d00b8053",
+          "url": "https://github.com/galacticusorg/galacticus/commit/380311c81f358602f5bfc449c3a59549db810f16"
+        },
+        "date": 1669721047849,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Likelihood - localGroupMassMetallicityRelation",
+            "value": 3.45982964385489,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Milky Way model - Likelihood - localGroupMassSizeRelation",
+            "value": 4.64974015742461,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Milky Way model - Likelihood - localGroupMassVelocityDispersionRelation",
+            "value": 0.794502076751787,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Milky Way model - Likelihood - localGroupOccupationFraction",
+            "value": 23.6686716744117,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Milky Way model - Likelihood - localGroupStellarMassFunction",
+            "value": 65.2960772707066,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Milky Way model - Likelihood - localGroupStellarMassHaloMassRelation",
+            "value": 11.0722562418528,
+            "unit": "-logℒ"
           }
         ]
       }
