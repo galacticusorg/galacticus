a2a1065bffc6b7217f511db3615bc75b90e24017
github-action-benchmark <github@users.noreply.github.com>
2023-01-13 23:16:47 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for edf56bd05e127325a2415787df342c4e72f15671

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673575192471,
+  "lastUpdate": 1673651807583,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
@@ -6245,6 +6245,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 27887.2181311663,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "edf56bd05e127325a2415787df342c4e72f15671",
+          "message": "feat: Allow for smoothed transitions in high-pass filters\n\nMake use of these in the COSMOS SHMR likelihood class to avoid having sharp transitions in the likelihood function.",
+          "timestamp": "2023-01-13T17:13:09Z",
+          "tree_id": "b1c263cc0161db07919d832a57f94b36c905bbc8",
+          "url": "https://github.com/galacticusorg/galacticus/commit/edf56bd05e127325a2415787df342c4e72f15671"
+        },
+        "date": 1673651805661,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 63.681,
+            "unit": "seconds",
+            "range": 0.315732323337358
           }
         ]
       }
