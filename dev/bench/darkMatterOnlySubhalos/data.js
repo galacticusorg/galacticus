b26c107f1f7cb7f9204b8c51c49e7d3f7b2c73d5
github-action-benchmark <github@users.noreply.github.com>
2023-01-19 07:42:11 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 6b1d1a78f5991eaf1c8b3906fea05bee8179c2e1

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1674070333956,
+  "lastUpdate": 1674114131519,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
@@ -6590,6 +6590,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 26366.2430061311,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b1d1a78f5991eaf1c8b3906fea05bee8179c2e1",
+          "message": "Merge pull request #357 from galacticusorg/excursionSetsProgressBarFix\n\nFix an issue that the progress bar is inaccurate when solving the excursion sets problem with MPI",
+          "timestamp": "2023-01-18T21:09:36-08:00",
+          "tree_id": "5c02a2059732b8f299be3b725a8f536a1df6c8a8",
+          "url": "https://github.com/galacticusorg/galacticus/commit/6b1d1a78f5991eaf1c8b3906fea05bee8179c2e1"
+        },
+        "date": 1674114130063,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 51.939,
+            "unit": "seconds",
+            "range": 0.089481282958862
           }
         ]
       }
