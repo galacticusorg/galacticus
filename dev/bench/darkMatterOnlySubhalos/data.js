482c7d9c50970ea02aad83f212ea4f4d456a910e
github-action-benchmark <github@users.noreply.github.com>
2023-01-28 05:01:46 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 79b7325d1563d2760811f8624ec6fbb1a4fb6d91

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1674847886908,
+  "lastUpdate": 1674882106185,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
@@ -7142,6 +7142,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 27682.9945390393,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "79b7325d1563d2760811f8624ec6fbb1a4fb6d91",
+          "message": "Merge pull request #363 from galacticusorg/chandraDensity\n\nAvoid computing Chandrasekhar integrals if density is zero",
+          "timestamp": "2023-01-27T14:24:33-08:00",
+          "tree_id": "f8ba0b4169808a697f7051550c1ffa7c00304fad",
+          "url": "https://github.com/galacticusorg/galacticus/commit/79b7325d1563d2760811f8624ec6fbb1a4fb6d91"
+        },
+        "date": 1674882104785,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 55.861,
+            "unit": "seconds",
+            "range": 0.307936519432103
           }
         ]
       }
