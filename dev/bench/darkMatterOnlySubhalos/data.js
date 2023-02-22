0931afa5dcb6fd68d44b18f8121cfcbb50565f3f
github-action-benchmark <github@users.noreply.github.com>
2023-02-22 18:03:59 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 5df11a52eb6d0323b8ed5cf4a54d5c66442cec4b

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676836675484,
+  "lastUpdate": 1677089039817,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
@@ -8104,6 +8104,34 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 24145.1797388743,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "committer": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "id": "5df11a52eb6d0323b8ed5cf4a54d5c66442cec4b",
+          "message": "fix: Avoid floating point exceptions for empty mass distributions",
+          "timestamp": "2023-02-22T14:54:24Z",
+          "url": "https://github.com/galacticusorg/galacticus/commit/5df11a52eb6d0323b8ed5cf4a54d5c66442cec4b"
+        },
+        "date": 1677089037847,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 51.415,
+            "unit": "seconds",
+            "range": 0.0724327274097912
           }
         ]
       }
