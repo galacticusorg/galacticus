9e29430df90ca87377848dca0aee28512b487c2e
github-action-benchmark <github@users.noreply.github.com>
2023-04-04 05:04:22 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for cc4eee10982e01c5d5bced0d79a894ac1462c74d

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,45 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680268666930,
+  "lastUpdate": 1680584662473,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "name": "Andrew Benson",
-            "username": "abensonca",
-            "email": "abenson@carnegiescience.edu"
-          },
-          "committer": {
-            "name": "Andrew Benson",
-            "username": "abensonca",
-            "email": "abenson@carnegiescience.edu"
-          },
-          "id": "6d0b44bf076a9a4c4798316736b3d2b3ced9d883",
-          "message": "fix: Add missing module imports",
-          "timestamp": "2022-09-01T23:23:08Z",
-          "url": "https://github.com/galacticusorg/galacticus/commit/6d0b44bf076a9a4c4798316736b3d2b3ced9d883"
-        },
-        "date": 1662081255753,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Likelihood - subhaloMassFunction",
-            "value": 56.5636704361171,
-            "unit": "-logℒ"
-          },
-          {
-            "name": "Dark Matter Only Subhalos - Likelihood - subhaloRadialDistribution",
-            "value": 25.6097232447882,
-            "unit": "-logℒ"
-          },
-          {
-            "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
-            "value": 14937.6267911268,
-            "unit": "-logℒ"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8702,6 +8665,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 25371.9758848307,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "cc4eee10982e01c5d5bced0d79a894ac1462c74d",
+          "message": "fix: Filter out unrealistically tiny galaxies when computing the Local Group mass-size relation",
+          "timestamp": "2023-04-03T23:54:26Z",
+          "tree_id": "645ad338920c918458b3e3e346279b8f9b1cddbc",
+          "url": "https://github.com/galacticusorg/galacticus/commit/cc4eee10982e01c5d5bced0d79a894ac1462c74d"
+        },
+        "date": 1680584660955,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 49.956,
+            "unit": "seconds",
+            "range": 0.071835924160388
           }
         ]
       }
