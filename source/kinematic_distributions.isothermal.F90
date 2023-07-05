cfa6128990bf6c973714f169eeddfdd1ad38ed93
Andrew Benson <abenson@carnegiescience.edu>
2023-07-05 10:43:05 -0700

    feat: Support velocity dispersion method in isothermal kinematics distributions

--- source/kinematic_distributions.isothermal.F90
+++ source/kinematic_distributions.isothermal.F90
@@ -30,7 +30,8 @@
      !!{
      An isothermal kinematic distribution.
      !!}
-     double precision :: temperature_, massAtomicMean
+     double precision :: temperature_       , massAtomicMean, &
+          &              velocityDispersion_
    contains
      procedure :: isCollisional                  => isothermalIsCollisional
      procedure :: temperature                    => isothermalTemperature
@@ -57,38 +58,78 @@ contains
     implicit none
     type            (kinematicsDistributionIsothermal)                :: self
     type            (inputParameters                 ), intent(inout) :: parameters
-    double precision                                                  :: temperature, massAtomicMean
+    double precision                                                  :: temperature       , massAtomicMean, &
+         &                                                               velocityDispersion
 
-    !![
-    <inputParameter>
-      <name>temperature</name>
-      <description>The temperature of the distribution.</description>
-      <source>parameters</source>
-    </inputParameter>
-    <inputParameter>
-      <name>massAtomicMean</name>
-      <description>The mean atomic mass (in atomic mass units) of the distribution.</description>
-      <source>parameters</source>
-    </inputParameter>
-    !!]
-    self=kinematicsDistributionIsothermal(temperature,massAtomicMean)
+    if (parameters%isPresent('temperature')) then
+       !![
+       <inputParameter>
+	 <name>temperature</name>
+	 <description>The temperature of the distribution.</description>
+	 <source>parameters</source>
+       </inputParameter>
+       <inputParameter>
+	 <name>massAtomicMean</name>
+	 <description>The mean atomic mass (in atomic mass units) of the distribution.</description>
+	 <source>parameters</source>
+       </inputParameter>
+       !!]
+       self=kinematicsDistributionIsothermal(temperature_=temperature,massAtomicMean=massAtomicMean)
+    else
+       !![
+       <inputParameter>
+	 <name>velocityDispersion</name>
+	 <description>The velocity dispersion of the distribution.</description>
+	 <source>parameters</source>
+       </inputParameter>
+       !!]
+       self=kinematicsDistributionIsothermal(velocityDispersion_=velocityDispersion)
+    end if
     !![
     <inputParametersValidate source="parameters"/>
     !!]
     return
   end function isothermalConstructorParameters
   
-  function isothermalConstructorInternal(temperature_,massAtomicMean) result(self)
+  function isothermalConstructorInternal(temperature_,massAtomicMean,velocityDispersion_) result(self)
     !!{
     Constructor for {\normalfont \ttfamily isothermal} kinematics distribution class.
     !!}
+    use :: Error                       , only : Error_Report
+    use :: Numerical_Constants_Atomic  , only : atomicMassUnit
+    use :: Numerical_Constants_Physical, only : boltzmannsConstant
+    use :: Numerical_Constants_Prefixes, only : kilo
     implicit none
-    type            (kinematicsDistributionIsothermal)                :: self
-    double precision                                  , intent(in   ) :: temperature_, massAtomicMean
+    type            (kinematicsDistributionIsothermal)                          :: self
+    double precision                                  , intent(in   ), optional :: temperature_       , massAtomicMean, &
+         &                                                                         velocityDispersion_
     !![
-    <constructorAssign variables="temperature_, massAtomicMean"/>
+    <constructorAssign variables="temperature_, massAtomicMean, velocityDispersion_"/>
     !!]
 
+    if (present(velocityDispersion_) .and. present(temperature_)) then
+       call Error_Report('can not provide both [temperature] and [velocityDispersion]'//{introspection:location})
+    else if (present(temperature_       )) then
+       if (.not.present(massAtomicMean)) call Error_Report('[massAtomicMean] must be provided'    //{introspection:location})
+       self%velocityDispersion_=+sqrt(                         &
+            &                         +     boltzmannsConstant &
+            &                         *self%temperature_       &
+            &                         /self%massAtomicMean     &
+            &                         /     atomicMassUnit     &
+            &                        )                         &
+            &                   /kilo
+    else if (present(velocityDispersion_)) then
+       if (     present(massAtomicMean)) call Error_Report('[massAtomicMean] must not be provided'//{introspection:location})
+       self%massAtomicMean     =+1.0d0
+       self%temperature_       =+(                     &
+            &                    +velocityDispersion_ &
+            &                    *kilo                &
+            &                   )**2                  &
+            &                  *atomicMassUnit        &
+            &                  /boltzmannsConstant
+    else
+       call Error_Report('either [temperature] or [velocityDispersion] must be supplied'//{introspection:location})
+    end if
     return
   end function isothermalConstructorInternal
 
@@ -133,21 +174,12 @@ contains
     !!{
     Return the 1D velocity dispersion at the specified {\normalfont \ttfamily coordinates} in an isothermal kinematic distribution.
     !!}
-    use :: Numerical_Constants_Atomic  , only : atomicMassUnit
-    use :: Numerical_Constants_Physical, only : boltzmannsConstant
-    use :: Numerical_Constants_Prefixes, only : kilo
     implicit none
     class(kinematicsDistributionIsothermal), intent(inout) :: self
     class(coordinate                      ), intent(in   ) :: coordinates
     class(massDistributionClass           ), intent(inout) :: massDistributionEmbedding
     !$GLC attributes unused :: coordinates, massDistributionEmbedding
     
-    isothermalVelocityDispersion1D=+sqrt(                         &
-         &                               +     boltzmannsConstant &
-         &                               *self%temperature_       &
-         &                               /self%massAtomicMean     &
-         &                               /     atomicMassUnit     &
-         &                              )                         &
-         &                          /kilo
+    isothermalVelocityDispersion1D=self%velocityDispersion_
     return
   end function isothermalVelocityDispersion1D
