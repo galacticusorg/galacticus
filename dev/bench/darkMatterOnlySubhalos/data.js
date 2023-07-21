26b0b126da18923d55c6247dcffc49d6211caa48
github-action-benchmark <github@users.noreply.github.com>
2023-07-21 17:17:16 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 1f32936198271b9e5df881509f9be6aa4998aa4b

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689878616368,
+  "lastUpdate": 1689959836831,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@obs.carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "380311c81f358602f5bfc449c3a59549db810f16",
-          "message": "Merge pull request #335 from galacticusorg/satelliteDistanceMinimum\n\nAdd a `nodeOperator` and `nodePropertyExtractor` to track the minimum distance of approach of a satellite to the center of its host halo",
-          "timestamp": "2022-11-28T21:33:33-08:00",
-          "tree_id": "7cb9bf93ba71ef9efadac4301d6ea1f0d00b8053",
-          "url": "https://github.com/galacticusorg/galacticus/commit/380311c81f358602f5bfc449c3a59549db810f16"
-        },
-        "date": 1669721034608,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Wall Time",
-            "value": 64.678,
-            "unit": "seconds",
-            "range": 0.17965411211561
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8740,6 +8710,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 25139.2189837662,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "1f32936198271b9e5df881509f9be6aa4998aa4b",
+          "message": "fix(style): Formatting only",
+          "timestamp": "2023-07-21T14:15:16Z",
+          "tree_id": "7a5cf54ad84d157ca14192aaccd9976aa0ca66ca",
+          "url": "https://github.com/galacticusorg/galacticus/commit/1f32936198271b9e5df881509f9be6aa4998aa4b"
+        },
+        "date": 1689959834294,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 49.396,
+            "unit": "seconds",
+            "range": 0.0246251903530547
           }
         ]
       }
