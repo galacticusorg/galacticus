a722fbbae20616e785443012e26a347756dd5146
github-action-benchmark <github@users.noreply.github.com>
2023-08-16 03:22:39 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 2081039481ead7c61f9b56dc67890ecf32755e16

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692069750425,
+  "lastUpdate": 1692156159898,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@obs.carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "baab79ac9a0bba2e7f9c4e4c4cb864ca535bbd41",
-          "message": "Merge pull request #351 from galacticusorg/sigmaMIntegration\n\nMake integration of σ(M) more robust",
-          "timestamp": "2023-01-13T17:02:28-08:00",
-          "tree_id": "359f77eaa4f9f8f97befa8164933b1db07d74300",
-          "url": "https://github.com/galacticusorg/galacticus/commit/baab79ac9a0bba2e7f9c4e4c4cb864ca535bbd41"
-        },
-        "date": 1673668324675,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Wall Time",
-            "value": 51.322,
-            "unit": "seconds",
-            "range": 0.0219909072109672
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8724,6 +8694,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 25139.2189837662,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "2081039481ead7c61f9b56dc67890ecf32755e16",
+          "message": "fix(style): Formatting only",
+          "timestamp": "2023-08-16T00:18:01Z",
+          "tree_id": "3df04dc9fbd5ead8203330071042a65f7523f28a",
+          "url": "https://github.com/galacticusorg/galacticus/commit/2081039481ead7c61f9b56dc67890ecf32755e16"
+        },
+        "date": 1692156157734,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 49.824,
+            "unit": "seconds",
+            "range": 0.0953645636492067
           }
         ]
       }
