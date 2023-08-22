df97899cc077954cd0e9f09d8989d2baa8238c02
github-action-benchmark <github@users.noreply.github.com>
2023-08-22 01:46:55 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 2e4d4ad34084440a26bcdfadc33601a4f16e53d7

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692249713731,
+  "lastUpdate": 1692668815857,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@obs.carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "e2cb6f3570438aa22bbdd4d6bf4ed18668d99434",
-          "message": "Merge pull request #356 from galacticusorg/font2008RoundingErrors\n\nfix: Avoid root search range failure in `font2008` hot halo ram pressure stripping class",
-          "timestamp": "2023-01-18T07:42:56-08:00",
-          "tree_id": "5c2c250b769545c02df451188ebbe2436c09018c",
-          "url": "https://github.com/galacticusorg/galacticus/commit/e2cb6f3570438aa22bbdd4d6bf4ed18668d99434"
-        },
-        "date": 1674070322350,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Wall Time",
-            "value": 61.492,
-            "unit": "seconds",
-            "range": 0.29433586257872
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8724,6 +8694,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 25236.854696677,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "2e4d4ad34084440a26bcdfadc33601a4f16e53d7",
+          "message": "fix: Remove OpenMP locks from `accretionDisksADAF` class\n\nThis are obsolete (and have been for a long time) as the tabulated solutions are now stored in each object of this class (and so are not shared).",
+          "timestamp": "2023-08-21T15:29:58-07:00",
+          "tree_id": "437fdaefcaddcf9efc219bb970f048b1e95bab2e",
+          "url": "https://github.com/galacticusorg/galacticus/commit/2e4d4ad34084440a26bcdfadc33601a4f16e53d7"
+        },
+        "date": 1692668813853,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 57.123,
+            "unit": "seconds",
+            "range": 0.0704137770608578
           }
         ]
       }
