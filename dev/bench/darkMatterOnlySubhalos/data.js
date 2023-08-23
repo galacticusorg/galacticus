3d5bb565ee2bf7e09bff7ed886f5400f50b4d4b2
github-action-benchmark <github@users.noreply.github.com>
2023-08-23 17:44:04 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 7cd7b6df772e1a6bde6d74da6fb8fc894db2bee6

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692757995830,
+  "lastUpdate": 1692812644502,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "distinct": true,
-          "id": "c17eea79b5ad78e21a97d2ab909b4a5b26a0af3a",
-          "message": "feat: Refactor the Gaussian emulator model likelihood class\n\n* Makes this class more efficient;\n\n* Handles cases where the simulator has intrinsic variance (i.e. exploits the fact that the emulator can not possibly have smaller variance than the simulator);\n\n* Adds a variogram class to provide more flexibility in variogram models.",
-          "timestamp": "2023-01-20T19:41:51Z",
-          "tree_id": "12e20e3efa8f3c824d5c0462c7bd52d7cd17e12b",
-          "url": "https://github.com/galacticusorg/galacticus/commit/c17eea79b5ad78e21a97d2ab909b4a5b26a0af3a"
-        },
-        "date": 1674268920224,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Wall Time",
-            "value": 59.694,
-            "unit": "seconds",
-            "range": 0.065881712181075
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8724,6 +8694,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 25257.8908941139,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "7cd7b6df772e1a6bde6d74da6fb8fc894db2bee6",
+          "message": "fix: Correct method descriptions\n\nIn several places \"cm\" was missing the \"m\".",
+          "timestamp": "2023-08-23T07:39:03-07:00",
+          "tree_id": "86aaa1b252e0b8d7d66fb1be787200c73580a158",
+          "url": "https://github.com/galacticusorg/galacticus/commit/7cd7b6df772e1a6bde6d74da6fb8fc894db2bee6"
+        },
+        "date": 1692812643258,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 48.338,
+            "unit": "seconds",
+            "range": 0.0342578458179109
           }
         ]
       }
