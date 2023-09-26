5e13d0a2515897c4ca9ecc8e093b6743f1f005ff
github-action-benchmark <github@users.noreply.github.com>
2023-09-26 07:16:56 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 54c7f9118c0af37e5841f387e790c6049de6dcc7

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695316891294,
+  "lastUpdate": 1695712616343,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abensonca@gmail.com",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "ffd38134fc6be768eb7091621c4c84dfd0b273e4",
-          "message": "Merge pull request #381 from galacticusorg/constrainedBranches\n\nFix constrained branch indicator propagation",
-          "timestamp": "2023-02-19T17:09:52Z",
-          "tree_id": "a7aacc54af6d0937f4279a0767c0c6da855cd283",
-          "url": "https://github.com/galacticusorg/galacticus/commit/ffd38134fc6be768eb7091621c4c84dfd0b273e4"
-        },
-        "date": 1676836666666,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Wall Time",
-            "value": 50.196,
-            "unit": "seconds",
-            "range": 0.0442538133941168
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8724,6 +8694,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 23643.7766921421,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "54c7f9118c0af37e5841f387e790c6049de6dcc7",
+          "message": "Merge pull request #479 from galacticusorg/noninstantFix\n\nCopy element index in to OpenMP parallel region",
+          "timestamp": "2023-09-26T01:11:06Z",
+          "tree_id": "c26dc4bde88c09d9cde120ca2bfadd947a0e07b8",
+          "url": "https://github.com/galacticusorg/galacticus/commit/54c7f9118c0af37e5841f387e790c6049de6dcc7"
+        },
+        "date": 1695712613815,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 53.814,
+            "unit": "seconds",
+            "range": 0.0215034880884339
           }
         ]
       }
