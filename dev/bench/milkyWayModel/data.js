798f393d08a87bd4cd4b467d683ac89c25f99c5b
github-action-benchmark <github@users.noreply.github.com>
2023-10-19 19:12:44 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for 434e3b1a99e6580da8e60b4cae63b6c3f8a5db14

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,36 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697731214244,
+  "lastUpdate": 1697742764370,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "name": "Andrew Benson",
-            "username": "abensonca",
-            "email": "abenson@carnegiescience.edu"
-          },
-          "committer": {
-            "name": "Andrew Benson",
-            "username": "abensonca",
-            "email": "abenson@carnegiescience.edu"
-          },
-          "id": "b97188142b9e3ec8e4e296e6592871911355231e",
-          "message": "fix: Catch zero mass black holes",
-          "timestamp": "2023-03-07T15:46:30Z",
-          "url": "https://github.com/galacticusorg/galacticus/commit/b97188142b9e3ec8e4e296e6592871911355231e"
-        },
-        "date": 1678218642863,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Milky Way model - Wall Time",
-            "value": 249.897,
-            "unit": "seconds",
-            "range": 0.6016644413621
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -10664,6 +10636,36 @@ window.BENCHMARK_DATA = {
             "name": "Milky Way model - Likelihood - localGroupStellarMassHaloMassRelation",
             "value": 9.54165136000034,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "434e3b1a99e6580da8e60b4cae63b6c3f8a5db14",
+          "message": "Merge pull request #495 from galacticusorg/optimization\n\nFurther optimization improvements",
+          "timestamp": "2023-10-19T16:03:13Z",
+          "tree_id": "c3abd7d712eb06c310a40d5ac9782f57693bf3d2",
+          "url": "https://github.com/galacticusorg/galacticus/commit/434e3b1a99e6580da8e60b4cae63b6c3f8a5db14"
+        },
+        "date": 1697742763542,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 234.309,
+            "unit": "seconds",
+            "range": 0.622494096359002
           }
         ]
       }
