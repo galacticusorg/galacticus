8d8dc9ef0f5f89999d2003be504a4ff7d9594d9e
github-action-benchmark <github@users.noreply.github.com>
2024-02-09 18:54:29 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for ed1160a3906155fe885404761ec066763ace0832

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707423018602,
+  "lastUpdate": 1707504869406,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "distinct": true,
-          "id": "b2b82e792403bd7f38c2a3097b550c1e5e6d3963",
-          "message": "feat: Add more informative labels in stellar mass function analysis classes",
-          "timestamp": "2023-05-12T21:09:16Z",
-          "tree_id": "a23e58cfc53d109b497605c0e5efeb3d54738a99",
-          "url": "https://github.com/galacticusorg/galacticus/commit/b2b82e792403bd7f38c2a3097b550c1e5e6d3963"
-        },
-        "date": 1683935954338,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Wall Time",
-            "value": 62.627,
-            "unit": "seconds",
-            "range": 0.374489118667049
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8784,6 +8754,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 23958.5193682761,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "ed1160a3906155fe885404761ec066763ace0832",
+          "message": "fix: Fix bug in tablulation of times for the `powerSpectrumPrimordialTransferredFile` class\n\nA missing index caused all times in the array to be identical, breaking interpolation.\n\nAlso makes warnings about mismatched cosmological parameters follow new conventions (i.e. magenta \"WARNING:\" included at the start of the message).",
+          "timestamp": "2024-02-09T07:52:01-08:00",
+          "tree_id": "464b9e14277cbe56ad0b333d2cafb7fa7390de98",
+          "url": "https://github.com/galacticusorg/galacticus/commit/ed1160a3906155fe885404761ec066763ace0832"
+        },
+        "date": 1707504867179,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 43.478,
+            "unit": "seconds",
+            "range": 0.0362712006975722
           }
         ]
       }
