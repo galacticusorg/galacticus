360539088aa7652778cf19a307bc0b02030f6f6a
github-action-benchmark <github@users.noreply.github.com>
2024-02-28 00:01:54 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for fe19e1a2fd3a716ef1ef35aed4e29eaf049dbf9d

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708716770716,
+  "lastUpdate": 1709078514705,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "distinct": true,
-          "id": "aef062b6a32bd9371c923188c298cf19e6bfd206",
-          "message": "fix: Correct comment",
-          "timestamp": "2023-05-24T16:42:17-07:00",
-          "tree_id": "7201af723f187860e72f6dc20e96f5bfc85bb605",
-          "url": "https://github.com/galacticusorg/galacticus/commit/aef062b6a32bd9371c923188c298cf19e6bfd206"
-        },
-        "date": 1684990710612,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Milky Way model - Wall Time",
-            "value": 216.595,
-            "unit": "seconds",
-            "range": 0.375377809681642
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -10708,6 +10678,36 @@ window.BENCHMARK_DATA = {
             "name": "Milky Way model - Likelihood - localGroupStellarMassHaloMassRelation",
             "value": 8.63620570693815,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe19e1a2fd3a716ef1ef35aed4e29eaf049dbf9d",
+          "message": "Merge pull request #567 from galacticusorg/featOutputSelectorTimes\n\nAllow specifying output selection via time in addition to redshift",
+          "timestamp": "2024-02-27T18:11:12Z",
+          "tree_id": "6f70047c8290171cd9eb1c3d8b80811457587cd9",
+          "url": "https://github.com/galacticusorg/galacticus/commit/fe19e1a2fd3a716ef1ef35aed4e29eaf049dbf9d"
+        },
+        "date": 1709078514133,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 150.987,
+            "unit": "seconds",
+            "range": 0.0646846194942114
           }
         ]
       }
