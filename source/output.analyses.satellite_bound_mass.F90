9269c85b019c296d9f561fbf25dd3a9f9102e5c7
Xiaolong Du <xdu@carnegiescience.edu>
2024-02-29 03:30:20 +0000

    feat: Add analyses classes for satellite properties such as the bound mass and maximum circular velocity.

---
+++ source/output.analyses.satellite_bound_mass.F90
@@ -0,0 +1,277 @@
+!! Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+!!           2019, 2020, 2021, 2022, 2023, 2024
+!!    Andrew Benson <abenson@carnegiescience.edu>
+!!
+!! This file is part of Galacticus.
+!!
+!!    Galacticus is free software: you can redistribute it and/or modify
+!!    it under the terms of the GNU General Public License as published by
+!!    the Free Software Foundation, either version 3 of the License, or
+!!    (at your option) any later version.
+!!
+!!    Galacticus is distributed in the hope that it will be useful,
+!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!    GNU General Public License for more details.
+!!
+!!    You should have received a copy of the GNU General Public License
+!!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
+  !!{
+  Implements an output analysis class that computes satellite bound mass fraction.
+  !!}
+
+  use    :: Numerical_Interpolation, only : interpolator
+  !$ use :: OMP_Lib, only : omp_lock_kind
+
+  !![
+  <outputAnalysis name="outputAnalysisSatelliteBoundMass">
+    <description>An output analysis class that computes satellite bound mass fraction as a function of time.</description>
+  </outputAnalysis>
+  !!]
+  type, extends(outputAnalysisClass) :: outputAnalysisSatelliteBoundMass
+     !!{
+     An output analysis class that computes satellite bound mass fraction as a function of time.
+     !!}
+     private
+     class           (outputTimesClass), pointer                   :: outputTimes_            => null()
+     type            (varying_string  )                            :: fileName
+     type            (interpolator    )                            :: interpolator_
+     !$ integer      (omp_lock_kind   )                            :: accumulateLock
+     double precision                                              :: logLikelihood_
+     double precision                                              :: relativeUncertainty              , boundMassInitial
+     double precision                  , dimension(:), allocatable :: time                             , fractionBoundMass              , &
+          &                                                           fractionBoundMassTarget          , varianceFractionBoundMassTarget
+   contains
+     final     ::                  satelliteBoundMassDestructor
+     procedure :: analyze       => satelliteBoundMassAnalyze
+     procedure :: finalize      => satelliteBoundMassFinalize
+     procedure :: reduce        => satelliteBoundMassReduce
+     procedure :: logLikelihood => satelliteBoundMassLogLikelihood
+  end type outputAnalysisSatelliteBoundMass
+
+  interface outputAnalysisSatelliteBoundMass
+     !!{
+     Constructors for the ``satelliteBoundMass'' output analysis class.
+     !!}
+     module procedure satelliteBoundMassConstructorParameters
+     module procedure satelliteBoundMassConstructorInternal
+  end interface outputAnalysisSatelliteBoundMass
+
+contains
+
+  function satelliteBoundMassConstructorParameters(parameters) result(self)
+    !!{
+    Constructor for the ``satelliteBoundMass'' output analysis class which takes a parameter set as input.
+    !!}
+    use :: Input_Parameters, only : inputParameter, inputParameters
+    use :: Output_Times    , only : outputTimesClass
+    implicit none
+    type            (outputAnalysisSatelliteBoundMass)                :: self
+    type            (inputParameters                 ), intent(inout) :: parameters
+    class           (outputTimesClass                ), pointer       :: outputTimes_
+    type            (varying_string                  )                :: fileName
+    double precision                                                  :: relativeUncertainty
+
+    !![
+    <inputParameter>
+      <name>fileName</name>
+      <description>The name of the file from which to read the target dataset.</description>
+      <source>parameters</source>
+    </inputParameter>
+    <inputParameter>
+      <name>relativeUncertainty</name>
+      <defaultValue>0.1d0</defaultValue>
+      <description>Relative uncertainty of the data points.</description>
+      <source>parameters</source>
+    </inputParameter>
+    <objectBuilder class="outputTimes" name="outputTimes_" source="parameters"/>
+    !!]
+    self=outputAnalysisSatelliteBoundMass(fileName,relativeUncertainty,outputTimes_)
+    !![
+    <inputParametersValidate source="parameters"/>
+    <objectDestructor name="outputTimes_"/>
+    !!]
+    return
+  end function satelliteBoundMassConstructorParameters
+  
+  function satelliteBoundMassConstructorInternal(fileName,relativeUncertainty,outputTimes_) result (self)
+    !!{
+    Constructor for the ``satelliteBoundMass'' output analysis class for internal use.
+    !!}
+    use :: HDF5_Access            , only : hdf5Access
+    use :: IO_HDF5                , only : hdf5Object
+    use :: Numerical_Interpolation, only : GSL_Interp_CSpline
+    use :: Table_Labels           , only : extrapolationTypeExtrapolate
+    implicit none
+    type            (outputAnalysisSatelliteBoundMass)                              :: self
+    type            (varying_string                  ), intent(in   )               :: fileName
+    class           (outputTimesClass                ), intent(inout), target       :: outputTimes_
+    double precision                                  , intent(in   )               :: relativeUncertainty
+    type            (hdf5Object                      )                              :: file
+    double precision                                  , allocatable  , dimension(:) :: time               , boundMass, &
+         &                                                                             fractionBoundMass
+    integer         (c_size_t                        )                              :: i
+    !![
+    <constructorAssign variables="fileName, relativeUncertainty, *outputTimes_"/>
+    !!]
+
+    ! Read properties from the file.
+    !$ call hdf5Access%set()
+    call file%openFile   (char(fileName),readOnly=.true.   )
+    call file%readDataset('time'        ,         time     )
+    call file%readDataset('boundMass'   ,         boundMass)
+    call file%close      (                                 )
+    !$ call hdf5Access%unset()
+    allocate(fractionBoundMass(size(boundMass)))
+    self%boundMassInitial=boundMass(1)
+    fractionBoundMass    =boundMass   /self%boundMassInitial
+    ! Build interpolator.
+    self%interpolator_=interpolator(time,log(fractionBoundMass),interpolationType=GSL_Interp_CSpline,extrapolationType=extrapolationTypeExtrapolate)
+    allocate(self%time                           (outputTimes_%count()))
+    allocate(self%fractionBoundMass              (outputTimes_%count()))
+    allocate(self%fractionBoundMassTarget        (outputTimes_%count()))
+    allocate(self%varianceFractionBoundMassTarget(outputTimes_%count()))
+    do i=1, outputTimes_%count()
+       self%time                   (i)=                                   outputTimes_%time(i)
+       self%fractionBoundMassTarget(i)=exp(self%interpolator_%interpolate(outputTimes_%time(i)))
+    end do
+    self%varianceFractionBoundMassTarget=(self%relativeUncertainty*self%fractionBoundMassTarget)**2
+    !$ call OMP_Init_Lock(self%accumulateLock)
+    self%fractionBoundMass=0.0d0
+    self%logLikelihood_   =0.0d0
+    return
+  end function satelliteBoundMassConstructorInternal
+
+  subroutine satelliteBoundMassDestructor(self)
+    !!{
+    Destructor for the {\normalfont \ttfamily satelliteBoundMass} output analysis class.
+    !!}
+    !$ use :: OMP_Lib, only : OMP_Destroy_Lock
+    implicit none
+    type(outputAnalysisSatelliteBoundMass), intent(inout) :: self
+
+    !![
+    <objectDestructor name="self%outputTimes_"/>
+    !!]
+    !$ call OMP_Destroy_Lock(self%accumulateLock)
+    return
+  end subroutine satelliteBoundMassDestructor
+
+  subroutine satelliteBoundMassAnalyze(self,node,iOutput)
+    !!{
+    Analyze the maximum velocity tidal track.
+    !!}
+    use    :: Galacticus_Nodes, only : nodeComponentSatellite
+    !$ use :: OMP_Lib         , only : OMP_Set_Lock      , OMP_Unset_Lock
+    implicit none
+    class           (outputAnalysisSatelliteBoundMass), intent(inout) :: self
+    type            (treeNode                        ), intent(inout) :: node
+    integer         (c_size_t                        ), intent(in   ) :: iOutput
+    class           (nodeComponentSatellite          ), pointer       :: satellite
+    double precision                                                  :: fractionMassBound
+
+    ! Skip non-satellites.
+    if (.not.node%isSatellite()) return
+    ! Extract the bound mass fraction.
+    satellite         => node%satellite()
+    fractionMassBound =  satellite%boundMass()/self%boundMassInitial
+    !$ call OMP_Set_Lock(self%accumulateLock)
+    self%fractionBoundMass(iOutput)=fractionMassBound
+    self%logLikelihood_            =+self%logLikelihood_                             &
+         &                          -0.5d0                                           &
+         &                          *(                                               &
+         &                            +fractionMassBound                             &
+         &                            -self%fractionBoundMassTarget        (iOutput) &
+         &                           )**2                                            &
+         &                          /  self%varianceFractionBoundMassTarget(iOutput)
+    !$ call OMP_Unset_Lock(self%accumulateLock)
+    return
+  end subroutine satelliteBoundMassAnalyze
+
+  subroutine satelliteBoundMassReduce(self,reduced)
+    !!{
+    Reduce over the maximum velocity tidal track output analysis.
+    !!}
+    use    :: Error  , only : Error_Report
+    !$ use :: OMP_Lib, only : OMP_Set_Lock, OMP_Unset_Lock
+    implicit none
+    class(outputAnalysisSatelliteBoundMass), intent(inout) :: self
+    class(outputAnalysisClass             ), intent(inout) :: reduced
+
+    select type (reduced)
+    class is (outputAnalysisSatelliteBoundMass)
+       !$ call OMP_Set_Lock(reduced%accumulateLock)       
+       reduced%fractionBoundMass=reduced%fractionBoundMass+self%fractionBoundMass
+       reduced%logLikelihood_   =reduced%logLikelihood_   +self%logLikelihood_
+       !$ call OMP_Unset_Lock(reduced%accumulateLock)
+    class default
+       call Error_Report('incorrect class'//{introspection:location})
+    end select
+    return
+  end subroutine satelliteBoundMassReduce
+
+  subroutine satelliteBoundMassFinalize(self)
+    !!{
+    Output results of the maximum velocity tidal track output analysis.
+    !!}
+#ifdef USEMPI
+    use :: MPI_Utilities                   , only : mpiSelf
+#endif
+    use :: Output_HDF5                     , only : outputFile
+    use :: HDF5_Access                     , only : hdf5Access
+    use :: IO_HDF5                         , only : hdf5Object
+    use :: Numerical_Constants_Astronomical, only : gigaYear
+    implicit none
+    class(outputAnalysisSatelliteBoundMass), intent(inout) :: self
+    type (hdf5Object                      )                :: analysesGroup, analysisGroup, &
+         &                                                    dataset
+
+#ifdef USEMPI
+    ! If running under MPI, accumulate tracks across all processes.
+    self%fractionBoundMass=mpiSelf%sum(self%fractionBoundMass)
+    self%logLikelihood_   =mpiSelf%sum(self%logLikelihood_   )
+#endif
+    !$ call hdf5Access%set()
+    analysesGroup=outputFile   %openGroup('analyses'                                                           )
+    analysisGroup=analysesGroup%openGroup('satelliteBoundMass','Analysis of the satellite bound mass vs. time.')
+    ! Write metadata describing this analysis.
+    call analysisGroup%writeAttribute('Satellite bound mass fraction vs. time.','description'                  )
+    call analysisGroup%writeAttribute("function1D"                             ,'type'                         )
+    call analysisGroup%writeAttribute('$t/\mathrm{Gyr}$'                       ,'xAxisLabel'                   )
+    call analysisGroup%writeAttribute('$M_\mathrm{bound}/M_\mathrm{bound,0}$'  ,'yAxisLabel'                   )
+    call analysisGroup%writeAttribute(.false.                                  ,'xAxisIsLog'                   )
+    call analysisGroup%writeAttribute(.true.                                   ,'yAxisIsLog'                   )
+    call analysisGroup%writeAttribute('time'                                   ,'xDataset'                     )
+    call analysisGroup%writeAttribute('fractionBoundMass'                      ,'yDataset'                     )
+    call analysisGroup%writeAttribute('fractionBoundMassTarget'                ,'yDatasetTarget'               )
+    call analysisGroup%writeAttribute(self%logLikelihood()                     ,'logLikelihood'                )
+    call analysisGroup%writeAttribute('Simulations'                            ,'targetLabel'                  )
+    call analysisGroup%writeDataset  (self%time                   ,'time'                   ,'Time'                           ,datasetReturned=dataset)
+    call dataset      %writeAttribute('Gyr'                       ,'units'                                                                            )
+    call dataset      %writeAttribute(gigaYear                    ,'unitsInSI'                                                                        )
+    call dataset      %close         (                                                                                                                )
+    call analysisGroup%writeDataset  (self%fractionBoundMass      ,'fractionBoundMass'      ,'Fraction of bound mass'         ,datasetReturned=dataset)
+    call dataset      %writeAttribute(' '                         ,'units'                                                                            )
+    call dataset      %writeAttribute(1.0d0                       ,'unitsInSI'                                                                        )
+    call dataset      %close         (                                                                                                                )
+    call analysisGroup%writeDataset  (self%fractionBoundMassTarget,'fractionBoundMassTarget','Fraction of bound mass [target]',datasetReturned=dataset)
+    call dataset      %writeAttribute(' '                         ,'units'                                                                            )
+    call dataset      %writeAttribute(1.0d0                       ,'unitsInSI'                                                                        )
+    call dataset      %close         (                                                                                                                )
+    call analysisGroup%close         (                                                                                                                )
+    call analysesGroup%close         (                                                                                                                )
+    !$ call hdf5Access%unset()
+    return
+  end subroutine satelliteBoundMassFinalize
+  
+  double precision function satelliteBoundMassLogLikelihood(self)
+    !!{
+    Return the log-likelihood of a maximum velocity tidal track output analysis.
+    !!}
+    implicit none
+    class(outputAnalysisSatelliteBoundMass), intent(inout) :: self
+
+    satelliteBoundMassLogLikelihood=self%logLikelihood_
+    return
+  end function satelliteBoundMassLogLikelihood
