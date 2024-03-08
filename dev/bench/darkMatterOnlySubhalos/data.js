84d52f7fbf26eafcc1bea566f0e0c9c83ab27bc2
github-action-benchmark <github@users.noreply.github.com>
2024-03-08 07:10:19 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 05526e1732a8eea0cf597557561ecd170cccbfc7

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709847125532,
+  "lastUpdate": 1709881819276,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@obs.carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "abenson@obs.carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "distinct": true,
-          "id": "9bc93f77147ac2c9e30a6debc1767e11b70214c3",
-          "message": "fix: Correct equation, and improve model description",
-          "timestamp": "2023-06-08T08:39:10-07:00",
-          "tree_id": "4c3a18f7c504d6ed8c1af559994b7cf600955a92",
-          "url": "https://github.com/galacticusorg/galacticus/commit/9bc93f77147ac2c9e30a6debc1767e11b70214c3"
-        },
-        "date": 1686250139281,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Wall Time",
-            "value": 54.617,
-            "unit": "seconds",
-            "range": 0.322973837949787
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8792,6 +8762,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 24198.9059210542,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "05526e1732a8eea0cf597557561ecd170cccbfc7",
+          "message": "Merge pull request #574 from galacticusorg/featRamPressurePosition\n\nAdd a ram pressure force class which uses relative positions",
+          "timestamp": "2024-03-08T04:04:16Z",
+          "tree_id": "b6e25e2eeca8b4854f2df2c45009fdc0c0b8969b",
+          "url": "https://github.com/galacticusorg/galacticus/commit/05526e1732a8eea0cf597557561ecd170cccbfc7"
+        },
+        "date": 1709881818113,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 42.974,
+            "unit": "seconds",
+            "range": 0.0449043427739097
           }
         ]
       }
