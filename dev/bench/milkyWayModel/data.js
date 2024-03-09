9bb482df87c2cdaa4cfbc0379d89de99347486fa
github-action-benchmark <github@users.noreply.github.com>
2024-03-09 00:29:07 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for cd809cf990fd32e4d6950fa725537232b4d2c208

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709881828372,
+  "lastUpdate": 1709944147823,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "distinct": true,
-          "id": "d7e8d701de321cad9797a17d560893304a79bdfb",
-          "message": "fix: Serialize deadlock reporting in threaded tree evolution",
-          "timestamp": "2023-06-08T13:50:40-07:00",
-          "tree_id": "86a1940c08b46a1edd34e0bc30a6b2c7f898f1cb",
-          "url": "https://github.com/galacticusorg/galacticus/commit/d7e8d701de321cad9797a17d560893304a79bdfb"
-        },
-        "date": 1686268519294,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Milky Way model - Wall Time",
-            "value": 244.325,
-            "unit": "seconds",
-            "range": 0.570580844400586
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -10712,6 +10682,36 @@ window.BENCHMARK_DATA = {
             "name": "Milky Way model - Likelihood - localGroupStellarMassHaloMassRelation",
             "value": 13.6138228434182,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "cd809cf990fd32e4d6950fa725537232b4d2c208",
+          "message": "feat: Add a merger tree operator to prune branches below a mass threshold and after the final output time",
+          "timestamp": "2024-03-08T21:26:02Z",
+          "tree_id": "8df396ca2f6f923003c691d78bb79edefea0ee22",
+          "url": "https://github.com/galacticusorg/galacticus/commit/cd809cf990fd32e4d6950fa725537232b4d2c208"
+        },
+        "date": 1709944147291,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 144.013,
+            "unit": "seconds",
+            "range": 0.222719779095201
           }
         ]
       }
