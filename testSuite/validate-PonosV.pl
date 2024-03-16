4372afab9f39d21a430619ea863635c6ee61f9df
Andrew Benson <abenson@carnegiescience.edu>
2024-03-16 08:11:30 -0700

    feat: Validate using distribution of slopes

--- testSuite/validate-PonosV.pl
+++ testSuite/validate-PonosV.pl
@@ -144,9 +144,11 @@ print $statusSurfaceDensity.": Percentage of realizations above/below the PonosV
 my $nonZero     = which(($subhaloSurfaceDensity8 > 0.0) & ($subhaloSurfaceDensity9 > 0.0));
 my $alphas      = -log($subhaloSurfaceDensity8->($nonZero)        /$subhaloSurfaceDensity9->($nonZero)        )
                   /log(sqrt($massBoundMinimum8*$massBoundMaximum8)/sqrt($massBoundMinimum9*$massBoundMaximum9));
-my $alpha       = $alphas->average();
-my $statusSlope = abs($alpha-$alphaPonosV) < 0.2 ? "SUCCESS" : "FAIL";
-print $statusSlope.": Projected subhalo mass function slope Galacticus/PonosV: ".sprintf("%4.2f",$alpha)."/".sprintf("%4.2f",$alphaPonosV)."\n";
+(my $aboveSlope, my $belowSlope) = which_both($alphas > $alphaPonosV);
+my $percentageAboveSlope = 100.0*double(nelem($aboveSlope))/nelem($alphas);
+my $percentageBelowSlope = 100.0*double(nelem($belowSlope))/nelem($alphas);
+my $statusSlope = ($percentageAboveSlope > 5.0 || $percentageBelowSlope > 5.0) ? "SUCCESS" : "FAIL";
+print $statusSlope.": Percentage of realizations above/below the PonosV subhalo mass function slope: ".sprintf("%5.1f",$percentageAboveSlope)."/".sprintf("%5.1f",$percentageBelowSlope)."\n";
 
 # Interface with git.
 my $repo         = Git->repository(Directory => $ENV{'GALACTICUS_EXEC_PATH'});
@@ -182,8 +184,9 @@ $output =
     },
     slope =>
     {
-    	model  => $alpha      ->sclr(),
-	target => $alphaPonosV->sclr()
+	percentageBelow => $percentageBelowSlope->sclr(),
+	percentageAbove => $percentageAboveSlope->sclr(),
+	target          => $alphaPonosV         ->sclr()
     }
 };
 my $json = JSON::PP->new()->pretty()->encode($output);
