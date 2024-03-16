adefed903201ac98b0270fbbb9eefefb299280db
github-action-benchmark <github@users.noreply.github.com>
2024-03-16 06:33:28 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for 97efa132bd8578fd83bf04303d8c060c210aba3f

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710229943678,
+  "lastUpdate": 1710570807991,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abensonca@gmail.com",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "47ab81ae3e9a4086476941ec9dced579a87fd5a1",
-          "message": "Merge pull request #420 from galacticusorg/satelliteDestructionHelp\n\nDetect failure to destroy satellite halos",
-          "timestamp": "2023-06-21T23:31:05Z",
-          "tree_id": "0cd8f196cc3fdd8ab20782df8a7526eb4191599a",
-          "url": "https://github.com/galacticusorg/galacticus/commit/47ab81ae3e9a4086476941ec9dced579a87fd5a1"
-        },
-        "date": 1687401347851,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Milky Way model - Wall Time",
-            "value": 149.17,
-            "unit": "seconds",
-            "range": 0.179571712692209
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -10712,6 +10682,36 @@ window.BENCHMARK_DATA = {
             "name": "Milky Way model - Likelihood - localGroupStellarMassHaloMassRelation",
             "value": 8.57906746448903,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "97efa132bd8578fd83bf04303d8c060c210aba3f",
+          "message": "Merge pull request #583 from galacticusorg/orbitCalibration\n\nOrbit calibration",
+          "timestamp": "2024-03-16T02:37:27Z",
+          "tree_id": "d39ce6a0ac68f4b860f6e0f316e01a987fc7d42a",
+          "url": "https://github.com/galacticusorg/galacticus/commit/97efa132bd8578fd83bf04303d8c060c210aba3f"
+        },
+        "date": 1710570806835,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 396.953,
+            "unit": "seconds",
+            "range": 0.172075855353461
           }
         ]
       }
