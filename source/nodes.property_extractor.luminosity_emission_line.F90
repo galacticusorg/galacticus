19104507b28a272694918d1341efeb0750587350
Andrew Benson <abenson@carnegiescience.edu>
2024-03-19 14:59:44 +0000

    fix: Correct method name in descriptor

--- source/nodes.property_extractor.luminosity_emission_line.F90
+++ source/nodes.property_extractor.luminosity_emission_line.F90
@@ -68,10 +68,10 @@
    contains
      !![
      <methods>
-       <method description="Return a hashed descriptor of the object which incorporates the time and metallicity binning of the star formation history." method="luminosityHistoryHashedDescriptor"/>
-       <method description="Compute the mean luminosity of the stellar population in the given bin of the star formation history."                       method="luminosityMean"                   />
-       <method description="Return the index of the template time to use."                                                                               method="indexTemplateTime"                />
-       <method description="Return the index of the template luminosities to use."                                                                       method="indexTemplateNode"                />
+       <method description="Return a hashed descriptor of the object which incorporates the time and metallicity binning of the star formation history." method="historyHashedDescriptor"/>
+       <method description="Compute the mean luminosity of the stellar population in the given bin of the star formation history."                       method="luminosityMean"         />
+       <method description="Return the index of the template time to use."                                                                               method="indexTemplateTime"      />
+       <method description="Return the index of the template luminosities to use."                                                                       method="indexTemplateNode"      />
      </methods>
      !!]
      final     ::                            emissionLineLuminosityDestructor
