100a096a5ccf962068325a418651c774af10c263
github-action-benchmark <github@users.noreply.github.com>
2024-03-24 09:45:40 +0000

    add Dark matter-only subhalos validation (customSmallerIsBetter) benchmark result for f615861cadb2a4e73c7862356394a39e77f263da

---
+++ dev/valid/darkMatterOnlySubhalos/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711273540208,
+  "repoUrl": "https://github.com/galacticusorg/galacticus",
+  "entries": {
+    "Dark matter-only subhalos validation": [
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f615861cadb2a4e73c7862356394a39e77f263da",
+          "message": "Merge pull request #591 from galacticusorg/featReportDuplicateClasses\n\nImprove error reporting when duplicate classes are found",
+          "timestamp": "2024-03-24T05:37:04Z",
+          "tree_id": "7dd8521dbe621b14f19ed9f4671572b87f738798",
+          "url": "https://github.com/galacticusorg/galacticus/commit/f615861cadb2a4e73c7862356394a39e77f263da"
+        },
+        "date": 1711273539493,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloMassFunction",
+            "value": 16.8575596681005,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloRadialDistribution",
+            "value": 10.8696851475892,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
+            "value": 20405.9424512488,
+            "unit": "-logℒ"
+          }
+        ]
+      }
+    ]
+  }
+}
