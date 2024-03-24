739246e49e8fba33922f0fe39cb360968bb4073a
github-action-benchmark <github@users.noreply.github.com>
2024-03-24 02:00:24 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for f43288f4a2a7113da59a7f6bbf2e6da233a5609c

---
+++ dev/bench/milkyWay/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711245624758,
+  "repoUrl": "https://github.com/galacticusorg/galacticus",
+  "entries": {
+    "Milky Way model benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abensonca@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "f43288f4a2a7113da59a7f6bbf2e6da233a5609c",
+          "message": "fix: Specify `shell` for Actions steps",
+          "timestamp": "2024-03-23T21:21:49Z",
+          "url": "https://github.com/galacticusorg/galacticus/commit/f43288f4a2a7113da59a7f6bbf2e6da233a5609c"
+        },
+        "date": 1711245624424,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 462.973,
+            "unit": "seconds",
+            "range": 0.533049810048917
+          }
+        ]
+      }
+    ]
+  }
+}
