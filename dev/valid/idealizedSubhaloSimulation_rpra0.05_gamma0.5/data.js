0476cba31ceb379df391b876605a4c7fd81f4cdb
github-action-benchmark <github@users.noreply.github.com>
2024-03-24 20:02:22 +0000

    add Idealized subhalo simulations (rₚ/rₐ=0.05; γ=0.5) (customSmallerIsBetter) benchmark result for dd6e1662f0acba14f218907cc55de615ce90eb9d

---
+++ dev/valid/idealizedSubhaloSimulation_rpra0.05_gamma0.5/data.js
@@ -0,0 +1,45 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711310542349,
+  "repoUrl": "https://github.com/galacticusorg/galacticus",
+  "entries": {
+    "Idealized subhalo simulations (rₚ/rₐ=0.05; γ=0.5)": [
+      {
+        "commit": {
+          "author": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "committer": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "id": "dd6e1662f0acba14f218907cc55de615ce90eb9d",
+          "message": "fix: Allow explicit specification of artifact name",
+          "timestamp": "2024-03-24T16:02:18Z",
+          "url": "https://github.com/galacticusorg/galacticus/commit/dd6e1662f0acba14f218907cc55de615ce90eb9d"
+        },
+        "date": 1711310541780,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.05; γ=0.5) - Likelihood - satelliteBoundMass",
+            "value": 21.5812642916508,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.05; γ=0.5) - Likelihood - satelliteRadiusVelocityMaximum",
+            "value": 7.89676175266811,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.05; γ=0.5) - Likelihood - satelliteVelocityMaximum",
+            "value": 9.84983054159055,
+            "unit": "-logℒ"
+          }
+        ]
+      }
+    ]
+  }
+}
