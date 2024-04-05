647b41c6f363dacccab3f55b0d48f214fdeb26ac
github-action-benchmark <github@users.noreply.github.com>
2024-04-05 06:46:56 +0000

    add Dark matter-only subhalos benchmarks (customSmallerIsBetter) benchmark result for 49bb79e525b43544af3c0342b3199b8e3f656141

--- dev/bench/darkMatterOnlySubhalos/data.js
+++ dev/bench/darkMatterOnlySubhalos/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712107830215,
+  "lastUpdate": 1712299616363,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Dark matter-only subhalos benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@obs.carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "abenson@obs.carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "distinct": true,
-          "id": "4c70ce3c81e7f2a407ca546ff3baa2ff272b2025",
-          "message": "feat: Add a `galacticFilter` to select the most massive branch",
-          "timestamp": "2023-07-19T08:48:42-07:00",
-          "tree_id": "a92196979c460ef8b9a16f704989aecf4a6c6a0f",
-          "url": "https://github.com/galacticusorg/galacticus/commit/4c70ce3c81e7f2a407ca546ff3baa2ff272b2025"
-        },
-        "date": 1689791830618,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Dark Matter Only Subhalos - Wall Time",
-            "value": 57.647,
-            "unit": "seconds",
-            "range": 0.100717922932756
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -8760,6 +8730,36 @@ window.BENCHMARK_DATA = {
             "name": "Dark Matter Only Subhalos - Likelihood - subhaloVelocityMaximumMean",
             "value": 20597.2509639535,
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "49bb79e525b43544af3c0342b3199b8e3f656141",
+          "message": "Merge pull request #598 from galacticusorg/fixJiang2014OrbitsFileLock\n\nFix file locking in the `virialOrbitJiang2014` class",
+          "timestamp": "2024-04-05T02:44:00Z",
+          "tree_id": "9dec9b16322946f41101f690f9b298053c8dd390",
+          "url": "https://github.com/galacticusorg/galacticus/commit/49bb79e525b43544af3c0342b3199b8e3f656141"
+        },
+        "date": 1712299614699,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos - Wall Time",
+            "value": 51.504,
+            "unit": "seconds",
+            "range": 0.0662751838926614
           }
         ]
       }
