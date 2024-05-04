34dbbe5a6e0a0fda81d4c1062c131e2a9648b508
github-action-benchmark <github@users.noreply.github.com>
2024-05-04 21:41:51 +0000

    add Idealized subhalo simulations (rₚ/rₐ=0.4; γ=0.0) (customSmallerIsBetter) benchmark result for 9178bdef24ce965d2519bffbe7e3c2c35cfda6ba

--- dev/bench/idealizedSubhaloSimulation_rpra0.4_gamma0.0/data.js
+++ dev/bench/idealizedSubhaloSimulation_rpra0.4_gamma0.0/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714798097552,
+  "lastUpdate": 1714858911077,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Idealized subhalo simulations (rₚ/rₐ=0.4; γ=0.0)": [
@@ -567,6 +567,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/galacticusorg/galacticus/commit/8b2d9cc16747e1474f9541a994a8e22d6cfc5f4d"
         },
         "date": 1714798096947,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.4; γ=0.0) - Likelihood - satelliteBoundMass",
+            "value": 26.8526923426831,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.4; γ=0.0) - Likelihood - satelliteRadiusVelocityMaximum",
+            "value": 12.8565298045395,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.4; γ=0.0) - Likelihood - satelliteVelocityMaximum",
+            "value": 11.5356195289584,
+            "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9178bdef24ce965d2519bffbe7e3c2c35cfda6ba",
+          "message": "Merge pull request #608 from galacticusorg/fixFFTWConditionalCompile\n\nDo not attempt to use FFTW3 functions if FFTW is not available",
+          "timestamp": "2024-05-04T16:43:35Z",
+          "tree_id": "143a2d9d031ea9c3756fef0c19c1f5f722d8d735",
+          "url": "https://github.com/galacticusorg/galacticus/commit/9178bdef24ce965d2519bffbe7e3c2c35cfda6ba"
+        },
+        "date": 1714858910277,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
