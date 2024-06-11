3704af1816a010b2b5fbdef3d9b57994e4f80bed
github-action-benchmark <github@users.noreply.github.com>
2024-06-11 02:40:24 +0000

    add Idealized subhalo simulations (rₚ/rₐ=0.2; γ=0.0) (customSmallerIsBetter) benchmark result for 4ad361755c0d321c89156b0980bb54b295e88eeb

--- dev/bench/idealizedSubhaloSimulation_rpra0.2_gamma0.0/data.js
+++ dev/bench/idealizedSubhaloSimulation_rpra0.2_gamma0.0/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718052824702,
+  "lastUpdate": 1718073624663,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Idealized subhalo simulations (rₚ/rₐ=0.2; γ=0.0)": [
@@ -1072,6 +1072,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/galacticusorg/galacticus/commit/ae9ddd16decd3d03a25d12d10e5dd825de933cd8"
         },
         "date": 1718052824038,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.2; γ=0.0) - Likelihood - satelliteBoundMass",
+            "value": 13.9314461307549,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.2; γ=0.0) - Likelihood - satelliteRadiusVelocityMaximum",
+            "value": 6.5537461106679,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.2; γ=0.0) - Likelihood - satelliteVelocityMaximum",
+            "value": 5.00086399252571,
+            "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "4ad361755c0d321c89156b0980bb54b295e88eeb",
+          "message": "feat: Include time of merging in data recorded for merged subhalos",
+          "timestamp": "2024-06-10T14:35:05-07:00",
+          "tree_id": "9a19ec4a657d7f202f5e8f5a5f66542a9db21f2b",
+          "url": "https://github.com/galacticusorg/galacticus/commit/4ad361755c0d321c89156b0980bb54b295e88eeb"
+        },
+        "date": 1718073624025,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
