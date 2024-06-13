bb7e3bd6793f8212eb9d3ab7aedf5022f75eb02b
github-action-benchmark <github@users.noreply.github.com>
2024-06-13 22:35:41 +0000

    add Idealized subhalo simulations (rₚ/rₐ=0.01; γ=1.5) (customSmallerIsBetter) benchmark result for 1549918dad1ef0fa23d15cb49e152059c518559d

--- dev/bench/idealizedSubhaloSimulation_rpra0.01_gamma1.5/data.js
+++ dev/bench/idealizedSubhaloSimulation_rpra0.01_gamma1.5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718159528016,
+  "lastUpdate": 1718318141167,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Idealized subhalo simulations (rₚ/rₐ=0.01; γ=1.5)": [
@@ -1150,6 +1150,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/galacticusorg/galacticus/commit/56d90f3a6cb4d70076de74229d29aa55319d6420"
         },
         "date": 1718159526918,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.01; γ=1.5) - Likelihood - satelliteBoundMass",
+            "value": 57.2226085652273,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.01; γ=1.5) - Likelihood - satelliteRadiusVelocityMaximum",
+            "value": 29.5560046299289,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.01; γ=1.5) - Likelihood - satelliteVelocityMaximum",
+            "value": 23.0366044363298,
+            "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1549918dad1ef0fa23d15cb49e152059c518559d",
+          "message": "Merge pull request #626 from galacticusorg/featJHLFAnalysis\n\nAdd an `outputAnalysis` class to compute high-z H and J-band luminosity functions",
+          "timestamp": "2024-06-13T15:36:26Z",
+          "tree_id": "98180f3e3a40e91a2134b1221370e6f76995f38f",
+          "url": "https://github.com/galacticusorg/galacticus/commit/1549918dad1ef0fa23d15cb49e152059c518559d"
+        },
+        "date": 1718318140033,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
