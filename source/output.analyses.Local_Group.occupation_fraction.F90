263dac9c7a1195565ea4860361b6eaa5111d52d5
Andrew Benson <abenson@carnegiescience.edu>
2024-06-15 08:30:10 -0700

    fix: Add missing destructors
    Also correct mis-spelled `constructorAssign` directive.

--- source/output.analyses.Local_Group.occupation_fraction.F90
+++ source/output.analyses.Local_Group.occupation_fraction.F90
@@ -428,6 +428,7 @@ contains
     <objectDestructor name="outputAnalysisDistributionOperator_"                   />
     <objectDestructor name="galacticFilterHaloNotIsolated_"                        />
     <objectDestructor name="galacticFilterHostMassRange_"                          />
+    <objectDestructor name="galacticFilterSurveyGeometry_"                         />
     <objectDestructor name="galacticFilter_"                                       />
     <objectDestructor name="surveyGeometry_"                                       />
     !!]
