8d66ee8094056b4cbfc3290ac361568732f69868
github-action-benchmark <github@users.noreply.github.com>
2024-09-27 20:52:42 +0000

    add Idealized subhalo simulations (rₚ/rₐ=0.2; γ=0.0) (customSmallerIsBetter) benchmark result for a11ac6ab603648f4ab70ab8c91756ec8e3fec9b9

--- dev/bench/idealizedSubhaloSimulation_rpra0.2_gamma0.0/data.js
+++ dev/bench/idealizedSubhaloSimulation_rpra0.2_gamma0.0/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727069684145,
+  "lastUpdate": 1727470362330,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Idealized subhalo simulations (rₚ/rₐ=0.2; γ=0.0)": [
@@ -3308,6 +3308,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/galacticusorg/galacticus/commit/7f2a178a2ac175eaaa1b89dee2f23ee99070855b"
         },
         "date": 1727069683339,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.2; γ=0.0) - Likelihood - satelliteBoundMass",
+            "value": "13.931453287519465",
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.2; γ=0.0) - Likelihood - satelliteRadiusVelocityMaximum",
+            "value": "6.553744936818157",
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.2; γ=0.0) - Likelihood - satelliteVelocityMaximum",
+            "value": "5.0008631128237795",
+            "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a11ac6ab603648f4ab70ab8c91756ec8e3fec9b9",
+          "message": "Merge pull request #710 from galacticusorg/featDiskInstabilityRetainedFractions\n\nAllow different fractions of angular momentum to be retained in disk and spheroid during disk instabilities",
+          "timestamp": "2024-09-27T13:58:28Z",
+          "tree_id": "f8fe09995b75aad4c6e9f8b051f983427f0d0715",
+          "url": "https://github.com/galacticusorg/galacticus/commit/a11ac6ab603648f4ab70ab8c91756ec8e3fec9b9"
+        },
+        "date": 1727470361508,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
