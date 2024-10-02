14edb02352b3573a723fa24fb66a4d3e3f09a717
Andrew Benson <abenson@carnegiescience.edu>
2024-10-02 15:15:21 -0700

    feat: Add an option to set dark matter profile scale radii on mass increases by a given factor looking "backwards" along each branch
    Normally, if the scale radius is to be update when the halo mass increases by a factor of, for example, 2, we start from the tip of the branch, set a scale radius there, and walk forwards up the branch until the mass is increased by this factor. An alternative is introduced in which the scale radius is always set for the end fo the branch (just before it mergers), and propagated backward until the mass of trhe branch is decreased by a factor 2. This can be useful to ensure that the scale radius is correct at the time of branch merging.

--- source/nodes.operators.physics.dark_matter_profile_scale.set.F90
+++ source/nodes.operators.physics.dark_matter_profile_scale.set.F90
@@ -21,15 +21,17 @@
   Implements a node operator class that sets dark matter profile scale radius.
   !!}
 
+  use :: Galacticus_Nodes          , only : treeNodeLinkedList
   use :: Dark_Matter_Profile_Scales, only : darkMatterProfileScaleRadiusClass
   
   type :: branchStack
      !!{
      Type used to create a stack of branches being processed.
      !!}
-     double precision                       :: mass              , radiusScale
-     type            (branchStack), pointer :: next     => null()
-     integer         (kind_int8  )          :: indexTip
+     double precision                              :: mass              , radiusScale
+     type            (branchStack       ), pointer :: next     => null()
+     type            (treeNodeLinkedList), pointer :: node     => null()
+     integer         (kind_int8         )          :: indexTip
   end type branchStack
   
   !![
@@ -47,6 +49,7 @@
      class           (darkMatterProfileScaleRadiusClass), pointer :: darkMatterProfileScaleRadius_ => null()
      type            (branchStack                      ), pointer :: branch                        => null()
      double precision                                             :: factorReset
+     logical                                                      :: forward
    contains
      final     ::                       darkMatterProfileScaleSetConstructorDestructor
      procedure :: nodeTreeInitialize => darkMatterProfileScaleSetNodeTreeInitialize
@@ -73,6 +76,7 @@ contains
     type            (inputParameters                      ), intent(inout) :: parameters
     class           (darkMatterProfileScaleRadiusClass    ), pointer       :: darkMatterProfileScaleRadius_
     double precision                                                       :: factorReset
+    logical                                                                :: forward
 
     !![
     <inputParameter>
@@ -81,9 +85,15 @@ contains
       <description>The factor by which a node must increase in mass before its scale radius is reset.</description>
       <source>parameters</source>
     </inputParameter>
+    <inputParameter>
+      <name>forward</name>
+      <defaultValue>.true.</defaultValue>
+      <description>If true, updates to the scale radius are determined by walking forward along the branch until the mass exceeds by {\normalfont \ttfamily [factor]} that for which the scale radius was last computed. If false, updates are computed by walking backward along the branch until the mass is less than $1/${\normalfont \ttfamily [factor]} of that for which the scale radius was last computed.</description>
+      <source>parameters</source>
+    </inputParameter>
     <objectBuilder class="darkMatterProfileScaleRadius" name="darkMatterProfileScaleRadius_" source="parameters"/>
     !!]
-    self=nodeOperatorDarkMatterProfileScaleSet(factorReset,darkMatterProfileScaleRadius_)
+    self=nodeOperatorDarkMatterProfileScaleSet(factorReset,forward,darkMatterProfileScaleRadius_)
     !![
     <inputParametersValidate source="parameters"/>
     <objectDestructor name="darkMatterProfileScaleRadius_"/>
@@ -91,7 +101,7 @@ contains
     return
   end function darkMatterProfileScaleSetConstructorParameters
 
-  function darkMatterProfileScaleSetConstructorInternal(factorReset,darkMatterProfileScaleRadius_) result(self)
+  function darkMatterProfileScaleSetConstructorInternal(factorReset,forward,darkMatterProfileScaleRadius_) result(self)
     !!{
     Constructor for the {\normalfont \ttfamily darkMatterProfileScaleSet} node operator class which takes a parameter set as input.
     !!}
@@ -100,8 +110,9 @@ contains
     type            (nodeOperatorDarkMatterProfileScaleSet)                        :: self
     class           (darkMatterProfileScaleRadiusClass    ), intent(in   ), target :: darkMatterProfileScaleRadius_
     double precision                                       , intent(in   )         :: factorReset
+    logical                                                , intent(in   )         :: forward
     !![
-    <constructorAssign variables="factorReset, *darkMatterProfileScaleRadius_"/>
+    <constructorAssign variables="factorReset, forward, *darkMatterProfileScaleRadius_"/>
     !!]
 
     return
@@ -128,29 +139,70 @@ contains
     implicit none
     class           (nodeOperatorDarkMatterProfileScaleSet), intent(inout), target  :: self
     type            (treeNode                             ), intent(inout), target  :: node
-    class           (nodeComponentBasic                   )               , pointer :: basic
+    class           (nodeComponentBasic                   )               , pointer :: basic              , basicDescendant
     class           (nodeComponentDarkMatterProfile       )               , pointer :: darkMatterProfile
     type            (branchStack                          )               , pointer :: branchPrevious
-    double precision                                                                :: radiusScalePrevious
+    type            (treeNodeLinkedList                   )               , pointer :: nodeNext
+    type            (treeNode                             )               , pointer :: nodeDescendant
+    double precision                                                                :: radiusScalePrevious, radiusScaleUpdated
     logical                                                                         :: radiusScaleNew
 
     if (self%factorReset > 0.0d0) then
        basic => node%basic()
        if (.not.associated(node%firstChild)) then
-          ! This is the tip of a branch. Push the branch onto the stack, and record the initial mass.
+          ! This is the tip of a branch. Push the branch onto the stack, and build a stack of nodes at which new scale radii must
+          ! be computed.
           branchPrevious => self%branch
           nullify (self%branch)
           allocate(self%branch)
           self%branch%next     => branchPrevious
-          self%branch%mass     =  basic         %mass    ()
           self%branch%indexTip =  node          %uniqueID()
+          if (self%forward) then
+             self%branch%mass=basic%mass()
+          else
+             nodeDescendant => node
+             do while (nodeDescendant%isPrimaryProgenitor())
+                nodeDescendant => nodeDescendant%parent
+             end do
+             allocate(self%branch%node)
+             basicDescendant                  => nodeDescendant %basic()
+             self           %branch%node%node => nodeDescendant
+             self           %branch%node%next => null()
+             self           %branch%mass      =  basicDescendant%mass ()
+             do while (associated(nodeDescendant))
+                basicDescendant => nodeDescendant%basic()
+                if (basicDescendant%mass() < self%branch%mass/self%factorReset) then
+                   nodeNext => self%branch%node
+                   nullify (self%branch%node)
+                   allocate(self%branch%node)
+                   self%branch%mass      =  basicDescendant%mass()
+                   self%branch%node%node => nodeDescendant
+                   self%branch%node%next => nodeNext
+                end if
+                nodeDescendant => nodeDescendant%firstChild
+             end do
+          end if
           ! In this case we must always compute a new scale radius.
           radiusScaleNew=.true.
        else
-          ! This is not a branch tip - a new scale radius is computed only if the prior mass is exceeded by the required factor.
-          radiusScaleNew=basic%mass() > self%factorReset*self%branch%mass
-          ! If a new scale radius is to be computed, record the mass at which this occurred.
-          if (radiusScaleNew) self%branch%mass=basic%mass()
+          ! This is not a branch tip.
+          if (self%forward) then
+             ! A new scale radius is computed only if the prior mass is exceeded by the required factor.
+             radiusScaleNew=basic%mass() > self%factorReset*self%branch%mass
+             ! If a new scale radius is to be computed, record the mass at which this occurred.
+             if (radiusScaleNew) self%branch%mass=basic%mass()
+          else
+             ! A new scale radius is computed only if we have reached the prior node for which it was computed.
+             radiusScaleNew=associated(self%branch%node%node,node)
+             if (radiusScaleNew) then
+                nodeNext => self%branch%node%next
+                deallocate(self%branch%node)
+                self%branch%node => nodeNext
+             end if
+             ! If there are no more nodes along this branch at which to compute the scale radius, mark that scale radius is not to
+             ! be computed.
+             if (.not.associated(self%branch%node)) radiusScaleNew=.false.
+          end if
        end if
     else
        ! The reset factor is zero - we assign a new scale radius for every node.
@@ -159,8 +211,27 @@ contains
     darkMatterProfile => node%darkMatterProfile(autoCreate=.true.)
     if (radiusScaleNew) then
        ! Compute a new scale radius, and save it on our current branch.
-       radiusScalePrevious=self%darkMatterProfileScaleRadius_%radius(node)
-       if (associated(self%branch)) self%branch%radiusScale=radiusScalePrevious
+       if (self%forward .or. self%factorReset <= 0.0d0) then
+          nodeDescendant =>             node
+       else
+          nodeDescendant => self%branch%node%node
+       end if
+       radiusScaleUpdated=self%darkMatterProfileScaleRadius_%radius(nodeDescendant)
+       ! If the current node was one for which we needed to compute a new scale radius, pop the node stack here. This can happen
+       ! if the branch tip node is a node for which we must compute the scale radius.
+       if (self%factorReset > 0.0d0 .and. .not.self%forward  .and. associated(self%branch%node%node,node)) then
+          nodeNext => self%branch%node%next
+          deallocate(self%branch%node)
+          self%branch%node => nodeNext
+       end if
+       ! If using the "forward" approach, or this is the branch tip, we update the actual scale radius now, as we want to apply it
+       ! to this node. Otherwise, use the previous value.
+       if (self%factorReset <= 0.0d0 .or. self%forward .or. .not.associated(node%firstChild)) then
+          radiusScalePrevious=            radiusScaleUpdated
+       else
+          radiusScalePrevious=self%branch%radiusScale
+       end if
+       if (associated(self%branch)) self%branch%radiusScale=radiusScaleUpdated
     else
        ! Use the previously computed scale radius for this branch.
        radiusScalePrevious=self%branch%radiusScale
