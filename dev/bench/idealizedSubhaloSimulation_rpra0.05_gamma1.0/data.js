68ddffd360aea729bc605826aafa301c730f3e48
github-action-benchmark <github@users.noreply.github.com>
2024-10-09 05:45:34 +0000

    add Idealized subhalo simulations (rₚ/rₐ=0.05; γ=1.0) (customSmallerIsBetter) benchmark result for 1d9bd33ff24f246ef6308c5db0c18f6b69ae5278

--- dev/bench/idealizedSubhaloSimulation_rpra0.05_gamma1.0/data.js
+++ dev/bench/idealizedSubhaloSimulation_rpra0.05_gamma1.0/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728337942296,
+  "lastUpdate": 1728452734513,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Idealized subhalo simulations (rₚ/rₐ=0.05; γ=1.0)": [
@@ -3575,6 +3575,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/galacticusorg/galacticus/commit/b5b68023ef964b251e13a79791ce095afb0dd7a5"
         },
         "date": 1728337941057,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.05; γ=1.0) - Likelihood - satelliteBoundMass",
+            "value": "69.36737845286848",
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.05; γ=1.0) - Likelihood - satelliteRadiusVelocityMaximum",
+            "value": "39.245729892367294",
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.05; γ=1.0) - Likelihood - satelliteVelocityMaximum",
+            "value": "32.23649324777072",
+            "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d9bd33ff24f246ef6308c5db0c18f6b69ae5278",
+          "message": "Merge pull request #714 from galacticusorg/depup/lesgourg/class_public\n\nfix(deps): update lesgourg/class_public to 3.2.5",
+          "timestamp": "2024-10-08T23:31:20Z",
+          "tree_id": "5f8854e1cb78c48be87c7e375468c128f7ca8607",
+          "url": "https://github.com/galacticusorg/galacticus/commit/1d9bd33ff24f246ef6308c5db0c18f6b69ae5278"
+        },
+        "date": 1728452733564,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
