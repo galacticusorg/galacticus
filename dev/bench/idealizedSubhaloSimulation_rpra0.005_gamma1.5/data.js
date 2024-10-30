9ee0eee66450f682e3402dad93b1e646a810fb61
github-action-benchmark <github@users.noreply.github.com>
2024-10-30 10:30:37 +0000

    add Idealized subhalo simulations (rₚ/rₐ=0.005; γ=1.5) (customSmallerIsBetter) benchmark result for d3a981eeaf261e216556f72b776631d8557991ff

--- dev/bench/idealizedSubhaloSimulation_rpra0.005_gamma1.5/data.js
+++ dev/bench/idealizedSubhaloSimulation_rpra0.005_gamma1.5/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730187400844,
+  "lastUpdate": 1730284237029,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Idealized subhalo simulations (rₚ/rₐ=0.005; γ=1.5)": [
@@ -4111,6 +4111,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/galacticusorg/galacticus/commit/ce0ddd69a8f3fc9f3025e415359ae14a97a1c4df"
         },
         "date": 1730187400244,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.005; γ=1.5) - Likelihood - satelliteBoundMass",
+            "value": "48.57555996098561",
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.005; γ=1.5) - Likelihood - satelliteRadiusVelocityMaximum",
+            "value": "35.056147793037816",
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Idealized Subhalo Simulation (rₚ/rₐ=0.005; γ=1.5) - Likelihood - satelliteVelocityMaximum",
+            "value": "23.74933605926893",
+            "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d3a981eeaf261e216556f72b776631d8557991ff",
+          "message": "Merge pull request #732 from galacticusorg/treeBuildControllerMainBranch\n\nImplement a merger tree build controller class which builds trees containing only the main branch and certain descendants",
+          "timestamp": "2024-10-30T00:01:15Z",
+          "tree_id": "0f5d9880e45290e7bf0ecbee24a9de4709ff316c",
+          "url": "https://github.com/galacticusorg/galacticus/commit/d3a981eeaf261e216556f72b776631d8557991ff"
+        },
+        "date": 1730284235861,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
