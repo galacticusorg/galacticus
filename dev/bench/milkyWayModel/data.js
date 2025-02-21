786b368957f23b1dcd042abee12ef6e76160f709
github-action-benchmark <github@users.noreply.github.com>
2025-02-21 04:09:11 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for eb389a34da021edba4971aee5be6c473f3ddcb83

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740007831570,
+  "lastUpdate": 1740110951493,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abensonca@gmail.com",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "723170fb1690257be9b0588c0a83c9e559a584ae",
-          "message": "Merge pull request #611 from galacticusorg/fixUseDependencies\n\nCorrect logic used in parsing preprocessor conditionals when determining module `use` dependencies",
-          "timestamp": "2024-05-07T19:34:26Z",
-          "tree_id": "2043658ed9e475712830735bee55ae0d15d2927b",
-          "url": "https://github.com/galacticusorg/galacticus/commit/723170fb1690257be9b0588c0a83c9e559a584ae"
-        },
-        "date": 1715128564347,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Milky Way model - Wall Time",
-            "value": 460.331,
-            "unit": "seconds",
-            "range": 0.385511219027315
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -10652,6 +10622,36 @@ window.BENCHMARK_DATA = {
             "name": "Milky Way model - Likelihood - localGroupStellarMassHaloMassRelation",
             "value": "9.635242757023528",
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eb389a34da021edba4971aee5be6c473f3ddcb83",
+          "message": "Merge pull request #799 from galacticusorg/fixBlackHoleMergers\n\nSet the mass of the central black hole correctly after a merger",
+          "timestamp": "2025-02-20T15:38:24Z",
+          "tree_id": "70b5c41af6f2220ec8b0f3cceeeec58eaba84ab7",
+          "url": "https://github.com/galacticusorg/galacticus/commit/eb389a34da021edba4971aee5be6c473f3ddcb83"
+        },
+        "date": 1740110950552,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 44.021,
+            "unit": "seconds",
+            "range": 0.24431311876359
           }
         ]
       }
