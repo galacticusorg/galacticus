33afe470410555c8092ba2e41b582c37ff473c03
github-action-benchmark <github@users.noreply.github.com>
2025-04-11 22:06:03 +0000

    add Milky Way model benchmarks (customSmallerIsBetter) benchmark result for 45a79a8929de9e3d9f2d0fe2e33725bebbab9c54

--- dev/bench/milkyWayModel/data.js
+++ dev/bench/milkyWayModel/data.js
@@ -1,38 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744323599777,
+  "lastUpdate": 1744409162959,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Milky Way model benchmarks": [
-      {
-        "commit": {
-          "author": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "committer": {
-            "email": "abenson@carnegiescience.edu",
-            "name": "Andrew Benson",
-            "username": "abensonca"
-          },
-          "distinct": true,
-          "id": "61fb717b4259bc703919d1ea5eee5d215aeddf2a",
-          "message": "fix: Correct numerical calculations of mode masses for transfer functions",
-          "timestamp": "2024-06-26T14:27:13-07:00",
-          "tree_id": "b667cb4d55523956c454671235cdc886b89b5802",
-          "url": "https://github.com/galacticusorg/galacticus/commit/61fb717b4259bc703919d1ea5eee5d215aeddf2a"
-        },
-        "date": 1719455207219,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Milky Way model - Wall Time",
-            "value": 351.892,
-            "unit": "seconds",
-            "range": 0.185509029425836
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -10656,6 +10626,34 @@ window.BENCHMARK_DATA = {
             "name": "Milky Way model - Likelihood - localGroupStellarMassHaloMassRelation",
             "value": "14.647687720893128",
             "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "committer": {
+            "name": "Andrew Benson",
+            "username": "abensonca",
+            "email": "abenson@carnegiescience.edu"
+          },
+          "id": "45a79a8929de9e3d9f2d0fe2e33725bebbab9c54",
+          "message": "fix: Merge branch 'master' of github.com:galacticusorg/galacticus",
+          "timestamp": "2025-04-11T20:16:12Z",
+          "url": "https://github.com/galacticusorg/galacticus/commit/45a79a8929de9e3d9f2d0fe2e33725bebbab9c54"
+        },
+        "date": 1744409162256,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Milky Way model - Wall Time",
+            "value": 104.102,
+            "unit": "seconds",
+            "range": 0.137744691367198
           }
         ]
       }
