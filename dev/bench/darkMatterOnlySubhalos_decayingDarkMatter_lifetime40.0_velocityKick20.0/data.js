9f605a070e9480270263e6f996ef5c410ba87dc7
github-action-benchmark <github@users.noreply.github.com>
2025-04-16 05:50:41 +0000

    add Decaying dark matter-only subhalos benchmarks (τ=40 Gyr; vₖ=20 km/s) (customSmallerIsBetter) benchmark result for e57a3406a020a1da90a9145ee3c36ca435d45219

--- dev/bench/darkMatterOnlySubhalos_decayingDarkMatter_lifetime40.0_velocityKick20.0/data.js
+++ dev/bench/darkMatterOnlySubhalos_decayingDarkMatter_lifetime40.0_velocityKick20.0/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744409221528,
+  "lastUpdate": 1744782641105,
   "repoUrl": "https://github.com/galacticusorg/galacticus",
   "entries": {
     "Decaying dark matter-only subhalos benchmarks (τ=40 Gyr; vₖ=20 km/s)": [
@@ -1219,6 +1219,40 @@ window.BENCHMARK_DATA = {
           {
             "name": " (τ=40.0 Gyr; vₖ=20.0 km/s) - Likelihood - orbital radius function",
             "value": "24.950863109861032",
+            "unit": "-logℒ"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e57a3406a020a1da90a9145ee3c36ca435d45219",
+          "message": "Merge pull request #828 from galacticusorg/fixEnergyRadiatedReset\n\nRadiated energy was incorrectly set to zero when a halo became a subhalo",
+          "timestamp": "2025-04-15T22:50:49Z",
+          "tree_id": "7f50759045fad099f2f5d8ea8020831cea38faf8",
+          "url": "https://github.com/galacticusorg/galacticus/commit/e57a3406a020a1da90a9145ee3c36ca435d45219"
+        },
+        "date": 1744782640309,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": " (τ=40.0 Gyr; vₖ=20.0 km/s) - Likelihood - mass function",
+            "value": "12.302862641406291",
+            "unit": "-logℒ"
+          },
+          {
+            "name": " (τ=40.0 Gyr; vₖ=20.0 km/s) - Likelihood - orbital radius function",
+            "value": "24.66728066255219",
             "unit": "-logℒ"
           }
         ]
