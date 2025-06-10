a5fe1100bd900af2a7603cd62058c7002384ee3b
Andrew Benson <abenson@carnegiescience.edu>
2025-06-10 09:33:48 -0700

    feat: Add a warm dark matter reference model
    This is implemented as a set of parameter changes that act upon our cold dark matter reference model.
    
    Includes validation models to compare with the Symphony and COZMIC simulations.

---
+++ testSuite/validate-darkMatterOnlySubhalos-Symphony.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import sys
+import os
+import subprocess
+import validate
+
+# Run models to validate a dark matter only subhalo evolution model against data from the Symphony and COZMIC suites.
+# Andrew Benson (09-June-2025)
+
+# Create output path.
+try:
+    os.mkdir("outputs")
+except FileExistsError:
+    pass
+
+# Run the validation model for Symphony CDM.
+status = subprocess.run("cd ..; ./Galacticus.exe testSuite/parameters/validate_darkMatterOnlySubhalos_Symphony_CDM.xml parameters/reference/changeSymphony.xml",shell=True)
+if status.returncode != 0:
+    print("FAILED: dark matter-only subhalos validation model (CDM) failed to run")
+    sys.exit()
+
+# Extract and validate the likelihoods.
+validate.extract("outputs/validate_darkMatterOnlySubhalos_Symphony_CDM.hdf5","Dark Matter Only Subhalos (Symphony Milky Way)","darkMatterOnlySubhalosSymphonyMilkyWay","testSuite/parameters/validate_darkMatterOnlySubhalos_Symphony_CDM.xml")
+
+print("SUCCESS: dark matter-only subhalos validation model (CDM)")
+
+
+# Run the validation model COZMIC WDM 3keV.
+status = subprocess.run("cd ..; ./Galacticus.exe testSuite/parameters/validate_darkMatterOnlySubhalos_Symphony_CDM.xml parameters/reference/changeSymphony.xml parameters/reference/warmDarkMatter.xml testSuite/parameters/validate_darkMatterOnlySubhalos_COZMIC_WDM:3keV.xml",shell=True)
+if status.returncode != 0:
+    print("FAILED: dark matter-only subhalos validation model (WDM 3keV) failed to run")
+    sys.exit()
+
+# Extract and validate the likelihoods.
+validate.extract("outputs/validate_darkMatterOnlySubhalos_COZMIC_WDM:3keV.hdf5","Dark Matter Only Subhalos (COZMIC WDM 3keV Milky Way)","darkMatterOnlySubhalosCOZMICWDM3keVMilkyWay","testSuite/parameters/validate_darkMatterOnlySubhalos_Symphony_CDM.xml")
+
+print("SUCCESS: dark matter-only subhalos validation model (WDM 3keV)")
