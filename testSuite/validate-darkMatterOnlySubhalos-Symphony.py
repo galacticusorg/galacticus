c13e1b1bc7975ac973b104f5fe6b407212a3ed7a
Andrew Benson <abenson@carnegiescience.edu>
2025-06-12 15:20:46 -0700

    fix: Separate CDM and WDM 3keV validations
    This allows them to be processed separately into two different sets of pages

--- testSuite/validate-darkMatterOnlySubhalos-Symphony.py
+++ testSuite/validate-darkMatterOnlySubhalos-Symphony.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import validate
 
-# Run models to validate a dark matter only subhalo evolution model against data from the Symphony and COZMIC suites.
+# Run models to validate a dark matter only subhalo evolution model against data from the Symphony suite.
 # Andrew Benson (09-June-2025)
 
 # Create output path.
@@ -23,15 +23,3 @@ if status.returncode != 0:
 validate.extract("outputs/validate_darkMatterOnlySubhalos_Symphony_CDM.hdf5","Dark Matter Only Subhalos (Symphony Milky Way)","darkMatterOnlySubhalosSymphonyMilkyWay","testSuite/parameters/validate_darkMatterOnlySubhalos_Symphony_CDM.xml")
 
 print("SUCCESS: dark matter-only subhalos validation model (CDM)")
-
-
-# Run the validation model COZMIC WDM 3keV.
-status = subprocess.run("cd ..; ./Galacticus.exe testSuite/parameters/validate_darkMatterOnlySubhalos_Symphony_CDM.xml parameters/reference/changeSymphony.xml parameters/reference/warmDarkMatter.xml testSuite/parameters/validate_darkMatterOnlySubhalos_COZMIC_WDM:3keV.xml",shell=True)
-if status.returncode != 0:
-    print("FAILED: dark matter-only subhalos validation model (WDM 3keV) failed to run")
-    sys.exit()
-
-# Extract and validate the likelihoods.
-validate.extract("outputs/validate_darkMatterOnlySubhalos_COZMIC_WDM:3keV.hdf5","Dark Matter Only Subhalos (COZMIC WDM 3keV Milky Way)","darkMatterOnlySubhalosCOZMICWDM3keVMilkyWay","testSuite/parameters/validate_darkMatterOnlySubhalos_Symphony_CDM.xml")
-
-print("SUCCESS: dark matter-only subhalos validation model (WDM 3keV)")
