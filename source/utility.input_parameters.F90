a8cdaa31d8020958caac1f6e1fee0f23d2a56293
Andrew Benson <abenson@carnegiescience.edu>
2025-07-08 13:41:23 -0700

    fix: Update progenitor mass function parameter files to work with latest constraint parameter files

--- source/utility.input_parameters.F90
+++ source/utility.input_parameters.F90
@@ -423,7 +423,7 @@ contains
     if (present(changeFiles).and.size(changeFiles) > 0) then
        do i=1,size(changeFiles)
           !$omp critical (FoX_DOM_Access)
-          changesDoc =>  XML_Parse(char(changeFiles(i)),iostat=errorStatus)
+          changesDoc =>  XML_Parse(char(File_Name_Expand(char(changeFiles(i)))),iostat=errorStatus)
           !$omp end critical (FoX_DOM_Access)
           if (errorStatus /= 0) then
              if (File_Exists(changeFiles(i))) then
