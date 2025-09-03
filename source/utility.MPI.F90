667cbac348f386a00675cfe62e88a2ab4f16a0e7
Andrew Benson <abenson@carnegiescience.edu>
2025-09-03 11:15:06 -0700

    feat: Add an MPI sum reduction for rank-1 real arrays

--- source/utility.MPI.F90
+++ source/utility.MPI.F90
@@ -107,11 +107,13 @@ module MPI_Utilities
           &                           mpiSumArrayTwoSizeT        , mpiSumArrayThreeSizeT
      procedure ::                     mpiSumScalarDouble         , mpiSumArrayDouble      , &
           &                           mpiSumArrayTwoDouble       , mpiSumArrayThreeDouble
+     procedure ::                     mpiSumArrayReal
      generic   :: sum              => mpiSumScalarInt            , mpiSumArrayInt         , &
           &                           mpiSumScalarDouble         , mpiSumArrayDouble      , &
           &                           mpiSumArrayTwoDouble       , mpiSumArrayThreeDouble , &
           &                           mpiSumScalarSizeT          , mpiSumArraySizeT       , &
-          &                           mpiSumArrayTwoSizeT        , mpiSumArrayThreeSizeT
+          &                           mpiSumArrayTwoSizeT        , mpiSumArrayThreeSizeT  , &
+          &                           mpiSumArrayReal
      procedure ::                     mpiAnyLogicalScalar
      generic   :: any              => mpiAnyLogicalScalar
      procedure ::                     mpiAllLogicalScalar
@@ -1166,6 +1168,42 @@ contains
 #endif
     return
   end function mpiSumScalarSizeT
+
+  function mpiSumArrayReal(self,array,mask)
+    !!{
+    Sum a real array over all processes, returning it to all processes.
+    !!}
+    use :: Error  , only : Error_Report
+#ifdef USEMPI
+    use :: MPI_F08, only : MPI_AllReduce, MPI_Real, MPI_Sum
+#endif
+    implicit none
+    class           (mpiObject), intent(in   )                                    :: self
+    real                       , intent(in   ), dimension( :          )           :: array
+    logical                    , intent(in   ), dimension(0:          ), optional :: mask
+    real                                      , dimension(size(array))            :: mpiSumArrayReal
+#ifdef USEMPI
+    real                                      , dimension(size(array))            :: maskedArray
+    integer                                                                       :: iError          , activeCount
+#endif
+
+#ifdef USEMPI
+    ! Sum the array over all processes.
+    maskedArray=array
+    activeCount=self%count()
+    if (present(mask)) then
+       if (.not.mask(self%rank())) maskedArray=0.0
+       activeCount=count(mask)
+    end if
+    call MPI_AllReduce(maskedArray,mpiSumArrayReal,size(array),MPI_Real,MPI_Sum,mpiSelf%communicator,iError)
+    if (iError /= 0) call Error_Report('MPI all reduce failed'//{introspection:location})
+#else
+    !$GLC attributes unused :: self, array, mask
+    mpiSumArrayReal=0.0
+    call Error_Report('code was not compiled for MPI'//{introspection:location})
+#endif
+    return
+  end function mpiSumArrayReal
 
   function mpiSumArrayDouble(self,array,mask)
     !!{
