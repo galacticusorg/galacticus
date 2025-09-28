05b921576d54c7a5e2463bcc7a7c76dffd944d87
Andrew Benson <abenson@carnegiescience.edu>
2025-09-28 16:35:29 -0700

    fix: Remove non-existant (non-declared, not used) arguments from constructor function

--- source/nodes.property_extractor.radius_orbital_projected.F90
+++ source/nodes.property_extractor.radius_orbital_projected.F90
@@ -72,7 +72,7 @@ contains
     return
   end function radiusOrbitalProjectedConstructorParameters
 
-  function radiusOrbitalProjectedConstructorInternal(useLastIsolatedTime,cosmologyFunctions_,cosmologyParameters_,darkMatterProfileDMO_,virialDensityContrast_,virialDensityContrastDefinition_) result(self)
+  function radiusOrbitalProjectedConstructorInternal() result(self)
     !!{
     Internal constructor for the \refClass{nodePropertyExtractorRadiusOrbitalProjected} output analysis property extractor class.
     !!}
