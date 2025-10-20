ad83e86f143370be644c60ac96b52dd28c1a28a8
github-action-benchmark <github@users.noreply.github.com>
2025-10-20 21:46:33 +0000

    add Dark matter-only subhalos benchmarks (COZMIC Milky Way WDM 10keV resolutionX1) (customSmallerIsBetter) benchmark result for ce82e1607cbd130bdfe09df824f2ebce1b1a8170

---
+++ dev/bench/darkMatterOnlySubhalosCOZMICWDM10keVMilkyWayX1/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1760996793520,
+  "repoUrl": "https://github.com/galacticusorg/galacticus",
+  "entries": {
+    "Dark matter-only subhalos benchmarks (COZMIC Milky Way WDM 10keV resolutionX1)": [
+      {
+        "commit": {
+          "author": {
+            "email": "abensonca@gmail.com",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ce82e1607cbd130bdfe09df824f2ebce1b1a8170",
+          "message": "Merge pull request #951 from galacticusorg/featCOZMICWDMValidation\n\nAdd validation runs for all COZMIC WDM models",
+          "timestamp": "2025-10-20T14:52:17Z",
+          "tree_id": "f5f08b5585e485c2fbc67efa92041b9b512314b3",
+          "url": "https://github.com/galacticusorg/galacticus/commit/ce82e1607cbd130bdfe09df824f2ebce1b1a8170"
+        },
+        "date": 1760996792545,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Dark Matter Only Subhalos (COZMIC WDM:10keV resolution X1 Milky Way) - Likelihood - subhaloMassFunction",
+            "value": 1.5537575934314771,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Dark Matter Only Subhalos (COZMIC WDM:10keV resolution X1 Milky Way) - Likelihood - subhaloRadialDistribution",
+            "value": 1.1901735843874777,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Dark Matter Only Subhalos (COZMIC WDM:10keV resolution X1 Milky Way) - Likelihood - subhaloVelocityMaximumMean",
+            "value": 56.5864399154691,
+            "unit": "-logℒ"
+          }
+        ]
+      }
+    ]
+  }
+}
