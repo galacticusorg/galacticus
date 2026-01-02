26da2f1c08745e403f462c99ef1a825db30700b2
Andrew Benson <abenson@obs.carnegiescience.edu>
2026-01-02 13:30:50 -0800

    feat: Bump copyright year to 2026

--- source/hdf5FCInteropC.c
+++ source/hdf5FCInteropC.c
@@ -1,3 +1,22 @@
+// Copyright 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+//           2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026
+//    Andrew Benson <abenson@carnegiescience.edu>
+//
+// This file is part of Galacticus.
+//
+//    Galacticus is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Galacticus is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
+
 /* C-side determination of interoperable HDF5 types  */
 
 #include "H5Tpublic.h"
