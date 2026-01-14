13be6c3ebd6b6ede2d37d41a06771b346038a39d
Andrew Benson <abenson@carnegiescience.edu>
2026-01-14 13:42:33 -0800

    fix: Add the LaTeX `listings` package to the embedded LaTeX checker

--- scripts/aux/embeddedAnalyzer.pl
+++ scripts/aux/embeddedAnalyzer.pl
@@ -161,6 +161,7 @@ sub testLaTeX {
     print $LaTeX "\\usepackage{amssymb}\n";
     print $LaTeX "\\usepackage{amsmath}\n";
     print $LaTeX "\\usepackage{color}\n";
+    print $LaTeX "\\usepackage{listings}\n";
     print $LaTeX "\\usepackage{tensor}\n";
     print $LaTeX "\\input{".$ENV{'GALACTICUS_EXEC_PATH'}."/doc/commands}\n";
     print $LaTeX "\\input{".$ENV{'GALACTICUS_EXEC_PATH'}."/doc/Glossary}\n";
