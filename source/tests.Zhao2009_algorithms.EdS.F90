e7835ec31f6c58d0209161ff1e6d43195e90448b
Andrew Benson <abenson@obs.carnegiescience.edu>
2026-01-20 07:23:27 -0800

    fix: Replace broken link with Internet Archive copy

--- source/tests.Zhao2009_algorithms.EdS.F90
+++ source/tests.Zhao2009_algorithms.EdS.F90
@@ -25,7 +25,7 @@ Einstein-de Sitter Universe.
 program Test_Zhao2009_Flat
   !!{
   Tests the \cite{zhao_accurate_2009} halo mass formation history and halo concentration algorithms in an Einstein-de Sitter
-  Universe. Comparisons are made to the \href{http://202.127.29.4/dhzhao/mandc_calculator.htm}{``{\normalfont \ttfamily mandc}''} Note that
+  Universe. Comparisons are made to the \href{https://web.archive.org/web/20220313133927/http://202.127.29.4/dhzhao/mandc_calculator.htm}{``{\normalfont \ttfamily mandc}''} Note that
   comparison tolerances are relatively large since we have not attempted to match details (such as critical density
   calculation) with ``{\normalfont \ttfamily mandc}''.
   !!}
