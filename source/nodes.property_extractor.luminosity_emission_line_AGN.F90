693c0e292fbc0f4f61c832311b3185f24a3fa810
Andrew Benson <abenson@carnegiescience.edu>
2026-02-03 07:34:55 -0800

    fix: Correct default AGN emission line table file name

--- source/nodes.property_extractor.luminosity_emission_line_AGN.F90
+++ source/nodes.property_extractor.luminosity_emission_line_AGN.F90
@@ -114,7 +114,7 @@ contains
     !![
     <inputParameter>
       <name>cloudyTableFileName</name>
-      <defaultValue>var_str('%DATASTATICPATH%/hiiRegionsc/emissionLineLuminosities_AGN.hdf5')</defaultValue>
+      <defaultValue>var_str('%DATASTATICPATH%/hiiRegions/emissionLineLuminosities_AGN.hdf5')</defaultValue>
       <source>parameters</source>
       <description>The file of emission line luminosities to use.</description>
     </inputParameter>
