1962d1328487cb623a0925f9012860970843fefb
Yu Zhao <yzhao@login1.obshpc.carnegiescience.edu>
2026-07-14 15:20:08 -0700

    Description updates

--- source/satellites/dynamical_friction/acceleration/Lancaster2019.F90
+++ source/satellites/dynamical_friction/acceleration/Lancaster2019.F90
@@ -17,99 +17,98 @@
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
 
-  !!{
-  Implementation of a satellite dynamical friction class which uses the model of \cite{petts_semi-analytic_2015} for the
-  Coulomb logarithm.
-  !!}
+  !!{RST
+  Implementation of a satellite dynamical friction class which uses the model of :cite:t:`lancaster_dynamical_2020` for the Coulomb logarithm.
+  !!}  
 
   use :: Cosmology_Parameters, only : cosmologyParametersClass
 
   !![
-  <satelliteDynamicalFriction name="satelliteDynamicalFrictionLancaster2019">
+  <satelliteDynamicalFriction name="satelliteDynamicalFrictionLancaster2020" docformat="rst">
    <description>
+   A satellite dynamical friction class which computes the Coulomb logarithm following the prescription of :cite:t:`lancaster_dynamical_2020`. The Coulomb logarithm is evaluated using :math:`\Lambda = r_\mathrm{orbital}/b_{90}`, where :math:`b_{90}=GM_\mathrm{sat}/v_\mathrm{orbital}^2`.
    </description>
   </satelliteDynamicalFriction>
   !!]
-  type, extends(satelliteDynamicalFrictionChandrasekhar1943) :: satelliteDynamicalFrictionLancaster2019
-     !!{
-     Implementation of a satellite dynamical friction class which uses the model of \cite{petts_semi-analytic_2015} for the
-     Coulomb logarithm.
+  type, extends(satelliteDynamicalFrictionChandrasekhar1943) :: satelliteDynamicalFrictionLancaster2020
+     !!{RST
+     Implementation of a satellite dynamical friction class which uses the model of :cite:t:`lancaster_dynamical_2020` for the Coulomb logarithm.
      !!}
      private
    contains
-     final     ::                     lancaster2019Destructor
-     procedure :: coulombLogarithm => lancaster2019CoulombLogarithm
-  end type satelliteDynamicalFrictionLancaster2019
+     final     ::                     lancaster2020Destructor
+     procedure :: coulombLogarithm => lancaster2020CoulombLogarithm
+  end type satelliteDynamicalFrictionLancaster2020
 
-  interface satelliteDynamicalFrictionLancaster2019
-     !!{
-     Constructors for the lancaster2019 satellite dynamical friction class.
+  interface satelliteDynamicalFrictionLancaster2020
+     !!{RST
+     Constructors for the lancaster2020 satellite dynamical friction class.
      !!}
-     module procedure lancaster2019ConstructorParameters
-     module procedure lancaster2019ConstructorInternal
-  end interface satelliteDynamicalFrictionLancaster2019
+     module procedure lancaster2020ConstructorParameters
+     module procedure lancaster2020ConstructorInternal
+  end interface satelliteDynamicalFrictionLancaster2020
 
 contains
 
-  function lancaster2019ConstructorParameters(parameters) result(self)
-    !!{
-    Constructor for the \refClass{satelliteDynamicalFrictionLancaster2019} satellite dynamical friction class which builds the object from a parameter set.
+  function lancaster2020ConstructorParameters(parameters) result(self)
+    !!{RST
+    Constructor for the :galacticus-class:`satelliteDynamicalFrictionLancaster2020` satellite dynamical friction class which builds the object from a parameter set.
     !!}
     use :: Input_Parameters, only : inputParameter, inputParameters
     implicit none
-    type   (satelliteDynamicalFrictionlancaster2019)                :: self
-    type   (inputParameters                    ), intent(inout) :: parameters
+    type   (satelliteDynamicalFrictionlancaster2020)                :: self
+    type   (inputParameters                        ), intent(inout) :: parameters
     
-    self=satelliteDynamicalFrictionLancaster2019()
+    self = satelliteDynamicalFrictionLancaster2020()
     !![
     <inputParametersValidate source="parameters"/>
     !!]
     return
-  end function lancaster2019ConstructorParameters
+  end function lancaster2020ConstructorParameters
 
-  function lancaster2019ConstructorInternal( ) result(self)
-    !!{
-    Internal constructor for the \refClass{satelliteDynamicalFrictionlancaster2019} satellite dynamical friction class.
+  function lancaster2020ConstructorInternal( ) result(self)
+    !!{RST
+    Internal constructor for the :galacticus-class:`satelliteDynamicalFrictionLancaster2020` satellite dynamical friction class.
     !!}
     implicit none
-    type   (satelliteDynamicalFrictionlancaster2019)                        :: self
+    type   (satelliteDynamicalFrictionLancaster2020) :: self
 
     return
-  end function lancaster2019ConstructorInternal
+  end function lancaster2020ConstructorInternal
 
-  subroutine lancaster2019Destructor(self)
-    !!{
-    Destructor for the \refClass{satelliteDynamicalFrictionLancaster2019} satellite dynamical friction class.
+  subroutine lancaster2020Destructor(self)
+    !!{RST
+    Destructor for the :galacticus-class:`satelliteDynamicalFrictionLancaster2020` satellite dynamical friction class.
     !!}
     implicit none
-    type(satelliteDynamicalFrictionLancaster2019), intent(inout) :: self
+    type(satelliteDynamicalFrictionLancaster2020), intent(inout) :: self
 
     return
-  end subroutine lancaster2019Destructor
+  end subroutine lancaster2020Destructor
   
-  double precision function lancaster2019CoulombLogarithm(self,node) result(coulombLogarithm)
-    !!{
-    Evaluate the Coulomb logarithm for the \cite{petts_semi-analytic_2015} dynamical friction model.
+  double precision function lancaster2020CoulombLogarithm(self,node) result(coulombLogarithm)
+    !!{RST
+    Evaluate the Coulomb logarithm for the :cite:t:`lancaster_dynamical_2020` dynamical friction model.
     !!}
     use :: Galacticus_Nodes                , only : nodeComponentSatellite, treeNode
     use :: Numerical_Constants_Astronomical, only : gravitationalConstant_internal
     use :: Vectors                         , only : Vector_Magnitude
     implicit none
-    class           (satelliteDynamicalFrictionLancaster2019), intent(inout) :: self
-    type            (treeNode                           ), intent(inout) :: node
-    class           (nodeComponentSatellite             ), pointer       :: satellite
-    double precision                                     , dimension(3)  :: position               , velocity    
-    double precision                                                     :: speedOrbital           , radiusOrbital          , &
-         &                                                                  massSatellite          , l90
+    class           (satelliteDynamicalFrictionLancaster2020), intent   (inout) :: self
+    type            (treeNode                               ), intent   (inout) :: node
+    class           (nodeComponentSatellite                 ), pointer          :: satellite
+    double precision                                         , dimension(3    ) :: position     , velocity    
+    double precision                                                            :: speedOrbital , radiusOrbital, &
+         &                                                                         massSatellite, l90
 
     satellite               =>  node     %satellite (        )
     massSatellite           =   satellite%boundMass (        )
     position                =   satellite%position  (        )
     velocity                =   satellite%velocity  (        )
-    radiusOrbital           =   Vector_Magnitude    (position )
-    speedOrbital            =   Vector_Magnitude    (velocity )
+    radiusOrbital           =   Vector_Magnitude    (position)
+    speedOrbital            =   Vector_Magnitude    (velocity)
 
-    l90   = gravitationalConstant_internal*massSatellite/(speedOrbital**2.0d0)
-    coulombLogarithm=radiusOrbital/l90
+    l90              = gravitationalConstant_internal*massSatellite/(speedOrbital**2.0d0)
+    coulombLogarithm = radiusOrbital/l90
     return
-  end function lancaster2019CoulombLogarithm
+  end function lancaster2020CoulombLogarithm
--- source/satellites/dynamical_friction/acceleration/scale.F90
+++ source/satellites/dynamical_friction/acceleration/scale.F90
@@ -17,37 +17,20 @@
 !!    You should have received a copy of the GNU General Public License
 !!    along with Galacticus.  If not, see <http://www.gnu.org/licenses/>.
 
-  !+ Contributions to this file made by:  Anthony Pullen, Andrew Benson.
-
-  !!{
-  Implementation of a satellite dynamical friction class which applies the core-stalling model of \cite{kaur_stalling_2018} to another dynamical
-  friction class.
+  !!{RST
+  Implementation of a satellite dynamical friction class which scales the cceleration returned by another satellite dynamical friction class.
   !!}
 
   !![
-  <satelliteDynamicalFriction name="satelliteDynamicalFrictionScale">
+  <satelliteDynamicalFriction name="satelliteDynamicalFrictionScale" docformat="rst">
    <description>
-    A satellite dynamical friction class which applies the core-stalling model of \cite{kaur_stalling_2018} to another dynamical
-    friction class. Specifically, the acceleration due to dynamical friction is given by:
-    \begin{equation}
-     \mathbf{a}_\mathrm{df} = \mathbf{a}^\prime_\mathrm{df} [S_\mathrm{trail}(r/r_*)+S_\mathrm{lead}(r/r_*)],
-    \end{equation}
-    where $\mathbf{a}^\prime_\mathrm{df}$ is the acceleration provided by the child dynamical friction class, and
-    $S_\mathrm{trail}(x)$ and $S_\mathrm{lead}(x)$ are the torque suppression factors defined by
-    \cite[][eqn.~79]{kaur_stalling_2018}. The functional forms for $S(x)$ are taken from Figure~8 of
-    \cite{kaur_stalling_2018}---extrapolation in $\log(S)$ vs. $x$ is used to extend $S(x)$ to lower $x$ than shown in that figure,
-    while for values of $x$ higher than that shown in that figure $S(x)$ is held constant at the maximum value shown.
-  
-    The stalling radius, $r_*$, is computed using equation~(55) of \cite{kaur_stalling_2018}. For dark matter halo profiles with a
-    central cusp (for which that equation has no solution), the acceleration provided by the child dynamical friction class is
-    returned unmodified.
+    A satellite dynamical friction class which multiplies the acceleration returned by another satellite dynamical friction class by a constant scale factor.
    </description>
   </satelliteDynamicalFriction>
   !!]
   type, extends(satelliteDynamicalFrictionClass) :: satelliteDynamicalFrictionScale
-     !!{
-     Implementation of a satellite dynamical friction class which applies the core-stalling model of \cite{kaur_stalling_2018}
-     to another dynamical friction class.
+     !!{RST
+     Implementation of a satellite dynamical friction class which multiplies the acceleration returned by another satellite dynamical friction class by a constant scale factor.
      !!}
      private
      class           (satelliteDynamicalFrictionClass), pointer :: satelliteDynamicalFriction_         => null()
@@ -58,7 +41,7 @@
   end type satelliteDynamicalFrictionScale
 
   interface satelliteDynamicalFrictionScale
-     !!{
+     !!{RST
      Constructors for the scale satellite dynamical friction class.
      !!}
      module procedure scaleConstructorParameters
@@ -72,8 +55,8 @@
 contains
 
   function scaleConstructorParameters(parameters) result(self)
-    !!{
-    Constructor for the \refClass{satelliteDynamicalFrictionScale} satellite dynamical friction class which builds the object from a parameter set.
+    !!{RST
+    Constructor for the :galacticus-class:`satelliteDynamicalFrictionScale` satellite dynamical friction class which builds the object from a parameter set.
     !!}
     use :: Input_Parameters, only : inputParameter, inputParameters
     implicit none
@@ -83,11 +66,13 @@ contains
     double precision                                      :: scaleFactor
 
     !![
-    <inputParameter>
+    <inputParameter docformat="rst">
       <name>scaleFactor</name>
-      <defaultValue>0.3d0</defaultValue>
+      <defaultValue>1.0d0</defaultValue>
+      <description>
+      The multiplicative scale factor applied to the dynamical friction acceleration. Values less than unity reduce the strength of dynamical friction, while values greater than unity increase it.
+      </description>
       <source>parameters</source>
-      <description>The fractional scatter in the solitonic core-halo mass relation (default corresponds to a 50\% fractional scatter).</description>
     </inputParameter>
     <objectBuilder class="satelliteDynamicalFriction" name="satelliteDynamicalFriction_" source="parameters"/>
     !!]
@@ -100,14 +85,14 @@ contains
   end function scaleConstructorParameters
 
   function scaleConstructorInternal(satelliteDynamicalFriction_,scaleFactor) result(self)
-    !!{
-    Internal constructor for the \refClass{satelliteDynamicalFrictionScale} satellite dynamical friction class.
+    !!{RST
+    Internal constructor for the :galacticus-class:`satelliteDynamicalFrictionScale` satellite dynamical friction class.
     !!}
     use :: Table_Labels, only : extrapolationTypeExtrapolate
     implicit none
-    type (satelliteDynamicalFrictionScale)                        :: self
-    class(satelliteDynamicalFrictionClass), intent(in   ), target :: satelliteDynamicalFriction_
-    double precision                      , intent(in)          :: scaleFactor
+    type (satelliteDynamicalFrictionScale)                     :: self
+    class(satelliteDynamicalFrictionClass), intent(in), target :: satelliteDynamicalFriction_
+    double precision                      , intent(in)         :: scaleFactor
     !![
     <constructorAssign variables="*satelliteDynamicalFriction_,scaleFactor"/>
     !!]
@@ -115,8 +100,8 @@ contains
   end function scaleConstructorInternal
 
   subroutine scaleDestructor(self)
-    !!{
-    Destructor for the simple cooling radius class.
+    !!{RST
+    Destructor for the :galacticus-class:`satelliteDynamicalFrictionScale` satellite dynamical friction class.
     !!}
     implicit none
     type(satelliteDynamicalFrictionScale), intent(inout) :: self
@@ -128,13 +113,13 @@ contains
   end subroutine scaleDestructor
 
   function scaleAcceleration(self,node)
-    !!{
-    Return an acceleration for satellites due to dynamical friction using the core-stalling model of \cite{kaur_stalling_2018}.
+    !!{RST
+    Return the acceleration due to dynamical friction for a satellite by scaling the acceleration returned by the child dynamical friction class.
     !!}
     implicit none
-    double precision                                    , dimension(3)          :: scaleAcceleration
+    double precision                                 , dimension(3)          :: scaleAcceleration
     class           (satelliteDynamicalFrictionScale), intent(inout), target :: self
-    type            (treeNode                          ), intent(inout)         :: node
+    type            (treeNode                       ), intent(inout)         :: node
     
     ! Compute the base acceleration.    
     scaleAcceleration=+self%satelliteDynamicalFriction_%acceleration(node)
