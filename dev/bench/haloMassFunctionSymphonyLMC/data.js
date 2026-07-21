34a3c3d0e753776d117f887c9819b606751e262b
github-action-benchmark <github@users.noreply.github.com>
2026-07-21 05:04:15 +0000

    add Halo mass function validation (Symphony LMC) (customSmallerIsBetter) benchmark result for 7b8382082e295748f204b4a71e38bc1f6d18f1aa

---
+++ dev/bench/haloMassFunctionSymphonyLMC/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784610255246,
+  "repoUrl": "https://github.com/galacticusorg/galacticus",
+  "entries": {
+    "Halo mass function validation (Symphony LMC)": [
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "7b8382082e295748f204b4a71e38bc1f6d18f1aa",
+          "message": "ci: replace obsoleted test with new test",
+          "timestamp": "2026-07-20T14:43:48-07:00",
+          "tree_id": "f58631c70bbedd47f7f0513c0aca211629521180",
+          "url": "https://github.com/galacticusorg/galacticus/commit/7b8382082e295748f204b4a71e38bc1f6d18f1aa"
+        },
+        "date": 1784610254404,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Halo mass function - Likelihood - Symphony LMC CDM resolutionX1 z=0.000 (38 realizations)",
+            "value": 2002.6205948962652,
+            "unit": "-logℒ"
+          }
+        ]
+      }
+    ]
+  }
+}
