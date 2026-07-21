890fbbc1b88c891e493d488537923b76ec1b3073
github-action-benchmark <github@users.noreply.github.com>
2026-07-21 05:04:24 +0000

    add Halo mass function validation (Symphony Milky Way z=0) (customSmallerIsBetter) benchmark result for 7b8382082e295748f204b4a71e38bc1f6d18f1aa

---
+++ dev/bench/haloMassFunctionSymphonyMilkyWayZ0/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784610263965,
+  "repoUrl": "https://github.com/galacticusorg/galacticus",
+  "entries": {
+    "Halo mass function validation (Symphony Milky Way z=0)": [
+      {
+        "commit": {
+          "author": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "committer": {
+            "email": "abenson@obs.carnegiescience.edu",
+            "name": "Andrew Benson",
+            "username": "abensonca"
+          },
+          "distinct": true,
+          "id": "7b8382082e295748f204b4a71e38bc1f6d18f1aa",
+          "message": "ci: replace obsoleted test with new test",
+          "timestamp": "2026-07-20T14:43:48-07:00",
+          "tree_id": "f58631c70bbedd47f7f0513c0aca211629521180",
+          "url": "https://github.com/galacticusorg/galacticus/commit/7b8382082e295748f204b4a71e38bc1f6d18f1aa"
+        },
+        "date": 1784610263199,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Halo mass function - Likelihood - Symphony MilkyWay CDM resolutionX1 z=0.000 (47 realizations)",
+            "value": 4183.375175007592,
+            "unit": "-logℒ"
+          },
+          {
+            "name": "Halo mass function - Likelihood - Symphony MilkyWay CDM resolutionX64 z=0.000",
+            "value": 179.9970864305419,
+            "unit": "-logℒ"
+          }
+        ]
+      }
+    ]
+  }
+}
